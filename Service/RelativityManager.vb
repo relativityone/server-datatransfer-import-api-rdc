bf0785de2ac837ddce49e458f8281c76b960093b
Keith Kaminski <kkaminski@kcura.com>
2006-08-25 15:20:25 +0000

    FUNCTIONALITY: Added version checking to WinRelativity.

---
+++ Service/RelativityManager.vb
@@ -0,0 +1,20 @@
+Namespace kCura.WinEDDS.Service
+	Public Class RelativityManager
+		Inherits kCura.EDDS.WebAPI.RelativityManagerBase.RelativityManager
+
+		Public Sub New(ByVal credentials As Net.NetworkCredential)
+			MyBase.New()
+			Me.Credentials = credentials
+			Me.Url = String.Format("{0}RelativityManager.asmx", kCura.WinEDDS.Config.WebServiceURL)
+			Me.Timeout = Settings.DefaultTimeOut
+		End Sub
+
+		Protected Overrides Function GetWebRequest(ByVal uri As System.Uri) As System.Net.WebRequest
+			Dim wr As System.Net.HttpWebRequest = DirectCast(MyBase.GetWebRequest(uri), System.Net.HttpWebRequest)
+			wr.UnsafeAuthenticatedConnectionSharing = True
+			wr.Credentials = Me.Credentials
+			Return wr
+		End Function
+
+	End Class
+End Namespace
