650d2e76a2c1099d090e2fe1703ce463f1f4c961
Autobuilder <autobuilder@kcura.com>
2008-02-01 17:52:40 +0000

    DEFECT: fixed error catching in read for re login exception

--- Service/ProductionManager.vb
+++ Service/ProductionManager.vb
@@ -132,7 +132,7 @@ Namespace kCura.WinEDDS.Service
 						'Return Me.DTOToWebAPIProduction(_productionManager.Read(productionArtifactID, _identity))
 					End If
 				Catch ex As System.Exception
-					If TypeOf ex Is System.Web.Services.Protocols.SoapException AndAlso ex.ToString.IndexOf("NeedToReLoginException") <> -1 AndAlso tries < Config.MaxReloginTries Then
+					If TypeOf ex Is System.Web.Services.Protocols.SoapException AndAlso ex.ToString.IndexOf("Need To Re Login") <> -1 AndAlso tries < Config.MaxReloginTries Then
 						Helper.AttemptReLogin(Me.Credentials, Me.CookieContainer)
 					Else
 						Throw
