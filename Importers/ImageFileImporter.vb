1541532f3a698eced098e61f94e485c1462467bc
Nick Kapuza <nkapuza@kcura.com>
2008-05-06 01:07:56 +0000

    FUNCTIONALITY: time log framework started off

--- Importers/ImageFileImporter.vb
+++ Importers/ImageFileImporter.vb
@@ -34,6 +34,7 @@ Namespace kCura.WinEDDS
 		Private WithEvents _processController As kCura.Windows.Process.Controller
 		Private _productionDTO As kCura.EDDS.WebAPI.ProductionManagerBase.Production
 		Private _keyFieldDto As kCura.EDDS.WebAPI.FieldManagerBase.Field
+		Private _timekeeper As New kCura.Utility.Timekeeper
 #End Region
 
 #Region "Accessors"
@@ -112,46 +113,55 @@ Namespace kCura.WinEDDS
 		End Sub
 
 		Private Sub ProcessList(ByVal al As System.Collections.ArrayList, ByRef sectionHasErrors As Boolean)
-			If al.Count = 0 Then Exit Sub
 			Try
-				If Not sectionHasErrors Then
-					If _productionArtifactID = 0 Then
-						Me.ProcessDocument(al)
+				If al.Count = 0 Then Exit Sub
+				Try
+					If Not sectionHasErrors Then
+						If _productionArtifactID = 0 Then
+							_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument")
+							Me.ProcessDocument(al)
+							_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument")
+						Else
+							Me.ProcessDocumentForProduction(al)
+						End If
 					Else
-						Me.ProcessDocumentForProduction(al)
+						Me.LogErrorInFile(al)
 					End If
-				Else
+				Catch ex As System.Exception
 					Me.LogErrorInFile(al)
-				End If
-			Catch ex As System.Exception
-				Me.LogErrorInFile(al)
-				Dim txt As String = ex.ToString.ToLower
-				If txt.IndexOf("kcurafatalexception") <> -1 Then
-					txt = ex.Message.Replace("System.Web.Services.Protocols.SoapException: Server was unable to process request. ---> System.Exception: kCuraFatalException:", "")
-					Dim sr As New System.IO.StringReader(txt)
-					Dim batesNumber As String = sr.ReadLine
-					sr.Close()
-					txt = "Error: the bates number '" & batesNumber & "' already exists in the system."
-				ElseIf txt.IndexOf("ix_") <> -1 AndAlso txt.IndexOf("duplicate") <> -1 Then
-					If txt.IndexOf("eddsdbo.file") <> -1 Then
-						txt = "Error creating document - one or more image's bates numbers in the document to import already exist in the system."
+					Dim txt As String = ex.ToString.ToLower
+					If txt.IndexOf("kcurafatalexception") <> -1 Then
+						txt = ex.Message.Replace("System.Web.Services.Protocols.SoapException: Server was unable to process request. ---> System.Exception: kCuraFatalException:", "")
+						Dim sr As New System.IO.StringReader(txt)
+						Dim batesNumber As String = sr.ReadLine
+						sr.Close()
+						txt = "Error: the bates number '" & batesNumber & "' already exists in the system."
+					ElseIf txt.IndexOf("ix_") <> -1 AndAlso txt.IndexOf("duplicate") <> -1 Then
+						If txt.IndexOf("eddsdbo.file") <> -1 Then
+							txt = "Error creating document - one or more image's bates numbers in the document to import already exist in the system."
+						Else
+							txt = "Error creating document - identifier field isn't being properly filled.  Please choose a different 'key' field."
+						End If
 					Else
-						txt = "Error creating document - identifier field isn't being properly filled.  Please choose a different 'key' field."
+						txt = ex.ToString
 					End If
-				Else
-					txt = ex.ToString
-				End If
-				sectionHasErrors = True
-				RaiseStatusEvent(kCura.Windows.Process.EventType.Error, txt, 1)
+					sectionHasErrors = True
+					RaiseStatusEvent(kCura.Windows.Process.EventType.Error, txt, 1)
+				End Try
+				al.Clear()
+				sectionHasErrors = False
+			Catch ex As Exception
+				Throw
 			End Try
-			al.Clear()
-			sectionHasErrors = False
 		End Sub
 
 		Public Overloads Overrides Function ReadFile(ByVal path As String) As Object
+			_timekeeper.MarkStart("ReadFile")
 			Try
 				Dim documentIdentifier As String = String.Empty
+				_timekeeper.MarkStart("ReadFile_CountLinesInFile")
 				_fileLineCount = kCura.Utility.File.CountLinesInFile(path)
+				_timekeeper.MarkEnd("ReadFile_CountLinesInFile")
 				Reader = New StreamReader(path)
 				RaiseStatusEvent(kCura.Windows.Process.EventType.Progress, "Begin Image Upload")
 				Dim al As New System.collections.ArrayList
@@ -160,7 +170,9 @@ Namespace kCura.WinEDDS
 				While Me.Continue
 					line = Me.GetLine
 					If (line(Columns.MultiPageIndicator).ToUpper = "Y") Then
+						_timekeeper.MarkStart("ReadFile_ProcessList")
 						Me.ProcessList(al, sectionHasErrors)
+						_timekeeper.MarkEnd("ReadFile_ProcessList")
 					End If
 					sectionHasErrors = sectionHasErrors Or Not Me.ProcessImageLine(line)
 					al.Add(line)
@@ -186,6 +198,8 @@ Namespace kCura.WinEDDS
 				End Try
 				RaiseFatalError(ex)
 			End Try
+			_timekeeper.MarkEnd("ReadFile")
+			_timekeeper.GenerateCsvReportItemsAsRows()
 		End Function
 
 		Public Function ProcessDocument(ByVal al As System.Collections.ArrayList) As Object
@@ -203,14 +217,20 @@ Namespace kCura.WinEDDS
 					fileLocation = "." & fileLocation
 				End If
 				Dim multipageindicator As String = String.Copy(identifierRow(Columns.MultiPageIndicator))
-
+				_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument_GetDocumentArtifactIDFromIdentifier")
 				Dim currentDocumentArtifactID As Int32 = _docManager.GetDocumentArtifactIDFromIdentifier(_fileUploader.CaseArtifactID, documentIdentifier, _selectedIdentifierField)
+				_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument_GetDocumentArtifactIDFromIdentifier")
 				If currentDocumentArtifactID > 0 Then
+					_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument_Update")
 					If _overwrite.ToLower = "strict" OrElse _overwrite.ToLower = "append" Then
+						_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument_Update_GetImagesForDocument")
 						GetImagesForDocument(al, fileDTOs, fullTextBuilder)
+						_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument_Update_GetImagesForDocument")
 						'If _replaceFullText Then fullTextFileGuid = _fileUploader.UploadTextAsFile(fullTextBuilder.FullText, _folderID, System.Guid.NewGuid.ToString)
 						Try
+							_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument_Update_ClearImagesFromDocument")
 							_docManager.ClearImagesFromDocument(_fileUploader.CaseArtifactID, currentDocumentArtifactID)
+							_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument_Update_ClearImagesFromDocument")
 						Catch ex As System.Exception
 							If ex.ToString.IndexOf("FK_ProductionDocumentFile") <> -1 Then
 								Throw New ProductionOverwriteException(DirectCast(fileDTOs(0), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase).Identifier)
@@ -231,15 +251,20 @@ Namespace kCura.WinEDDS
 					Else
 						Throw New OverwriteNoneException(documentIdentifier)
 					End If
+					_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument_Update")
 				Else
 					'Create Document
+					_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument_Create")
 					If _overwrite.ToLower = "strict" Then
 						Throw New OverwriteStrictException(documentIdentifier)
 					End If
 					GetImagesForDocument(al, fileDTOs, fullTextBuilder)
 					currentDocumentArtifactID = CreateDocument(documentIdentifier, fullTextBuilder)
+					_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument_Create")
 				End If
+				_timekeeper.MarkStart("ReadFile_ProcessList_ProcessDocument_CreateImages")
 				_fileManager.CreateImages(_fileUploader.CaseArtifactID, DirectCast(fileDTOs.ToArray(GetType(kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase)), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase()), currentDocumentArtifactID)
+				_timekeeper.MarkEnd("ReadFile_ProcessList_ProcessDocument_CreateImages")
 				Return retval
 			Catch ex As System.Exception
 				'Me.LogErrorInFile(al)
@@ -248,6 +273,7 @@ Namespace kCura.WinEDDS
 		End Function
 
 		Public Sub ProcessDocumentForProduction(ByVal al As System.Collections.ArrayList)
+			_timekeeper.MarkStart("ProcessDocumentForProduction")
 			Try
 				Dim identifierRow As String() = DirectCast(al(0), String())
 				Dim endRow As String() = DirectCast(al(al.Count - 1), String())
@@ -297,7 +323,9 @@ Namespace kCura.WinEDDS
 					productionDocumentFiles.Add(productionDocumentFile)
 				Next
 				_productionManager.CreateProductionDocumentFiles(_fileUploader.CaseArtifactID, DirectCast(productionDocumentFiles.ToArray(GetType(kCura.EDDS.WebAPI.ProductionManagerBase.ProductionDocumentFileInfoBase)), kCura.EDDS.WebAPI.ProductionManagerBase.ProductionDocumentFileInfoBase()), _productionArtifactID, currentDocumentArtifactID)
+				_timekeeper.MarkEnd("ProcessDocumentForProduction")
 			Catch ex As System.Exception
+				_timekeeper.MarkEnd("ProcessDocumentForProduction")
 				Me.LogErrorInFile(al)
 				Throw
 			End Try
@@ -308,25 +336,34 @@ Namespace kCura.WinEDDS
 #Region "Worker Methods"
 
 		Public Function ProcessImageLine(ByVal values As String()) As Boolean
-			Dim retval As Boolean = True
-			'check for existence
-			If values(Columns.BatesNumber).Trim = "" Then
-				Me.RaiseStatusEvent(Windows.Process.EventType.Error, String.Format("No image file specified on line."))
-				retval = False
-			ElseIf Not System.IO.File.Exists(Me.GetFileLocation(values)) Then
-				Me.RaiseStatusEvent(Windows.Process.EventType.Error, String.Format("Image file specified ( {0} ) does not exist.", values(Columns.FileLocation)))
-				retval = False
-			Else
-				Dim validator As New kCura.ImageValidator.ImageValidator
-				Dim path As String = Me.GetFileLocation(values)
-				Try
-					validator.ValidateImage(path)
-				Catch ex As System.Exception
-					Me.RaiseStatusEvent(Windows.Process.EventType.Error, String.Format("Error in '{0}': {1}", path, ex.Message))
+			_timekeeper.MarkStart("ProcessImageLine")
+			Try
+				Dim retval As Boolean = True
+				'check for existence
+				If values(Columns.BatesNumber).Trim = "" Then
+					Me.RaiseStatusEvent(Windows.Process.EventType.Error, String.Format("No image file specified on line."))
 					retval = False
-				End Try
-			End If
-			Return retval
+				ElseIf Not System.IO.File.Exists(Me.GetFileLocation(values)) Then
+					Me.RaiseStatusEvent(Windows.Process.EventType.Error, String.Format("Image file specified ( {0} ) does not exist.", values(Columns.FileLocation)))
+					retval = False
+				Else
+					_timekeeper.MarkStart("ProcessImageLine_ImageValidation")
+					Dim validator As New kCura.ImageValidator.ImageValidator
+					Dim path As String = Me.GetFileLocation(values)
+					Try
+						validator.ValidateImage(path)
+					Catch ex As System.Exception
+						Me.RaiseStatusEvent(Windows.Process.EventType.Error, String.Format("Error in '{0}': {1}", path, ex.Message))
+						retval = False
+					End Try
+					_timekeeper.MarkEnd("ProcessImageLine_ImageValidation")
+				End If
+				_timekeeper.MarkEnd("ProcessImageLine")
+				Return retval
+			Catch ex As Exception
+				_timekeeper.MarkEnd("ProcessImageLine")
+				Throw
+			End Try
 			'check to make sure image is good
 		End Function
 
@@ -360,32 +397,39 @@ Namespace kCura.WinEDDS
 		End Function
 
 		Private Function GetImagesForDocument(ByVal lines As ArrayList, ByVal fileDTOs As ArrayList, ByVal fullTextBuilder As kCura.EDDS.Types.FullTextBuilder) As String()
-			Dim valueArray As String()
-			For Each valueArray In lines
-				GetImageForDocument(GetFileLocation(valueArray), valueArray(Columns.BatesNumber), fileDTOs, fullTextBuilder)
-			Next
-			If _autoNumberImages Then
-				Dim file As kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase
-				Dim last As String
-				If fileDTOs.Count > 1 Then
-					Dim allSame As Boolean = True
-					last = DirectCast(fileDTOs(0), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase).Identifier
-					Dim i As Int32
-					For i = 0 To fileDTOs.Count - 1
-						file = DirectCast(fileDTOs(i), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase)
-						allSame = allSame AndAlso last = file.Identifier
-						If Not allSame Then Exit For
-						last = file.Identifier
-					Next
-					If allSame Then
-						Dim baseIdentifier As String = DirectCast(fileDTOs(0), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase).Identifier
-						For i = 1 To fileDTOs.Count - 1
+			_timekeeper.MarkStart("GetImagesForDocument")
+			Try
+				Dim valueArray As String()
+				For Each valueArray In lines
+					GetImageForDocument(GetFileLocation(valueArray), valueArray(Columns.BatesNumber), fileDTOs, fullTextBuilder)
+				Next
+				If _autoNumberImages Then
+					Dim file As kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase
+					Dim last As String
+					If fileDTOs.Count > 1 Then
+						Dim allSame As Boolean = True
+						last = DirectCast(fileDTOs(0), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase).Identifier
+						Dim i As Int32
+						For i = 0 To fileDTOs.Count - 1
 							file = DirectCast(fileDTOs(i), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase)
-							file.Identifier = baseIdentifier & "_" & i.ToString.PadLeft(fileDTOs.Count.ToString.Length, "0"c)
+							allSame = allSame AndAlso last = file.Identifier
+							If Not allSame Then Exit For
+							last = file.Identifier
 						Next
+						If allSame Then
+							Dim baseIdentifier As String = DirectCast(fileDTOs(0), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase).Identifier
+							For i = 1 To fileDTOs.Count - 1
+								file = DirectCast(fileDTOs(i), kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase)
+								file.Identifier = baseIdentifier & "_" & i.ToString.PadLeft(fileDTOs.Count.ToString.Length, "0"c)
+							Next
+						End If
 					End If
 				End If
-			End If
+				_timekeeper.MarkEnd("GetImagesForDocument")
+			Catch ex As Exception
+				_timekeeper.MarkEnd("GetImagesForDocument")
+				Throw
+			End Try
 		End Function
 
 		Private Function GetImagesForProductionDocument(ByVal lines As ArrayList, ByVal fileDTOs As ArrayList) As String()
@@ -396,49 +440,89 @@ Namespace kCura.WinEDDS
 		End Function
 
 		Private Sub GetImageForDocument(ByVal imageFileName As String, ByVal batesNumber As String, ByVal fileDTOs As ArrayList, ByVal fullTextBuilder As kCura.EDDS.Types.FullTextBuilder)
-			Dim filename As String = imageFileName.Substring(imageFileName.LastIndexOf("\") + 1)
-			RaiseStatusEvent(kCura.Windows.Process.EventType.Progress, String.Format("Uploading File '{0}'.", filename))
-			Dim extractedTextFileName As String = imageFileName.Substring(0, imageFileName.LastIndexOf("."c) + 1) & "txt"
-			Dim file As New kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase
-			If _copyFilesToRepository Then
-				file.FileGuid = _fileUploader.UploadFile(imageFileName, _folderID)
-			Else
-				file.FileGuid = System.Guid.NewGuid.ToString
-			End If
-			file.FileName = filename
-			file.Identifier = batesNumber
-			If _copyFilesToRepository Then
-				file.Location = _repositoryPath & file.FileGuid
-			Else
-				file.Location = imageFileName
-			End If
-			fileDTOs.Add(file)
-			If _replaceFullText AndAlso System.IO.File.Exists(extractedTextFileName) Then
-				Dim sr As New System.IO.StreamReader(extractedTextFileName, System.Text.Encoding.Default, True)
-				fullTextBuilder.AppendPage(sr.ReadToEnd)
-				sr.Close()
-			Else
-				If _replaceFullText AndAlso Not System.IO.File.Exists(extractedTextFileName) Then
-					RaiseStatusEvent(kCura.Windows.Process.EventType.Warning, String.Format("File '{0}' not found.  No text updated.", extractedTextFileName))
+			_timekeeper.MarkStart("GetImageForDocument")
+			Try
+				_timekeeper.MarkStart("GetImageForDocument_GetImageFileName")
+				Dim filename As String = imageFileName.Substring(imageFileName.LastIndexOf("\") + 1)
+				_timekeeper.MarkEnd("GetImageForDocument_GetImageFileName")
+
+				_timekeeper.MarkStart("GetImageForDocument_StatusUploadUpdate")
+				'RaiseStatusEvent(kCura.Windows.Process.EventType.Progress, String.Format("Uploading File '{0}'.", filename))
+				_timekeeper.MarkEnd("GetImageForDocument_StatusUploadUpdate")
+
+				_timekeeper.MarkStart("GetImageForDocument_GetExtractedTextFileName")
+				Dim extractedTextFileName As String = imageFileName.Substring(0, imageFileName.LastIndexOf("."c) + 1) & "txt"
+				_timekeeper.MarkEnd("GetImageForDocument_GetExtractedTextFileName")
+
+				Dim file As New kCura.EDDS.WebAPI.FileManagerBase.FileInfoBase
+
+				If _copyFilesToRepository Then
+					_timekeeper.MarkStart("GetImageForDocument_GetFileGuidFromUpload")
+					file.FileGuid = _fileUploader.UploadFile(imageFileName, _folderID)
+					_timekeeper.MarkEnd("GetImageForDocument_GetFileGuidFromUpload")
+				Else
+					_timekeeper.MarkStart("GetImageForDocument_GetFileGuidFromNewGuid")
+					file.FileGuid = System.Guid.NewGuid.ToString
+					_timekeeper.MarkEnd("GetImageForDocument_GetFileGuidFromNewGuid")
 				End If
-			End If
+				file.FileName = filename
+				file.Identifier = batesNumber
+				If _copyFilesToRepository Then
+					_timekeeper.MarkStart("GetImageForDocument_GetFileLocationFromGuid")
+					file.Location = _repositoryPath & file.FileGuid
+					_timekeeper.MarkEnd("GetImageForDocument_GetFileLocationFromGuid")
+				Else
+					_timekeeper.MarkStart("GetImageForDocument_GetFileLocationFromNothingNoUpload")
+					file.Location = imageFileName
+					_timekeeper.MarkEnd("GetImageForDocument_GetFileLocationFromNothingNoUpload")
+				End If
+
+				_timekeeper.MarkStart("GetImageForDocument_AddNewDtoToArraylist")
+				fileDTOs.Add(file)
+				_timekeeper.MarkEnd("GetImageForDocument_AddNewDtoToArraylist")
+
+				_timekeeper.MarkStart("GetImageForDocument_FullText")
+				If _replaceFullText AndAlso System.IO.File.Exists(extractedTextFileName) Then
+					Dim sr As New System.IO.StreamReader(extractedTextFileName, System.Text.Encoding.Default, True)
+					fullTextBuilder.AppendPage(sr.ReadToEnd)
+					sr.Close()
+				Else
+					If _replaceFullText AndAlso Not System.IO.File.Exists(extractedTextFileName) Then
+						RaiseStatusEvent(kCura.Windows.Process.EventType.Warning, String.Format("File '{0}' not found.  No text updated.", extractedTextFileName))
+					End If
+				End If
+				_timekeeper.MarkEnd("GetImageForDocument_FullText")
+
+				_timekeeper.MarkEnd("GetImageForDocument")
+			Catch ex As Exception
+				_timekeeper.MarkEnd("GetImageForDocument")
+				Throw
+			End Try
 		End Sub
 
 		Private Function CreateDocument(ByVal identifier As String, ByVal fullTextBuilder As kCura.EDDS.Types.FullTextBuilder) As Int32
-			Dim fieldID As Int32
-			Dim encoder As New System.Text.ASCIIEncoding
+			_timekeeper.MarkStart("GetImageForDocument")
 			Try
-				If fullTextBuilder.FullTextLength > LoadFileImporter.Settings.MAX_STRING_FIELD_LENGTH Then
-					fullTextBuilder.FilePointer = _textUploader.UploadTextAsFile(fullTextBuilder.FullTextString, _folderID, System.Guid.NewGuid.ToString)
-					fullTextBuilder.FullText = ""
-				End If
-				Return _docManager.CreateEmptyDocument(_fileUploader.CaseArtifactID, _folderID, encoder.GetBytes(identifier), _selectedIdentifierField, fullTextBuilder)
-			Catch ex As System.Exception
-				If kCura.WinEDDS.Config.UsesWebAPI Then
-					Throw New CreateDocumentException(ex)
-				Else
-					Throw
-				End If
+				Dim fieldID As Int32
+				Dim encoder As New System.Text.ASCIIEncoding
+				Try
+					If fullTextBuilder.FullTextLength > LoadFileImporter.Settings.MAX_STRING_FIELD_LENGTH Then
+						fullTextBuilder.FilePointer = _textUploader.UploadTextAsFile(fullTextBuilder.FullTextString, _folderID, System.Guid.NewGuid.ToString)
+						fullTextBuilder.FullText = ""
+					End If
+					Return _docManager.CreateEmptyDocument(_fileUploader.CaseArtifactID, _folderID, encoder.GetBytes(identifier), _selectedIdentifierField, fullTextBuilder)
+				Catch ex As System.Exception
+					If kCura.WinEDDS.Config.UsesWebAPI Then
+						Throw New CreateDocumentException(ex)
+					Else
+						Throw
+					End If
+				End Try
+				_timekeeper.MarkEnd("GetImageForDocument")
+
+			Catch ex As Exception
+				_timekeeper.MarkEnd("GetImageForDocument")
+				Throw
 			End Try
 		End Function
 
