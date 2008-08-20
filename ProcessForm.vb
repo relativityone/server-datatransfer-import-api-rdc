052750b31c526100ff36c2b551170b7ee3cf941f
Nick Kapuza <nkapuza@kcura.com>
2008-08-20 02:02:59 +0000

    DEFECT: WinEDDS Import|Import Load/Image  File Progress Window + Click Hide Details: The bottom border overlaps the Show Detail button and the processed record status. See attachment https://flex.kcura.com/FLEX/Application/Mask/View.aspx?AppID=1061368&ArtifactID=1095386&ArtifactTypeID=1000002
    WinEDDS -Import Image Load File - Ovelay/AppendOverlay| Last Modified By/Document Last Modified On should not be updated when Image overwriting fails for any reason lile:
    https://flex.kcura.com/FLEX/Application/Mask/View.aspx?AppID=1061368&ArtifactID=1095392&ArtifactTypeID=1000002
    
    WinEDDS Export: Application does not respect the value defined in Max Files in the Export:Volume Settings\SubDirectory Information.
    https://flex.kcura.com/FLEX/Application/Mask/View.aspx?AppID=1061368&ArtifactID=1095400&ArtifactTypeID=1000002
    
    WinEDDS Import| All the old Saved Field Maps are not loaded correctly. The new ones that are created today are loaded correctly
    https://flex.kcura.com/FLEX/Application/Mask/View.aspx?AppID=1061368&ArtifactID=1095443&ArtifactTypeID=1000002
    
    If use has rights to read sql share but not file creation/deletion application enters invalid mode and fatal error.
    https://flex.kcura.com/FLEX/Application/Mask/View.aspx?AppID=1061368&ArtifactID=1095457&ArtifactTypeID=1000002

--- ProcessForm.vb
+++ ProcessForm.vb
@@ -409,7 +409,7 @@ Namespace kCura.Windows.Process
 			_outputTextBox.Visible = False
 			_saveOutputButton.Visible = False
 			_statusBar.Visible = False
-			Me.Height = 125
+			Me.Height = 135
 		End Sub
 
 		Private Sub ShowDetail()
