260ef437b5299f6df5e360c64b2f283fd4d8ddc5
Will Johnston <wjohnston@kcura.com>
2009-02-04 23:25:11 +0000

    DEFECT: WinEDDS: Updated ImageLoadFile to make FullTextEncoding serializable and to allow it to handle both old field maps without those references and new maps with the references.
    http://kcuraflex/Flex/Application/Mask/Edit.uplx?AppID=1061368&ArtifactID=1095558&ArtifactTypeID=1000002

--- FileObjects/ImageLoadFile.vb
+++ FileObjects/ImageLoadFile.vb
@@ -1,30 +1,67 @@
 Namespace kCura.WinEDDS
-	<Serializable()> Public Class ImageLoadFile
-		<NonSerialized()> Public CaseInfo As kCura.EDDS.Types.CaseInfo
-		Public DestinationFolderID As Int32
-		Public FileName As String
-		Public Overwrite As String
-		Public ControlKeyField As String
-		Public ReplaceFullText As Boolean
-		Public ForProduction As Boolean
-		Public AutoNumberImages As Boolean
-		Public ProductionTable As System.Data.DataTable
-		Public ProductionArtifactID As Int32
-		Public BeginBatesFieldArtifactID As Int32
-		<NonSerialized()> Public SelectedCasePath As String = ""
-		<NonSerialized()> Public CaseDefaultPath As String = ""
-		<NonSerialized()> Public CopyFilesToDocumentRepository As Boolean = True
-		<NonSerialized()> Public Credential As Net.NetworkCredential
-		<NonSerialized()> Public CookieContainer As System.Net.CookieContainer
-		<NonSerialized()> Public FullTextEncoding As System.Text.Encoding
-		'<NonSerialized()> Public Identity As kCura.EDDS.EDDSIdentity
+  <Serializable()> Public Class ImageLoadFile
+    Implements System.Runtime.Serialization.ISerializable
 
-		Public Sub New()
-			'Public Sub New(ByVal identity As kCura.EDDS.EDDSIdentity)
-			MyBase.New()
-			Overwrite = "None"
-			ProductionArtifactID = 0
-			'Me.Identity = identity
-		End Sub
-	End Class
+    <NonSerialized()> Public CaseInfo As kCura.EDDS.Types.CaseInfo
+    Public DestinationFolderID As Int32
+    Public FileName As String
+    Public Overwrite As String
+    Public ControlKeyField As String
+    Public ReplaceFullText As Boolean
+    Public ForProduction As Boolean
+    Public AutoNumberImages As Boolean
+    Public ProductionTable As System.Data.DataTable
+    Public ProductionArtifactID As Int32
+    Public BeginBatesFieldArtifactID As Int32
+    Public FullTextEncoding As System.Text.Encoding
+    <NonSerialized()> Public SelectedCasePath As String = ""
+    <NonSerialized()> Public CaseDefaultPath As String = ""
+    <NonSerialized()> Public CopyFilesToDocumentRepository As Boolean = True
+    <NonSerialized()> Public Credential As Net.NetworkCredential
+    <NonSerialized()> Public CookieContainer As System.Net.CookieContainer
+    '<NonSerialized()> Public Identity As kCura.EDDS.EDDSIdentity
+
+    Public Sub New()
+      'Public Sub New(ByVal identity As kCura.EDDS.EDDSIdentity)
+      MyBase.New()
+      Overwrite = "None"
+      ProductionArtifactID = 0
+      'Me.Identity = identity
+    End Sub
+
+    Public Sub GetObjectData(ByVal info As System.Runtime.Serialization.SerializationInfo, ByVal context As System.Runtime.Serialization.StreamingContext) Implements System.Runtime.Serialization.ISerializable.GetObjectData
+      'info.AddValue("CaseInfo", Me.CaseInfo, CaseInfo.GetType)
+      info.AddValue("DestinationFolderID", Me.DestinationFolderID, GetType(Integer))
+      info.AddValue("FileName", Me.FileName, GetType(String))
+      info.AddValue("Overwrite", Me.Overwrite, GetType(String))
+      info.AddValue("ControlKeyField", Me.ControlKeyField, GetType(String))
+      info.AddValue("ReplaceFullText", Me.ReplaceFullText, GetType(Boolean))
+      info.AddValue("ForProduction", Me.ForProduction, GetType(Boolean))
+      info.AddValue("AutoNumberImages", Me.AutoNumberImages, GetType(Boolean))
+      info.AddValue("ProductionTable", Me.ProductionTable, GetType(System.Data.DataTable))
+      info.AddValue("ProductionArtifactID", Me.ProductionArtifactID, GetType(Integer))
+      info.AddValue("BeginBatesFieldArtifactID", Me.BeginBatesFieldArtifactID, GetType(Integer))
+      info.AddValue("FullTextEncoding", Me.FullTextEncoding, GetType(System.Text.Encoding))
+    End Sub
+
+    Private Sub New(ByVal info As System.Runtime.Serialization.SerializationInfo, ByVal Context As System.Runtime.Serialization.StreamingContext)
+      With info
+        Me.DestinationFolderID = info.GetInt32("DestinationFolderID")
+        Me.FileName = info.GetString("FileName")
+        Me.Overwrite = info.GetString("Overwrite")
+        Me.ControlKeyField = info.GetString("ControlKeyField")
+        Me.ReplaceFullText = info.GetBoolean("ReplaceFullText")
+        Me.ForProduction = info.GetBoolean("ForProduction")
+        Me.AutoNumberImages = info.GetBoolean("AutoNumberImages")
+        Me.ProductionTable = DirectCast(info.GetValue("ProductionTable", GetType(System.Data.DataTable)), System.Data.DataTable)
+        Me.BeginBatesFieldArtifactID = info.GetInt32("BeginBatesFieldArtifactID")
+        Try
+          Me.FullTextEncoding = DirectCast(info.GetValue("FullTextEncoding", GetType(System.Text.Encoding)), System.Text.Encoding)
+        Catch ex As Exception
+          Me.FullTextEncoding = System.Text.Encoding.Default
+        End Try
+      End With
+    End Sub
+
+  End Class
 End Namespace
