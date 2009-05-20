8531094870d13e02c4095ca62d17f9ded78a747b
Nick Kapuza <nkapuza@kcura.com>
2009-05-20 18:38:14 +0000

    DEFECT: Exposed API for building kwe and kwi files

---
+++ SettingsFactories/NativeSettingsFactory.vb
@@ -0,0 +1,199 @@
+Namespace kCura.WinEDDS
+	Public Class NativeSettingsFactory
+
+		Inherits SettingsFactoryBase
+		Private _loadFile As LoadFile
+		Private _docFields As DocumentFieldCollection
+
+
+		Public Sub New(ByVal login As String, ByVal password As String, ByVal caseArtifactID As Int32)
+			MyBase.New(login, password)
+			_loadFile = New LoadFile
+			_loadFile.ArtifactTypeID = 10
+			_loadFile.CookieContainer = Me.CookieContainer
+			_loadFile.CopyFilesToDocumentRepository = True
+			_loadFile.CreateFolderStructure = False
+			_loadFile.Credentials = Me.Credential
+			_loadFile.ExtractedTextFileEncoding = System.Text.Encoding.Default
+			_loadFile.ExtractedTextFileEncodingName = kCura.DynamicFields.Types.FieldColumnNameHelper.GetSqlFriendlyName(_loadFile.ExtractedTextFileEncoding.EncodingName).ToLower
+			_loadFile.ExtractFullTextFromNativeFile = False
+			_loadFile.ExtractMD5HashFromNativeFile = False
+			_loadFile.FieldMap = New kCura.WinEDDS.LoadFileFieldMap
+			_loadFile.FirstLineContainsHeaders = True
+			_loadFile.FolderStructureContainedInColumn = Nothing
+			_loadFile.FullTextColumnContainsFileLocation = False
+			_loadFile.GroupIdentifierColumn = ""
+			_loadFile.HierarchicalValueDelimiter = "\"c
+			_loadFile.LoadNativeFiles = False
+			_loadFile.MultiRecordDelimiter = ";"c
+			_loadFile.NativeFilePathColumn = Nothing
+			_loadFile.NewlineDelimiter = ChrW(174)
+			_loadFile.OverwriteDestination = "None"
+			_loadFile.QuoteDelimiter = ChrW(254)
+			_loadFile.RecordDelimiter = ChrW(20)
+			_loadFile.SourceFileEncoding = System.Text.Encoding.Default
+			Me.CaseArtifactID = caseArtifactID
+			_loadFile.SelectedCasePath = _loadFile.CaseInfo.DocumentPath
+			_loadFile.SelectedIdentifierField = _docFields.IdentifierFields(0)
+		End Sub
+
+#Region " Required "
+
+		Public WriteOnly Property CaseArtifactID() As Int32
+			Set(ByVal value As Int32)
+				_loadFile.CaseInfo = Me.CaseManager.Read(value)
+				_loadFile.CaseDefaultPath = _loadFile.CaseInfo.DocumentPath
+				_loadFile.DestinationFolderID = _loadFile.CaseInfo.RootFolderID
+				_docFields = Me.FieldManager.Query.RetrieveAllAsDocumentFieldCollection(value)
+			End Set
+		End Property
+
+#End Region
+
+#Region " No default "
+
+		Public WriteOnly Property SourceFilePath() As String
+			Set(ByVal Value As String)
+				_loadFile.FilePath = Value
+			End Set
+		End Property
+
+#End Region
+
+		Public WriteOnly Property CopyFilesToDocumentRepository() As Boolean
+			Set(ByVal value As Boolean)
+				_loadFile.CopyFilesToDocumentRepository = value
+			End Set
+		End Property
+
+		Public WriteOnly Property CreateFolderStructure() As Boolean
+			Set(ByVal value As Boolean)
+				_loadFile.CreateFolderStructure = value
+			End Set
+		End Property
+
+		Public WriteOnly Property DestinationFolderID() As Int32
+			Set(ByVal value As Int32)
+				_loadFile.DestinationFolderID = value
+			End Set
+		End Property
+
+		Public WriteOnly Property ExtractedTextFileEncoding() As System.Text.Encoding
+			Set(ByVal Value As System.Text.Encoding)
+				_loadFile.ExtractedTextFileEncoding = Value
+				_loadFile.ExtractedTextFileEncodingName = kCura.DynamicFields.Types.FieldColumnNameHelper.GetSqlFriendlyName(_loadFile.ExtractedTextFileEncoding.EncodingName).ToLower
+			End Set
+		End Property
+
+#Region " Field Map "
+
+		Public Sub AddMappedItem(ByVal sourceColumnIndex As Int32, ByVal fieldArtifactID As Int32)
+			Me.AddMappedItem(sourceColumnIndex, _docFields.Item(fieldArtifactID))
+		End Sub
+
+		Public Sub AddMappedItem(ByVal sourceColumnIndex As Int32, ByVal fieldDisplayName As String)
+			Me.AddMappedItem(sourceColumnIndex, _docFields.Item(fieldDisplayName))
+		End Sub
+
+		Public Sub AddMappedItem(ByVal sourceColumnIndex As Int32, ByVal field As DocumentField)
+			_loadFile.FieldMap.Add(New kCura.WinEDDS.LoadFileFieldMap.LoadFileFieldMapItem(field, sourceColumnIndex))
+		End Sub
+
+#End Region
+
+		Public WriteOnly Property FirstLineContainsHeaders() As Boolean
+			Set(ByVal value As Boolean)
+				_loadFile.FirstLineContainsHeaders = value
+			End Set
+		End Property
+
+		Public WriteOnly Property FolderStructureContainedInColumn() As String
+			Set(ByVal value As String)
+				_loadFile.FolderStructureContainedInColumn = value
+			End Set
+		End Property
+
+		Public WriteOnly Property FullTextColumnContainsFileLocation() As Boolean
+			Set(ByVal value As Boolean)
+				_loadFile.FullTextColumnContainsFileLocation = value
+			End Set
+		End Property
+
+		Public WriteOnly Property HierarchicalValueDelimiter() As Char
+			Set(ByVal value As Char)
+				_loadFile.HierarchicalValueDelimiter = value
+			End Set
+		End Property
+
+		Public WriteOnly Property LoadNativeFiles() As Boolean
+			Set(ByVal value As Boolean)
+				_loadFile.LoadNativeFiles = value
+			End Set
+		End Property
+
+		Public WriteOnly Property MultiRecordDelimiter() As Char
+			Set(ByVal value As Char)
+				_loadFile.MultiRecordDelimiter = value
+			End Set
+		End Property
+
+		Public WriteOnly Property NativeFilePathColumnIndex() As Int32
+			Set(ByVal value As Int32)
+				If value = -1 Then
+					_loadFile.NativeFilePathColumn = Nothing
+				Else
+					_loadFile.NativeFilePathColumn = "(" & value + 1 & ")"
+				End If
+			End Set
+		End Property
+
+		Public WriteOnly Property NewlineProxy() As Char
+			Set(ByVal value As Char)
+				_loadFile.NewlineDelimiter = value
+			End Set
+		End Property
+
+		Public WriteOnly Property OverwriteDestination() As OverwriteType
+			Set(ByVal value As OverwriteType)
+				Select Case value
+					Case SettingsFactoryBase.OverwriteType.Append
+						_loadFile.OverwriteDestination = "None"
+					Case SettingsFactoryBase.OverwriteType.AppendOverlay
+						_loadFile.OverwriteDestination = "Append"
+					Case SettingsFactoryBase.OverwriteType.Overlay
+						_loadFile.OverwriteDestination = "Strict"
+				End Select
+			End Set
+		End Property
+
+		Public WriteOnly Property QuoteProxy() As Char
+			Set(ByVal value As Char)
+				_loadFile.QuoteDelimiter = value
+			End Set
+		End Property
+
+		Public WriteOnly Property RecordDelimiter() As Char
+			Set(ByVal value As Char)
+				_loadFile.RecordDelimiter = value
+			End Set
+		End Property
+
+		Public WriteOnly Property SelectedDestinationRepository() As String
+			Set(ByVal value As String)
+				_loadFile.SelectedCasePath = value
+			End Set
+		End Property
+
+		Public WriteOnly Property SourceFileEncoding() As System.Text.Encoding
+			Set(ByVal value As System.Text.Encoding)
+				_loadFile.SourceFileEncoding = value
+			End Set
+		End Property
+
+		Public Overrides Sub Save(ByVal location As String)
+			MyBase.SaveObject(location, _loadFile)
+		End Sub
+
+	End Class
+End Namespace
+
---
+++ SettingsFactories/SettingsFactoryBase.vb
@@ -0,0 +1,92 @@
+Namespace kCura.WinEDDS
+	Public MustInherit Class SettingsFactoryBase
+
+		Public Enum OverwriteType
+			Append
+			Overlay
+			AppendOverlay
+		End Enum
+
+		Private _credential As System.Net.NetworkCredential
+		Private _cookieContainer As System.Net.CookieContainer
+		Private _caseManager As kCura.WinEDDS.Service.CaseManager
+		Private _folderManager As kCura.WinEDDS.Service.FolderManager
+		Private _fieldManager As kCura.WinEDDS.Service.FieldManager
+		Private _productionManager As kCura.WinEDDS.Service.ProductionManager
+
+		Protected ReadOnly Property Credential() As System.Net.NetworkCredential
+			Get
+				Return _credential
+			End Get
+		End Property
+
+		Protected ReadOnly Property CookieContainer() As System.Net.CookieContainer
+			Get
+				Return _cookieContainer
+			End Get
+		End Property
+
+		Protected ReadOnly Property CaseManager() As kCura.WinEDDS.Service.CaseManager
+			Get
+				If _caseManager Is Nothing Then
+					_caseManager = New Service.CaseManager(_credential, _cookieContainer)
+				End If
+				Return _caseManager
+			End Get
+		End Property
+
+		Protected ReadOnly Property FolderManager() As kCura.WinEDDS.Service.FolderManager
+			Get
+				If _folderManager Is Nothing Then
+					_folderManager = New Service.FolderManager(_credential, _cookieContainer)
+				End If
+				Return _folderManager
+			End Get
+		End Property
+
+		Protected ReadOnly Property FieldManager() As kCura.WinEDDS.Service.FieldManager
+			Get
+				If _fieldManager Is Nothing Then
+					_fieldManager = New Service.FieldManager(_credential, _cookieContainer)
+				End If
+				Return _fieldManager
+			End Get
+		End Property
+
+		Protected ReadOnly Property ProductionManager() As kCura.WinEDDS.Service.ProductionManager
+			Get
+				If _productionManager Is Nothing Then
+					_productionManager = New Service.ProductionManager(_credential, _cookieContainer)
+				End If
+				Return _productionManager
+			End Get
+		End Property
+
+		Public Sub New(ByVal login As String, ByVal password As String)
+			_credential = New Net.NetworkCredential(login, password)
+			System.Net.ServicePointManager.CertificatePolicy = New TrustAllCertificatePolicy
+			_cookieContainer = New System.Net.CookieContainer
+			Dim userManager As New kCura.WinEDDS.Service.UserManager(_credential, _cookieContainer)
+			If userManager.Login(_credential.UserName, _credential.Password) Then
+				kCura.WinEDDS.Service.Settings.AuthenticationToken = userManager.GetLatestAuthenticationToken()
+			Else
+				Throw New System.Exception("Invalid login/password")
+			End If
+		End Sub
+
+		Protected Sub SaveObject(ByVal location As String, ByVal settings As Object)
+			Dim sw As New System.IO.StreamWriter(location)
+			Dim serializer As New System.Runtime.Serialization.Formatters.Soap.SoapFormatter
+			Try
+				serializer.Serialize(sw.BaseStream, settings)
+				sw.Close()
+			Catch ex As System.Exception
+				Throw New System.Exception("Settings object save failed", ex)
+			End Try
+		End Sub
+
+		Public MustOverride Sub Save(ByVal location As String)
+
+
+	End Class
+End Namespace
