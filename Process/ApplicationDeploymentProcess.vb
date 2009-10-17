a600c52cdb5b918b036167ed337c5971e4c9d717
Jed Cassinelli <jed@milyli.com>
2009-10-17 16:05:10 +0000

    FUNCTIONALITY: Template Manager: Modified TM RDC UI to use the TextDisplayForm for displaying results.  Added ApplicationDeploymentProcess class to handle calling the web service.  Added ImportApplicationFile to Application to handle creating the new process and loading the form.

---
+++ Process/ApplicationDeploymentProcess.vb
@@ -0,0 +1,57 @@
+Namespace kCura.WinEDDS
+	Public Class ApplicationDeploymentProcess
+		Inherits kCura.Windows.Process.ProcessBase
+
+#Region " Constructors "
+
+		Public Sub New(ByVal application As Xml.XmlDocument, ByVal credential As Net.NetworkCredential, ByVal cookieContainer As Net.CookieContainer, ByVal caseInfo As kCura.EDDS.Types.CaseInfo)
+			MyBase.New()
+			_application = application
+			_credential = credential
+			_cookieContainer = cookieContainer
+			_caseInfo = caseInfo
+		End Sub
+
+#End Region
+
+#Region " Protected Methods "
+
+		Protected Overrides Sub Execute()
+			Dim installationParameters As New kCura.EDDS.WebAPI.TemplateManagerBase.ApplicationInstallationParameters
+			installationParameters.CaseId = _caseInfo.ArtifactID
+
+			Dim applicationDeploymentSystem As New WinEDDS.Service.TemplateManager(_credential, Me._cookieContainer)
+			Dim installationResult As kCura.EDDS.WebAPI.TemplateManagerBase.ApplicationInstallationResult = applicationDeploymentSystem.InstallTemplate(_application, installationParameters)
+
+			If installationResult.Success Then
+				Dim installedArtifacts As New System.Text.StringBuilder
+				For Each applicationArtifact As kCura.EDDS.WebAPI.TemplateManagerBase.ApplicationArtifact In installationResult.ApplicationArtifacts
+					installedArtifacts.AppendFormat(System.Globalization.CultureInfo.CurrentCulture, "Created {0}: {1} (ID = {2}){3}", applicationArtifact.Type, applicationArtifact.Name, applicationArtifact.ArtifactId, System.Environment.NewLine)
+				Next
+				WriteStatus(String.Format(System.Globalization.CultureInfo.CurrentCulture, "Installation successful.{0}{0}{1}", System.Environment.NewLine, installedArtifacts))
+			Else
+				WriteStatus(String.Format(System.Globalization.CultureInfo.CurrentCulture, "Error installing Application: {0}", installationResult.ExceptionMessage))
+			End If
+		End Sub
+
+#End Region
+
+#Region " Private Fields "
+
+		Private _application As Xml.XmlDocument
+		Private _caseInfo As kCura.EDDS.Types.CaseInfo
+		Private _cookieContainer As Net.CookieContainer
+		Private _credential As Net.NetworkCredential
+
+#End Region
+
+#Region " Private Methods "
+
+		Private Sub WriteStatus(ByVal message As String)
+			Me.ProcessObserver.RaiseStatusEvent("", message)
+		End Sub
+
+#End Region
+
+	End Class
+End Namespace
