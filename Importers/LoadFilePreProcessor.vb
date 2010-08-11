67b385a6ae997f93744f32c34cbfa4bef4d9b4ea
Adam Kadzban <akadzban@kcura.com>
2010-08-11 09:49:37 -0500

    MERGE: merge
    so i can get keith's changes

--- Importers/LoadFilePreProcessor.vb
+++ Importers/LoadFilePreProcessor.vb
@@ -1,16 +1,71 @@
-﻿Imports kCura.Utility.File.LineCounter
+﻿'Imports kCura.Utility.File.LineCounter
 Namespace kCura.WinEDDS
 	Public Class LoadFilePreProcessor
 		Inherits LoadFileReader
+
+		Public Enum EventType
+			Begin
+			Complete
+			Progress
+		End Enum
+
+		Public Class EventArgs
+			Private _newlinesRead As Int64
+			Private _bytesRead As Int64
+			Private _totalBytes As Int64
+			Private _stepSize As Int64
+			Private _type As EventType
+
+			Public ReadOnly Property NewlinesRead() As Int64
+				Get
+					Return _newlinesRead
+				End Get
+			End Property
+
+			Public ReadOnly Property BytesRead() As Int64
+				Get
+					Return _bytesRead
+				End Get
+			End Property
+
+			Public ReadOnly Property TotalBytes() As Int64
+				Get
+					Return _totalBytes
+				End Get
+			End Property
+
+			Public ReadOnly Property StepSize() As Int64
+				Get
+					Return _stepSize
+				End Get
+			End Property
+			Public ReadOnly Property Type() As EventType
+				Get
+					Return _type
+				End Get
+			End Property
+
+			Public Sub New(ByVal type As EventType, ByVal newlines As Int64, ByVal bytes As Int64, ByVal total As Int64, ByVal [step] As Int64)
+				_newlinesRead = newlines
+				_bytesRead = bytes
+				_totalBytes = total
+				_stepSize = [step]
+				_type = type
+			End Sub
+		End Class
+
+		Private WithEvents _haltListener As New HaltListener
+
 		Public Sub New(ByVal args As LoadFile, ByVal trackErrorsAsFieldValues As Boolean)
 			MyBase.New(args, trackErrorsAsFieldValues)
 		End Sub
-		Public Event OnEvent(ByVal e As kCura.Utility.File.LineCounter.EventArgs)
 
+		Public Event OnEvent(ByVal e As kCura.WinEDDS.LoadFilePreProcessor.EventArgs)
 
 		Public Sub CountLines()
 			Me.ReadFile(_settings.FilePath)
 		End Sub
+
 		Protected Sub ProcessStart(ByVal newlines As Int64, ByVal bytes As Int64, ByVal total As Int64, ByVal [step] As Int64)
 			RaiseOnEvent(EventType.Begin, newlines, bytes, total, [step])
 		End Sub
@@ -25,6 +80,10 @@ Namespace kCura.WinEDDS
 
 		Protected Sub RaiseOnEvent(ByVal type As EventType, ByVal newlines As Int64, ByVal bytes As Int64, ByVal total As Int64, ByVal [step] As Int64)
 			RaiseEvent OnEvent(New EventArgs(type, newlines, bytes, total, [step]))
+		End Sub
+
+		Public Sub StopCounting()
+			_haltListener.Halt()
 		End Sub
 
 		Public Overrides Function ReadFile(ByVal path As String) As Object
@@ -42,6 +101,21 @@ Namespace kCura.WinEDDS
 			End While
 			Me.RaiseOnEvent(EventType.Complete, Me.CurrentLineNumber, Me.Reader.BaseStream.Position, Me.Reader.BaseStream.Length, stepsize)
 		End Function
+
+
+		Public Class HaltListener
+			Public Event StopProcess()
+			Public Sub Halt()
+				RaiseEvent StopProcess()
+			End Sub
+		End Class
+
+		Private Sub _haltListener_StopProcess() Handles _haltListener.StopProcess
+			_continue = False
+		End Sub
+
+
 	End Class
+
 End Namespace
 
--- Importers/LoadFileReader.vb
+++ Importers/LoadFileReader.vb
@@ -53,8 +53,8 @@ Namespace kCura.WinEDDS
 		Private _codesCreated As Int32 = 0
 		Private _errorLogFileName As String = ""
 		Private _errorLogWriter As System.IO.StreamWriter
-		'Private WithEvents _loadFilePreProcessor As kCura.WinEDDS.LoadFilePreProcessor
-		Private WithEvents _lineCounter As kCura.Utility.File.LineCounter
+		Private WithEvents _loadFilePreProcessor As kCura.WinEDDS.LoadFilePreProcessor
+		'Private WithEvents _lineCounter As kCura.Utility.File.LineCounter
 		Private _recordCount As Int64 = -1
 		Private _genericTimestamp As System.DateTime
 		Private _trackErrorsInFieldValues As Boolean
@@ -109,7 +109,7 @@ Namespace kCura.WinEDDS
 				Next
 			End If
 
-			Me.InitializeLineCounter(args.FilePath)
+			'Me.InitializeLineCounter(args.FilePath)
 			'Reader = New System.IO.StreamReader(args.FilePath, args.SourceFileEncoding)
 		End Sub
 
@@ -117,10 +117,10 @@ Namespace kCura.WinEDDS
 
 #Region " Initialization "
 
-		Private Sub InitializeLineCounter(ByVal path As String)
-			_lineCounter = New kCura.Utility.File.LineCounter
-			_lineCounter.Path = path
-		End Sub
+		'Private Sub InitializeLineCounter(ByVal path As String)
+		'	_lineCounter = New kCura.Utility.File.LineCounter
+		'	_lineCounter.Path = path
+		'End Sub
 
 		Private Sub EnsureReader()
 			If Me.Reader Is Nothing Then
@@ -255,7 +255,7 @@ Namespace kCura.WinEDDS
 
 #Region " Status Communication "
 
-		Private Sub _lineCounter_OnEvent(ByVal e As kCura.Utility.File.LineCounter.EventArgs) Handles _lineCounter.OnEvent
+		Private Sub _loadFilePreProcessor_OnEvent(ByVal e As kCura.Utility.File.LineCounter.EventArgs) Handles _loadFilePreProcessor.OnEvent
 			Select Case e.Type
 				Case kCura.Utility.File.LineCounter.EventType.Begin
 					_genericTimestamp = System.DateTime.Now
@@ -265,7 +265,7 @@ Namespace kCura.WinEDDS
 				Case kCura.Utility.File.LineCounter.EventType.Complete
 					_recordCount = e.NewlinesRead
 					RaiseEvent DataSourcePrep(New DataSourcePrepEventArgs(DataSourcePrepEventArgs.EventType.ReadEvent, e.TotalBytes, e.TotalBytes, e.StepSize, _genericTimestamp, System.DateTime.Now))
-					Dim path As String = _lineCounter.Path
+					Dim path As String = _settings.FilePath '_lineCounter.Path
 					_columnHeaders = GetColumnNames(_settings)
 					Reader = New System.IO.StreamReader(path, _sourceFileEncoding)
 					If _firstLineContainsColumnNames Then
@@ -338,7 +338,8 @@ Namespace kCura.WinEDDS
 		End Property
 
 		Public Sub Halt() Implements Api.IArtifactReader.Halt
-			_lineCounter.StopCounting()
+			'_lineCounter.StopCounting()
+			_loadFilePreProcessor.StopCounting()
 		End Sub
 
 		Public ReadOnly Property SizeInBytes() As Long Implements Api.IArtifactReader.SizeInBytes
@@ -359,11 +360,11 @@ Namespace kCura.WinEDDS
 		End Sub
 
 		Public Function CountRecords() As Int64 Implements Api.IArtifactReader.CountRecords
-			Me.InitializeLineCounter(_settings.FilePath)
-			_lineCounter.CountLines(_sourceFileEncoding, New kCura.Utility.File.LineCounter.LineCounterArgs(Me.Bound, Me.Delimiter))
+			'Me.InitializeLineCounter(_settings.FilePath)
+			'_lineCounter.CountLines(_sourceFileEncoding, New kCura.Utility.File.LineCounter.LineCounterArgs(Me.Bound, Me.Delimiter))
 
-			'_loadFilePreProcessor = New kCura.WinEDDS.LoadFilePreProcessor(_settings, _trackErrorsInFieldValues)
-			'_loadFilePreProcessor.CountLines()
+			_loadFilePreProcessor = New kCura.WinEDDS.LoadFilePreProcessor(_settings, _trackErrorsInFieldValues)
+			_loadFilePreProcessor.CountLines()
 			Return _recordCount
 		End Function
 
