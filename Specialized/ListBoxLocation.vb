c842ebb55e5326dbfc3553cca8583a8dedd7b686
Perry Marchant <pmarchant@kcura.com>
2011-03-28 10:08:14 -0500

    FXCOP: Declare event handlers correctly.

---
+++ Specialized/ListBoxLocation.vb
@@ -0,0 +1,16 @@
+﻿Namespace kCura.Windows.Forms
+	''' <summary>
+	''' Used to describe which side a ListBox control is on
+	''' </summary>
+	''' <remarks></remarks>
+	Public Enum ListBoxLocation
+		''' <summary>
+		''' A ListBox on the left side
+		''' </summary>
+		Left = 0
+		''' <summary>
+		''' A ListBox on the right side
+		''' </summary>
+		Right = 1
+	End Enum
+End Namespace
