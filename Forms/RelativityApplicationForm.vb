1092d265688b21d48277e660f5c9e84e027c4809
Perry Marchant <pmarchant@kcura.com>
2011-04-08 15:31:46 -0500

    Changed display of ActiveSyncs to EventHandlers in RDC RA preview tree control.

--- Forms/RelativityApplicationForm.vb
+++ Forms/RelativityApplicationForm.vb
@@ -434,7 +434,7 @@ Namespace kCura.EDDS.WinForm
 				PopulateChildren(objNode, "Tabs", "Tab", item)
 				PopulateChildren(objNode, "Views", "View", item)
 				PopulateChildren(objNode, "ObjectRules", "ObjectRule", item)
-				PopulateChildren(objNode, "EventHandlers", "EventHandler", item)
+				PopulateChildren(objNode, "EventHandlers", "ActiveSync", item)
 			Next
 			Dim externalTabsNode = TreeView1.Nodes.Add("External Tabs")
 			externalTabsNode.NodeFont = boldfont
