9ac84dacbe59ea19e3a931de5ea78038fa1fbc23
Justin Terada <jterada@kcura.com>
2011-04-26 16:57:59 -0500

    DEFECT: [Salesforce:84392] : Do not throw an exception from DataReaderReader when a native file is not found, instead rely on BulkLoadFileImporter.ManageDocuments to do this check, bringing this class in line with LoadFileReader
    * Reviewed by Nick Kapuza

--- DataReaderReader.vb
+++ DataReaderReader.vb
@@ -181,9 +181,12 @@ Namespace kCura.WinEDDS.ImportExtension
 									'Only one file field is allowed
 									Exit For
 								Else
-									Throw New Exception("File" + field.ValueAsString + " does not exist or you don't have sufficient privilidges to access it")
+									' [Salesforce:84392]
+									' We used to throw an exception here, but it caused the import to be aborted
+									' so right now we silently ignore this error and this error will be caught by
+									' BulkLoadFileImporter.ManageDocuments. This is consistent with other
+									' implementations of IArtifactReader (see LoadFileReader.ReadArtifact).
 								End If
-
 							End If
 						End If
 					Next
