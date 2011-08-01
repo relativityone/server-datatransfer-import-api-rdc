795fe074c9aaf75616948c3e27d7c7f441ae24e1
Mikhail(Misha) Kogan <mkogan@kcura.com>
2011-08-01 11:46:00 -0500

    FUNCTIONALITY: Change Soap Exception to pass throug a serializable object istead of just pure xml. The good thing is that object can be seralised back on the other side (caller of webservice)

--- ThreadExceptionHandler.vb
+++ ThreadExceptionHandler.vb
@@ -1,4 +1,7 @@
-﻿Namespace kCura.EDDS.WinForm
+﻿Imports System.Xml.Serialization
+Imports System.Xml
+
+Namespace kCura.EDDS.WinForm
 	Public Class ThreadExceptionHandler
 		Public Sub Application_ThreadException(ByVal sender As System.Object, ByVal e As Threading.ThreadExceptionEventArgs)
 			Try
@@ -8,18 +11,20 @@
 				If (result = DialogResult.Abort) Then
 					System.Windows.Forms.Application.Exit()
 				End If
-			Catch
+			Catch ex As System.Exception
 				' Fatal error, terminate program
 				Try
-					MessageBox.Show("Fatal Error", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+					MessageBox.Show(String.Format("Fatal Error. {0}Message: {1} {2} Stack Trace: {3}", vbNewLine, ex.Message, vbNewLine, ex.StackTrace), "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop)
 				Finally
-					System.Windows.Forms.Application.Exit()
+					'System.Windows.Forms.Application.Exit()
 				End Try
 			End Try
 		End Sub
 
 
 		Private Function ShowThreadExceptionDialog(ByVal ex As Exception) As DialogResult
+
+			Dim parsedException As System.Exception = ParseExceptionForMoreInfo(ex)
 
 			Dim errorMessage As String =
 			"Unhandled Exception:" &
@@ -30,6 +35,21 @@
 			ex.StackTrace
 
 			Return MessageBox.Show(errorMessage, "Application Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop)
+		End Function
+
+		Private Function ParseExceptionForMoreInfo(ByVal ex As Exception) As Exception
+			Dim resultException As System.Exception = ex
+
+			If TypeOf ex Is System.Web.Services.Protocols.SoapException Then
+				Dim soapException As System.Web.Services.Protocols.SoapException = DirectCast(ex, System.Web.Services.Protocols.SoapException)
+				Dim xs As New XmlSerializer(GetType(Relativity.SoapExceptionDetail))
+				Dim doc As New System.Xml.XmlDocument
+				doc.LoadXml(soapException.Detail.OuterXml)
+				Dim xr As XmlReader = doc.CreateNavigator.ReadSubtree
+				Dim detailedException As Relativity.SoapExceptionDetail = TryCast(xs.Deserialize(xr), Relativity.SoapExceptionDetail)
+			End If
+
+			Return resultException
 		End Function
 
 	End Class
