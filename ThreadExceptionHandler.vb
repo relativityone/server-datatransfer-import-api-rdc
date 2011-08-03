63cc98a6f7367e1bf469d28ed682cd21494d24a9
Mikhail(Misha) Kogan <mkogan@kcura.com>
2011-08-03 15:51:17 -0500

    DEFECT: BCP Path exception was showing stack trace in the friendly field. Fixed that

--- ThreadExceptionHandler.vb
+++ ThreadExceptionHandler.vb
@@ -5,13 +5,6 @@ Namespace kCura.EDDS.WinForm
 	Public Class ThreadExceptionHandler
 		Public Sub Application_ThreadException(ByVal sender As System.Object, ByVal e As Threading.ThreadExceptionEventArgs)
 			Try
-				'' Exit the program if the user clicks Abort.
-				'Dim result As DialogResult = ShowThreadExceptionDialog(e.Exception)
-
-				'If (result = DialogResult.Abort) Then
-				'	System.Windows.Forms.Application.Exit()
-				'End If
-
 				ShowErrorDialog(e.Exception)
 			Catch ex As System.Exception
 				' Fatal error, terminate program
@@ -31,36 +24,6 @@ Namespace kCura.EDDS.WinForm
 				Environment.Exit(1)
 			End If
 		End Sub
-
-		Private Function ShowThreadExceptionDialog(ByVal ex As Exception) As DialogResult
-
-			Dim parsedException As System.Exception = ParseExceptionForMoreInfo(ex)
-
-			Dim errorMessage As String =
-			"Unhandled Exception:" &
-			vbCrLf & vbCrLf &
-			ex.Message & vbCrLf & vbCrLf &
-			ex.GetType().ToString() & vbCrLf & vbCrLf &
-			"Stack Trace:" & vbCrLf &
-			ex.StackTrace
-
-			Return MessageBox.Show(errorMessage, "Application Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop)
-		End Function
-
-		Private Function ParseExceptionForMoreInfo(ByVal ex As Exception) As Exception
-			Dim resultException As System.Exception = ex
-
-			If TypeOf ex Is System.Web.Services.Protocols.SoapException Then
-				Dim soapException As System.Web.Services.Protocols.SoapException = DirectCast(ex, System.Web.Services.Protocols.SoapException)
-				Dim xs As New XmlSerializer(GetType(Relativity.SoapExceptionDetail))
-				Dim doc As New System.Xml.XmlDocument
-				doc.LoadXml(soapException.Detail.OuterXml)
-				Dim xr As XmlReader = doc.CreateNavigator.ReadSubtree
-				Dim detailedException As Relativity.SoapExceptionDetail = TryCast(xs.Deserialize(xr), Relativity.SoapExceptionDetail)
-			End If
-
-			Return resultException
-		End Function
 
 	End Class
 End Namespace
