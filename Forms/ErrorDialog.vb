d19532df7139e8634465dbfdc648f451907b43d8
Mikhail(Misha) Kogan <mkogan@kcura.com>
2011-08-04 16:23:58 -0500

    FUNCTIONALITY: Made it bigger

--- Forms/ErrorDialog.vb
+++ Forms/ErrorDialog.vb
@@ -49,7 +49,7 @@ Public Class ErrorDialog
 		'
 		'_okButton
 		'
-		Me._okButton.Location = New System.Drawing.Point(157, 52)
+		Me._okButton.Location = New System.Drawing.Point(292, 89)
 		Me._okButton.Name = "_okButton"
 		Me._okButton.Size = New System.Drawing.Size(68, 23)
 		Me._okButton.TabIndex = 1
@@ -57,7 +57,7 @@ Public Class ErrorDialog
 		'
 		'_cancelButton
 		'
-		Me._cancelButton.Location = New System.Drawing.Point(225, 52)
+		Me._cancelButton.Location = New System.Drawing.Point(360, 89)
 		Me._cancelButton.Name = "_cancelButton"
 		Me._cancelButton.Size = New System.Drawing.Size(68, 23)
 		Me._cancelButton.TabIndex = 2
@@ -65,18 +65,19 @@ Public Class ErrorDialog
 		'
 		'_errorText
 		'
-		Me._errorText.Location = New System.Drawing.Point(4, 76)
+		Me._errorText.Location = New System.Drawing.Point(4, 113)
 		Me._errorText.Multiline = True
 		Me._errorText.Name = "_errorText"
 		Me._errorText.ReadOnly = True
 		Me._errorText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-		Me._errorText.Size = New System.Drawing.Size(288, 192)
+		Me._errorText.Size = New System.Drawing.Size(424, 192)
 		Me._errorText.TabIndex = 3
 		Me._errorText.Visible = False
 		'
 		'LinkLabel1
 		'
-		Me.LinkLabel1.Location = New System.Drawing.Point(4, 60)
+		Me.LinkLabel1.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.LinkLabel1.Location = New System.Drawing.Point(4, 97)
 		Me.LinkLabel1.Name = "LinkLabel1"
 		Me.LinkLabel1.Size = New System.Drawing.Size(126, 16)
 		Me.LinkLabel1.TabIndex = 4
@@ -90,13 +91,13 @@ Public Class ErrorDialog
 		Me.txtBoxFriendlyError.Multiline = True
 		Me.txtBoxFriendlyError.Name = "txtBoxFriendlyError"
 		Me.txtBoxFriendlyError.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-		Me.txtBoxFriendlyError.Size = New System.Drawing.Size(288, 45)
+		Me.txtBoxFriendlyError.Size = New System.Drawing.Size(427, 82)
 		Me.txtBoxFriendlyError.TabIndex = 5
 		'
 		'ErrorDialog
 		'
 		Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
-		Me.ClientSize = New System.Drawing.Size(296, 89)
+		Me.ClientSize = New System.Drawing.Size(433, 112)
 		Me.ControlBox = False
 		Me.Controls.Add(Me.txtBoxFriendlyError)
 		Me.Controls.Add(Me.LinkLabel1)
@@ -105,9 +106,9 @@ Public Class ErrorDialog
 		Me.Controls.Add(Me._okButton)
 		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
 		Me.MaximizeBox = False
-		Me.MaximumSize = New System.Drawing.Size(304, 116)
+		Me.MaximumSize = New System.Drawing.Size(441, 343)
 		Me.MinimizeBox = False
-		Me.MinimumSize = New System.Drawing.Size(304, 116)
+		Me.MinimumSize = New System.Drawing.Size(441, 139)
 		Me.Name = "ErrorDialog"
 		Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
 		Me.Text = "Error"
@@ -139,19 +140,19 @@ Public Class ErrorDialog
 	Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
 		If LinkLabel1.Text.ToLower = "show error text" Then
 			LinkLabel1.Text = "Hide Error Text"
-			Me.Width = 304
-			Me.Height = 308
-			Me.MaximumSize = New System.Drawing.Size(304, 308)
-			Me.MinimumSize = New System.Drawing.Size(304, 308)
+			Me.Width = 441
+			Me.Height = 343
+			Me.MaximumSize = New System.Drawing.Size(441, 343)
+			Me.MinimumSize = New System.Drawing.Size(441, 343)
 			_errorText.Visible = True
 			_errorText.Focus()
 			_errorText.SelectAll()
 		Else
 			LinkLabel1.Text = "Show Error Text"
-			Me.Width = 304
-			Me.Height = 116
-			Me.MaximumSize = New System.Drawing.Size(304, 116)
-			Me.MinimumSize = New System.Drawing.Size(304, 116)
+			Me.Width = 441
+			Me.Height = 139
+			Me.MaximumSize = New System.Drawing.Size(441, 139)
+			Me.MinimumSize = New System.Drawing.Size(441, 139)
 			_errorText.Focus()
 			_errorText.Visible = False
 		End If
