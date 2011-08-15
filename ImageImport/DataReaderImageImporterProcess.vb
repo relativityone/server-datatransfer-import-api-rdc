393a5a8e780d3255124246bf6b4dd0ddf77e2bbc
Philip Soderling <psoderling@kcura.com>
2011-08-15 15:31:50 -0500

    MERGE: Intermediate merge w/Perry

--- ImageImport/DataReaderImageImporterProcess.vb
+++ ImageImport/DataReaderImageImporterProcess.vb
@@ -15,24 +15,7 @@ Namespace kCura.WinEDDS.ImportExtension
 			_sourceData = sourceData
 		End Sub
 
-		'Private Function AddColumnIndexToName(ByVal dr As System.Data.IDataReader, ByVal columnName As String) As String
-		'	'function: convert [columnName] to [columnName]([index]) if [columnName] is a column in DR.  Example: FileLocation -> FileLocation(3)
-		'	'*Note: I want the result to be a 1-based index. DR uses a 0-based index.  Computers, man, computers.
-		'	Dim retColumnName As String = columnName
-		'	If Not retColumnName Is Nothing Then
-		'		retColumnName = retColumnName.ToLower
-		'		Try
-		'			Dim iIndex As Integer = dr.GetOrdina(retColumnName)
-		'			retColumnName = String.Concat(retColumnName, "(", iIndex + 1, ")")
-		'		Catch ex As IndexOutOfRangeException
-		'			Throw New IndexOutOfRangeException(String.Format("ERROR: {0} must be a field in the source data", columnName), ex)
-		'		End Try
-		'	End If
-		'	Return retColumnName
-		'End Function
-
 		Protected Overrides Function GetImageFileImporter() As kCura.WinEDDS.BulkImageFileImporter
-
 			Select Case ImageLoadFile.Overwrite.ToLower
 				Case "append"
 					ImageLoadFile.Overwrite = "none"
@@ -44,13 +27,6 @@ Namespace kCura.WinEDDS.ImportExtension
 
 			Return New DataReaderImageImporter(1003697, ImageLoadFile, New kCura.Windows.Process.Controller, System.Guid.NewGuid, _sourceData, ServiceURL)
 
-			'Dim temp As DataReaderImporter = New DataReaderImporter(DirectCast(Me.LoadFile, kCura.WinEDDS.ImportExtension.DataReaderLoadFile))
-			'Dim dr As System.Data.IDataReader = temp.SourceData
-			''These settings need to have [columnName]([index])
-			'LoadFile.FolderStructureContainedInColumn = AddColumnIndexToName(dr, LoadFile.FolderStructureContainedInColumn)
-			'LoadFile.NativeFilePathColumn = AddColumnIndexToName(dr, LoadFile.NativeFilePathColumn)
-			'temp.DestinationFolder = AddColumnIndexToName(dr, temp.DestinationFolder)
-			'Return DirectCast(temp, kCura.WinEDDS.BulkLoadFileImporter)
 		End Function
 
 		Protected Overrides Sub Execute()
