8c0fc06f3818f413c58fc70343296f7b5a9bfe29
Bardia Noohi <bnoohi@kcura.com>
2011-08-23 12:48:33 -0500

    DEFECT: Fixing other failing tests. This code makes my eyes burn. I have stared into the abyss and it stared back into me...

--- Importers/FileDirectoryImporter.vb
+++ Importers/FileDirectoryImporter.vb
@@ -28,7 +28,7 @@ Namespace kCura.WinEDDS
 			' create webservice proxies
 			_documentManager = New kCura.WinEDDS.Service.DocumentManager(_credential, _cookieContainer, ServiceURL)
 			_folderManager = New kCura.WinEDDS.Service.FolderManager(_credential, _cookieContainer, ServiceURL)
-			_folderManager.ServiceURL = ServiceURL
+			'_folderManager.ServiceURL = ServiceURL
 			_fileManager = New kCura.WinEDDS.Service.FileManager(_credential, _cookieContainer, ServiceURL)
 			_uploader = New kCura.WinEDDS.FileUploader(_credential, _importFileDirectorySettings.CaseInfo.ArtifactID, _documentManager.GetDocumentDirectoryByCaseArtifactID(_importFileDirectorySettings.CaseInfo.ArtifactID) & "\", _cookieContainer, ServiceURL)
 
