f1242b5da37ebfc871cb0d2c6ae1098f0e8c9bb2
Bardia Noohi <bnoohi@kcura.com>
2011-08-23 12:48:33 -0500

    DEFECT: Fixing other failing tests. This code makes my eyes burn. I have stared into the abyss and it stared back into me...

--- ImageImport/ImageImportBulkArtifactJob.vb
+++ ImageImport/ImageImportBulkArtifactJob.vb
@@ -208,11 +208,11 @@ Namespace kCura.Relativity.DataReaderClient
 		End Sub
 
 		Private Sub EnsureFieldNameIsValid(ByVal imageSettingsField As String, ByVal forFieldName As String)
-			If String.IsNullOrEmpty(imageSettingsField) Then
+			If String.IsNullOrEmpty(forFieldName) Then
 				Throw New Exception("No field name specified for " & forFieldName)
 			End If
 
-			If Not SourceData.SourceData.Columns.Contains(imageSettingsField) Then
+			If Not SourceData.SourceData.Columns.Contains(forFieldName) Then
 				Throw New Exception(String.Format("No field named {0} found in the DataTable for " & forFieldName, imageSettingsField))
 			End If
 		End Sub
