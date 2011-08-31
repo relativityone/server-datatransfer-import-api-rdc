3439fc38a8f984eec3b49774bbab8d713f08369e
Bardia Noohi <bnoohi@kcura.com>
2011-08-31 10:37:00 -0500

    DEFECT: Fixed Import API integration tests

--- ImageImport/ImageImportBulkArtifactJob.vb
+++ ImageImport/ImageImportBulkArtifactJob.vb
@@ -1,47 +1,42 @@
-Imports kCura.WinEDDS.ImportExtension
-Imports kCura.WinEDDS
-
 Namespace kCura.Relativity.DataReaderClient
 	Public Class ImageImportBulkArtifactJob
-		Inherits LoadFileJobBase
 
 #Region " Public Events and Variables "
+
+		Public Event OnMessage(ByVal status As Status)
+		Public Event OnError(ByVal row As IDictionary)
+
 		Public Settings As ImageSettings
 		Public SourceData As ImageSourceIDataReader
+		Private _caseManager As kCura.WinEDDS.Service.CaseManager
 
 #End Region
 
-#Region "Private Variables"
-		Private ReadOnly _cookieContainer As New Net.CookieContainer
+#Region " Private variables "
+		Private WithEvents _observer As kCura.Windows.Process.ProcessObserver
+		Private cookieMonster As New Net.CookieContainer
 #End Region
 
-#Region "Constructors"
+#Region " Public Methods "
+
 		Public Sub New()
 			Me.Settings = New ImageSettings
 			Me.SourceData = New ImageSourceIDataReader
 		End Sub
-#End Region
 
-#Region "Events"
-		Public Shadows Event OnError(ByVal row As IDictionary)
-		Public Shadows Event OnMessage(ByVal status As Status)
-#End Region
-
-#Region "Public Routines"
-		Public Overrides Sub Execute()
+		Public Sub Execute()
 			If IsSettingsValid() Then
-				'TODO: Remove this? What database data???
 				RaiseEvent OnMessage(New Status("Getting source data from database"))
-
-				MapSuppliedFieldNamesToActual(Settings, SourceData.SourceData)
 
 				SelectServiceURL()
 
-				Dim process As ImportImageFileProcess = CreateImageImporterProcess(SourceData.SourceData, Settings.WebServiceURL)
+				MapSuppliedFieldNamesToActual(Settings, SourceData.SourceData)
+
+				Dim process As New kCura.WinEDDS.ImportExtension.DataReaderImageImporterProcess(SourceData.SourceData, Settings.WebServiceURL)
 				_observer = process.ProcessObserver
 
 				RaiseEvent OnMessage(New Status("Updating settings"))
-				process.ImageLoadFile = Me.CreateLoadFile(Settings)
+				process.ImageLoadFile = Me.CreateLoadFile()
 
 				RaiseEvent OnMessage(New Status("Executing"))
 				Try
@@ -54,145 +49,9 @@ Namespace kCura.Relativity.DataReaderClient
 			End If
 		End Sub
 
-		Protected Overridable Function CreateImageImporterProcess(ByVal sourceData As DataTable, ByVal webServiceUrl As String) As ImportImageFileProcess
-			Return New DataReaderImageImporterProcess(sourceData, webServiceUrl)
-		End Function
 #End Region
 
-#Region "Private Functions"
-		Private Function CreateLoadFile(ByVal imgSettings As ImageSettings) As WinEDDS.ImageLoadFile
-			Dim tempImgSettings As WinEDDS.ImageSettingsFactory = MapInputToSettingsFactory(imgSettings)
-			Dim tempLoadFile As WinEDDS.ImageLoadFile = tempImgSettings.ToLoadFile()
-
-			'These are ALL of the image file settings
-			tempLoadFile.AutoNumberImages = imgSettings.AutoNumberImages
-			'These 3 will get set as a result of ImageSettingsFactory.CaseArtifactID
-			'tempLoadFile.CaseInfo
-			'tempLoadFile.CaseDefaultPath
-			'tempLoadFile.DestinationFolderID
-			'These 2 will get set in ImageSettingsFactory
-			'tempLoadFile.Credential = CType(GetCredentials(Settings), Net.NetworkCredential)
-			'tempLoadFile.CookieContainer = _cookieContainer
-			tempLoadFile.ForProduction = imgSettings.ForProduction
-			tempLoadFile.FullTextEncoding = imgSettings.ExtractedTextEncoding
-			tempLoadFile.ReplaceFullText = imgSettings.ExtractedTextFieldContainsFilePath
-			tempLoadFile.SendEmailOnLoadCompletion = imgSettings.SendEmailOnLoadCompletion
-
-			Return tempLoadFile
-		End Function
-
-		Private Function GetCredentials(ByVal imgSettings As ImageSettings) As Net.ICredentials
-			Dim credential As System.Net.ICredentials = Nothing
-			If credential Is Nothing Then
-				Try
-					credential = kCura.WinEDDS.Api.LoginHelper.LoginWindowsAuthWithServiceURL(_cookieContainer, imgSettings.WebServiceURL)
-				Catch
-				End Try
-			End If
-
-			While credential Is Nothing
-				credential = kCura.WinEDDS.Api.LoginHelper.LoginUsernamePasswordWithServiceURL(imgSettings.RelativityUsername, imgSettings.RelativityPassword, _cookieContainer, imgSettings.WebServiceURL)
-				Exit While
-			End While
-			Return credential
-		End Function
-
-		Private Function GetDefaultIdentifierFieldID(ByVal credential As Net.NetworkCredential, ByVal caseArtifactID As Int32) As Int32
-			Dim retval As Int32
-			Dim tempFieldQuery As WinEDDS.Service.FieldQuery = New WinEDDS.Service.FieldQuery(credential, _cookieContainer, Settings.WebServiceURL)
-
-			Dim dt As System.Data.DataTable = tempFieldQuery.RetrievePotentialBeginBatesFields(caseArtifactID).Tables(0)
-			For Each identifierRow As System.Data.DataRow In dt.Rows
-				If CType(identifierRow("FieldCategoryID"), Global.Relativity.FieldCategory) = Global.Relativity.FieldCategory.Identifier Then
-					retval = CType(identifierRow("ArtifactID"), Int32)
-				End If
-			Next
-			Return retval
-		End Function
-
-		Protected Overrides Function IsSettingsValid() As Boolean
-			Try
-				ValidateRelativitySettings()
-				ValidateDataSourceSettings()
-			Catch ex As Exception
-				RaiseEvent OnMessage(New Status(ex.Message))
-				Return False
-			End Try
-			Return True
-		End Function
-
-		Private Sub MapSuppliedFieldNamesToActual(ByVal imageSettings As ImageSettings, ByRef srcDataTable As DataTable)
-			'kCura.WinEDDS.ImportExtension.ImageDataTableReader contains the 'real' field names
-			srcDataTable.Columns(imageSettings.BatesNumberField).ColumnName = "BatesNumber"
-			srcDataTable.Columns(imageSettings.DocumentIdentifierField).ColumnName = "DocumentIdentifier"
-			srcDataTable.Columns(imageSettings.FileLocationField).ColumnName = "FileLocation"
-		End Sub
-
-		Private Function MapInputToSettingsFactory(ByVal imgSettings As ImageSettings) As WinEDDS.ImageSettingsFactory
-			Dim tempCredential As Net.NetworkCredential = CType(GetCredentials(imgSettings), Net.NetworkCredential)
-			Dim tempImgSettings As WinEDDS.ImageSettingsFactory = New WinEDDS.ImageSettingsFactory(tempCredential, imgSettings.CaseArtifactId, imgSettings.WebServiceURL)
-
-			With tempImgSettings
-				If imgSettings.BeginBatesFieldArtifactID > 0 Then
-					.BeginBatesFieldArtifactID = imgSettings.BeginBatesFieldArtifactID
-				End If
-
-				If imgSettings.DestinationFolderArtifactID > 0 Then
-					.DestinationFolderID = imgSettings.DestinationFolderArtifactID
-				End If
-
-				Select Case imgSettings.NativeFileCopyMode
-					Case NativeFileCopyModeEnum.CopyFiles
-						.CopyFilesToDocumentRepository = True
-					Case NativeFileCopyModeEnum.SetFileLinks, Nothing
-						.CopyFilesToDocumentRepository = False
-					Case Else
-						Throw New Exception("ERROR with  NativeFileCopyMode")
-				End Select
-
-				Select Case imgSettings.OverwriteMode
-					Case OverwriteModeEnum.Append
-						.OverwriteDestination = WinEDDS.SettingsFactoryBase.OverwriteType.Append
-					Case OverwriteModeEnum.AppendOverlay
-						.OverwriteDestination = WinEDDS.SettingsFactoryBase.OverwriteType.AppendOverlay
-					Case OverwriteModeEnum.Overlay
-						.OverwriteDestination = WinEDDS.SettingsFactoryBase.OverwriteType.Overlay
-				End Select
-
-				If Not imgSettings.OverwriteMode = OverwriteModeEnum.Overlay AndAlso imgSettings.IdentityFieldId > 0 Then
-					.IdentityFieldId = imgSettings.IdentityFieldId
-				Else
-					.IdentityFieldId = GetDefaultIdentifierFieldID(tempCredential, imgSettings.CaseArtifactId)
-				End If
-
-				If imgSettings.ProductionArtifactID = 0 Then
-					.ProductionArtifactID = 0
-					'.ProductionTable = Nothing	'Don't know what this should be
-				Else
-					.ProductionArtifactID = imgSettings.ProductionArtifactID
-				End If
-
-				If imgSettings.StartRecordNumber > 0 Then
-					.StartLineNumber = imgSettings.StartRecordNumber
-				End If
-			End With
-
-			Return tempImgSettings
-		End Function
-#End Region
-
-#Region "Private Routines"
-		Protected Overrides Sub SelectServiceURL()
-			If Not String.IsNullOrWhiteSpace(Settings.WebServiceURL) Then
-				RaiseEvent OnMessage(New Status(String.Format("Using supplied ServiceURL {0}", Settings.WebServiceURL)))
-			ElseIf Not String.IsNullOrWhiteSpace(WinEDDS.Config.AppConfigWebServiceURL) Then
-				RaiseEvent OnMessage(New Status(String.Format("Using application configuration ServiceURL {0}", WinEDDS.Config.AppConfigWebServiceURL)))
-				Settings.WebServiceURL = WinEDDS.Config.AppConfigWebServiceURL
-			Else
-				RaiseEvent OnMessage(New Status(String.Format("Using default ServiceURL {0}", WinEDDS.Config.WebServiceURL)))
-				Settings.WebServiceURL = WinEDDS.Config.WebServiceURL
-			End If
-		End Sub
+#Region " Private Methods "
 
 		Private Sub ValidateDataSourceSettings()
 			'This expects the DataTable in SourceData to have already been set
@@ -211,6 +70,103 @@ Namespace kCura.Relativity.DataReaderClient
 			End If
 		End Sub
 
+		Private Function CreateLoadFile() As WinEDDS.ImageLoadFile
+			Dim credential As System.Net.NetworkCredential = DirectCast(GetCredentials(Settings), Net.NetworkCredential)
+			Dim casemanager As kCura.WinEDDS.Service.CaseManager = GetCaseManager(credential, Settings.WebServiceURL)
+			Dim tempLoadFile As New kCura.WinEDDS.ImageLoadFile
+			'tempLoadFile.DataTable = SourceData.SourceData
+
+			'These are ALL of the image file settings
+			tempLoadFile.AutoNumberImages = Settings.AutoNumberImages
+			tempLoadFile.CaseInfo = casemanager.Read(Settings.CaseArtifactId)
+			'tempLoadFile.ControlKeyField = "Identifier"
+			tempLoadFile.Credential = credential
+			tempLoadFile.CookieContainer = cookieMonster
+			tempLoadFile.CopyFilesToDocumentRepository = Settings.CopyFilesToDocumentRepository
+			tempLoadFile.DestinationFolderID = tempLoadFile.CaseInfo.RootFolderID
+			tempLoadFile.ForProduction = Settings.ForProduction
+			tempLoadFile.FullTextEncoding = Settings.ExtractedTextEncoding
+			tempLoadFile.Overwrite = Settings.OverwriteMode.ToString
+			tempLoadFile.ReplaceFullText = Settings.ExtractedTextFieldContainsFilePath
+			If tempLoadFile.Overwrite = OverwriteModeEnum.Overlay.ToString Then
+				tempLoadFile.IdentityFieldId = GetDefaultIdentifierFieldID(credential, Settings.CaseArtifactId)
+			Else
+				tempLoadFile.IdentityFieldId = Settings.IdentityFieldId	'e.x Control Number
+			End If
+
+			If Settings.ProductionArtifactID = 0 Then
+				tempLoadFile.ProductionArtifactID = 0
+				'tempLoadFile.ProductionTable = Nothing	'Don't know what this should be
+			Else
+				tempLoadFile.ProductionArtifactID = Settings.ProductionArtifactID
+			End If
+
+			tempLoadFile.SelectedCasePath = tempLoadFile.CaseInfo.DocumentPath
+			tempLoadFile.SendEmailOnLoadCompletion = False
+			tempLoadFile.StartLineNumber = 0
+			'tempLoadFile.BeginBatesFieldArtifactID = 1035276
+
+			Return tempLoadFile
+
+		End Function
+
+		Private Sub MapSuppliedFieldNamesToActual(ByVal imageSettings As ImageSettings, ByRef srcDataTable As DataTable)
+			'kCura.WinEDDS.ImportExtension.ImageDataTableReader contains the 'real' field names
+			srcDataTable.Columns(imageSettings.BatesNumberField).ColumnName = "BatesNumber"
+			srcDataTable.Columns(imageSettings.DocumentIdentifierField).ColumnName = "DocumentIdentifier"
+			srcDataTable.Columns(imageSettings.FileLocationField).ColumnName = "FileLocation"
+		End Sub
+
+		Private Function GetDefaultIdentifierFieldID(ByVal credential As System.Net.NetworkCredential, ByVal caseArtifactID As Int32) As Int32
+			Dim retval As Int32
+			Dim dt As System.Data.DataTable = New kCura.WinEDDS.Service.FieldQuery(credential, cookieMonster, Settings.WebServiceURL).RetrievePotentialBeginBatesFields(caseArtifactID).Tables(0)
+			For Each identifierRow As System.Data.DataRow In dt.Rows
+				If CType(identifierRow("FieldCategoryID"), Global.Relativity.FieldCategory) = Global.Relativity.FieldCategory.Identifier Then
+					retval = CType(identifierRow("ArtifactID"), Int32)
+				End If
+			Next
+			Return retval
+		End Function
+
+		Private Function GetCaseManager(ByVal credentials As Net.ICredentials, webURL As String) As kCura.WinEDDS.Service.CaseManager
+			Return New kCura.WinEDDS.Service.CaseManager(credentials, cookieMonster, webURL)
+		End Function
+
+		Private Function GetCredentials(ByVal settings As ImageSettings) As System.Net.ICredentials
+			Dim credential As System.Net.ICredentials = Nothing
+			If credential Is Nothing Then
+				Try
+					credential = kCura.WinEDDS.Api.LoginHelper.LoginWindowsAuthWithServiceURL(cookieMonster, settings.WebServiceURL)
+				Catch
+				End Try
+			End If
+
+			While credential Is Nothing
+				credential = kCura.WinEDDS.Api.LoginHelper.LoginUsernamePasswordWithServiceURL(settings.RelativityUsername, settings.RelativityPassword, cookieMonster, settings.WebServiceURL)
+				Exit While
+			End While
+			Return credential
+		End Function
+
+
+#End Region
+
+#Region " Validation "
+
+		Private Function IsSettingsValid() As Boolean
+			Try
+				'	ValidateSqlCommandSettings()
+				ValidateRelativitySettings()
+				ValidateDataSourceSettings()
+				'ValidateOverwriteModeSettings()
+				'ValidateExtractedTextSettings()
+			Catch ex As Exception
+				RaiseEvent OnMessage(New Status(ex.Message))
+				Return False
+			End Try
+			Return True
+		End Function
+
 		Private Sub ValidateRelativitySettings()
 			If Settings.RelativityUsername Is Nothing OrElse Settings.RelativityUsername = String.Empty Then
 				Throw New Exception(String.Format("{0} must be set", "RelativityUsername"))
@@ -221,14 +177,47 @@ Namespace kCura.Relativity.DataReaderClient
 			If Settings.CaseArtifactId = 0 Then
 				Throw New Exception(String.Format("{0} must be set and cannot be 0", "CaseArtifactId"))
 			End If
+			If Settings.ArtifactTypeId = 0 Then
+				Throw New Exception(String.Format("{0} must be set and cannot be 0", "ArtifactTypeId"))
+			End If
 		End Sub
+
+		Private Sub ValidateOverwriteModeSettings()
+			If Settings.OverwriteMode = OverwriteModeEnum.Overlay Then
+				If Settings.OverlayIdentifierSourceFieldName Is Nothing OrElse Settings.OverlayIdentifierSourceFieldName.Trim = String.Empty Then
+					Throw New Exception("When Overwrite Mode is set to Overlay, Overlay Identifier Field must be set.")
+				End If
+			End If
+		End Sub
+
+		Private Sub ValidateExtractedTextSettings()
+			If Settings.ExtractedTextFieldContainsFilePath Then
+				If Settings.ExtractedTextEncoding Is Nothing Then
+					Throw New Exception("ExtractedTextEncoding not set")
+				End If
+			End If
+		End Sub
+
+		Protected Sub SelectServiceURL()
+			If Not String.IsNullOrWhiteSpace(Settings.WebServiceURL) Then
+				RaiseEvent OnMessage(New Status(String.Format("Using supplied ServiceURL {0}", Settings.WebServiceURL)))
+			ElseIf Not String.IsNullOrWhiteSpace(WinEDDS.Config.AppConfigWebServiceURL) Then
+				RaiseEvent OnMessage(New Status(String.Format("Using application configuration ServiceURL {0}", WinEDDS.Config.AppConfigWebServiceURL)))
+				Settings.WebServiceURL = WinEDDS.Config.AppConfigWebServiceURL
+			Else
+				RaiseEvent OnMessage(New Status(String.Format("Using default ServiceURL {0}", WinEDDS.Config.WebServiceURL)))
+				Settings.WebServiceURL = WinEDDS.Config.WebServiceURL
+			End If
+		End Sub
+
 #End Region
 
 #Region " Event Handling "
-		Private Sub _observer_ErrorReportEvent(ByVal row As IDictionary) Handles _observer.ErrorReportEvent
+
+		Private Sub _observer_ErrorReportEvent(ByVal row As System.Collections.IDictionary) Handles _observer.ErrorReportEvent
 			RaiseEvent OnError(row)
 
-			Dim retval As New Text.StringBuilder
+			Dim retval As New System.Text.StringBuilder
 			retval.AppendFormat("[error] ")
 			For Each key As String In row.Keys
 				retval.Append(key)
@@ -256,6 +245,7 @@ Namespace kCura.Relativity.DataReaderClient
 		Private Sub _observer_OnProcessProgressEvent(ByVal evt As kCura.Windows.Process.ProcessProgressEvent) Handles _observer.OnProcessProgressEvent
 			RaiseEvent OnMessage(New Status(String.Format("[Timestamp: {0}] [Progress Info: {1} of {2}]", System.DateTime.Now, evt.TotalRecordsProcessedDisplay, evt.TotalRecordsDisplay)))
 		End Sub
+
 #End Region
 
 	End Class
