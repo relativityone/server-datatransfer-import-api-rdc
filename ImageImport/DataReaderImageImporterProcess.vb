94717a609d2ae092eaeb99880e09ad15446ab279
Bardia Noohi <bnoohi@kcura.com>
2011-09-20 17:06:18 -0500

    MERGE: Pulling from 7.2

--- ImageImport/DataReaderImageImporterProcess.vb
+++ ImageImport/DataReaderImageImporterProcess.vb
@@ -21,7 +21,7 @@ Namespace kCura.WinEDDS.ImportExtension
 					ImageLoadFile.Overwrite = "both"
 			End Select
 
-			Return New DataReaderImageImporter(1003697, ImageLoadFile, New kCura.Windows.Process.Controller, System.Guid.NewGuid, _sourceData)
+			Return New DataReaderImageImporter(1003697, ImageLoadFile, Me.ProcessController, System.Guid.NewGuid, _sourceData)
 
 		End Function
 
