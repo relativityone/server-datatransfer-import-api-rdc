ab4addec46ab4933409e9df49f6279ff8c6163ed
Philip Soderling <psoderling@kcura.com>
2011-10-21 12:30:25 -0500

    kSDK: Added ability to get the uploade mode (direct vs web) to ImportAPI; added accompanying UploadTypeEnum

---
+++ Enumeration/UploadTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kCura.Relativity.ImportAPI.Enumeration
+{
+	public enum UploadTypeEnum
+	{
+		Web,
+		Direct
+	}
+}
--- ImportAPI.cs
+++ ImportAPI.cs
@@ -22,6 +22,7 @@ namespace kCura.Relativity.ImportAPI
 
 		private String _userName;
 		private String _password;
+		private CaseManager _caseManager;
 		private CookieContainer _cookieMonster;
 		private ICredentials _credentials;
 
@@ -58,8 +59,7 @@ namespace kCura.Relativity.ImportAPI
 		/// <returns></returns>
 		public IEnumerable<Workspace> Workspaces()
 		{
-
-			var cm = new CaseManager(_credentials, _cookieMonster);
+			var cm = GetCaseManager();
 			var wsds = cm.RetrieveAll();
 			var dt = wsds.Tables[0];
 
@@ -80,24 +80,24 @@ namespace kCura.Relativity.ImportAPI
 		public IEnumerable<Field> GetWorkspaceFields(int workspaceArtifactID, int artifactTypeID)
 		{
 			var fm = new WinEDDS.Service.FieldManager(_credentials, _cookieMonster);
-            //This returned collection contains fields excluding those with one of the following FieldCategory's
-            // FieldCategory.AutoCreate
-            // FieldCategory.Batch
-            // FieldCategory.FileInfo
-            // FieldCategory.FileSize
-            // FieldCategory.MarkupSetMarker
-            // FieldCategory.MultiReflected
-            // FieldCategory.ProductionMarker
-            // FieldCategory.Reflected
-            // See kCura.WinEDDS.Service.FieldQuery.RetrieveAllAsArray -Phil S. 10/19/2011
+			//This returned collection contains fields excluding those with one of the following FieldCategory's
+			// FieldCategory.AutoCreate
+			// FieldCategory.Batch
+			// FieldCategory.FileInfo
+			// FieldCategory.FileSize
+			// FieldCategory.MarkupSetMarker
+			// FieldCategory.MultiReflected
+			// FieldCategory.ProductionMarker
+			// FieldCategory.Reflected
+			// See kCura.WinEDDS.Service.FieldQuery.RetrieveAllAsArray -Phil S. 10/19/2011
 			var fields = fm.Query.RetrieveAllAsDocumentFieldCollection(workspaceArtifactID, artifactTypeID);
 
 			return (from DocumentField docfield in fields
-			        select new Field
-			               	{
-                                ArtifactID = docfield.FieldID, ArtifactTypeId = docfield.FieldTypeID, Name = docfield.FieldName, FieldLength = docfield.FieldLength, FieldTypeID = (FieldTypeEnum)Enum.ToObject(typeof(FieldTypeEnum),
-                                    docfield.FieldTypeID), AssociatedObjectTypeID = docfield.AssociatedObjectTypeID, UseUnicode = docfield.UseUnicode, FieldCategory = (FieldCategoryEnum)Enum.ToObject(typeof(FieldCategoryEnum), docfield.FieldCategoryID)
-			               	}).ToList();
+					select new Field
+							{
+								ArtifactID = docfield.FieldID, ArtifactTypeId = docfield.FieldTypeID, Name = docfield.FieldName, FieldLength = docfield.FieldLength, FieldTypeID = (FieldTypeEnum)Enum.ToObject(typeof(FieldTypeEnum),
+									docfield.FieldTypeID), AssociatedObjectTypeID = docfield.AssociatedObjectTypeID, UseUnicode = docfield.UseUnicode, FieldCategory = (FieldCategoryEnum)Enum.ToObject(typeof(FieldCategoryEnum), docfield.FieldCategoryID)
+							}).ToList();
 		}
 
 
@@ -123,12 +123,49 @@ namespace kCura.Relativity.ImportAPI
 			return new ImportBulkArtifactJob(_userName, _password);						
 		}
 
+		/// <summary>
+		/// This method returns the UploadMode that will be used to upload files to
+		/// the workspace specified by <paramref name="caseArtifactID"/>.
+		/// </summary>
+		/// <param name="caseArtifactID">The Artifact ID of the destination workspace.</param>
+		/// <returns></returns>
+		public UploadTypeEnum GetFileUploadMode(int caseArtifactID)
+		{
+			var cm = GetCaseManager();
+			var caseInfo = cm.Read(caseArtifactID);
 
-
-
-
+			return GetFileUploadMode(caseArtifactID, caseInfo.DocumentPath);
+		}
 
 #region "Private items"
+
+		/// <summary>
+		/// Create a repository path based on the given workspace ArtifactID and
+		/// the default path for that workspace. This code is a combination of
+		/// kCura.WinEDDS.BulkLoadFileImporter:New()  and
+		/// kCura.WinEDDS.BulkImageFileImporter:New()
+		/// -Phil S. 10/21/2011
+		/// </summary>
+		/// <param name="caseArtifactID"></param>
+		/// <param name="defaultCasePath"></param>
+		/// <returns></returns>
+		private string CreateRepositoryPath(int caseArtifactID, string defaultCasePath)
+		{
+			var repoSuffix = "\\EDDS" + caseArtifactID + "\\";
+			var returnRepoPath = defaultCasePath.TrimEnd('\\') + repoSuffix;
+
+			return returnRepoPath;
+		}
+
+		private CaseManager GetCaseManager()
+		{
+			if (_caseManager == null)
+			{
+				_caseManager = new CaseManager(_credentials, _cookieMonster);
+			}
+
+			return _caseManager;
+		}
 
 		private ICredentials GetCredentials()
 		{
@@ -142,6 +179,43 @@ namespace kCura.Relativity.ImportAPI
 			}
 
 			return credentials;
+		}
+
+		/// <summary>
+		/// This method determines the upload mode used for the workspace specified
+		/// by <paramref name="caseArtifactID"/>. This code is taken from kCura.WinEDDS.FileUploader:SetType()
+		/// -Phil S. 10/21/2011
+		/// </summary>
+		/// <param name="caseArtifactID"></param>
+		/// <param name="defaultCasePath"></param>
+		/// <returns></returns>
+		private UploadTypeEnum GetFileUploadMode(int caseArtifactID, string defaultCasePath)
+		{
+			UploadTypeEnum returnUploadType;
+			var destFolderPath = CreateRepositoryPath(caseArtifactID, defaultCasePath);
+
+			try
+			{
+				var dummyText = System.Guid.NewGuid().ToString().Replace("-", String.Empty).Substring(0, 5);
+				//If the destination folder path is empty, we only need to test file Read/Write permissions
+				if (!String.IsNullOrEmpty(destFolderPath))
+				{
+					if (!System.IO.Directory.Exists(destFolderPath))
+					{
+						System.IO.Directory.CreateDirectory(destFolderPath);
+					}
+				}
+
+				System.IO.File.Create(destFolderPath + dummyText).Close();
+				System.IO.File.Delete(destFolderPath + dummyText);
+				returnUploadType = UploadTypeEnum.Direct;
+			}
+			catch (Exception)
+			{
+				returnUploadType = UploadTypeEnum.Web;
+			}
+
+			return returnUploadType;
 		}
 
 #endregion
