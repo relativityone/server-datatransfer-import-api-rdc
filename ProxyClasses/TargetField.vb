d457761b286a0fecb8d59c92fea76b11750e3b05
Justin Smits <jsmits@kcura.com>
2011-12-16 14:26:43 -0600

    FUNCTIONALITY: Moving appMappingData to Relativity.Core

---
+++ ProxyClasses/TargetField.vb
@@ -0,0 +1,15 @@
+﻿Namespace kCura.EDDS.WebAPI.TemplateManagerBase
+	Partial Public Class TargetField
+		Public Shared ReadOnly Property Empty() As TargetField
+			Get
+				Return New TargetField() With {.MyName = " - "}
+			End Get
+		End Property
+		Public ReadOnly Property IsEmpty() As Boolean
+			Get
+				Return Me.ArtifactID = 0
+			End Get
+		End Property
+	End Class
+End Namespace
+
