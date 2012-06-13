21f599a2e1099b41ec68e637430c43682fe4daa9
Adam Sorna <asorna@kcura.com>
2012-06-13 09:33:25 -0500

    DEFECT: Continuation of commit# 48900:6d4228febbfd7e2621c2ed0cafe8f1a4db2733d2 Fixed Issue "RDC Preview shows red text (indicating an error) if the extracted text being imported contains the word "error"" Added a class that I mistakenly left out of my last commit.

---
+++ Exceptions/ErrorMessage.vb
@@ -0,0 +1,9 @@
+﻿Public Class ErrorMessage
+	Private _message As System.String
+	Public Sub New(ex As System.Exception)
+		_message = ex.Message
+	End Sub
+	Public Overrides Function ToString() As String
+		Return _message
+	End Function
+End Class
