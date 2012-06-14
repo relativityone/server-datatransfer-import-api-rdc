6676d75861345b1e73c8fd86f3b353dd8c910782
Adam Sorna <asorna@kcura.com>
2012-06-14 16:32:03 -0500

    DEFECT: Fixed Issue "RDC Preview shows red text (indicating an error) if the extracted text being imported contains the word "error"" Added a class for pairing values for a DataTable to a Boolean indication of whether or not the value is an Error, and added support for its use to determine for the Form whether or not the cell containing the item is an error or not.

--- Exceptions/ErrorMessage.vb
+++ Exceptions/ErrorMessage.vb
@@ -1,9 +1,11 @@
-﻿Public Class ErrorMessage
-	Private _message As System.String
-	Public Sub New(ex As System.Exception)
-		_message = ex.Message
-	End Sub
-	Public Overrides Function ToString() As String
-		Return _message
-	End Function
-End Class
+﻿Namespace kCura.WinEDDS.Exceptions
+	Public Class ErrorMessage
+		Private _message As String
+		Public Sub New(msg As String)
+			_message = msg
+		End Sub
+		Public Overrides Function ToString() As String
+			Return _message
+		End Function
+	End Class
+End Namespace
---
+++ Types/DataTableCellValueBooleanPair.vb
@@ -0,0 +1,22 @@
+﻿Namespace kCura.WinEDDS
+	Public Class DataTableCellValueBooleanPair
+		Private _value As String
+		Private _isError As Boolean
+		Public Sub New(obj As Object)
+			Reset(obj)
+		End Sub
+		Public Sub Reset(obj As Object)
+			_value = obj.ToString()
+			_isError = TypeOf obj Is Exceptions.ErrorMessage
+		End Sub
+		Public Overrides Function ToString() As String
+			Return _value
+		End Function
+		Public ReadOnly Property IsError As Boolean
+			Get
+				Return _isError
+			End Get
+		End Property
+	End Class
+End Namespace
+
