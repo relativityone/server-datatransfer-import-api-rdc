bfe027f1b9cafba1978f1961c22faef58ccb71ff
Philip Soderling <psoderling@kcura.com>
2012-08-27 13:14:56 -0500

    kSDK: Created new project to extend the ImportAPI class--we don't want this functionality shipped to clients; implemented functionality for audit spoofing similar to what used to exist in the ImportAPI class; created a method to dispose of the token via AuditManager when all jobs are done; updated DataReader assembly to expose its internals to this new project

---
+++ ExtendedImportAPI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using kCura.Relativity.DataReaderClient;
+using kCura.WinEDDS.Service;
+
+namespace kCura.Relativity.ImportAPI.Extension {
+	public class ExtendedImportAPI : ImportAPI {
+		public ExtendedImportAPI(string UserName, string Password)
+			: base(UserName, Password) {
+		}
+
+		public ExtendedImportAPI(string UserName, string Password, string WebServiceURL)
+			: base(UserName, Password, WebServiceURL) {
+		}
+
+		public ExtendedImportAPI(string WebServiceURL)
+			: base(WebServiceURL) {
+		}
+
+		public void CleanUpAfterJobWithSpoofing(ImportBulkArtifactJob job) {
+			if (job != null && !String.IsNullOrWhiteSpace(job.Settings.OnBehalfOfUserToken)) {
+				var am = new AuditManager(Credentials, CookieCache);
+
+				try {
+					am.DeleteAuditToken(job.Settings.OnBehalfOfUserToken);
+				} catch {
+				}
+			}
+		}
+
+		public ImportBulkArtifactJob NewNativeDocumentImportJobWithSpoofing(string token) {
+			var returnJob = NewNativeDocumentImportJob();
+
+			returnJob.Settings.OnBehalfOfUserToken = token;
+
+			return returnJob;
+		}
+	}
+}
