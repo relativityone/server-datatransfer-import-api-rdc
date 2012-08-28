67e19c90315244a1e277edf4823fb854d60833d6
Philip Soderling <psoderling@kcura.com>
2012-08-28 12:18:43 -0500

    kSDK: Renamed 'SkipExtractedTExtEncodingCheck' to 'DisableExtractedTextEncodingCheck' to have a more consistent setting name (w.r.t the other 'Disable[SOMETHING]' property names) and updated all integration tests accordingly

--- ImportLoadFileJob.vb
+++ ImportLoadFileJob.vb
@@ -90,7 +90,7 @@ Namespace kCura.Relativity.DataReaderClient
 				process.MaximumErrorCount = Settings.MaximumErrorCount
 				process.DisableUserSecurityCheck = Settings.DisableUserSecurityCheck
 				process.AuditLevel = Settings.AuditLevel
-				process.SkipExtractedTextEncodingCheck = Settings.SkipExtractedTextEncodingCheck
+				process.SkipExtractedTextEncodingCheck = Settings.DisableExtractedTextEncodingCheck
 				process.DisableExtractedTextFileLocationValidation = Settings.DisableExtractedTextFileLocationValidation
 
 				RaiseEvent OnMessage(New Status("Updating settings"))
@@ -374,8 +374,8 @@ Namespace kCura.Relativity.DataReaderClient
 
 		Private Sub ValidateExtractedTextSettings()
 			If Settings.ExtractedTextFieldContainsFilePath Then
-				If Settings.SkipExtractedTextEncodingCheck.HasValue AndAlso Settings.SkipExtractedTextEncodingCheck AndAlso Settings.ExtractedTextEncoding Is Nothing Then
-					Throw New ImportSettingsConflictException("SkipExtractedTextEncodingCheck", "ExtractedTextEncoding", "ExtractedTextEncoding must be set if SkipExtractedTextEncodingCheck is enabled.")
+				If Settings.DisableExtractedTextEncodingCheck.HasValue AndAlso Settings.DisableExtractedTextEncodingCheck AndAlso Settings.ExtractedTextEncoding Is Nothing Then
+					Throw New ImportSettingsConflictException("DisableExtractedTextEncodingCheck", "ExtractedTextEncoding", "ExtractedTextEncoding must be set if DisableExtractedTextEncodingCheck is enabled.")
 				End If
 				If Settings.ExtractedTextEncoding Is Nothing Then
 					Throw New ImportSettingsException("ExtractedTextEncoding", String.Empty)
