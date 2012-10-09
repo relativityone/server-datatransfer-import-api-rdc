3dba113dfb9be9d7e12f1a08184d311c36976f2b
Arunas Slepikas <aslepikas@kcura.com>
2012-10-09 11:07:43 -0500

    Backed out changeset: 39049eeb9bae, waiting until next sprint

--- Forms/ExportForm.vb
+++ Forms/ExportForm.vb
@@ -1257,20 +1257,20 @@ Public Class ExportForm
 		Select Case Me.ExportFile.TypeOfExport
 			Case ExportFile.ExportType.AncestorSearch
 				_exportFile.ViewID = CType(_filters.SelectedValue, Int32)
-				_exportFile.LoadFilesPrefix = System.Security.SecurityElement.Escape(DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString)
+				_exportFile.LoadFilesPrefix = DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString
 			Case ExportFile.ExportType.ArtifactSearch
 				_exportFile.ArtifactID = CType(_filters.SelectedValue, Int32)
 				If Not _application.IsAssociatedSearchProviderAccessible(_exportFile.CaseArtifactID, _exportFile.ArtifactID) Then
 					Me.Cursor = System.Windows.Forms.Cursors.Default
 					Return Nothing
 				End If
-				_exportFile.LoadFilesPrefix = System.Security.SecurityElement.Escape(DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString)
+				_exportFile.LoadFilesPrefix = DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString
 			Case ExportFile.ExportType.ParentSearch
 				_exportFile.ViewID = CType(_filters.SelectedValue, Int32)
-				_exportFile.LoadFilesPrefix = System.Security.SecurityElement.Escape(DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString)
+				_exportFile.LoadFilesPrefix = DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString
 			Case ExportFile.ExportType.Production
 				_exportFile.ArtifactID = CType(_filters.SelectedValue, Int32)
-				_exportFile.LoadFilesPrefix = System.Security.SecurityElement.Escape(DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString)
+				_exportFile.LoadFilesPrefix = DirectCast(_filters.SelectedItem, System.Data.DataRowView)(_filters.DisplayMember).ToString
 				If _nativeFileNameSource.SelectedItem.ToString.ToLower = "identifier" Then
 					_exportFile.ExportNativesToFileNamedFrom = ExportNativeWithFilenameFrom.Identifier
 				Else
