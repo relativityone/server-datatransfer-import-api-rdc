258f332843eae3d5737055488e450152d431bc51
Adam Sorna <asorna@kcura.com>
2013-07-05 10:16:03 -0500

    Atlas: IAPI: Changed folder activity to pull existing folder info from ExtendedFolder view
    --HG--
    branch : feature-iapijune13

--- Importers/BulkLoadFileImporter.vb
+++ Importers/BulkLoadFileImporter.vb
@@ -690,13 +690,18 @@ Namespace kCura.WinEDDS
 				path = path.Replace("\\", "\")
 			End While
 			path = path.Replace(":", "_")
+
 			If Not path.Length = 0 Then
 				If path.Chars(0) <> "\"c Then
 					path = "\" & path
 				End If
 			End If
 			path = path.TrimEnd(New Char() {"\"c})
-			If path = "" Then path = "\"
+
+			' On the RelativityWebAPI side of the coin, we're joining over the ExtendedFolder View FullPath, in which folders are delimited ' \ '
+			path = path.Replace("\", " \ ")
+
+			If path = "" Then path = " \ "
 			Return path
 		End Function
 
