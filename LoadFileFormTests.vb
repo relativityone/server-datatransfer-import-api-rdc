41dbc9d429bcf852a210ee65dd682a9c39c05d4a
Mikhail Kogan <mkogan@kcura.com>
2013-09-09 15:43:39 -0500

    FUNCTIONALITY: added test file
    --HG--
    branch : area-sharktopus

---
+++ LoadFileFormTests.vb
@@ -0,0 +1,18 @@
+﻿Imports NUnit.Framework
+
+Namespace kCura.EDDS.WinForm.Tests
+	<TestFixture()>
+	Public Class LoadFileFormTests
+
+
+		Private Class MockLoadFileForm
+			Inherits kCura.EDDS.WinForm.LoadFileForm
+			
+			Public Sub New()
+				MyBase.New()
+			End Sub
+		End Class
+
+
+	End Class
+End Namespace
