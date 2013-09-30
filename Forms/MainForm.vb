ed3e406ca8af32013f5d511778381413209b2276
Nick Kapuza <nkapuza@kcura.com>
2013-09-30 14:51:05 -0500

    DEFECT: DAC245: Inconsistant visaul presentation for Allow Export/Import permissions

--- Forms/MainForm.vb
+++ Forms/MainForm.vb
@@ -381,18 +381,22 @@ Namespace kCura.EDDS.WinForm
 					UpdateStatus("Workspace Loaded - File Transfer Mode: " & _application.GetConnectionStatus)
 					PopulateObjectTypeDropDown()
 					_optionsMenuCheckConnectivityItem.Enabled = True
+					ImportMenu.Enabled = _application.UserHasImportPermission
+					ExportMenu.Enabled = _application.UserHasExportPermission
+					ImportMenu.Visible = _application.UserHasImportPermission
+					ExportMenu.Visible = _application.UserHasExportPermission
 				Case appEvent.AppEventType.LogOn
 					UpdateUserName(_application.LoggedInUser)
 				Case appEvent.AppEventType.ExitApplication
 					Me.Close()
-                Case appEvent.AppEventType.WorkspaceFolderSelected
-
-                    'disable import and export menus if no permission
-                    ImportMenu.Enabled = _application.UserHasImportPermission
-                    ExportMenu.Enabled = _application.UserHasExportPermission
-
-                    'UpdateStatus("Case Folder Load: " + _application.SelectedCaseInfo.RootFolderID.ToString)
-            End Select
+				Case appEvent.AppEventType.WorkspaceFolderSelected
+					'disable import and export menus if no permission
+					ImportMenu.Enabled = _application.UserHasImportPermission
+					ExportMenu.Enabled = _application.UserHasExportPermission
+					ImportMenu.Visible = _application.UserHasImportPermission
+					ExportMenu.Visible = _application.UserHasExportPermission
+					'UpdateStatus("Case Folder Load: " + _application.SelectedCaseInfo.RootFolderID.ToString)
+			End Select
 		End Sub
 
 		Private Sub UpdateStatus(ByVal text As String)
