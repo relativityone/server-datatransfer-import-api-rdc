faf209ff1fdb14e1219bb9624919ea5cff3f6286
Mike Goldstein <mgoldstein@kcura.com>
2014-06-11 09:45:43 -0500

    DEFECT: fixing unit tests for storage location
    --HG--
    branch : area-sharktopus

--- TestObjectFactories/QueryFieldFactory.vb
+++ TestObjectFactories/QueryFieldFactory.vb
@@ -37,6 +37,7 @@
 				_dt.Columns.Add("RelationalTableName", GetType(String))
 				_dt.Columns.Add("RelationalTableColumnName", GetType(String))
 				_dt.Columns.Add("RelationalTableColumnName2", GetType(String))
+				_dt.Columns.Add("StorageLocation", GetType(Int32))
 			End If
 			Return _dt.NewRow
 		End Function
@@ -71,7 +72,8 @@
 		 ByVal dataSource As String, _
 		 ByVal relationalTableName As String, _
 		 ByVal relationalTableColumnName As String, _
-		 ByVal relationalTableColumnName2 As String _
+		 ByVal relationalTableColumnName2 As String, _
+		 ByVal strorageLocation As Int32 _
 		) As kCura.WinEDDS.ViewFieldInfo
 			Dim row As System.Data.DataRow = Me.GenerateRow
 			row("FieldArtifactId") = fieldArtifactID
@@ -104,84 +106,85 @@
 			row("RelationalTableName") = relationalTableName
 			row("RelationalTableColumnName") = relationalTableColumnName
 			row("RelationalTableColumnName2") = relationalTableColumnName2
+			row("StorageLocation") = strorageLocation
 			Return New kCura.WinEDDS.ViewFieldInfo(row)
 		End Function
 
 		Public Function GetIdentifierQueryField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(5, 1000186, Relativity.FieldCategory.Identifier, Relativity.FieldTypeHelper.FieldType.Varchar, 10, "Document", -1, False, "Identifier", "Identifier", "Identifier", "", -1, True, "", -1, "", "Identifier", -1, -1, "", -1, -1, "", True, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(5, 1000186, Relativity.FieldCategory.Identifier, Relativity.FieldTypeHelper.FieldType.Varchar, 10, "Document", -1, False, "Identifier", "Identifier", "Identifier", "", -1, True, "", -1, "", "Identifier", -1, -1, "", -1, -1, "", True, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetExtractedTextField() As kCura.WinEDDS.ViewFieldInfo
 			'BigData_ET_#
-			Return Me.GenerateQueryField(6, 1000187, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "Extracted Text", "ExtractedText", "Extracted Text", "", -1, True, "", -1, "", "Extracted Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(6, 1000187, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "Extracted Text", "ExtractedText", "Extracted Text", "", -1, True, "", -1, "", "Extracted Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericLongTextField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(6, 1001440, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "Long Text", "LongText", "Long Text", "", -1, True, "", -1, "", "Long Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(6, 1001440, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "Long Text", "LongText", "Long Text", "", -1, True, "", -1, "", "Long Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetRenamedGenericLongTextField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(6, 1001440, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "OtherLong Text", "OtherLongText", "OtherLong Text", "", -1, True, "", -1, "", "OtherLong Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(6, 1001440, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "OtherLong Text", "OtherLongText", "OtherLong Text", "", -1, True, "", -1, "", "OtherLong Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetSameNameDifferentIdGenericLongTextField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(6, 1441000, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "Long Text", "LongText", "Long Text", "", -1, True, "", -1, "", "Long Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(6, 1441000, Relativity.FieldCategory.FullText, Relativity.FieldTypeHelper.FieldType.Text, 10, "Document", -1, False, "Long Text", "LongText", "Long Text", "", -1, True, "", -1, "", "Long Text", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericBooleanField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(14497, 1000357, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Boolean, 10, "Document", -1, False, "Bool", "Bool", "Bool", "", -1, False, "", -1, "", "Bool", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(14497, 1000357, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Boolean, 10, "Document", -1, False, "Bool", "Bool", "Bool", "", -1, False, "", -1, "", "Bool", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericSingleCodeField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(391085, 1001118, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Code, 10, "Document", 1000097, False, "Single Choice", "SingleChoice", "Single Choice", "", -1, True, "", -1, "", "Single Choice", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(391085, 1001118, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Code, 10, "Document", 1000097, False, "Single Choice", "SingleChoice", "Single Choice", "", -1, True, "", -1, "", "Single Choice", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericMultiCodeField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(391081, 1001117, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.MultiCode, 10, "Document", 1000096, False, "MCode", "MCode", "MCode", "", -1, False, "", -1, "", "MCode", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(391081, 1001117, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.MultiCode, 10, "Document", 1000096, False, "MCode", "MCode", "MCode", "", -1, False, "", -1, "", "MCode", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetArtifactIdField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(13090, 1000248, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Integer, 10, "Document", -1, True, "Artifact ID", "ArtifactID", "ArtifactID", "", -1, False, "", -1, "", "ArtifactID", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(13090, 1000248, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Integer, 10, "Document", -1, True, "Artifact ID", "ArtifactID", "ArtifactID", "", -1, False, "", -1, "", "ArtifactID", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericNumericField(ByVal type As Relativity.FieldTypeHelper.FieldType) As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(13090, 1000248, Relativity.FieldCategory.Generic, type, 10, "Document", -1, False, "Decimal", "Decimal", "Decimal", "", -1, False, "", -1, "", "Decimal", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(13090, 1000248, Relativity.FieldCategory.Generic, type, 10, "Document", -1, False, "Decimal", "Decimal", "Decimal", "", -1, False, "", -1, "", "Decimal", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericUserField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(14490, 1000353, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.User, 10, "Document", -1, False, "User Field", "UserField", "User Field", "", -1, False, "", -1, "", "User Field", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(14490, 1000353, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.User, 10, "Document", -1, False, "User Field", "UserField", "User Field", "", -1, False, "", -1, "", "User Field", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericFileField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(393751, 1001134, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.File, 1000045, "Cmon", -1, False, "Attachment", "AttachmentName", "Attachment", "", -1, False, "", -1, "", "Filename", -1, -1, "", -1, 393751, "Attachment", False, "MetadataJoin", "File393751", "", "", "")
+			Return Me.GenerateQueryField(393751, 1001134, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.File, 1000045, "Cmon", -1, False, "Attachment", "AttachmentName", "Attachment", "", -1, False, "", -1, "", "Filename", -1, -1, "", -1, 393751, "Attachment", False, "MetadataJoin", "File393751", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericObjectField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(504869, 1001267, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Object, 10, "Document", -1, False, "WSP", "WSP", "WSP", "", 1000047, False, "", -1, "WithSpace", "Name", -1, -1, "", -1, -1, "", False, "MainTable", "o1000047_f504869", "", "", "")
+			Return Me.GenerateQueryField(504869, 1001267, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Object, 10, "Document", -1, False, "WSP", "WSP", "WSP", "", 1000047, False, "", -1, "WithSpace", "Name", -1, -1, "", -1, -1, "", False, "MainTable", "o1000047_f504869", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericObjectsField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(505158, 1001278, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Objects, 10, "Document", -1, False, "DcMon", "DcMon", "DcMon", "", 1000045, False, "", -1, "Cmon", "Name", -1, -1, "Name", -1, -1, "", False, "MainTable", "Document", "f505158f505168", "f505158ArtifactID", "f505168ArtifactID")
+			Return Me.GenerateQueryField(505158, 1001278, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Objects, 10, "Document", -1, False, "DcMon", "DcMon", "DcMon", "", 1000045, False, "", -1, "Cmon", "Name", -1, -1, "Name", -1, -1, "", False, "MainTable", "Document", "f505158f505168", "f505158ArtifactID", "f505168ArtifactID", 0)
 		End Function
 
 		Public Function GenerateMultiReflectedUserField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(505162, 1001282, Relativity.FieldCategory.MultiReflected, Relativity.FieldTypeHelper.FieldType.User, 10, "Document", -1, False, "DcMon::User", "DcMonUser", "DcMon::User", "", 1000045, False, "", 504867, "Cmon", "User", 1000045, 505158, "Name", 0, -1, "", False, "MainTable", "Cmon", "f505158f505168", "f505158ArtifactID", "f505168ArtifactID")
+			Return Me.GenerateQueryField(505162, 1001282, Relativity.FieldCategory.MultiReflected, Relativity.FieldTypeHelper.FieldType.User, 10, "Document", -1, False, "DcMon::User", "DcMonUser", "DcMon::User", "", 1000045, False, "", 504867, "Cmon", "User", 1000045, 505158, "Name", 0, -1, "", False, "MainTable", "Cmon", "f505158f505168", "f505158ArtifactID", "f505168ArtifactID", 0)
 		End Function
 
 		Public Function GetGenericDateField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(504869, 1001267, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Date, 10, "Document", -1, False, "Date", "Date", "Date", "d", -1, False, "", -1, "", "Date", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(504869, 1001267, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Date, 10, "Document", -1, False, "Date", "Date", "Date", "d", -1, False, "", -1, "", "Date", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetGenericDateTimeField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(504869, 1001267, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Date, 10, "Document", -1, False, "Date", "Date", "Date", "g", -1, False, "", -1, "", "Date", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "")
+			Return Me.GenerateQueryField(504869, 1001267, Relativity.FieldCategory.Generic, Relativity.FieldTypeHelper.FieldType.Date, 10, "Document", -1, False, "Date", "Date", "Date", "g", -1, False, "", -1, "", "Date", -1, -1, "", -1, -1, "", False, "MainTable", "Document", "", "", "", 0)
 		End Function
 
 		Public Function GetDocumentFolderNameField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(15, 1000196, Relativity.FieldCategory.FolderName, Relativity.FieldTypeHelper.FieldType.Varchar, 10, "Document", -1, False, "Folder Name", "FolderName", "Folder Name", "", -1, False, "", -1, "", "Name", -1, -1, "", -1, -1, "", False, "Artifact", "Folder", "", "", "")
+			Return Me.GenerateQueryField(15, 1000196, Relativity.FieldCategory.FolderName, Relativity.FieldTypeHelper.FieldType.Varchar, 10, "Document", -1, False, "Folder Name", "FolderName", "Folder Name", "", -1, False, "", -1, "", "Name", -1, -1, "", -1, -1, "", False, "Artifact", "Folder", "", "", "", 0)
 		End Function
 
 		Public Function GetChildDynamicObjectParentField() As kCura.WinEDDS.ViewFieldInfo
-			Return Me.GenerateQueryField(516343, 1001339, Relativity.FieldCategory.ParentArtifact, Relativity.FieldTypeHelper.FieldType.Object, 1000053, "ProjectChild", -1, True, "Project", "Project", "Project", "", 1000052, False, "", -1, "Project", "Name", -1, -1, "", -1, -1, "", False, "MainTable", "o1000052_f516343", "", "", "")
+			Return Me.GenerateQueryField(516343, 1001339, Relativity.FieldCategory.ParentArtifact, Relativity.FieldTypeHelper.FieldType.Object, 1000053, "ProjectChild", -1, True, "Project", "Project", "Project", "", 1000052, False, "", -1, "Project", "Name", -1, -1, "", -1, -1, "", False, "MainTable", "o1000052_f516343", "", "", "", 0)
 		End Function
 
 		Public Function GetAllDocumentFields() As ViewFieldInfo()
