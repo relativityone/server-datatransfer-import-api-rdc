28c8070c2f1edcab46c7a9a0685d7c1561c3e604
aberestenko <aberestenko@kcura.com>
2014-07-29 11:27:17 -0500

    Sync up with release-7.5 code
    --HG--
    branch : feature-branch-5

--- FieldMap.vb
+++ FieldMap.vb
@@ -1,6 +1,4 @@
 Imports kCura.Windows.Forms
-Imports System.Drawing
-Imports System.Collections.Generic
 
 Namespace kCura.WinEDDS.UIControls
 
@@ -42,8 +40,7 @@ Namespace kCura.WinEDDS.UIControls
 		Friend WithEvents _loadFileColumnsLabel As System.Windows.Forms.Label
 		Friend WithEvents _fieldColumns As kCura.Windows.Forms.TwoListBox
 		Friend WithEvents _loadFileColumns As kCura.Windows.Forms.TwoListBox
-
-		Private Sub InitializeComponent()
+		<System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
 			Me._fieldColumnsLabel = New System.Windows.Forms.Label
 			Me._loadFileColumnsLabel = New System.Windows.Forms.Label
 			Me._fieldColumns = New kCura.Windows.Forms.TwoListBox
@@ -53,9 +50,9 @@ Namespace kCura.WinEDDS.UIControls
 			'
 			'_fieldColumnsLabel
 			'
-			Me._fieldColumnsLabel.Name = "_fieldColumnsLabel"
 			Me._fieldColumnsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-			Me._fieldColumnsLabel.Location = New System.Drawing.Point(0, 4)
+			Me._fieldColumnsLabel.Location = New System.Drawing.Point(0, 8)
+			Me._fieldColumnsLabel.Name = "_fieldColumnsLabel"
 			Me._fieldColumnsLabel.Size = New System.Drawing.Size(145, 16)
 			Me._fieldColumnsLabel.TabIndex = 4
 			Me._fieldColumnsLabel.Text = "Workspace Fields"
@@ -63,10 +60,9 @@ Namespace kCura.WinEDDS.UIControls
 			'
 			'_loadFileColumnsLabel
 			'
-			Me._loadFileColumnsLabel.Name = "_loadFileColumnsLabel"
-			Me._loadFileColumnsLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
 			Me._loadFileColumnsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-			Me._loadFileColumnsLabel.Location = New System.Drawing.Point(574, 4)
+			Me._loadFileColumnsLabel.Location = New System.Drawing.Point(572, 8)
+			Me._loadFileColumnsLabel.Name = "_loadFileColumnsLabel"
 			Me._loadFileColumnsLabel.Size = New System.Drawing.Size(140, 16)
 			Me._loadFileColumnsLabel.TabIndex = 7
 			Me._loadFileColumnsLabel.Text = "Load File Fields"
@@ -74,25 +70,25 @@ Namespace kCura.WinEDDS.UIControls
 			'
 			'_fieldColumns
 			'
-			Me._fieldColumns.Name = "_fieldColumns"
-			Me._fieldColumns.Location = New System.Drawing.Point(0, 20)
-			Me._fieldColumns.Size = New System.Drawing.Size(356, 264)
 			Me._fieldColumns.AlternateRowColors = True
 			Me._fieldColumns.KeepButtonsCentered = True
 			Me._fieldColumns.LeftOrderControlsVisible = False
+			Me._fieldColumns.Location = New System.Drawing.Point(0, 24)
+			Me._fieldColumns.Name = "_fieldColumns"
 			Me._fieldColumns.RightOrderControlVisible = True
+			Me._fieldColumns.Size = New System.Drawing.Size(364, 276)
 			Me._fieldColumns.TabIndex = 8
 			Me._fieldColumns.OuterBox = kCura.Windows.Forms.ListBoxLocation.Left
 			'
 			'_loadFileColumns
 			'
-			Me._loadFileColumns.Name = "_loadFileColumns"
-			Me._loadFileColumns.Location = New System.Drawing.Point(360, 20)
-			Me._loadFileColumns.Size = New System.Drawing.Size(356, 264)
 			Me._loadFileColumns.AlternateRowColors = True
 			Me._loadFileColumns.KeepButtonsCentered = True
 			Me._loadFileColumns.LeftOrderControlsVisible = True
+			Me._loadFileColumns.Location = New System.Drawing.Point(360, 24)
+			Me._loadFileColumns.Name = "_loadFileColumns"
 			Me._loadFileColumns.RightOrderControlVisible = False
+			Me._loadFileColumns.Size = New System.Drawing.Size(388, 276)
 			Me._loadFileColumns.TabIndex = 9
 			Me._loadFileColumns.OuterBox = kCura.Windows.Forms.ListBoxLocation.Right
 			'
@@ -103,93 +99,11 @@ Namespace kCura.WinEDDS.UIControls
 			Me.Controls.Add(Me._loadFileColumnsLabel)
 			Me.Controls.Add(Me._fieldColumnsLabel)
 			Me.Name = "FieldMap"
-			Me.Size = New System.Drawing.Size(716, 288)
-
+			Me.Size = New System.Drawing.Size(732, 292)
 			Me.ResumeLayout(False)
 
 		End Sub
 
-#End Region
-
-#Region "Resizing"
-		'These member variables are populated with data needed to resize the controls
-
-		'Avoid adjusting the layout if the size hasn't changed
-		Private _layoutControlSize As Size
-
-		' Used to keep track of whether we need to calculate the layout values.  In addition to
-		' initial population, they may need to be populated later due to autoscaling.  Autoscaling
-		' will change the distance between concrols which we would not expect to change.  If this
-		' happens, the _layout info which contains the relative location of controls needs to be 
-		' updated.
-		Private _layoutReferenceDistance As Int32 = 0
-
-		Private _layoutRatioList As List(Of RelativeLayoutData)
-		Private _layoutDifferenceList As List(Of RelativeLayoutData)
-
-		Private Function CalcReferenceDistance() As Int32
-			Return _fieldColumnsLabel.Width
-		End Function
-
-		Private Sub OnControl_Layout(ByVal sender As Object, ByVal e As System.Windows.Forms.LayoutEventArgs) Handles MyBase.Layout
-			System.Diagnostics.Debug.WriteLine("LoadFileForm.OnForm_Layout occurred.  Width=" + Me.Width.ToString() + ", Height=" + Me.Height.ToString())
-
-			If _layoutReferenceDistance <> CalcReferenceDistance() Then
-				InitializeLayout()
-			Else
-				AdjustLayout()
-			End If
-		End Sub
-
-		Private Sub InitializeLayout()
-			_layoutControlSize = Me.Size
-
-			'Layout properties which are based on a ratio to another layout property. 
-			If _layoutRatioList Is Nothing Then
-				_layoutRatioList = New List(Of RelativeLayoutData)
-
-				'When the width of the dialog increases by 2 pixels, each groupbox increases by 1 pixel.  The ratio is 1/2 = .5
-				_layoutRatioList.Add(New RelativeLayoutData(Me, LayoutBasePropertyTypeForRatio.Width, _fieldColumns, LayoutRelativePropertyTypeForRatio.Width, 0.5))
-				_layoutRatioList.Add(New RelativeLayoutData(Me, LayoutBasePropertyTypeForRatio.Width, _loadFileColumns, LayoutRelativePropertyTypeForRatio.Width, 0.5))
-			End If
-
-			_layoutRatioList.ForEach(Sub(x)
-																 x.InitalizeRatioValues()
-															 End Sub)
-
-			'Layout properties which are directly based on another layout property
-			If _layoutDifferenceList Is Nothing Then
-				_layoutDifferenceList = New List(Of RelativeLayoutData)
-
-				_layoutDifferenceList.Add(New RelativeLayoutData(Me, LayoutBasePropertyTypeForDifference.Height, _fieldColumns, LayoutRelativePropertyTypeForDifference.Height))
-				_layoutDifferenceList.Add(New RelativeLayoutData(Me, LayoutBasePropertyTypeForDifference.Height, _loadFileColumns, LayoutRelativePropertyTypeForDifference.Height))
-
-				_layoutDifferenceList.Add(New RelativeLayoutData(_fieldColumns, LayoutBasePropertyTypeForDifference.Right, _loadFileColumns, LayoutRelativePropertyTypeForDifference.Left))
-
-				' No need to adjust _loadFileColumnsLabel because it is using anchoring
-			End If
-
-			_layoutDifferenceList.ForEach(Sub(x)
-																			x.InitializeDifference()
-																		End Sub)
-
-			_layoutReferenceDistance = CalcReferenceDistance()
-		End Sub
-
-		Public Sub AdjustLayout()
-			
-			If Not _layoutControlSize.Equals(Me.Size) Then
-				For Each x As RelativeLayoutData In _layoutRatioList
-					x.AdjustRelativeControlBasedOnRatio()
-				Next
-
-				For Each x As RelativeLayoutData In _layoutDifferenceList
-					x.AdjustRelativeControlBasedOnDifference()
-				Next
-
-				_layoutControlSize = Me.Size
-			End If
-		End Sub
 #End Region
 
 #Region "Properties"
