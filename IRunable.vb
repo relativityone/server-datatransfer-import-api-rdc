dec4aaf1fd74acca2833db3197eaaceef3624008
aberestenko <aberestenko@kcura.com>
2014-09-16 16:09:49 -0500

    Sync with default branch
    --HG--
    branch : feature-branch-5

--- IRunable.vb
+++ IRunable.vb
@@ -2,19 +2,18 @@
 	''' <summary>
 	''' Defines what it takes to be a runable process.
 	''' </summary>
-	''' <remarks></remarks>
 	Public Interface IRunable
 		''' <summary>
 		''' A unique identifier that will be assigned to the object when it's started.
 		''' </summary>
-		''' <value>A <see cref="System.Guid"> that identifies the object.</see></value>
-		''' <returns></returns>
+		''' <value>
+		''' A <see cref="System.Guid"> that identifies the object.</see>
+		''' </value>
 		Property ProcessID() As Guid
 
 		''' <summary>
 		''' The method that will be executed by the managing process runner.
 		''' </summary>
-		''' <remarks></remarks>
 		Sub StartProcess()
 	End Interface
 End Namespace
--- ProcessEventType.vb
+++ ProcessEventType.vb
@@ -2,22 +2,18 @@
 	''' <summary>
 	''' Possible event types for the <see cref="kCura.Windows.Process.ProcessEvent"></see> object.
 	''' </summary>
-	''' <remarks></remarks>
 	Public Enum ProcessEventTypeEnum
 		''' <summary>
 		''' Information message type
 		''' </summary>
-		''' <remarks></remarks>
 		Status = 0
 		''' <summary>
 		''' Warning message type
 		''' </summary>
-		''' <remarks></remarks>
 		Warning = 1
 		''' <summary>
 		''' Error message type
 		''' </summary>
-		''' <remarks></remarks>
 		[Error] = 2
 	End Enum
 End Namespace
