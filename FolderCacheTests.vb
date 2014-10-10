6a2a7eeed7244bb3b8ddae884c45c5c6811172fa
aberestenko <aberestenko@kcura.com>
2014-10-10 15:10:19 -0500

    Sync with area-sharktopus.
    --HG--
    branch : feature-branch-5

---
+++ FolderCacheTests.vb
@@ -0,0 +1,27 @@
+﻿Imports NUnit.Framework
+Imports Rhino.Mocks
+Imports kCura.WinEDDS.Service
+
+'Todo: this tests need cleaning and functionality
+Namespace kCura.WinEDDS.NUnit
+
+	'<TestFixture()> 
+	Public Class FolderCacheTests
+
+#Region " Members "
+		Private _mockRepo As MockRepository = Nothing
+		Dim _mockFolderManager As FolderManager
+#End Region
+
+#Region " Setup "
+
+		<SetUp()> Public Sub SetUp()
+			_mockRepo = New MockRepository()
+			_mockFolderManager = _mockRepo.DynamicMock(Of FolderManager)()
+		End Sub
+
+#End Region
+
+	End Class
+
+End Namespace
