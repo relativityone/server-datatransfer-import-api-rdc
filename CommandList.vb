31830ea5554c842dc0b0757826d7cde5d8fe03f1
aberestenko <aberestenko@kcura.com>
2014-10-10 15:10:19 -0500

    Sync with area-sharktopus.
    --HG--
    branch : feature-branch-5

---
+++ CommandList.vb
@@ -0,0 +1,8 @@
+Namespace kCura.CommandLine
+	Public Class CommandList
+		Inherits System.Collections.Generic.List(Of kCura.CommandLine.Command)
+		Public Sub New()
+			MyBase.New()
+		End Sub
+	End Class
+End Namespace
