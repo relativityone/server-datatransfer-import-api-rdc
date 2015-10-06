a8be7636ba56886c5f19ee38689776712f9f5d93
Peter Lembke <plembke@kcura.com>
2015-10-06 16:17:03 -0500

    Production - UAJ413 - updated the production export process to work.  mainly fixed the queries, and some of the RDC to accomodate for the fact that we no longer automatically pick the columns on the doc table that were choosen for the beg bates and end bates.  still need to fix the output to autoamatically include the bates values from the production information and to limit the number of documents to the ones in the production.
    --HG--
    branch : area-productions

--- Exporters/Exporter.vb
+++ Exporters/Exporter.vb
@@ -188,10 +188,10 @@ Namespace kCura.WinEDDS
 				End While
 
 				_productionExportProduction = production
-				With _fieldManager.Read(Me.Settings.CaseArtifactID, production.BeginBatesFieldArtifactID)
-					_beginBatesColumn = Relativity.SqlNameHelper.GetSqlFriendlyName(.DisplayName)
-					If Not allAvfIds.Contains(.ArtifactViewFieldID) Then allAvfIds.Add(.ArtifactViewFieldID)
-				End With
+'				With _fieldManager.Read(Me.Settings.CaseArtifactID, production.BeginBatesFieldArtifactID)
+'					_beginBatesColumn = Relativity.SqlNameHelper.GetSqlFriendlyName(.DisplayName)
+'					If Not allAvfIds.Contains(.ArtifactViewFieldID) Then allAvfIds.Add(.ArtifactViewFieldID)
+'				End With
 			End If
 
 			If Me.Settings.ExportImages AndAlso Me.Settings.LogFileFormat = LoadFileType.FileFormat.IPRO_FullText Then
