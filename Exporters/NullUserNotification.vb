adaca3796cf3d58927a36c0b2cac24939129740a
Nick Kapuza <nkapuza@kcura.com>
2016-05-03 15:57:22 -0500

    REFACTOR: Moving naked MsgBox calls behind an interface, filling that interface out in kCura.EDDS.WinForm
    --HG--
    branch : release-9.3-dev

---
+++ Exporters/NullUserNotification.vb
@@ -0,0 +1,22 @@
+﻿Namespace kCura.WinEDDS.Exporters
+	Public Class NullUserNotification
+		Implements IUserNotification
+
+		Private Sub Log(message As String)
+			Console.WriteLine(message)
+		End Sub
+
+		Public Sub Alert(message As String) Implements IUserNotification.Alert
+			Log(message)
+		End Sub
+
+		Public Sub AlertCriticalError(message As String) Implements IUserNotification.AlertCriticalError
+			Log("Critical: " & message)
+		End Sub
+
+		Public Function AlertWarningSkippable(message As String) As Boolean Implements IUserNotification.AlertWarningSkippable
+			Log("Skippable: " & message)
+			Return True
+		End Function
+	End Class
+End Namespace
