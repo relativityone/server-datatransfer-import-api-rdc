645df31dad97c5de46a302d11ed5463bf126ff37
Nick Kapuza <nkapuza@kcura.com>
2016-08-04 12:01:19 -0500

    rolling summary frame update rate changes back to 9.3 and they're hidden behind a non-documented config.

--- Config.vb
+++ Config.vb
@@ -43,6 +43,7 @@ Namespace kCura.WinEDDS
 							If Not tempDict.Contains("ForceWebUpload") Then tempDict.Add("ForceWebUpload", "False")
 							If Not tempDict.Contains("UsePipeliningForNativeAndObjectImports") Then tempDict.Add("UsePipeliningForNativeAndObjectImports", "False")
 							If Not tempDict.Contains("DisableTextFileEncodingCheck") Then tempDict.Add("DisableTextFileEncodingCheck", "False")
+							If Not tempDict.Contains("ProcessFormRefreshRate") Then tempDict.Add("ProcessFormRefreshRate", "0")
 							_configDictionary = tempDict
 						End If
 					End SyncLock
@@ -172,6 +173,16 @@ Namespace kCura.WinEDDS
 					Return CType(ConfigSettings("UsePipeliningForNativeAndObjectImports"), Boolean)
 				Catch
 					Return False
+				End Try
+			End Get
+		End Property
+
+		Public Shared ReadOnly Property ProcessFormRefreshRate As Long
+			Get
+				Try
+					Return CType(ConfigSettings("ProcessFormRefreshRate"), Long)
+				Catch
+					Return 0
 				End Try
 			End Get
 		End Property
