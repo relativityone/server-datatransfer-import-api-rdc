89789c60e09c1640ccfed3e4993a6c4afa967507
Matthew Wiemer <mwiemer@outlook.com>
2016-08-05 11:43:22 -0500

    Idk why this is happening
    For some reason these all appeared in my working directory after the last revert commit. We need them all, and I didn't touch them, so here  they are in a commit again. I'm suspicious of a bug it gitkraken

---
+++ WriteTests/SanityTests.vb
@@ -0,0 +1,18 @@
+﻿Imports NUnit.Framework
+Imports kCura.Relativity.DataReaderClient.NUnit
+Imports System.Configuration
+
+Namespace kCura.Relativity.DataReaderClient.NUnit.WriteTests
+    <TestFixture()> _
+    Public Class SanityTests
+
+#Region "Tests"
+        <Test(), _
+        Category("InfrastructureTests")> _
+        Public Sub SanityTest1()
+            Assert.That(1, [Is].EqualTo(1))
+        End Sub
+
+#End Region
+    End Class
+End Namespace
