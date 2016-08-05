c4444ec820528745399dffa192adb5ac4a418d0c
Matthew Wiemer <mwiemer@outlook.com>
2016-08-05 11:21:03 -0500

    idk what happened here but now all the build scripts are back
    This reverts commit 8d5c95f7c6599413ab5724dfc0384b22f449e5db.

---
+++ Exceptions/RepositoryException.vb
@@ -0,0 +1,9 @@
+﻿Namespace kCura.Relativity.DataReaderClient.NUnit.Exceptions
+	Public Class RepositoryException
+		Inherits System.Exception
+
+		Sub New(ByVal message As String)
+			MyBase.New(message)
+		End Sub
+	End Class
+End Namespace
---
+++ WriteTests/FixtureSetupTeardown.vb
@@ -0,0 +1,18 @@
+﻿Imports NUnit.Framework
+Imports kCura.Relativity.DataReaderClient.NUnit.Helpers
+Imports System.Configuration
+Imports System.Data.SqlClient
+
+Namespace kCura.Relativity.DataReaderClient.NUnit.WriteTests
+	<SetUpFixture()> _
+	Public Class FixtureSetupTeardown
+		Inherits kCura.NUnit.Integration.FixtureSetupTeardownBase
+
+		Public Sub New()
+			Dim theseWorkspaceIDs As New System.Collections.Generic.List(Of Int32)
+			theseWorkspaceIDs.Add(1016621)
+			theseWorkspaceIDs.Add(1016623)
+			Me.WorkspaceIDs = theseWorkspaceIDs
+		End Sub
+	End Class
+End Namespace
