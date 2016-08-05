b036c9d79782a7995fed09b5e88996753ea88e84
Matthew Wiemer <mwiemer@outlook.com>
2016-08-05 10:57:30 -0500

    Revert "wired in an empty kepler service"

--- ImportCredentialException.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class ImportCredentialException
-	Inherits System.Exception
-
-	Public Sub New(ByVal message As String, ByVal userName As String, ByVal URL As String)
-		MyBase.new(String.Format("{0}" + vbCrLf + "Username: {1}" + vbCrLf + "URL: {2}", message, userName, URL))
-
-	End Sub
-End Class
