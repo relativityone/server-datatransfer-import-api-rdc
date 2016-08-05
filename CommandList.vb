247ee909f3e0f86178622579ed2d4386c58284d1
Matthew Wiemer <mwiemer@outlook.com>
2016-08-05 10:57:30 -0500

    Revert "wired in an empty kepler service"

--- CommandList.vb
+++
@@ -1,8 +0,0 @@
-Namespace kCura.CommandLine
-	Public Class CommandList
-		Inherits System.Collections.Generic.List(Of kCura.CommandLine.Command)
-		Public Sub New()
-			MyBase.New()
-		End Sub
-	End Class
-End Namespace
