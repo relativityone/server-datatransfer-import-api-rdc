7d51a8356e73aea6037404db90d3785e585d9d26
Matthew Wiemer <mwiemer@outlook.com>
2016-08-05 10:57:30 -0500

    Revert "wired in an empty kepler service"

--- IObservable.vb
+++
@@ -1,8 +0,0 @@
-﻿Namespace kCura.Windows.Process.Generic
-	Public Interface IObservable(Of T)
-		Event OnProcessFatalException(ByVal ex As Exception)
-		Event OnProcessEvent(ByVal evt As ProcessEvent(Of T))
-		Event OnProcessProgressEvent(ByVal evt As ProcessProgressEvent)
-		Event OnProcessComplete(ByVal closeForm As Boolean, ByVal exportFilePath As String, ByVal exportLogs As Boolean)
-	End Interface
-End Namespace
