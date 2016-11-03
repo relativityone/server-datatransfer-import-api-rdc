23d2918f067c6fdd79207b26b8e45930b784c416
Artur Mitianiec <amitianiec@kcura.com>
2016-11-03 04:09:11 -0500

    REL-110593 RIP Export failed when Identifier type field is not selected  - adding new _fieldsNonFile class field

--- FieldProviderCache.vb
+++ FieldProviderCache.vb
@@ -6,6 +6,8 @@ Namespace kCura.WinEDDS
 		Implements IFieldProviderCache
 
 		Private _fields As DocumentFieldCollection
+		Private _fieldsNonFile As DocumentFieldCollection
+
 		Private ReadOnly _cookieContainer As CookieContainer
 		Private ReadOnly _credential As ICredentials
 		
@@ -18,22 +20,14 @@ Namespace kCura.WinEDDS
 
 		Public Sub ResetCache() Implements IFieldProviderCache.ResetCache
 			_fields = Nothing
+			_fieldsNonFile = Nothing
 		End Sub
 
 		Public ReadOnly Property CurrentFields(artifactTypeId As Int32, workspaceId As Integer,
 												Optional ByVal refresh As Boolean = False) As DocumentFieldCollection Implements IFieldProviderCache.CurrentFields
 			Get
 				If _fields Is Nothing OrElse refresh Then
-					_fields = New DocumentFieldCollection
-					Dim fieldManager As New Service.FieldQuery(_credential, _cookieContainer)
-					Dim fields() As EDDS.WebAPI.DocumentManagerBase.Field
-					fields = fieldManager.RetrieveAllAsArray(workspaceId, artifactTypeID)
-					Dim i As Int32
-					For i = 0 To fields.Length - 1
-						With fields(i)
-							_fields.Add(New DocumentField(.DisplayName, .ArtifactID, .FieldTypeID, .FieldCategoryID, .CodeTypeID, .MaxLength, .AssociativeArtifactTypeID, .UseUnicodeEncoding, .ImportBehavior, .EnableDataGrid))
-						End With
-					Next
+					_fields = GetFields(artifactTypeId, workspaceId, Function(field) true)
 				End If
 				Return _fields
 			End Get
@@ -42,23 +36,31 @@ Namespace kCura.WinEDDS
 		Public ReadOnly Property CurrentNonFileFields(artifactTypeId As Int32, workspaceId As Integer,
 												Optional ByVal refresh As Boolean = False) As DocumentFieldCollection Implements IFieldProviderCache.CurrentNonFileFields
 			Get
-				If _fields Is Nothing OrElse refresh Then
-					_fields = New DocumentFieldCollection
-					Dim fieldManager As New Service.FieldQuery(_credential, _cookieContainer)
-					Dim fields() As EDDS.WebAPI.DocumentManagerBase.Field
-					fields = fieldManager.RetrieveAllAsArray(workspaceId, artifactTypeID)
-					Dim i As Int32
-					For i = 0 To fields.Length - 1
-						With fields(i)
-							If fields(i).FieldTypeID <> 9 Then
-								_fields.Add(New DocumentField(.DisplayName, .ArtifactID, .FieldTypeID, .FieldCategoryID, .CodeTypeID, .MaxLength, .AssociativeArtifactTypeID, .UseUnicodeEncoding, .ImportBehavior, .EnableDataGrid))
-							End If
-						End With
-					Next
+				If _fieldsNonFile Is Nothing OrElse refresh Then
+					_fieldsNonFile = GetFields(artifactTypeId, workspaceId, Function(field) field.FieldTypeID <> 9)
 				End If
-				Return _fields
+				Return _fieldsNonFile
 			End Get
 		End Property
+
+		Private Function GetFields(artifactTypeId As Int32, workspaceId As Integer, filterFun As Func(Of EDDS.WebAPI.DocumentManagerBase.Field, Boolean)) As DocumentFieldCollection
+
+			Dim retFields As New DocumentFieldCollection
+
+			Dim fieldManager As New Service.FieldQuery(_credential, _cookieContainer)
+			Dim fields() As EDDS.WebAPI.DocumentManagerBase.Field
+			fields = fieldManager.RetrieveAllAsArray(workspaceId, artifactTypeID)
+			Dim i As Int32
+			For i = 0 To fields.Length - 1
+				With fields(i)
+					If filterFun(fields(i)) Then
+						retFields.Add(New DocumentField(.DisplayName, .ArtifactID, .FieldTypeID, .FieldCategoryID, .CodeTypeID, .MaxLength, .AssociativeArtifactTypeID, .UseUnicodeEncoding, .ImportBehavior, .EnableDataGrid))
+					End If
+				End With
+			Next
+			Return retFields
+		End Function
+
 	End Class
 
 End Namespace
