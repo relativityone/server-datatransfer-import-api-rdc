0085f71b139826c8d8c102289fbab805d4214380
Matt Gathman <mgathman@kcura.com>
2016-12-01 14:25:34 -0600

    REL-115424: RDC is now getting the identity server url via webApi, Additonally, add a new Credentials provider interface and implementation for use during relogin attempts in RDC

--- Credentials/OAuth2ClientCredentials.vb
+++ Credentials/OAuth2ClientCredentials.vb
@@ -5,7 +5,7 @@ Imports System.Threading.Tasks
 Namespace kCura.WinEDDS.Credentials
 
 	Public Class OAuth2ClientCredentials
-		Implements ICredentials
+		Implements ICredentialsProvider
 
 		Private Const _OAUTH_USERNAME As String = "XxX_BearerTokenCredentials_XxX"
 		Private ReadOnly _tokenProvider As Relativity.OAuth2Client.Interfaces.ITokenProvider 
@@ -14,14 +14,14 @@ Namespace kCura.WinEDDS.Credentials
 			_tokenProvider = tokenProvider
 		End Sub
 
-		Public Function GetCredential(uri As Uri, authType As String) As System.Net.NetworkCredential Implements ICredentials.GetCredential
+		Public Function GetCredentials() As System.Net.NetworkCredential Implements ICredentialsProvider.GetCredentials
 
 			dim creds As NetworkCredential = GetCredentialsAsync().Result
 			return creds
 
 		End Function
 
-		Public Async Function GetCredentialsAsync() As Task(Of System.Net.NetworkCredential)
+		Public Async Function GetCredentialsAsync() As Task(Of System.Net.NetworkCredential) Implements ICredentialsProvider.GetCredentialsAsync
 			
 			dim token As String = Await _tokenProvider.GetAccessTokenAsync().ConfigureAwait(False)
 			dim creds As System.Net.NetworkCredential = new NetworkCredential(_OAUTH_USERNAME, token)
