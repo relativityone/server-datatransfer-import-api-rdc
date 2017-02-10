2e2b7b8bc90b027ceae219fcbf13fa9ed28a11fc
Bob Pacione <bpacione@kcura.com>
2017-02-10 12:34:38 -0600

    REL-116558 Extracting preview choices and folders methods into helper class

--- Application.vb
+++ Application.vb
@@ -63,7 +63,6 @@ Namespace kCura.EDDS.WinForm
 		Private WithEvents _optionsForm As OptionsForm
 		Private Shared _cache As New Hashtable
 		Private _documentRepositoryList As String()
-		Private _totalFolders As New System.Collections.Specialized.HybridDictionary
 #End Region
 
 #Region "Properties"
@@ -675,8 +674,10 @@ Namespace kCura.EDDS.WinForm
 		Public Function BuildFoldersAndCodesDataSource(ByVal al As ArrayList, ByVal previewCodeCount As System.Collections.Specialized.HybridDictionary) As DataTable
 			Try
 				Dim dt As New DataTable
-				Dim folderCount As Int32 = GetFolderCount(al)
-				Dim codeFieldColumnIndexes As ArrayList = GetCodeFieldColumnIndexes(DirectCast(al(0), System.Array))
+				Dim previewChoices As New PreviewChoicesHelper
+				Dim folderCount As Int32 = previewChoices.GetFolderCount(al)
+
+				Dim codeFieldColumnIndexes As ArrayList = previewChoices.GetCodeFieldColumnIndexes(DirectCast(al(0), System.Array))
 
 				'setup columns
 				dt.Columns.Add("Field Name")
@@ -719,61 +720,6 @@ Namespace kCura.EDDS.WinForm
 			End Try
 			Return Nothing
 		End Function
-
-		Public Function GetFolderCount(ByVal al As ArrayList) As Int32
-			_totalFolders.Clear()
-			Dim fieldValue As String
-			Dim item As Object
-			Dim fields As Api.ArtifactField()
-			Dim folderColumnIndex As Int32 = GetFolderColumnIndex(DirectCast(al(0), System.Array))
-
-			If folderColumnIndex <> -1 Then
-				For Each item In al
-					If Not item Is Nothing Then
-						fields = DirectCast(item, Api.ArtifactField())
-						If folderColumnIndex <> -1 Then
-							fieldValue = fields(folderColumnIndex).ValueAsString
-							AddFoldersToTotalFolders(fieldValue)
-						End If
-					End If
-				Next
-				Return _totalFolders.Count
-			End If
-			Return -1
-		End Function
-
-		Private Function GetFolderColumnIndex(ByVal firstRow As Array) As Int32
-			Dim folderColumnIndex As Int32 = -1
-			Dim currentIndex As Int32 = 0
-			For Each field As Api.ArtifactField In firstRow
-				If field.ArtifactID = -2 And field.DisplayName = "Parent Folder Identifier" Then folderColumnIndex = currentIndex
-				currentIndex += 1
-			Next
-			Return folderColumnIndex
-		End Function
-
-		Private Function GetCodeFieldColumnIndexes(ByVal firstRow As Array) As ArrayList
-			Dim codeFieldColumnIndexes As New ArrayList
-			Dim currentIndex As Int32 = 0
-			For Each field As Api.ArtifactField In firstRow
-				If field.Type = Relativity.FieldTypeHelper.FieldType.Code OrElse field.Type = Relativity.FieldTypeHelper.FieldType.MultiCode Then
-					codeFieldColumnIndexes.Add(currentIndex)
-				End If
-				currentIndex += 1
-			Next
-			Return codeFieldColumnIndexes
-		End Function
-
-		Private Sub AddFoldersToTotalFolders(ByVal folderPath As String)
-			If folderPath <> String.Empty AndAlso folderPath <> "\" Then
-				If folderPath.LastIndexOf("\"c) < 1 Then
-					If Not _totalFolders.Contains(folderPath) Then _totalFolders.Add(folderPath, String.Empty)
-				Else
-					If Not _totalFolders.Contains(folderPath) Then _totalFolders.Add(folderPath, String.Empty)
-					AddFoldersToTotalFolders(folderPath.Substring(0, folderPath.LastIndexOf("\"c)))
-				End If
-			End If
-		End Sub
 
 		Friend Function EnsureConnection() As Boolean
 			Dim retval = False
