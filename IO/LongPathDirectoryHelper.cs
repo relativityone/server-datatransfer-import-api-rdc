462342c4270de3e2afba2cc1a7293ad29e7ecc67
Lukasz Starzyk <lstarzyk@kcura.com>
2017-03-16 13:23:17 +0100

    REL-129851 - Added IDirectoryHelper interface Removed usages of System.IO.Directory in VolumeManager. Not required methods are accessed through IDirectoryHelper interface Added LongPathDirectoryHelper which implements IDirectoryHelper and provides access to directories with path longer than 260 characters

---
+++ IO/LongPathDirectoryHelper.cs
@@ -0,0 +1,17 @@
+﻿using ZetaLongPaths;
+
+namespace kCura.WinEDDS.Core.IO
+{
+	public class LongPathDirectoryHelper : IDirectoryHelper
+	{
+		public bool Exists(string directoryPath)
+		{
+			return ZlpIOHelper.DirectoryExists(directoryPath);
+		}
+
+		public void CreateDirectory(string directoryPath)
+		{
+			ZlpIOHelper.CreateDirectory(directoryPath);
+		}
+	}
+}
