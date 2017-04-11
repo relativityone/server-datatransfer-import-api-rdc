6dfb3554ffcb9af93a45d67fa41023ab8a0aef5a
Lukasz Starzyk <lstarzyk@kcura.com>
2017-04-11 13:17:00 +0200

    REL-134797 - Added implementations of IFileNameProvider interface for naming output files after Identifier and Productions

---
+++ FileNaming/IdentifierExportFileNameProvider.vb
@@ -0,0 +1,19 @@
+﻿Imports kCura.WinEDDS.Exporters
+
+Namespace kCura.WinEDDS
+	Public Class IdentifierExportFileNameProvider
+		Implements IFileNameProvider
+
+		Private ReadOnly _exportedObjectInfo As ObjectExportInfo
+		Private ReadOnly _exportSettings As ExportFile
+
+		Public Sub New(exportedObjectInfo As ObjectExportInfo, exportSettings As ExportFile)
+			_exportedObjectInfo = exportedObjectInfo
+			_exportSettings = exportSettings
+		End Sub
+
+		Public Function GetName() As String Implements IFileNameProvider.GetName
+			Return _exportedObjectInfo.NativeFileName(_exportSettings.AppendOriginalFileName)
+		End Function
+	End Class
+End Namespace
---
+++ FileNaming/ProductionExportFileNameProvider.vb
@@ -0,0 +1,22 @@
+﻿Imports kCura.WinEDDS.Exporters
+
+Namespace kCura.WinEDDS
+	Public Class ProductionExportFileNameProvider
+		Implements IFileNameProvider
+
+		Private ReadOnly _exportedObjectInfo As ObjectExportInfo
+		Private ReadOnly _exportSettings As ExportFile
+		Private ReadOnly _nameTextAndNativesAfterBegBates As Boolean
+		
+		Public Sub New(exportedObjectInfo As ObjectExportInfo, exportSettings As ExportFile, nameTextAndNativesAfterBegBates As Boolean)
+			_exportedObjectInfo = exportedObjectInfo
+			_exportSettings = exportSettings
+			_nameTextAndNativesAfterBegBates = nameTextAndNativesAfterBegBates
+		End Sub
+
+		Public Function GetName() As String Implements IFileNameProvider.GetName
+			Return _exportedObjectInfo.ProductionBeginBatesFileName(_exportSettings.AppendOriginalFileName, _nameTextAndNativesAfterBegBates)
+		End Function
+	End Class
+End Namespace
+
