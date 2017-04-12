486035d7718d9aec4a5c6cd207add1218daf1472
Matt Gathman <mgathman@kcura.com>
2017-04-11 19:48:03 -0500

    REL-132666: Partial move over to async all the way

---
+++ Credentials/LoginForm.vb
@@ -0,0 +1,87 @@
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+Imports kCura.WinEDDS.Credentials
+Imports Relativity.OAuth2Client.Implicit.RelativityWebBrowser
+
+Namespace Credentials
+	Public Class LoginForm
+		Inherits System.Windows.Forms.Form
+
+#Region " Windows Form Designer generated code "
+
+		Public Sub New()
+			MyBase.New()
+
+			'This call is required by the Windows Form Designer.
+			InitializeComponent()
+
+		End Sub
+
+		'Form overrides dispose to clean up the component list.
+		Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing Then
+				If Not (components Is Nothing) Then
+					components.Dispose()
+				End If
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		'Required by the Windows Form Designer
+		Private components As System.ComponentModel.IContainer
+		Private _browser As RelativityWebBrowser
+		'NOTE: The following procedure is required by the Windows Form Designer
+		'It can be modified using the Windows Form Designer.  
+		'Do not modify it using the code editor.
+		<System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+			Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LoginForm))
+			Me.SuspendLayout()
+			
+			Me._browser = New RelativityWebBrowser()
+			Me._browser.AllowNavigation = True
+			Me._browser.Dock = DockStyle.Fill
+			Me.Controls.Add(_browser)
+
+			'
+			'LoginForm
+			'
+			Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+			Me.Size = new System.Drawing.Size(600, 600)
+			Me.Name = "LoginForm"
+			Me.ShowInTaskbar = False
+			Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+			Me.Text = "Relativity Desktop Client | Login"
+			Me.TopMost = True
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+		End Sub
+
+#End Region
+
+		Public ReadOnly Property Browser() As RelativityWebBrowser
+			Get
+				return _browser
+			End Get
+		End Property
+
+		Private Sub LoginForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+			Me.Focus()
+			_browser.Show()
+			_browser.AllowNavigation = True
+		End Sub
+
+		'Private Async Sub LoginForm_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Shown
+		'	Try
+		'		await RelativityWebApiCredentialsProvider.Instance().GetCredentialsAsync()
+		'	Catch ex As TaskCanceledException
+		'		'Login form was closed, ignore
+		'	End Try
+			
+		'End Sub
+
+		 Private Sub LoginForm_Closing(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+			RelativityWebApiCredentialsProvider.Instance().Cancel()
+		 End Sub
+
+	End Class
+End Namespace
--- Credentials/OAuth2ImplicitCredentials.vb
+++ Credentials/OAuth2ImplicitCredentials.vb
@@ -1,9 +1,12 @@
-﻿Imports System.Net
+﻿Imports System.Diagnostics
+Imports System.Net
 Imports System.Threading
 Imports System.Threading.Tasks
+Imports Credentials
 Imports Relativity.OAuth2Client.Implicit
 Imports Relativity.OAuth2Client.Implicit.LoginView
 Imports Relativity.OAuth2Client.Implicit.RelativityWebBrowser
+Imports Relativity.OAuth2Client.Interfaces
 Imports Relativity.OAuth2Client.Interfaces.Events
 Imports Relativity.OAuth2Client.TokenProviders.ProviderFactories
 
@@ -15,29 +18,31 @@ Namespace kCura.WinEDDS.Credentials
 		Private Const _OAUTH_USERNAME As String = "XxX_BearerTokenCredentials_XxX"
 		Private Const _REDIRECT_URI As String = "http://relativityimplicit/"
 		Private ReadOnly _tokenProvider As Relativity.OAuth2Client.Interfaces.ITokenProvider
-		Private Readonly _loginView As ILoginView
-		Private ReadOnly _cancellationTokenSource As CancellationTokenSource
+		Private _loginFormFacotry As Func(Of LoginForm)
+		Private _cancellationTokenSource As CancellationTokenSource
 		
 		Public WithEvents Events As IOAuth2ClientEvents
 
 		Public ReadOnly Property Browser() As RelativityWebBrowser
 			Get
-				return _loginView.Browser
+				return LoginForm.Browser
 			End Get
 		End Property
 
-		Public Sub New(stsUri As Uri, clientID As String, browser As RelativityWebBrowser)
-			_loginView = New LoginView(stsUri, new Uri(_REDIRECT_URI), clientID, browser)
-			Dim providerFactory As Relativity.OAuth2Client.Interfaces.ITokenProviderFactory = New ImplicitTokenProviderFactory(_loginView)
+		Public ReadOnly Property LoginForm() As LoginForm
+			Get
+				return _loginFormFacotry.Invoke()
+			End Get
+		End Property
+
+		Public Sub New(stsUri As Uri, clientID As String, formFactory As Func(Of LoginForm))
+			_loginFormFacotry = formFactory
+			Dim loginView As ILoginView = New LoginView(stsUri, new Uri(_REDIRECT_URI), clientID, LoginForm.browser)
+			Dim providerFactory As Relativity.OAuth2Client.Interfaces.ITokenProviderFactory = New ImplicitTokenProviderFactory(loginView)
 			Dim tokenProvider As Relativity.OAuth2Client.Interfaces.ITokenProvider = providerFactory.GetTokenProvider("WebApi", New String() {"UserInfoAccess"})
 			_tokenProvider = tokenProvider
 			Events = _tokenProvider.Events
-			_cancellationTokenSource = New CancellationTokenSource()
-		End Sub
-
-		Public Sub New(tokenProvider As Relativity.OAuth2Client.Interfaces.ITokenProvider)
-			_tokenProvider = tokenProvider
-			Events = _tokenProvider.Events
+			AddHandler Events.TokenRequested, AddressOf On_TokenRequested
 			_cancellationTokenSource = New CancellationTokenSource()
 		End Sub
 
@@ -54,18 +59,28 @@ Namespace kCura.WinEDDS.Credentials
 		End Function
 
 		Public Async Function GetCredentialsAsync() As Task(Of System.Net.NetworkCredential) Implements ICredentialsProvider.GetCredentialsAsync
-
-			Dim token As String = Await _tokenProvider.GetAccessTokenAsync(_cancellationTokenSource.Token).ConfigureAwait(False)
+			Dim token As String = Await _tokenProvider.GetAccessTokenAsync(_cancellationTokenSource.Token)
+			LoginComplete()
 			Dim creds As System.Net.NetworkCredential = New NetworkCredential(_OAUTH_USERNAME, token)
 			Return creds
 
 		End Function
 
-		Private Sub Cancel() Implements ICredentialsProvider.Cancel
+		Public Sub Cancel() Implements ICredentialsProvider.Cancel
 			If(Not _cancellationTokenSource.IsCancellationRequested)
 				_cancellationTokenSource.Cancel()
 			End If
 		End Sub
+
+		Private Sub LoginComplete()
+			_cancellationTokenSource = New CancellationTokenSource()
+			LoginForm.Close()
+		End Sub
+
+		Private Sub On_TokenRequested(source As ITokenProvider, args As ITokenRequestEventArgs)
+			LoginForm.Show()
+		End Sub
+		
 	End Class
 
 
