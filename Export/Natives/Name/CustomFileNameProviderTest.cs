e22aa8eb87f8333c28d2fa716982e324bbab984c
artur.mitianiec <amitianiec@kcura.com>
2017-04-13 10:17:44 +0200

    Adding new UT

---
+++ Export/Natives/Name/CustomFileNameProviderTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using kCura.WinEDDS.Core.Export.Natives.Name;
+using kCura.WinEDDS.Core.Export.Natives.Name.Factories;
+using kCura.WinEDDS.Core.Model;
+using kCura.WinEDDS.Exporters;
+using Moq;
+using NUnit.Framework;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.Natives.Name
+{
+	public class CustomFileNameProviderTest
+	{
+		private CustomFileNameProvider _subjectUnderTest;
+
+		private ObjectExportInfo _exportObjectInfo;
+
+		private Mock<IFileNamePartProviderContainer> _fileNamePartProviderContainerMock;
+
+		[SetUp]
+		public void Init()
+		{
+			_exportObjectInfo = new ObjectExportInfo();
+
+			_fileNamePartProviderContainerMock = new Mock<IFileNamePartProviderContainer>();
+		}
+
+		[Test]
+		public void ItShouldReturnFileNameTest()
+		{
+			// Arrange
+			var firstDescriptor = new FieldDescriptorPart(1);
+			var secondDescriptor = new SeparatorDescriptorPart("");
+
+			string firstPartName = "First";
+			string secondPartName = "Second";
+
+			Mock<IFileNamePartProvider> separatorProviderMock = new Mock<IFileNamePartProvider>();
+			Mock<IFileNamePartProvider> fieldProviderMock = new Mock<IFileNamePartProvider>();
+
+			separatorProviderMock.Setup(mock => mock.GetPartName(firstDescriptor, It.IsAny<int>(), It.IsAny<int>())).Returns(firstPartName);
+			fieldProviderMock.Setup(mock => mock.GetPartName(secondDescriptor, It.IsAny<int>(), It.IsAny<int>())).Returns(secondPartName);
+
+			_fileNamePartProviderContainerMock.Setup(mock => mock.GetProvider(firstDescriptor)).Returns(separatorProviderMock.Object);
+			_fileNamePartProviderContainerMock.Setup(mock => mock.GetProvider(secondDescriptor)).Returns(fieldProviderMock.Object);
+
+			_subjectUnderTest = new CustomFileNameProvider(new List<DescriptorPart>
+				{
+					firstDescriptor,
+					secondDescriptor
+				}, 
+				_fileNamePartProviderContainerMock.Object);
+
+			// Act
+			string retFileName = _subjectUnderTest.GetName(_exportObjectInfo);
+
+			// Assert
+			Assert.That(retFileName, Is.EqualTo($"{firstPartName}{secondPartName}"));
+		}
+	}
+}
---
+++ Export/Natives/Name/Factories/FileNamePartProviderContainerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using kCura.WinEDDS.Core.Export.Natives.Name;
+using kCura.WinEDDS.Core.Export.Natives.Name.Factories;
+using kCura.WinEDDS.Core.Model;
+using NUnit.Framework;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.Natives.Name.Factories
+{
+	public class FileNamePartProviderContainerTest
+	{
+		private class HelperDescriptorBasePart : DescriptorPart
+		{
+		}
+
+		private FileNamePartProviderContainer _subjectUnderTest;
+
+		[Test]
+		public void ItShouldThrowExceptionForNotSupportedDescriptorType()
+		{
+			_subjectUnderTest = new FileNamePartProviderContainer();
+
+			Assert.Throws<Exception>(() => _subjectUnderTest.GetProvider(new HelperDescriptorBasePart()));
+		}
+
+		[Test]
+		public void ItShouldReturnFieldDescriptorPartProvider()
+		{
+			var part = new FieldDescriptorPart(1);
+
+			_subjectUnderTest = new FileNamePartProviderContainer();
+
+			IFileNamePartProvider retProvider = _subjectUnderTest.GetProvider(part);
+			Assert.That(retProvider, Is.TypeOf<FieldFileNamePartProvider>());
+		}
+
+		[Test]
+		public void ItShouldReturnSeparatorDescriptorPartProvider()
+		{
+			var part = new SeparatorDescriptorPart("");
+
+			_subjectUnderTest = new FileNamePartProviderContainer();
+
+			IFileNamePartProvider retProvider = _subjectUnderTest.GetProvider(part);
+			Assert.That(retProvider, Is.TypeOf<SeparatorFileNamePartProvider>());
+		}
+	}
+}
