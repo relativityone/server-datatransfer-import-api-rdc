ef87928d6354023d8eff1fe5229a88acd8b7501b
artur.mitianiec <amitianiec@kcura.com>
2017-04-20 12:35:39 +0200

    Adding new UTs

--- Export/Natives/Name/FieldFileNamePartProvider.cs
+++ Export/Natives/Name/FieldFileNamePartProvider.cs
@@ -1,7 +1,5 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using kCura.WinEDDS.Core.Model;
 using kCura.WinEDDS.Core.Model.Export.Process;
