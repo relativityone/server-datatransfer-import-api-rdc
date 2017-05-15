5c6b7cec7a70e8f1e8a2e4de6ae6279210fa7e63
Matt Gathman <mgathman@kcura.com>
2017-05-15 16:43:38 -0500

    REL-132666: updated OAuth2Client nuget, and removed code that is no longer needed

--- Credentials/OAuth2ImplicitCredentials.vb
+++ Credentials/OAuth2ImplicitCredentials.vb
@@ -22,7 +22,6 @@ Namespace kCura.WinEDDS.Credentials
 		Private Const _OAUTH_USERNAME As String = "XxX_BearerTokenCredentials_XxX"
 		Private Const _REDIRECT_URI As String = "http://relativityimplicit/"
 		Private _tokenProvider As Relativity.OAuth2Client.Interfaces.ITokenProvider
-		Private Readonly _loginFormFacotry As Func(Of LoginForm)
 		Private ReadOnly _stsUri As Uri
 		Private ReadOnly _clientId As String
 		Private _loginView As ILoginView
@@ -32,20 +31,8 @@ Namespace kCura.WinEDDS.Credentials
 		
 		Public WithEvents Events As IOAuth2ClientEvents
 
-		Public ReadOnly Property Browser() As RelativityWebBrowser
-			Get
-				return LoginForm.Browser
-			End Get
-		End Property
-
-		Public ReadOnly Property LoginForm() As LoginForm
-			Get
-				return _loginFormFacotry.Invoke()
-			End Get
-		End Property
 		
-		Public Sub New(stsUri As Uri, clientID As String, formFactory As Func(Of LoginForm), onTokenHandler As TokenResponseHandler)
-			_loginFormFacotry = formFactory
+		Public Sub New(stsUri As Uri, clientID As String, onTokenHandler As TokenResponseHandler)
 			_stsUri = stsUri
 			_clientId = clientID
 			_onTokenHandler = onTokenHandler
@@ -64,18 +51,8 @@ Namespace kCura.WinEDDS.Credentials
 
 		Public Async Function GetCredentialsAsync() As Task(Of System.Net.NetworkCredential) Implements ICredentialsProvider.GetCredentialsAsync
 			Try
-				Dim expired As Boolean = Not _tokenProvider.Expiration.HasValue Or (_tokenProvider.Expiration.HasValue AndAlso _tokenProvider.Expiration.Value < DateTime.UtcNow.AddSeconds(5))
-
-				If _loginView.Browser.IsDisposed And expired
-					CreateTokenProvider()
-				End If
-
-				If _cancellationTokenSource.IsCancellationRequested
-					_cancellationTokenSource = New CancellationTokenSource()
-				End If
-
 				Dim token As String = Await _tokenProvider.GetAccessTokenAsync(_cancellationTokenSource.Token)
-				LoginComplete()
+
 				Dim creds As System.Net.NetworkCredential = New NetworkCredential(_OAUTH_USERNAME, token)
 				_currentCredentials = creds
 				Return creds
@@ -85,20 +62,14 @@ Namespace kCura.WinEDDS.Credentials
 		End Function
 
 		Private Sub CreateTokenProvider()
-			_loginView = New LoginView(_stsUri, new Uri(_REDIRECT_URI), _clientId, LoginForm.browser)
+			_loginView = New LoginView(_stsUri, new Uri(_REDIRECT_URI), _clientId)
 			Dim providerFactory As Relativity.OAuth2Client.Interfaces.ITokenProviderFactory = New ImplicitTokenProviderFactory(_loginView)
 			Dim tokenProvider As Relativity.OAuth2Client.Interfaces.ITokenProvider = providerFactory.GetTokenProvider("WebApi", New String() {"UserInfoAccess"})
 			_tokenProvider = tokenProvider
 			Events = _tokenProvider.Events
 			AddHandler Events.TokenRetrieved, _onTokenHandler
-			AddHandler Events.TokenRequested, AddressOf On_TokenRequested
-			AddHandler LoginForm.Closed, AddressOf On_LoginFormClosing
 		End Sub
 
-		Private Sub LoginComplete()
-			_cancellationTokenSource = New CancellationTokenSource()
-			LoginForm.Close()
-		End Sub
 
 		Private Sub On_LoginFormClosing(ByVal sender As Object, ByVal e As EventArgs)
 			If(Not _cancellationTokenSource.IsCancellationRequested)
@@ -106,12 +77,6 @@ Namespace kCura.WinEDDS.Credentials
 				_cancellationTokenSource = New CancellationTokenSource()
 			End If
 		 End Sub
-
-		Private Sub On_TokenRequested(source As ITokenProvider, args As ITokenRequestEventArgs)
-			LoginForm.TopLevel = True
-			LoginForm.TopMost = True
-			LoginForm.Show()
-		End Sub
 		
 	End Class
 
