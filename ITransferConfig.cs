5b28c2ffabd3fcb75bd7bc601cbc36bcc1726477
Patryk Stepien <pstepien@kcura.com>
2017-05-15 15:53:25 +0200

    Add AsperaConnectionMonitor for detecting connection inactivity

--- ITransferConfig.cs
+++ ITransferConfig.cs
@@ -4,5 +4,7 @@ namespace kCura.WinEDDS.Core
 	public interface ITransferConfig
 	{
 		int ImportBatchSize { get; }
+
+		int ConnectionTimeout { get; }
 	}
 }
--- TransferConfig.cs
+++ TransferConfig.cs
@@ -4,5 +4,6 @@ namespace kCura.WinEDDS.Core
 	public class TransferConfig : ITransferConfig
 	{
 		public int ImportBatchSize => Config.ImportBatchSize;
+		public int ConnectionTimeout => 30;
 	}
 }
