0d9ba0b5c4188ef03f9b928a1c9a142834c3033a
Matt Gathman <mgathman@kcura.com>
2017-05-17 16:07:22 -0500

    REL-132666: updates to RDC OAuth2Client

--- Application.vb
+++ Application.vb
@@ -66,7 +66,7 @@ Namespace kCura.EDDS.WinForm
 
 		Public Sub SetImplicitCredentialProvider()
 			Dim authEndpoint As string = String.Format("{0}/{1}", GetIdentityServerLocation(), "connect/authorize")
-			Dim implicitProvider = new OAuth2ImplicitCredentials(New Uri(authEndpoint), "d589f9922d802d5df895945765", AddressOf On_TokenRetrieved)
+			Dim implicitProvider = new OAuth2ImplicitCredentials(New Uri(authEndpoint), "Relativity Desktop Client", AddressOf On_TokenRetrieved)
 			RelativityWebApiCredentialsProvider.Instance().SetProvider(implicitProvider)
 		End Sub
 
