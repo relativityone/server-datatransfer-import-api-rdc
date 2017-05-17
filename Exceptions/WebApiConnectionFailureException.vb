fa2e2445e318f3c8f6040fad9cd7d968bafede68
Matt Gathman <mgathman@kcura.com>
2017-05-17 11:19:07 -0500

    REL-132666: better error handling, and bug fixes

---
+++ Exceptions/WebApiConnectionFailureException.vb
@@ -0,0 +1,13 @@
+﻿Namespace kCura.WinEDDS.Exceptions
+	Public Class WebApiConnectionFailureException
+		Inherits System.Exception
+
+		Public Sub New (message As String)
+			MyBase.New(message)
+		End Sub
+
+	End Class
+	
+End Namespace
+
+
