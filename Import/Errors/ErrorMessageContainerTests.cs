6939ce477d3f9f7a44b0179d1e2fb9b229d13ccc
Patryk Stepien <pstepien@kcura.com>
2017-05-17 09:23:59 +0200

    Add IErrorContainer and it's implementations for handling errors

---
+++ Import/Errors/ErrorMessageContainerTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using kCura.WinEDDS.Core.Import.Errors;
+using NUnit.Framework;
+
+namespace kCura.WinEDDS.Core.NUnit.Import.Errors
+{
+	[TestFixture]
+	public class ErrorMessageContainerTests
+	{
+		private ErrorMessageContainer _instance;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_instance = new ErrorMessageContainer();
+		}
+
+		[Test]
+		public void ItShouldReturnSortedErrors()
+		{
+			var lineErrors = GetTestData();
+
+			// ACT
+			foreach (var lineError in lineErrors)
+			{
+				_instance.WriteError(lineError);
+			}
+
+			var result = _instance.GetAllErrors();
+
+			// ASSERT
+			CollectionAssert.AreEqual(result.Select(x => x.LineNumber), lineErrors.OrderBy(x => x.LineNumber).Select(x => x.LineNumber));
+		}
+
+		private static List<LineError> GetTestData()
+		{
+			var clientError1 = new LineError
+			{
+				LineNumber = 1,
+				ErrorType = ErrorType.client
+			};
+			var clientError2 = new LineError
+			{
+				LineNumber = 5,
+				ErrorType = ErrorType.client
+			};
+			var clientError3 = new LineError
+			{
+				LineNumber = 9,
+				ErrorType = ErrorType.client
+			};
+			var serverError1 = new LineError
+			{
+				LineNumber = 2,
+				ErrorType = ErrorType.server
+			};
+			var serverError2 = new LineError
+			{
+				LineNumber = 4,
+				ErrorType = ErrorType.server
+			};
+			var serverError3 = new LineError
+			{
+				LineNumber = 8,
+				ErrorType = ErrorType.server
+			};
+
+			var lineErrors = new List<LineError>
+			{
+				clientError1,
+				clientError2,
+				clientError3,
+				serverError1,
+				serverError2,
+				serverError3
+			};
+			return lineErrors;
+		}
+	}
+}
---
+++ Import/Errors/ErrorReporterTests.cs
@@ -0,0 +1,86 @@
+﻿using kCura.WinEDDS.Core.Import.Errors;
+using Moq;
+using NUnit.Framework;
+
+namespace kCura.WinEDDS.Core.NUnit.Import.Errors
+{
+	[TestFixture]
+	public class ErrorReporterTests
+	{
+		private const string _MAX_ERROR_COUNT_REACHED_MESSAGE = "Maximum number of errors for display reached.  Export errors to view full list.";
+
+		private ErrorReporter _instance;
+		private Mock<ITransferConfig> _transferConfig;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_transferConfig = new Mock<ITransferConfig>();
+
+			_instance = new ErrorReporter(_transferConfig.Object);
+		}
+
+		[Test]
+		public void ItShouldRaiseErrorEvent()
+		{
+			_transferConfig.Setup(x => x.DefaultMaximumErrorCount).Returns(10);
+
+			var eventRaised = false;
+			LineError expectedLineError = new LineError();
+			LineError actualLineError = null;
+
+			_instance.ErrorOccurred += (s, e) =>
+			{
+				eventRaised = true;
+				actualLineError = e;
+			};
+
+			// ACT
+			_instance.WriteError(expectedLineError);
+
+			// ASSERT
+			Assert.That(eventRaised, Is.True);
+			Assert.That(actualLineError, Is.EqualTo(expectedLineError));
+		}
+
+		[Test]
+		public void ItShouldSkipEventWhenLimitExceeded()
+		{
+			_transferConfig.Setup(x => x.DefaultMaximumErrorCount).Returns(0);
+
+			var eventRaised = false;
+			LineError lineError = new LineError();
+
+			_instance.ErrorOccurred += (s, e) => eventRaised = true;
+
+			// ACT
+			_instance.WriteError(lineError);
+
+			// ASSERT
+			Assert.That(eventRaised, Is.False);
+		}
+
+		[Test]
+		public void ItShouldRaiseLimitReachedMessage()
+		{
+			_transferConfig.Setup(x => x.DefaultMaximumErrorCount).Returns(1);
+
+			var eventRaised = false;
+			LineError lineError = new LineError();
+			LineError actualLineError = null;
+
+			_instance.ErrorOccurred += (s, e) =>
+			{
+				eventRaised = true;
+				actualLineError = e;
+			};
+
+			// ACT
+			_instance.WriteError(lineError);
+
+			// ASSERT
+			Assert.That(eventRaised, Is.True);
+			Assert.That(actualLineError.Message, Is.EqualTo(_MAX_ERROR_COUNT_REACHED_MESSAGE));
+		}
+	}
+}
