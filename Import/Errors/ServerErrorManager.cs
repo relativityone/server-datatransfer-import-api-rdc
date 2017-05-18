d17a30602f2dfa1f3f4d2e85f7b54ff0cf8d87e0
Patryk Stepien <pstepien@kcura.com>
2017-05-18 14:28:13 +0200

    Handle errors after uploading BCP files

--- Import/Errors/ServerErrorManager.cs
+++ Import/Errors/ServerErrorManager.cs
@@ -1,33 +1,37 @@
 ﻿using kCura.Utility;
 using kCura.WinEDDS.Service;
-using Relativity;
 
 namespace kCura.WinEDDS.Core.Import.Errors
 {
-	public class ServerErrorManager
+	public class ServerErrorManager : IServerErrorManager
 	{
 		private readonly BulkImportManager _bulkImportManager;
 		private readonly ServerErrorFile _serverErrorFile;
 		private readonly ServerErrorFileDownloader _serverErrorFileDownloader;
+		private readonly ImportContext _importContext;
 
-		public ServerErrorManager(BulkImportManager bulkImportManager, ServerErrorFile serverErrorFile, ServerErrorFileDownloader serverErrorFileDownloader)
+		public ServerErrorManager(BulkImportManager bulkImportManager, ServerErrorFile serverErrorFile, ServerErrorFileDownloader serverErrorFileDownloader, ImportContext importContext)
 		{
 			_bulkImportManager = bulkImportManager;
 			_serverErrorFile = serverErrorFile;
 			_serverErrorFileDownloader = serverErrorFileDownloader;
+			_importContext = importContext;
 		}
 
-		public void ManageErrors(int artifactTypeId, CaseInfo caseInfo, string runId, int keyFieldId)
+		public void ManageErrors()
 		{
-			if (!_bulkImportManager.NativeRunHasErrors(caseInfo.ArtifactID, runId))
+			if (!_bulkImportManager.NativeRunHasErrors(_importContext.Args.CaseInfo.ArtifactID, _importContext.RunId))
 			{
 				return;
 			}
 
-			var errorFileKey = _bulkImportManager.GenerateNonImageErrorFiles(caseInfo.ArtifactID, runId, artifactTypeId, true, keyFieldId);
+			//TODO
+			var keyFieldId = -1;
+			var errorFileKey = _bulkImportManager.GenerateNonImageErrorFiles(_importContext.Args.CaseInfo.ArtifactID, _importContext.RunId,
+				_importContext.Args.ArtifactTypeID, true, keyFieldId);
 			//TODO write status
 
-			GenericCsvReader reader = _serverErrorFileDownloader.DownloadErrorFile(errorFileKey.LogKey, caseInfo);
+			GenericCsvReader reader = _serverErrorFileDownloader.DownloadErrorFile(errorFileKey.LogKey, _importContext.Args.CaseInfo);
 			_serverErrorFile.HandleServerErrors(reader);
 		}
 	}
