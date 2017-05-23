80569a913a156eb28c66c61192f2466066292510
Patryk Stepien <pstepien@kcura.com>
2017-05-23 16:29:49 +0200

    Refactor PushMetadataFilesTask and add unit tests

---
+++ Import/Tasks/Helpers/MetadataFilesServerExecutionTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using kCura.EDDS.WebAPI.BulkImportManagerBase;
+using kCura.WinEDDS.Core.Import;
+using kCura.WinEDDS.Core.Import.Helpers;
+using kCura.WinEDDS.Core.Import.Managers;
+using kCura.WinEDDS.Core.Import.Tasks.Helpers;
+using Moq;
+using NUnit.Framework;
+using BulkImportManager = kCura.WinEDDS.Service.BulkImportManager;
+
+namespace kCura.WinEDDS.Core.NUnit.Import.Tasks.Helpers
+{
+	[TestFixture]
+	public class MetadataFilesServerExecutionTests
+	{
+		private MetadataFilesServerExecution _instance;
+
+		private ImportContext _importContext;
+		private Mock<ITransferConfig> _transferConfig;
+		private Mock<INativeLoadInfoFactory> _nativeLoadInfoFactory;
+		private Mock<IBulkImportManager> _bulkImportManager;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_importContext = new ImportContext();
+
+			_transferConfig = new Mock<ITransferConfig>();
+			_nativeLoadInfoFactory = new Mock<INativeLoadInfoFactory>();
+			_bulkImportManager = new Mock<IBulkImportManager>();
+
+			_instance = new MetadataFilesServerExecution(_importContext, _transferConfig.Object, _nativeLoadInfoFactory.Object, _bulkImportManager.Object);
+		}
+
+		[Test]
+		public void ItShouldExecuteImportOnServer()
+		{
+			var metadataFilesInfo = new MetadataFilesInfo();
+			NativeLoadInfo settings = new NativeLoadInfo();
+
+			_transferConfig.Setup(x => x.IoErrorNumberOfRetries).Returns(1);
+			_nativeLoadInfoFactory.Setup(x => x.Create(metadataFilesInfo, _importContext)).Returns(settings);
+
+
+			// ACT
+			_instance.Import(metadataFilesInfo);
+
+			// ASSERT
+			_nativeLoadInfoFactory.Verify(x => x.Create(metadataFilesInfo, _importContext), Times.Once);
+			_bulkImportManager.Verify(x => x.BulkImport(settings, _importContext), Times.Once);
+		}
+
+		[Test]
+		[TestCaseSource(nameof(TimeoutAndPermissionExceptions))]
+		public void ItShouldNotRetryForTimeoutAndPermissionExceptions(Exception e)
+		{
+			var metadataFilesInfo = new MetadataFilesInfo();
+			NativeLoadInfo settings = new NativeLoadInfo();
+
+			_transferConfig.Setup(x => x.IoErrorNumberOfRetries).Returns(3);
+			_nativeLoadInfoFactory.Setup(x => x.Create(metadataFilesInfo, _importContext)).Returns(settings);
+
+			_bulkImportManager.Setup(x => x.BulkImport(settings, _importContext)).Throws(e);
+
+			// ACT
+			Assert.That(() => _instance.Import(metadataFilesInfo), Throws.TypeOf(e.GetType()));
+
+			// ASSERT
+			_bulkImportManager.Verify(x => x.BulkImport(settings, _importContext), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldRetryException()
+		{
+			var metadataFilesInfo = new MetadataFilesInfo();
+			NativeLoadInfo settings = new NativeLoadInfo();
+
+			var numberOfRetires = 3;
+
+			_transferConfig.Setup(x => x.IoErrorNumberOfRetries).Returns(numberOfRetires);
+			_nativeLoadInfoFactory.Setup(x => x.Create(metadataFilesInfo, _importContext)).Returns(settings);
+
+			_bulkImportManager.Setup(x => x.BulkImport(settings, _importContext)).Throws(new Exception());
+
+			// ACT
+			Assert.That(() => _instance.Import(metadataFilesInfo), Throws.Exception);
+
+			// ASSERT
+			_bulkImportManager.Verify(x => x.BulkImport(settings, _importContext), Times.Exactly(numberOfRetires));
+		}
+
+		private static Exception[] TimeoutAndPermissionExceptions()
+		{
+			return new Exception[]
+			{
+				new BulkImportManager.BulkImportSqlTimeoutException(new SoapExceptionDetail()),
+				new BulkImportManager.BulkImportSqlException(new SoapExceptionDetail()),
+				new BulkImportManager.InsufficientPermissionsForImportException(""),
+				new WebException("timed out")
+			};
+		}
+	}
+}
---
+++ Import/Tasks/PushMetadataFilesTaskTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using kCura.WinEDDS.Core.Import;
+using kCura.WinEDDS.Core.Import.Errors;
+using kCura.WinEDDS.Core.Import.Factories;
+using kCura.WinEDDS.Core.Import.Tasks;
+using kCura.WinEDDS.Core.Import.Tasks.Helpers;
+using Moq;
+using NUnit.Framework;
+
+namespace kCura.WinEDDS.Core.NUnit.Import.Tasks
+{
+	[TestFixture]
+	public class PushMetadataFilesTaskTests
+	{
+		private PushMetadataFilesTask _instance;
+
+		private ImportBatchContext _importBatchContext;
+		private ImportContext _importContext;
+		private MetadataFilesInfo _metadataFilesInfo;
+
+		private Mock<IFileUploader> _fileUploader;
+		private Mock<IMetadataFilesServerExecution> _metadataFilesServerExecution;
+		private Mock<IServerErrorManager> _serverErrorManager;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_fileUploader = new Mock<IFileUploader>();
+
+			_metadataFilesServerExecution = new Mock<IMetadataFilesServerExecution>();
+			_serverErrorManager = new Mock<IServerErrorManager>();
+
+			_importContext = new ImportContext();
+			_metadataFilesInfo = new MetadataFilesInfo();
+			_importBatchContext = new ImportBatchContext(_importContext, 1000)
+			{
+				MetadataFilesInfo = _metadataFilesInfo
+			};
+
+			var fileUploaderFactory = new Mock<IFileUploaderFactory>();
+			fileUploaderFactory.Setup(x => x.CreateBcpFileUploader()).Returns(_fileUploader.Object);
+
+			_instance = new PushMetadataFilesTask(_metadataFilesServerExecution.Object, fileUploaderFactory.Object, _serverErrorManager.Object);
+		}
+
+		[Test]
+		public void ItShouldUploadMetadataFiles_GoldWorkflow()
+		{
+			_fileUploader.Setup(x => x.WaitForUploadToComplete()).Returns(new Dictionary<FileMetadata, UploadResult>());
+
+			// ACT
+			_instance.PushMetadataFiles(_importBatchContext);
+
+			// ASSERT
+			_fileUploader.Verify(x => x.UploadFile(It.IsAny<FileMetadata>()), Times.Exactly(4));
+			_metadataFilesServerExecution.Verify(x => x.Import(_metadataFilesInfo), Times.Once);
+			_serverErrorManager.Verify(x => x.ManageErrors(_importContext), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldThrowExceptionWhenFileUploadFailed()
+		{
+			IDictionary<FileMetadata, UploadResult> uploadResults = new Dictionary<FileMetadata, UploadResult>
+			{
+				{new FileMetadata {FileGuid = "1"}, new UploadResult {Success = true}},
+				{new FileMetadata {FileGuid = "2"}, new UploadResult {Success = false}}
+			};
+			_fileUploader.Setup(x => x.WaitForUploadToComplete()).Returns(uploadResults);
+
+			// ACT
+			Assert.That(() => _instance.PushMetadataFiles(_importBatchContext), Throws.Exception);
+
+			// ASSERT
+			_metadataFilesServerExecution.Verify(x => x.Import(It.IsAny<MetadataFilesInfo>()), Times.Never);
+			_serverErrorManager.Verify(x => x.ManageErrors(_importContext), Times.Never);
+		}
+	}
+}
