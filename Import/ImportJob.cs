8e918e15590e88e585301e77cc946230166b02fa
Patryk Stepien <pstepien@kcura.com>
2017-06-22 11:13:06 +0200

    REL-148711 RDC does not close Aspera client processes

--- Import/ImportJob.cs
+++ Import/ImportJob.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Castle.Windsor;
 using kCura.WinEDDS.Api;
 using kCura.WinEDDS.Core.Import.Factories;
 using kCura.WinEDDS.Core.Import.Helpers;
@@ -20,12 +21,13 @@ namespace kCura.WinEDDS.Core.Import
 		private readonly ICancellationProvider _cancellationProvider;
 		private readonly IJobFinishStatisticsHandler _jobFinishStatisticsHandler;
 		private readonly ILog _log;
+		private readonly IWindsorContainer _container;
 
 		public event EventHandler<ImportContext> Initialized;
 
 		public ImportJob(ITransferConfig config, IImportBatchJobFactory batchJobBatchJobFactory, IImportStatusManager importStatusManager, 
 			IImportMetadata importer, IImporterSettings importerSettings, IImportExceptionHandlerExec importExceptionHandlerExec,
-			ICancellationProvider cancellationProvider, IJobFinishStatisticsHandler jobFinishStatisticsHandler, ILog log)
+			ICancellationProvider cancellationProvider, IJobFinishStatisticsHandler jobFinishStatisticsHandler, ILog log, IWindsorContainer container)
 		{
 			_config = config;
 			_batchJobBatchJobFactory = batchJobBatchJobFactory;
@@ -36,6 +38,7 @@ namespace kCura.WinEDDS.Core.Import
 			_cancellationProvider = cancellationProvider;
 			_jobFinishStatisticsHandler = jobFinishStatisticsHandler;
 			_log = log;
+			_container = container;
 
 			_context = new ImportContext
 			{
@@ -72,6 +75,7 @@ namespace kCura.WinEDDS.Core.Import
 					finally
 					{
 						_importStatusManager.RaiseEndImportEvent(this);
+						_container.Dispose();
 					}
 					return true;
 				}, false, _importer.CleanUp);
