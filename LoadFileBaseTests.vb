527a35d380573abccd9c617e8abca8ea5c398991
Matt Shanahan <mshanahan@kcura.com>
2017-07-25 13:53:10 -0500

    [DEVOPS-16313] Fixed some mistakes in porting ExpectedMessage properties to Assert.Throws

--- LoadFileBaseTests.vb
+++ LoadFileBaseTests.vb
@@ -6,9 +6,10 @@ Namespace kCura.WinEDDS.NUnit
 
 		<Test()>
 		Public Sub GetFieldStringValue_Throw_Error_Correct_Test()
-			Assert.Throws(Of kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException)(Sub()
+			Dim thrownException As kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException = Assert.Throws(Of kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException)(Sub()
 			                                                                                                 kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar("Morethat5CharsForsureeee", 2, 5, 10, "Test")
-			                                                                                             End Sub, "Error in line 10, column ""C"".  Input length exceeds maximum set length of 5 for the Test field.")
+			                                                                                             End Sub)
+			Assert.AreEqual("Error in line 10, column ""C"".  Input length exceeds maximum set length of 5 for the Test field.", thrownException.Message)
 		End Sub
 
 		<Test()>
