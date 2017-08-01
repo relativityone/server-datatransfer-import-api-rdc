2683c659b5f4531eeea00859a1937ab4d29736d3
Stanislaw Wolny <swolny@kcura.com>
2017-08-01 11:00:50 +0200

    merged develop; refactored references; minor fixes

--- LoadFileBaseTests.vb
+++ LoadFileBaseTests.vb
@@ -2,24 +2,27 @@ Imports NUnit.Framework
 
 Namespace kCura.WinEDDS.NUnit
 	<TestFixture()>
- Public Class LoadFileBaseTests
-
-		<Test(), ExpectedException(GetType(kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException), ExpectedMessage:="Error in line 10, column ""C"".  Input length exceeds maximum set length of 5 for the Test field.")>
-  Public Sub GetFieldStringValue_Throw_Error_Correct_Test()
-			kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar("Morethat5CharsForsureeee", 2, 5, 10, "Test")
-  End Sub
+	Public Class LoadFileBaseTests
 
 		<Test()>
-  Public Sub GetFieldStringValue_Return_Correct_Value_Not_Empty()
+		Public Sub GetFieldStringValue_Throw_Error_Correct_Test()
+			Dim thrownException As kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException = Assert.Throws(Of kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException)(Sub()
+			                                                                                                 kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar("Morethat5CharsForsureeee", 2, 5, 10, "Test")
+			                                                                                             End Sub)
+			Assert.AreEqual("Error in line 10, column ""C"".  Input length exceeds maximum set length of 5 for the Test field.", thrownException.Message)
+		End Sub
+
+		<Test()>
+		Public Sub GetFieldStringValue_Return_Correct_Value_Not_Empty()
 			Dim value As String = "Morethat5CharsForsureeee"
 			Assert.AreEqual(value, kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar(value, 2, 100, 10, "Test"))
-  End Sub
+		End Sub
 
 		<Test()>
-  Public Sub GetFieldStringValue_Return_Correct_Value_Empty()
+		Public Sub GetFieldStringValue_Return_Correct_Value_Empty()
 			Dim value As String = ""
 			Assert.AreEqual(Nothing, kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar(value, 2, 100, 10, "Test"))
-  End Sub
+		End Sub
 
- End Class
-End NameSpace
+	End Class
+End Namespace
