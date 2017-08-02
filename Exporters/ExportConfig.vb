2eae9c06c04d6b4377d50abcb3329fc2105749cb
artur.mitianiec <amitianiec@kcura.com>
2017-08-02 11:02:34 +0200

    Extract export settings to the new interface/class

---
+++ Exporters/ExportConfig.vb
@@ -0,0 +1,20 @@
+﻿Namespace kCura.WinEDDS
+
+Public Class ExportConfig
+		Implements IExportConfig
+
+	Public ReadOnly Property ExportBatchSize As Integer Implements IExportConfig.ExportBatchSize
+		Get
+			return Config.ExportBatchSize
+		End Get
+	End Property
+
+	Public ReadOnly Property ExportThreadCount As Integer Implements IExportConfig.ExportThreadCount
+		Get
+			Return Config.ExportThreadCount
+		End Get
+	End Property
+
+End Class
+
+End Namespace
