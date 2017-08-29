6618b87d679f12bdb209d9ab0840e0e4fc574db7
Eric Kostecki <ekostecki@kcura.com>
2017-08-29 15:51:51 -0500

    REL-152938: When a user exits the certificate prompt they get an error when they attempt to sign in

--- Forms/CertificatePromptForm.vb
+++ Forms/CertificatePromptForm.vb
@@ -6,15 +6,25 @@
 	Public Event AllowUntrustedCertificates As Action
 	Public Event DenyUntrustedCertificates As Action
 
+	Private CertsAllowed As Nullable(Of Boolean)
+
 	Private Sub LoginForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
 		Me.Focus()
 	End Sub
+	
+	Private Sub CertForm_Closing(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Closing
+		If(Not CertsAllowed.HasValue)
+			CloseRDC_Click(sender, e)
+		End If
+	End Sub
 
 	Private Sub CloseRDC_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+		CertsAllowed = False
 		RaiseEvent DenyUntrustedCertificates()
 	End Sub
 
 	Private Sub AllowUntrustedCertificates_Click(sender As Object, e As EventArgs) Handles AllowButton.Click
+		CertsAllowed = True
 		Close()
 		RaiseEvent AllowUntrustedCertificates()
 	End Sub
