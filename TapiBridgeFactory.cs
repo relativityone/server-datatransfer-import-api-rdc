0f6820506f77d273b27724e2e9e56fbcf5fd51b4
Scott Parillo <scott.parillo@relativity.com>
2017-10-03 07:18:48 -0500

    REL-168881 - Moved WinEDDS/Relativity Logging code to kCura.WinEDDS. - This shifts the responsibility to construct log objects out of TAPI and back into WinEDDS. This avoids constructing the object multiple times per request. - Per review with Platform, using the Logging singleton to avoid potential overhead with on-prem instances.

--- TapiBridgeFactory.cs
+++ TapiBridgeFactory.cs
@@ -10,8 +10,6 @@
 namespace kCura.WinEDDS.TApi
 {
     using System;
-    using System.IO;
-    using System.Reflection;
     using System.Threading;
 
     using Relativity.Logging;
@@ -28,15 +26,18 @@ namespace kCura.WinEDDS.TApi
         /// <param name="parameters">
         /// The native file transfer parameters
         /// </param>
+        /// <param name="log">
+        /// The Relativity logging instance.
+        /// </param>
         /// <param name="token">
         /// The cancellation token.
         /// </param>
         /// <returns>
         /// The <see cref="TapiBridge"/> instance.
         /// </returns>
-        public static TapiBridge CreateUploadBridge(TapiBridgeParameters parameters, CancellationToken token)
+        public static TapiBridge CreateUploadBridge(TapiBridgeParameters parameters, ILog log, CancellationToken token)
         {
-            var transferLog = GetTransferLog(parameters);
+            var transferLog = GetTransferLog(parameters, log);
             return new TapiBridge(parameters, TransferDirection.Upload, transferLog, token);
         }
 
@@ -46,10 +47,13 @@ namespace kCura.WinEDDS.TApi
         /// <param name="parameters">
         /// The native file transfer parameters
         /// </param>
+        /// <param name="log">
+        /// The Relativity logging instance.
+        /// </param>
         /// <returns>
         /// The <see cref="ITransferLog"/> instance.
         /// </returns>
-        private static ITransferLog GetTransferLog(TapiBridgeParameters parameters)
+        private static ITransferLog GetTransferLog(TapiBridgeParameters parameters, ILog log)
         {
             try
             {
@@ -63,35 +67,14 @@ namespace kCura.WinEDDS.TApi
                     return new NullTransferLog();
                 }
 
-                var options = Relativity.Logging.Factory.LogFactory.GetOptionsFromAppDomain() ?? new LoggerOptions();
-                if (string.IsNullOrEmpty(options.Application))
-                {
-                    options.Application = "597C83DB-41C5-497D-B58E-7490479AD27E";
-                }
-
-                if (string.IsNullOrEmpty(options.ConfigurationFileLocation))
-                {
-                    options.ConfigurationFileLocation = GetLoggerConfigFilePath(parameters.LogConfigFile);
-                }
-
-                if (string.IsNullOrEmpty(options.System))
-                {
-                    options.System = "Import";
-                }
-
-                if (string.IsNullOrEmpty(options.SubSystem))
-                {
-                    options.SubSystem = "TransferAPI";
-                }
-
-                return new RelativityTransferLog(Relativity.Logging.Factory.LogFactory.GetLogger(options), false);
+                return new RelativityTransferLog(log, false);
             }
             catch (Exception e)
             {
                 try
                 {
                     Relativity.Logging.Tools.InternalLogger.WriteTokCuraEventLog(
-                        "Failed to setup WinEDDS logging. Exception: " + e,
+                        "Failed to setup TAPI logging. Exception: " + e,
                         "WinEDDS");
                 }
                 catch (Exception)
@@ -100,47 +83,6 @@ namespace kCura.WinEDDS.TApi
                 }
 
                 return new NullTransferLog();
-            }
-        }
-
-        /// <summary>
-        /// Gets the logger configuration file path.
-        /// </summary>
-        /// <param name="logConfigFile">
-        /// The Relativity Logging configuration file.
-        /// </param>
-        /// <returns>
-        /// The full path.
-        /// </returns>
-        private static string GetLoggerConfigFilePath(string logConfigFile)
-        {
-            try
-            {
-                var path = logConfigFile;
-                if (string.IsNullOrEmpty(path) || (Path.IsPathRooted(path) && !File.Exists(path)))
-                {
-                    return string.Empty;
-                }
-
-                if (Path.IsPathRooted(path) && File.Exists(path))
-                {
-                    return path;
-                }
-
-                // Be careful - unit tests result can return null.
-                var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
-                var directory = Directory.GetParent(assembly.Location).FullName;
-                var file = Path.Combine(directory, "LoggerConfig.xml");
-                if (!File.Exists(file))
-                {
-                    file = Path.Combine(directory, "LogConfig.xml");
-                }
-
-                return file;
-            }
-            catch (Exception)
-            {
-                return string.Empty;
             }
         }
     }
