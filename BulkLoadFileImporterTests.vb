a8025843abd13303bcc8055d34635e9d49670faf
Pawel Nowak <pawel.nowak@relativity.com>
2017-11-14 16:08:14 +0100

    REL-179267 Pushed temporary branch - we inject in this way ioReporter

--- BulkLoadFileImporterTests.vb
+++ BulkLoadFileImporterTests.vb
@@ -1,7 +1,11 @@
-﻿Imports kCura.EDDS.WebAPI.BulkImportManagerBase
+﻿Imports Helpers
+Imports kCura.EDDS.WebAPI.BulkImportManagerBase
 Imports NUnit.Framework
 Imports kCura.Windows.Process
 Imports kCura.WinEDDS.LoadFileFieldMap
+Imports kCura.WinEDDS.TApi
+Imports Relativity.Logging
+Imports Relativity.Transfer
 
 Namespace kCura.WinEDDS.NUnit
 	<TestFixture()> Public Class BulkLoadFileImporterTests
@@ -10,6 +14,7 @@ Namespace kCura.WinEDDS.NUnit
 		Dim _args As LoadFile
 		Dim _guid As System.Guid
 		Dim _controller As Controller
+        Dim _ioReporter As IIoReporter
 		Dim _keyPathExistsAlready As Boolean
 		Dim _keyValExistsAlready As Boolean
 #End Region
@@ -22,6 +27,13 @@ Namespace kCura.WinEDDS.NUnit
 			_args.CaseInfo.RootArtifactID = 1
 			_guid = New Guid("E09E18F3-D0C8-4CFC-96D1-FBB350FAB3E1")
 			_controller = New Controller()
+
+		    Dim ioWarningPublisher As New IoWarningPublisher()
+		    Dim fileSystemService As IFileSystemService = New FileSystemService()
+		    Dim waitAndRetryPolicy As IWaitAndRetryPolicy = New WaitAndRetryPolicy(kCura.Utility.Config.IOErrorNumberOfRetries, kCura.Utility.Config.IOErrorWaitTimeInSeconds)
+		    Dim fileInfoFailedExceptionHelper As IFileInfoFailedExceptionHelper = New FileInfoFailedExceptionHelper
+
+		    _ioReporter = New IoReporter(fileSystemService, waitAndRetryPolicy, Log.Logger, ioWarningPublisher, fileInfoFailedExceptionHelper, Config.DisableNativeLocationValidation)
 
 			_keyPathExistsAlready = RegKeyHelper.SubKeyPathExists(RegKeyHelper.RelativityKeyPath)
 			_keyValExistsAlready = False
@@ -48,7 +60,7 @@ Namespace kCura.WinEDDS.NUnit
 #Region " Basic Retry + Batch Size  Tests"
 
 		<Test(), Ignore("")> Public Sub BulkImportLoadFile_CatchWebException_Retries_CallsLowerBatchSize_True()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 			Try
 				bulkImporter.TryBulkImport(New ObjectLoadInfo())
 			Catch ex As Exception
@@ -59,7 +71,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test()> Public Sub BulkImportLoadFile_NoException_NoRetries_CallsLowerBatchSize_False()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(False))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(False))
 			bulkImporter.TryBulkImport(New ObjectLoadInfo())
 			Assert.AreEqual(500, bulkImporter.BatchSize)
 			Assert.AreEqual(0, bulkImporter.PauseCalled)
@@ -68,7 +80,7 @@ Namespace kCura.WinEDDS.NUnit
 		<Test()> Public Sub BulkImportLoadFile_CatchSystemException_Retries_CallsLowerBatchSize_False()
 			Dim manager As New MockBulkImportManagerWebExceptions(True)
 			manager.ErrorMessage = New System.Exception("bombed out")
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, manager)
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, manager)
 			Try
 				bulkImporter.TryBulkImport(New ObjectLoadInfo())
 			Catch ex As Exception
@@ -81,7 +93,7 @@ Namespace kCura.WinEDDS.NUnit
 
 
 		<Test(), Ignore("")> Public Sub BulkImportLoadFile_CatchSQLException_Retries_CallsLowerBatchSize_True()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
 			Try
 				bulkImporter.TryBulkImport(New ObjectLoadInfo())
 			Catch ex As Exception
@@ -95,7 +107,7 @@ Namespace kCura.WinEDDS.NUnit
 
 #Region " Retry-Until-It-Works Tests"
 		<Test(), Ignore("")> Public Sub BulkImportLoadFile_CatchSQLException_500RetryTo400_ThenWork()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(400))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(400))
 			bulkImporter.TryBulkImport(New ObjectLoadInfo())
 
 			Assert.AreEqual(400, bulkImporter.BatchSize)
@@ -104,7 +116,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test(), Ignore("")> Public Sub BulkImportLoadFile_CatchSQLException_500RetryToMinimum_ThenWork()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(300))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(300))
 			bulkImporter.MinimumBatch = 300
 			bulkImporter.TryBulkImport(New ObjectLoadInfo())
 			Assert.AreEqual(300, bulkImporter.BatchSize)
@@ -113,7 +125,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test(), Ignore("")> Public Sub BulkImportLoadFile_CatchSQLException_500Retry_HitMinimum_ThrowException()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(200))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(200))
 			bulkImporter.MinimumBatch = 350
 			Try
 				bulkImporter.TryBulkImport(New ObjectLoadInfo())
@@ -129,19 +141,19 @@ Namespace kCura.WinEDDS.NUnit
 
 #Region " Batch Size Adjustment "
 		<Test()> Public Sub BulkImportLoadFile_Lower_500BatchSize_to300()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 			bulkImporter.BatchSize = 300
 			Assert.AreEqual(300, bulkImporter.BatchSize)
 		End Sub
 		<Test()> Public Sub BulkImportLoadFile_Lower_500BatchSize_ToMinimum300()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 			bulkImporter.MinimumBatch = 300
 			bulkImporter.BatchSize = 300
 			Assert.AreEqual(300, bulkImporter.BatchSize)
 		End Sub
 
 		<Test()> Public Sub BulkImportLoadFile_Lower_500BatchSize_To200_PastMinimum300()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 			bulkImporter.MinimumBatch = 300
 			bulkImporter.BatchSize = 200
 			Assert.AreEqual(300, bulkImporter.BatchSize)
@@ -151,7 +163,7 @@ Namespace kCura.WinEDDS.NUnit
 
 #Region " GetMappedFields "
 		<Test()> Public Sub GetMappedFields_AllObjectFieldsSelected_AllFieldsObjectFieldContainsArtifactId()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 
 			Dim FieldMap As New LoadFileFieldMap
 			FieldMap.Add(New LoadFileFieldMapItem(New DocumentField("Field1", 1, Relativity.FieldTypeHelper.FieldType.Object, 1, 0, 0, 0, True, Nothing, False), 0))
@@ -166,7 +178,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test()> Public Sub GetMappedFields_NoObjectFieldsSelected_NoFieldsObjectFieldContainsArtifactId()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 
 			Dim FieldMap As New LoadFileFieldMap
 			FieldMap.Add(New LoadFileFieldMapItem(New DocumentField("Field1", 1, Relativity.FieldTypeHelper.FieldType.Object, 1, 0, 0, 0, True, Nothing, False), 0))
@@ -181,7 +193,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test()> Public Sub GetMappedFields_NoSelection_NoFieldsObjectFieldContainsArtifactId()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 
 			Dim FieldMap As New LoadFileFieldMap
 			FieldMap.Add(New LoadFileFieldMapItem(New DocumentField("Field1", 1, Relativity.FieldTypeHelper.FieldType.Object, 1, 0, 0, 0, True, Nothing, False), 0))
@@ -196,7 +208,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test()> Public Sub GetMappedFields_OneObjectFieldSelected_OneFieldsObjectFieldContainsArtifactId()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 
 			Dim FieldMap As New LoadFileFieldMap
 			FieldMap.Add(New LoadFileFieldMapItem(New DocumentField("Field1", 1, Relativity.FieldTypeHelper.FieldType.Object, 1, 0, 0, 0, True, Nothing, False), 0))
@@ -211,7 +223,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test()> Public Sub GetMappedFields_NoneObjectFieldsSelected_NoFieldsObjectFieldContainsArtifactId()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 
 			Dim FieldMap As New LoadFileFieldMap
 			FieldMap.Add(New LoadFileFieldMapItem(New DocumentField("Field01", 1, Relativity.FieldTypeHelper.FieldType.Object, 1, 0, 0, 0, True, Nothing, False), 0))
@@ -252,7 +264,7 @@ Namespace kCura.WinEDDS.NUnit
 		End Sub
 
 		<Test()> Public Sub GetMappedFields_TwoObjectFieldsSelected_TwoFieldsObjectFieldContainsArtifactId()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 
 			Dim FieldMap As New LoadFileFieldMap
 			FieldMap.Add(New LoadFileFieldMapItem(New DocumentField("Field01", 1, Relativity.FieldTypeHelper.FieldType.Object, 1, 0, 0, 0, True, Nothing, False), 0))
@@ -298,7 +310,7 @@ Namespace kCura.WinEDDS.NUnit
 		<Test>
 		<TestCaseSource("OverlayTypeSource")>
 		Public Sub GetMassImportOverlayBehavior(ByVal inputOverlayType As LoadFile.FieldOverlayBehavior, expectedOverlayType As kCura.EDDS.WebAPI.BulkImportManagerBase.OverlayBehavior)
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerWebExceptions(True))
 			Assert.AreEqual(bulkImporter.ConvertOverlayBehaviorEnum(inputOverlayType), expectedOverlayType)
 		End Sub
 
@@ -323,7 +335,7 @@ Namespace kCura.WinEDDS.NUnit
 		<TestCase("\aaa\bbb\cc", "aaa\\\\\\\\\\bbb\\cc")>
 		<TestCase("\SourceCode\Mainline\EDDS\kCura.WinEDDS\Importers", ".\SourceCode\Mainline\EDDS\kCura.WinEDDS\Importers")>
 		Public Sub CleanDestinationFolderPath(ByVal expected As String, ByVal input As String)
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
 			Dim actual As String = bulkImporter.CleanFolderPath(input)
 			Assert.AreEqual(expected, actual)
 		End Sub
@@ -333,21 +345,21 @@ Namespace kCura.WinEDDS.NUnit
 #Region "GetMaxExtractedTextLength"
 		<Test()>
 		Public Sub GetMaxExtractedTextLength_Return_Correct_Value_Nothing()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
 			Dim encoding As System.Text.Encoding = Nothing
 			Assert.AreEqual(1073741824, bulkImporter.GetMaxExtractedTextLength(encoding))
 		End Sub
 
 		<Test()>
 		Public Sub GetMaxExtractedTextLength_Return_Correct_Value_UTF8()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
 			Dim encoding As System.Text.Encoding = System.Text.Encoding.UTF8
 			Assert.AreEqual(1073741824, bulkImporter.GetMaxExtractedTextLength(encoding))
 		End Sub
 
 		<Test()>
 		Public Sub GetMaxExtractedTextLength_Return_Correct_Value_Other()
-			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
+			Dim bulkImporter As MockBulkLoadFileImporter = New MockBulkLoadFileImporter(_args, _controller, _ioReporter, 0, False, False, _guid, True, "S", True, New MockBulkImportManagerSqlExceptions(True))
 			Dim encoding As System.Text.Encoding = System.Text.Encoding.UTF32
 			Assert.AreEqual(2147483647, bulkImporter.GetMaxExtractedTextLength(encoding))
 		End Sub
