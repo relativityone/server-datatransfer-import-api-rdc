d5dd514d94c0b11db18864a2814eea69daf99a4f
Pawel Nowak <pawel.nowak@relativity.com>
2017-11-23 17:10:03 +0100

    REL-183299 Finished injecting ioReporter to the BulkImageFileImporter; Added IoReporterFactory

---
+++ IoReporterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Relativity.Logging;
+using Relativity.Transfer;
+
+
+namespace kCura.WinEDDS.TApi
+{
+    /// <summary>
+    /// Represents a class to create <see cref="IoReporter"/> instances.
+    /// </summary>
+    public static class IoReporterFactory
+    {
+        /// <summary>
+        /// reates a <see cref="IoReporter"/> instance.
+        /// </summary>
+        /// <returns>The <see cref="IoReporter"/> instance.</returns>
+        public static IIoReporter CreateIoReporter(int numberOfRetries, int waitTimeBetweenRetryAttempts, bool disableNativeLocationValidation, IFileInfoFailedExceptionHelper fileInfoFailedExceptionHelper, ILog logger)
+        {
+            var fileSystemService = new FileSystemService();
+            var waitAndRetryPolicy = new WaitAndRetryPolicy(numberOfRetries, waitTimeBetweenRetryAttempts);
+            var ioWarningPublisher = new IoWarningPublisher();
+            var ioReporter = new IoReporter(fileSystemService, waitAndRetryPolicy, logger, ioWarningPublisher, fileInfoFailedExceptionHelper, disableNativeLocationValidation);
+
+            return ioReporter;
+        }
+    }
+}
