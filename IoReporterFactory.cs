61203c571597aed10b9dfe9c4a23e56345e968e6
Pawel Nowak <pawel.nowak@relativity.com>
2017-11-27 20:25:28 +0100

    REL-183299 Renamed ioReporter property to ioReporterInstance; Passed IoWarningPublished as parameter to factory method; fixed IoWarninhHandler to rise proper warning

--- IoReporterFactory.cs
+++ IoReporterFactory.cs
@@ -14,11 +14,10 @@ namespace kCura.WinEDDS.TApi
         /// reates a <see cref="IoReporter"/> instance.
         /// </summary>
         /// <returns>The <see cref="IoReporter"/> instance.</returns>
-        public static IIoReporter CreateIoReporter(int numberOfRetries, int waitTimeBetweenRetryAttempts, bool disableNativeLocationValidation, IFileInfoFailedExceptionHelper fileInfoFailedExceptionHelper, ILog logger)
+        public static IIoReporter CreateIoReporter(int numberOfRetries, int waitTimeBetweenRetryAttempts, bool disableNativeLocationValidation, IFileInfoFailedExceptionHelper fileInfoFailedExceptionHelper, ILog logger, IoWarningPublisher ioWarningPublisher)
         {
             var fileSystemService = new FileSystemService();
             var waitAndRetryPolicy = new WaitAndRetryPolicy(numberOfRetries, waitTimeBetweenRetryAttempts);
-            var ioWarningPublisher = new IoWarningPublisher();
             var ioReporter = new IoReporter(fileSystemService, waitAndRetryPolicy, logger, ioWarningPublisher, fileInfoFailedExceptionHelper, disableNativeLocationValidation);
 
             return ioReporter;
