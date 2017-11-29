2a01dc96e4d4f1f0c390d54852b44ca48636269d
Pawel Nowak <pawel.nowak@relativity.com>
2017-11-29 15:56:53 +0100

    REL-183299 Added new exception for invalid character in path

---
+++ FileInfoInvalidPathException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.WinEDDS.TApi
+{
+    /// <summary>
+    /// Represent error that occures when there are illegal characters in the file path.
+    /// </summary>
+    public class FileInfoInvalidPathException : Exception
+    {
+        /// <summary>
+        /// Constructor for <see cref="FileInfoInvalidPathException"/> class
+        /// </summary>
+        public FileInfoInvalidPathException(string message) : base(message)
+        {
+        }
+    }
+}
