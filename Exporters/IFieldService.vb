3b672bb6fc2859e5e8aad1c08a974a067bc4a6f3
Patryk Stepien <patryk.stepien@relativity.com>
2017-11-30 11:21:00 +0100

    Create requests for LongText

--- Exporters/IFieldService.vb
+++ Exporters/IFieldService.vb
@@ -2,6 +2,7 @@
 	Public Interface IFieldService
 		Inherits IFieldLookupService
 
+		'TODO change type to ViewFieldInfo
 		Function GetColumns() As ArrayList
 
 		Function GetColumnHeader() As String
