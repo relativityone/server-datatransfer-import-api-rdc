3de72ac96b51506e1a52091ef1eafb9aa4ccc14d
Adrian Krzysztofek <adrian.krzysztofek@relativity.com>
2017-11-30 21:34:39 +0100

    Image previewer clean-up.

--- TestableImageFilePreviewer.vb
+++ TestableImageFilePreviewer.vb
@@ -2,13 +2,10 @@
 	Public Class TestableImageFilePreviewer
 		Inherits ImageFilePreviewer
 
-		Public Sub New(ByVal args As ImageLoadFile, ByVal controller As kCura.Windows.Process.Controller, ByVal doRetryLogic As Boolean)
-			MyBase.New(args, controller, doRetryLogic)
+		Public Sub New(ByVal controller As kCura.Windows.Process.Controller, ByVal doRetryLogic As Boolean)
+			MyBase.New(controller, doRetryLogic)
 		End Sub
 
-		Protected Overrides Sub InitializeUploaders(ByVal args As ImageLoadFile)
-			'do nothing
-		End Sub
-
+		
 	End Class
 End Namespace
