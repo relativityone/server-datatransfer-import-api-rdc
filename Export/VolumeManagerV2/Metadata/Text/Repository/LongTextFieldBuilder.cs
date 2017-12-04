016c8d1c53c4f56f2b4999a1c0cd9f81184efaba
Patryk Stepien <patryk.stepien@relativity.com>
2017-12-04 14:07:57 +0100

    Create LongTextRepository entry only for LongText fields

--- Export/VolumeManagerV2/Metadata/Text/Repository/LongTextFieldBuilder.cs
+++ Export/VolumeManagerV2/Metadata/Text/Repository/LongTextFieldBuilder.cs
@@ -23,21 +23,24 @@ namespace kCura.WinEDDS.Core.Export.VolumeManagerV2.Metadata.Text.Repository
 			for (int i = 0; i < _fieldService.GetColumns().Length; i++)
 			{
 				ViewFieldInfo field = _fieldService.GetColumns()[i];
-				if (_longTextHelper.IsTextTooLong(artifact, field.AvfColumnName))
+				if (_longTextHelper.IsLongTextField(field))
 				{
-					if (!(field is CoalescedTextViewField))
+					if (_longTextHelper.IsTextTooLong(artifact, field.AvfColumnName))
 					{
-						string tempLocation = Path.GetTempFileName();
-						TextExportRequest exportRequest = TextExportRequest.CreateRequestForLongText(artifact, field.FieldArtifactId, tempLocation);
-						LongText longText = LongText.CreateFromMissingValue(artifact.ArtifactID, field.FieldArtifactId, exportRequest);
+						if (!(field is CoalescedTextViewField))
+						{
+							string tempLocation = Path.GetTempFileName();
+							TextExportRequest exportRequest = TextExportRequest.CreateRequestForLongText(artifact, field.FieldArtifactId, tempLocation);
+							LongText longText = LongText.CreateFromMissingValue(artifact.ArtifactID, field.FieldArtifactId, exportRequest);
+							longTexts.Add(longText);
+						}
+					}
+					else
+					{
+						string longTextValue = _longTextHelper.GetTextFromField(artifact, field.AvfColumnName);
+						LongText longText = LongText.CreateFromExistingValue(artifact.ArtifactID, field.FieldArtifactId, longTextValue);
 						longTexts.Add(longText);
 					}
-				}
-				else
-				{
-					string longTextValue = _longTextHelper.GetTextFromField(artifact, field.AvfColumnName);
-					LongText longText = LongText.CreateFromExistingValue(artifact.ArtifactID, field.FieldArtifactId, longTextValue);
-					longTexts.Add(longText);
 				}
 			}
 
