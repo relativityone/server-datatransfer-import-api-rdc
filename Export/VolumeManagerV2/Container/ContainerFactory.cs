da234bd1fc0e252af607ef497a79818fe7ca0efb
Patryk Stepien <patryk.stepien@relativity.com>
2017-12-05 18:56:23 +0100

    Create empty container when using old export

--- Export/VolumeManagerV2/Container/ContainerFactory.cs
+++ Export/VolumeManagerV2/Container/ContainerFactory.cs
@@ -5,11 +5,14 @@ namespace kCura.WinEDDS.Core.Export.VolumeManagerV2.Container
 {
 	public class ContainerFactory : IContainerFactory
 	{
-		public IWindsorContainer Create(Exporter exporter, string columnHeader, string[] columnNamesInOrder)
+		public IWindsorContainer Create(Exporter exporter, string columnHeader, string[] columnNamesInOrder, bool useOldExport)
 		{
 			var container = new WindsorContainer();
 
-			container.Install(new ExportInstaller(exporter, columnHeader, columnNamesInOrder));
+			if (!useOldExport)
+			{
+				container.Install(new ExportInstaller(exporter, columnHeader, columnNamesInOrder));
+			}
 
 			return container;
 		}
