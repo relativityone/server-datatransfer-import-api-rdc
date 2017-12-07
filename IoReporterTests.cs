e4959104cd0401d646b940a633ccd6fc80baca3d
Scott Parillo <scott.parillo@relativity.com>
2017-12-06 18:17:32 -0600

    REL-184221 - Cleaned up Xml docs. - Added an overload to IWaitAndRetryPolicy for supporting return values. Added setters to properties to be more flexible. - IoReporter uses the new policy overload to simply return the value.

--- IoReporterTests.cs
+++ IoReporterTests.cs
@@ -1,42 +1,57 @@
-﻿using System;
-using Moq;
-using NUnit.Framework;
-using Relativity.Logging;
-using Relativity.Transfer;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IoReporterTests.cs" company="kCura Corp">
+//   kCura Corp (C) 2017 All Rights Reserved.
+// </copyright>
+// <summary>
+//   Represents <see cref="IoReporter"/> tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace kCura.WinEDDS.TApi.NUnit.Integration
 {
+    using System;
+
+    using Moq;
+
+    using global::NUnit.Framework;
+
+    using global::Relativity.Logging;
+    using global::Relativity.Transfer;
+
+    /// <summary>
+    /// Represents <see cref="IoReporter"/> tests.
+    /// </summary>
     [TestFixture]
     public class IoReporterTests
     {
-        private IIoReporter _ioReporterInstance;
-        private Mock<IFileSystemService> _fileService;
-        private Mock<IWaitAndRetryPolicy> _waitAndRetry;
-        private Mock<ILog> _logger;
-        private IoWarningPublisher _ioWarningPublisher;
-        private long _actualFileLength;
-        private Func<int, TimeSpan> _actualRetryDuractionFunc = null;
-        private Exception _expectedException; 
-        private ArgumentException _expectedArgumentException;
+        private IIoReporter ioReporterInstance;
+        private Mock<IFileSystemService> fileSystemService;
+        private Mock<IWaitAndRetryPolicy> mockedWaitAndRetry;
+        private IWaitAndRetryPolicy waitAndRetry;
+        private Mock<ILog> logger;
+        private IoWarningPublisher publisher;
+        private long actualFileLength;
+        private Func<int, TimeSpan> actualRetryDuractionFunc = null;
+        private Exception expectedException; 
+        private ArgumentException expectedArgumentException;
         private const string _FILE_NAME = "TestFileName";
         private const string _EXPECTED_DEFAULT_EXCEPTION_MESSAGE = "Expected exception message";
         private const string _EXPECTED_INVALID_PATH_EXCEPTION_MESSAGE = "Illegal characters in path.";
-        private const string _EXPECTED_RETHROWN_EXCEPTION_MESSAGE = "rethrowed exception";
-        private string _expectedLogWarningMessage;
-        private string _expectedLogErrorMessage;
-        private string _actualExceptionMessage = string.Empty;
-        private string _actualLogWarningMessage = string.Empty;
-        private string _actualInvalidPathExceptionMessage = string.Empty;
-        private string _actualLogErrorMessage = string.Empty;
+        private string expectedLogWarningMessage;
+        private string expectedLogErrorMessage;
+        private string actualExceptionMessage = string.Empty;
+        private string actualLogWarningMessage = string.Empty;
+        private string actualInvalidPathExceptionMessage = string.Empty;
+        private string actualLogErrorMessage = string.Empty;
 
         [SetUp]
         public void Setup()
         {
-            _fileService = new Mock<IFileSystemService>();
-            _waitAndRetry = new Mock<IWaitAndRetryPolicy>();
-            _logger = new Mock<ILog>();
-
-            _ioWarningPublisher = new IoWarningPublisher();
+            this.fileSystemService = new Mock<IFileSystemService>();
+            this.mockedWaitAndRetry = new Mock<IWaitAndRetryPolicy>();
+            this.waitAndRetry = null;
+            this.logger = new Mock<ILog>();
+            this.publisher = new IoWarningPublisher();
         }
 
         [TestCase(1)]
@@ -44,12 +59,10 @@ namespace kCura.WinEDDS.TApi.NUnit.Integration
         [TestCase(1299)]
         public void ItShouldGetFileLength(int expectedLength)
         {
-            GivenTheWaitAndRetryCallback();
-            GivenTheFileLength(expectedLength);
-            
-            WhenExecutingTheGetFileLength();
-            
-            ThenTheActualFileLengthShouldEqual(expectedLength);
+            this.GivenTheRealWaitAndRetryPolicy();
+            this.GivenTheFileLength(expectedLength);
+            this.WhenExecutingTheGetFileLength();
+            this.ThenTheActualFileLengthShouldEqual(expectedLength);
         }
 
         [TestCase(1, 1)]
@@ -60,153 +73,177 @@ namespace kCura.WinEDDS.TApi.NUnit.Integration
         [TestCase(2, 0)]
         public void ItShouldCalculateProperRetryDuration(int retryAttempt, int waitTimeBetweenRetryAttempts)
         {
-            GivenTheWaitAndRetryReturns(waitTimeBetweenRetryAttempts);
-            GivenTheWaitAndRetryCallback();
-            
-            WhenExecutingTheGetFileLength();
-            
-            ThenTheActualRetryDuractionShouldCalculated(retryAttempt, waitTimeBetweenRetryAttempts);
+            this.GivenTheWaitAndRetryReturns(waitTimeBetweenRetryAttempts);
+            this.GivenTheMockedWaitAndRetryPolicyCallback(1);
+            this.WhenExecutingTheGetFileLength();
+            this.ThenTheActualRetryDuractionShouldCalculated(retryAttempt, waitTimeBetweenRetryAttempts);
         }
 
         [Test]
         public void ItShouldRetryOnExceptionWhenNotDisabledNativeLocationValidation()
         {
-            GivenTheExpectedException();
-            GivenTheExpectedLogWarningMessage();
-            
-            GivenTheWaitAndRetryCallback();
-            GivenTheLoggerWarningCallback();
-            
-            WhenExecutingTheGetFileLength();
-            
-            ThenTheActualExceptionMessageShouldEqual();
-            ThenTheActualLogWarningMessageShouldEqual();
-            ThenTheLoggerWarningShouldBeInvokedOneTime();
+            this.GivenTheExpectedException();
+            this.GivenTheExpectedLogWarningMessage();
+            this.GivenTheMockedWaitAndRetryPolicyCallback(1);
+            this.GivenTheLoggerWarningCallback();
+            this.WhenExecutingTheGetFileLength();
+            this.ThenTheActualExceptionMessageShouldEqual();
+            this.ThenTheActualLogWarningMessageShouldEqual();
+            this.ThenTheLoggerWarningShouldBeInvokedOneTime();
         }
         
         [Test]
         public void ItShouldRetryOnExceptionWhenDisabledNativeLocationValidation()
         {
-            GivenTheExpectedInvalidPathException();
-            GivenTheExpectedLogErrorMessage();
-            GivenTheWaitAndRetryCallbackThatThrowsArgumentException();
-            GivenTheLoggerErrorCallback();
-            
-            WhenExecutingIoReporterGetFileLengthThenThwowsException(true);
-
-            ThenTheActualInvalidPathExceptionMessageShouldEqual();
-            ThenTheActualLogErrorMessageShouldEqual();
-            ThenTheLoggerErrorShouldBeInvokedOneTime();
+            this.GivenTheExpectedInvalidPathException();
+            this.GivenTheExpectedLogErrorMessage();
+            this.GivenTheWaitAndRetryCallbackThatThrowsArgumentException();
+            this.GivenTheLoggerErrorCallback();
+            this.WhenExecutingIoReporterGetFileLengthThenThwowsException(true);
+            this.ThenTheActualInvalidPathExceptionMessageShouldEqual();
+            this.ThenTheActualLogErrorMessageShouldEqual();
+            this.ThenTheLoggerErrorShouldBeInvokedOneTime();
         }
 
         private void ThenTheLoggerErrorShouldBeInvokedOneTime()
         {
-            _logger.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+            this.logger.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
         }
 
         private void ThenTheActualLogErrorMessageShouldEqual()
         {
-            Assert.That(_actualLogErrorMessage, Is.EqualTo(_expectedLogErrorMessage));
+            Assert.That(this.actualLogErrorMessage, Is.EqualTo(this.expectedLogErrorMessage));
         }
 
         private void ThenTheActualInvalidPathExceptionMessageShouldEqual()
         {
-            Assert.That(_actualInvalidPathExceptionMessage, Is.EqualTo(_EXPECTED_INVALID_PATH_EXCEPTION_MESSAGE));
+            Assert.That(this.actualInvalidPathExceptionMessage, Is.EqualTo(_EXPECTED_INVALID_PATH_EXCEPTION_MESSAGE));
         }
 
         #region "Helper methods"
 
         private void GivenTheWaitAndRetryReturns(int waitTimeBetweenRetryAttempts)
         {
-            _waitAndRetry.Setup(obj => obj.WaitTimeSecondsBetweenRetryAttempts).Returns(waitTimeBetweenRetryAttempts);
+            this.mockedWaitAndRetry.Setup(obj => obj.WaitTimeSecondsBetweenRetryAttempts).Returns(waitTimeBetweenRetryAttempts);
         }
 
-        private void GivenTheWaitAndRetryCallback()
+        /// <summary>
+        /// Givens the wait and retry.
+        /// </summary>
+        private void GivenTheRealWaitAndRetryPolicy()
         {
-            _waitAndRetry.Setup(obj => obj.WaitAndRetry<Exception>(It.IsAny<Func<int, TimeSpan>>(),
-                    It.IsAny<Action<Exception, TimeSpan>>(), It.IsAny<Action>()))
-                .Callback<Func<int, TimeSpan>, Action<Exception, TimeSpan>, Action>((retryDuration, retryAction, execFunc) =>
-                {
-                    _actualRetryDuractionFunc = retryDuration;
-                    retryAction(_expectedException, TimeSpan.Zero);
-                    execFunc();
-                });
+            this.waitAndRetry = new WaitAndRetryPolicy(1, 0);
+        }
+
+        private void GivenTheMockedWaitAndRetryPolicyCallback(long expected)
+        {
+            this.mockedWaitAndRetry
+                .Setup(
+                    obj => obj.WaitAndRetry<long, Exception>(
+                        It.IsAny<Func<int, TimeSpan>>(),
+                        It.IsAny<Action<Exception, TimeSpan>>(),
+                        It.IsAny<Func<long>>())).Callback<Func<int, TimeSpan>, Action<Exception, TimeSpan>, Func<long>>(
+                    (retryDuration, retryAction, execFunc) =>
+                        {
+                            this.actualRetryDuractionFunc = retryDuration;
+                            retryAction(this.expectedException, TimeSpan.Zero);
+                            execFunc();
+                        });
         }
 
         private void GivenTheFileLength(int expectedLength)
         {
-            _fileService.Setup(obj => obj.GetFileLength(_FILE_NAME)).Returns(expectedLength);
+            this.fileSystemService.Setup(obj => obj.GetFileLength(_FILE_NAME)).Returns(expectedLength);
         }
 
         private void GivenTheLoggerWarningCallback()
         {
-            _logger.Setup(logger => logger.LogWarning(It.IsAny<Exception>(), It.IsAny<string>(), It.IsAny<object[]>()))
-                .Callback<Exception, string, object[]>((ex, logWarningMessage, param) =>
-                {
-                    _actualExceptionMessage = ex.Message;
-                    _actualLogWarningMessage = logWarningMessage;
-                });
+            this.logger.Setup(
+                    log => log.LogWarning(It.IsAny<Exception>(), It.IsAny<string>(), It.IsAny<object[]>()))
+                .Callback<Exception, string, object[]>(
+                    (ex, logWarningMessage, param) =>
+                        {
+                            this.actualExceptionMessage = ex.Message;
+                            this.actualLogWarningMessage = logWarningMessage;
+                        });
         }
 
         private void GivenTheLoggerErrorCallback()
         {
-            _logger.Setup(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>(), It.IsAny<object[]>()))
+            this.logger.Setup(log => log.LogError(It.IsAny<Exception>(), It.IsAny<string>(), It.IsAny<object[]>()))
                 .Callback<Exception, string, object[]>((ex, logWarningMessage, param) =>
                 {
-                    _actualInvalidPathExceptionMessage = ex.Message;
-                    _actualLogErrorMessage = logWarningMessage;
+                    this.actualInvalidPathExceptionMessage = ex.Message;
+                    this.actualLogErrorMessage = logWarningMessage;
                 });
         }
 
         private void GivenTheWaitAndRetryCallbackThatThrowsArgumentException()
         {
-            _waitAndRetry.Setup(obj => obj.WaitAndRetry<Exception>(It.IsAny<Func<int, TimeSpan>>(),
-                    It.IsAny<Action<Exception, TimeSpan>>(), It.IsAny<Action>()))
-                .Callback<Func<int, TimeSpan>, Action<Exception, TimeSpan>, Action>((retryDuration, retryAction, execFunc) =>
-                {
-                    retryAction(_expectedArgumentException, TimeSpan.Zero);
-                });
+            this.mockedWaitAndRetry
+                .Setup(
+                    obj => obj.WaitAndRetry<long, Exception>(
+                        It.IsAny<Func<int, TimeSpan>>(),
+                        It.IsAny<Action<Exception, TimeSpan>>(),
+                        It.IsAny<Func<long>>())).Callback<Func<int, TimeSpan>, Action<Exception, TimeSpan>, Func<long>>(
+                    (retryDuration, retryAction, execFunc) =>
+                        {
+                            retryAction(this.expectedArgumentException, TimeSpan.Zero);
+                        });
         }
 
         private void GivenTheExpectedLogWarningMessage()
         {
-            _expectedLogWarningMessage = IoReporter.BuildIoReporterWarningMessage(_expectedException, 0);
+            this.expectedLogWarningMessage = IoReporter.BuildIoReporterWarningMessage(this.expectedException, 0);
         }
 
         private void GivenTheExpectedLogErrorMessage()
         {
-            _expectedLogErrorMessage = $"File {_FILE_NAME} not found: illegal characters in path.";
+            this.expectedLogErrorMessage = string.Format(
+                kCura.WinEDDS.TApi.Resources.Strings.ImportInvalidPathCharactersExceptionMessage,
+                _FILE_NAME);
         }
 
         private void GivenTheExpectedException()
         {
-            _expectedException = new Exception(_EXPECTED_DEFAULT_EXCEPTION_MESSAGE);
+            this.expectedException = new Exception(_EXPECTED_DEFAULT_EXCEPTION_MESSAGE);
         }
 
         private void GivenTheExpectedInvalidPathException()
         {
-            _expectedArgumentException = new ArgumentException(_EXPECTED_INVALID_PATH_EXCEPTION_MESSAGE);
+            this.expectedArgumentException = new ArgumentException(_EXPECTED_INVALID_PATH_EXCEPTION_MESSAGE);
         }
 
         private void WhenExecutingTheGetFileLength(bool disableNativeLocationValidation = false)
         {
-            _ioReporterInstance = new IoReporter(_fileService.Object, _waitAndRetry.Object, _logger.Object, _ioWarningPublisher, disableNativeLocationValidation);
-
-            _actualFileLength = _ioReporterInstance.GetFileLength(_FILE_NAME, 0);
+            var policy = this.waitAndRetry != null ? this.waitAndRetry : this.mockedWaitAndRetry.Object;
+            this.ioReporterInstance = new IoReporter(
+                this.fileSystemService.Object,
+                policy,
+                this.logger.Object,
+                this.publisher,
+                disableNativeLocationValidation);
+            this.actualFileLength = this.ioReporterInstance.GetFileLength(_FILE_NAME, 0);
         }
 
         private void WhenExecutingIoReporterGetFileLengthThenThwowsException(bool disableNativeLocationValidation)
         {
-            _ioReporterInstance = new IoReporter(_fileService.Object, _waitAndRetry.Object, _logger.Object, _ioWarningPublisher, disableNativeLocationValidation);
-
-            Assert.That(() => _ioReporterInstance.GetFileLength(_FILE_NAME, 0),
+            this.ioReporterInstance = new IoReporter(
+                this.fileSystemService.Object,
+                this.mockedWaitAndRetry.Object,
+                this.logger.Object,
+                this.publisher,
+                disableNativeLocationValidation);
+            Assert.That(
+                () => this.ioReporterInstance.GetFileLength(_FILE_NAME, 0),
                 Throws.Exception.TypeOf<FileInfoInvalidPathException>());
         }
 
         private void ThenTheActualRetryDuractionShouldCalculated(int retryAttempt, int waitTimeBetweenRetryAttempts)
         {
-            TimeSpan actualRetryDuraction = _actualRetryDuractionFunc(retryAttempt);
-            Assert.That(actualRetryDuraction,
+            TimeSpan actualRetryDuraction = this.actualRetryDuractionFunc(retryAttempt);
+            Assert.That(
+                actualRetryDuraction,
                 retryAttempt == 1
                     ? Is.EqualTo(TimeSpan.FromSeconds(0))
                     : Is.EqualTo(TimeSpan.FromSeconds(waitTimeBetweenRetryAttempts)));
@@ -214,22 +251,22 @@ namespace kCura.WinEDDS.TApi.NUnit.Integration
 
         private void ThenTheActualFileLengthShouldEqual(int expectedLength)
         {
-            Assert.That(_actualFileLength, Is.EqualTo(expectedLength));
+            Assert.That(this.actualFileLength, Is.EqualTo(expectedLength));
         }
 
         private void ThenTheLoggerWarningShouldBeInvokedOneTime()
         {
-            _logger.Verify(logger => logger.LogWarning(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+            this.logger.Verify(log => log.LogWarning(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
         }
 
         private void ThenTheActualLogWarningMessageShouldEqual()
         {
-            Assert.That(_actualLogWarningMessage, Is.EqualTo(_expectedLogWarningMessage));
+            Assert.That(this.actualLogWarningMessage, Is.EqualTo(this.expectedLogWarningMessage));
         }
 
         private void ThenTheActualExceptionMessageShouldEqual()
         {
-            Assert.That(_actualExceptionMessage, Is.EqualTo(_EXPECTED_DEFAULT_EXCEPTION_MESSAGE));
+            Assert.That(this.actualExceptionMessage, Is.EqualTo(_EXPECTED_DEFAULT_EXCEPTION_MESSAGE));
         }
 
         #endregion
