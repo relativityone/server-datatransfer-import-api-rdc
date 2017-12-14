cfa4ae8f494902568fea52756e897999badc68be
Patryk Stepien <patryk.stepien@relativity.com>
2017-12-14 11:36:10 +0100

    Add more logging

--- Export/VolumeManagerV2/Metadata/ErrorFileDestinationPathTests.cs
+++ Export/VolumeManagerV2/Metadata/ErrorFileDestinationPathTests.cs
@@ -1,6 +1,7 @@
 ﻿using kCura.WinEDDS.Core.Export.VolumeManagerV2.Metadata;
 using kCura.WinEDDS.Core.Export.VolumeManagerV2.Metadata.Paths;
 using NUnit.Framework;
+using Relativity.Logging;
 
 namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Metadata
 {
@@ -12,7 +13,7 @@ namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Metadata
 		{
 			ExportFile exportSettings = new ExportFile(1);
 
-			var instance = new ErrorFileDestinationPath(exportSettings);
+			var instance = new ErrorFileDestinationPath(exportSettings, new NullLogger());
 
 			//ACT
 			string path1 = instance.Path;
