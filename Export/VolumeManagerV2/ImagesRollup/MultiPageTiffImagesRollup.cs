4481760e95833403ba5393f1f107aa50711f5a56
Patryk Stepien <patryk.stepien@relativity.com>
2017-12-22 15:07:27 +0100

    Add unit tests for classes in ImagesRollup folder

--- Export/VolumeManagerV2/ImagesRollup/MultiPageTiffImagesRollup.cs
+++ Export/VolumeManagerV2/ImagesRollup/MultiPageTiffImagesRollup.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using kCura.Utility;
 using Relativity.Logging;
 
 namespace kCura.WinEDDS.Core.Export.VolumeManagerV2.ImagesRollup
 {
 	public class MultiPageTiffImagesRollup : MultiPageImagesRollup
 	{
-		public MultiPageTiffImagesRollup(ExportFile exportSettings, IFileHelper fileHelper, IStatus status, ILog logger, Image imageConverter) : base(exportSettings, fileHelper, status,
+		public MultiPageTiffImagesRollup(ExportFile exportSettings, IFileHelper fileHelper, IStatus status, ILog logger, IImage imageConverter) : base(exportSettings, fileHelper, status,
 			logger, imageConverter)
 		{
 		}
--- Export/VolumeManagerV2/ImagesRollup/PdfImagesRollup.cs
+++ Export/VolumeManagerV2/ImagesRollup/PdfImagesRollup.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using kCura.Utility;
 using Relativity.Logging;
 
 namespace kCura.WinEDDS.Core.Export.VolumeManagerV2.ImagesRollup
 {
 	public class PdfImagesRollup : MultiPageImagesRollup
 	{
-		public PdfImagesRollup(ExportFile exportSettings, IFileHelper fileHelper, IStatus status, ILog logger, Image imageConverter) : base(exportSettings, fileHelper, status, logger,
+		public PdfImagesRollup(ExportFile exportSettings, IFileHelper fileHelper, IStatus status, ILog logger, IImage imageConverter) : base(exportSettings, fileHelper, status, logger,
 			imageConverter)
 		{
 		}
