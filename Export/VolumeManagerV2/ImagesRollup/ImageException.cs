7e50a5724ccbae908855a3403cfed64585211308
Patryk Stepien <patryk.stepien@relativity.com>
2017-12-22 15:07:27 +0100

    Add unit tests for classes in ImagesRollup folder

---
+++ Export/VolumeManagerV2/ImagesRollup/ImageException.cs
@@ -0,0 +1,11 @@
+﻿using kCura.Utility;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.ImagesRollup
+{
+	public class ImageException : Image.ImageRollupException
+	{
+		public ImageException() : base("", null)
+		{
+		}
+	}
+}
---
+++ Export/VolumeManagerV2/ImagesRollup/PdfImagesRollupTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using kCura.WinEDDS.Core.Export.VolumeManagerV2.ImagesRollup;
+using kCura.WinEDDS.Exporters;
+using Moq;
+using Relativity.Logging;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.ImagesRollup
+{
+	public class PdfImagesRollupTests : MultiPageImagesRollupTests
+	{
+		protected override MultiPageImagesRollup CreateInstance(ExportFile exportSettings, IFileHelper fileHelper, IStatus status, IImage imageConverter)
+		{
+			return new PdfImagesRollup(exportSettings, fileHelper, status, new NullLogger(), imageConverter);
+		}
+
+		protected override void AssertImageConverterCall(Mock<IImage> imageConverter, ObjectExportInfo artifact)
+		{
+			string[] imagesList = artifact.Images.Cast<ImageExportInfo>().Select(x => x.TempLocation).ToArray();
+			imageConverter.Verify(x => x.ConvertImagesToMultiPagePdf(It.Is<string[]>(y => y.SequenceEqual(imagesList)), It.Is<string>(y => y.EndsWith(".tmp"))));
+		}
+
+		protected override string Extension()
+		{
+			return ".pdf";
+		}
+	}
+}
