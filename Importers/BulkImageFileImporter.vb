59d5236052e4f0e63411b3fae3e0dbd79c0f7404
Adrian Krzysztofek <adrian.krzysztofek@relativity.com>
2017-12-27 17:19:00 +0100

    REL-189898 - Correct warning/info messages during import.

--- Importers/BulkImageFileImporter.vb
+++ Importers/BulkImageFileImporter.vb
@@ -983,7 +983,7 @@ Namespace kCura.WinEDDS
 			If Not _imageReader Is Nothing Then
 				_imageReader.Cancel()
 			End If
-			RaiseStatusEvent(kCura.Windows.Process.EventType.Progress, $"Job has been stopped by the user - {Me.TotalTransferredFilesCount} images have been transferred.", Me.TotalTransferredFilesCount, Me.CurrentLineNumber)
+			RaiseStatusEvent(kCura.Windows.Process.EventType.Progress, $"Job has been stopped - {Me.TotalTransferredFilesCount} images have been transferred.", Me.TotalTransferredFilesCount, Me.CurrentLineNumber)
 			OnWriteStatusMessage(kCura.Windows.Process.EventType.Status, "Finalizing job…", TapiConstants.NoLineNumber, TapiConstants.NoLineNumber)
 		End Sub
 
