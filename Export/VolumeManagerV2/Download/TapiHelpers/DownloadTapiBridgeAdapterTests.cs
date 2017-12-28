709919b241c79926b8ae8fca0c3ddf262dc05fd4
Patryk Stepien <patryk.stepien@relativity.com>
2017-12-28 14:30:08 +0100

    Add unit tests for classes in TapiHelpers folder

---
+++ Export/VolumeManagerV2/Download/TapiHelpers/DownloadTapiBridgeAdapterTests.cs
@@ -0,0 +1,79 @@
+﻿using kCura.WinEDDS.Core.Export.VolumeManagerV2.Download.TapiHelpers;
+using kCura.WinEDDS.Core.Export.VolumeManagerV2.Statistics;
+using Moq;
+using NUnit.Framework;
+using Relativity.Transfer;
+using ITransferStatistics = kCura.WinEDDS.Core.Export.VolumeManagerV2.Statistics.ITransferStatistics;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Download.TapiHelpers
+{
+	[TestFixture]
+	public abstract class DownloadTapiBridgeAdapterTests
+	{
+		protected DownloadTapiBridgeAdapter Instance { get; set; }
+
+		protected Mock<ITapiBridge> TapiBridge { get; private set; }
+		protected Mock<IProgressHandler> ProgressHandler { get; private set; }
+		protected Mock<IMessagesHandler> MessagesHandler { get; private set; }
+		protected Mock<ITransferStatistics> TransferStatistics { get; private set; }
+
+		protected void SetUpMocks()
+		{
+			TapiBridge = new Mock<ITapiBridge>();
+			ProgressHandler = new Mock<IProgressHandler>();
+			MessagesHandler = new Mock<IMessagesHandler>();
+			TransferStatistics = new Mock<ITransferStatistics>();
+		}
+
+		[Test]
+		public void ItShouldAttachAndDetach()
+		{
+			Instance.Dispose();
+
+			//ASSERT
+			ProgressHandler.Verify(x => x.Attach(TapiBridge.Object), Times.Once);
+			ProgressHandler.Verify(x => x.Detach(), Times.Once);
+
+			MessagesHandler.Verify(x => x.Attach(TapiBridge.Object), Times.Once);
+			MessagesHandler.Verify(x => x.Detach(), Times.Once);
+
+			TransferStatistics.Verify(x => x.Attach(TapiBridge.Object), Times.Once);
+			TransferStatistics.Verify(x => x.Detach(), Times.Once);
+
+			TapiBridge.Verify(x => x.Dispose(), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldWaitForTransferJob()
+		{
+			//ACT
+			Instance.AddPath(new TransferPath());
+			Instance.WaitForTransferJob();
+
+			//ASSERT
+			TapiBridge.Verify(x => x.WaitForTransferJob(), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldNotWaitForTransferJobWhenNothingHasBeenAddedToQueue()
+		{
+			//ACT
+			Instance.WaitForTransferJob();
+
+			//ASSERT
+			TapiBridge.Verify(x => x.WaitForTransferJob(), Times.Never);
+		}
+
+		[Test]
+		public void ItShouldAddTransferPath()
+		{
+			TransferPath transferPath = new TransferPath();
+
+			//ACT
+			Instance.AddPath(transferPath);
+
+			//ASSERT
+			TapiBridge.Verify(x => x.AddPath(transferPath), Times.Once);
+		}
+	}
+}
---
+++ Export/VolumeManagerV2/Download/TapiHelpers/DownloadTapiBridgeWithEncodingConversionTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using kCura.WinEDDS.Core.Export.VolumeManagerV2.Download;
+using kCura.WinEDDS.Core.Export.VolumeManagerV2.Download.TapiHelpers;
+using Moq;
+using NUnit.Framework;
+using Relativity.Logging;
+using Relativity.Transfer;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Download.TapiHelpers
+{
+	[TestFixture]
+	public class DownloadTapiBridgeWithEncodingConversionTests : DownloadTapiBridgeAdapterTests
+	{
+		private Mock<ILongTextEncodingConverter> _longTextEncodingConverter;
+
+		[SetUp]
+		public void SetUp()
+		{
+			SetUpMocks();
+
+			_longTextEncodingConverter = new Mock<ILongTextEncodingConverter>();
+
+			Instance = new DownloadTapiBridgeWithEncodingConversion(TapiBridge.Object, ProgressHandler.Object, MessagesHandler.Object, TransferStatistics.Object,
+				_longTextEncodingConverter.Object, new NullLogger());
+		}
+
+		[Test]
+		public void ItShouldStartConverterAfterAddingFirstTransferPath()
+		{
+			//ACT
+			Instance.AddPath(new TransferPath());
+			Instance.AddPath(new TransferPath());
+			Instance.AddPath(new TransferPath());
+
+			//ASSERT
+			_longTextEncodingConverter.Verify(x => x.StartListening(TapiBridge.Object), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldAlwaysStopConverterAfterDownloadFinished()
+		{
+			TapiBridge.Setup(x => x.WaitForTransferJob()).Throws<Exception>();
+
+			//ACT
+			Instance.AddPath(new TransferPath());
+			Assert.Throws<Exception>(() => Instance.WaitForTransferJob());
+
+			//ASSERT
+			_longTextEncodingConverter.Verify(x => x.StopListening(TapiBridge.Object), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldWaitForConversionToComplete()
+		{
+			//ACT
+			Instance.AddPath(new TransferPath());
+			Instance.WaitForTransferJob();
+
+			//ASSERT
+			_longTextEncodingConverter.Verify(x => x.WaitForConversionCompletion(), Times.Once);
+		}
+	}
+}
