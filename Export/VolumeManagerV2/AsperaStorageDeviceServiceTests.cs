4a2706810b641efaa62caa9459223a760fc29fad
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2018-02-28 09:19:28 +0100

    Modified our service checker

--- Export/VolumeManagerV2/AsperaStorageDeviceServiceTests.cs
+++ Export/VolumeManagerV2/AsperaStorageDeviceServiceTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using kCura.WinEDDS.Core.Export.VolumeManagerV2;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using Relativity.Transfer;
 using Relativity.Transfer.Aspera;
@@ -14,37 +15,44 @@ namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2
 	[TestFixture]
 	public class AsperaStorageDeviceServiceTests
 	{
-		//[Test]
-		public void Connect()
+		[Test]
+		public async Task Connect()
 		{
+
 			try
 			{
 				// Note: you MUST use an environment that supports the file share/Aspera naming conventions.
 				//       The Hyper-V private cloud has already been fully updated.
+				const int WorkspaceId = 1049950;
 				var connectionInfo = new RelativityConnectionInfo(
 					new Uri("https://il1ddtapirap001.kcura.corp"),
-					new BasicAuthenticationCredential("serviceaccount@relativity.com", "Test1234!"));
-				IAsperaStorageDeviceService service = new AsperaStorageDeviceService(connectionInfo);
-
-				// Note: until the Kepler API changes have been made, you must be an admin and supply the resource pool.
-				var workspaceResults = service.GetStorageDevicesAsync(1017836);
-
-
-				var poolResults = workspaceResults.ConfigureAwait(false).GetAwaiter().GetResult();
-
-
-				foreach (AsperaStorageDevice device in poolResults.InvalidStorageDevices)
+					new BasicAuthenticationCredential("serviceaccount@relativity.com", "Test1234!"),
+					WorkspaceId);
+				using (ITransferLog transferLog = new SerilogTransferLog())
+				using (RelativityTransferHost transferHost = new RelativityTransferHost(connectionInfo, transferLog))
 				{
-					Console.WriteLine($"The storage device {device.Name} is invalid");
-				}
+					// The storage search API is obtained through the transfer host.
+					IFileStorageSearch service = transferHost.CreateFileStorageSearch();
 
-				foreach (AsperaStorageDevice device in poolResults.StorageDevices)
-				{
-					Console.WriteLine($"Storage device connection string: {device.ConnectionString}");
-					Console.WriteLine($"Storage device doc root: {device.DocRoot}");
-					Console.WriteLine($"Storage device name: {device.Name}");
-					Console.WriteLine($"Storage device Transfer credential: {device.TransferCredential.Name}");
-					Console.WriteLine($"Storage device Node credential: {device.NodeCredential.Name}");
+					// Note: until the Kepler API changes have been made, you must be an admin and supply the resource pool.
+					FileStorageSearchResults results = await service.GetResourcePoolFileSharesAsync("RelativityOne Pool");
+					var str = JsonConvert.SerializeObject(results);
+				foreach (RelativityFileShare fileShare in results.InvalidFileShares)
+					{
+						Console.WriteLine($"The file share {fileShare.Name} is invalid");
+					}
+
+					foreach (RelativityFileShare fileShare in results.FileShares)
+					{
+						Console.WriteLine($"File share URL: {fileShare.Url}");
+						Console.WriteLine($"File share doc root: {fileShare.DocRoot}");
+						Console.WriteLine($"File share name: {fileShare.Name}");
+						Console.WriteLine($"File share number: {fileShare.Number}");
+						Console.WriteLine($"File share transfer credential: {fileShare.TransferCredential.Name}");
+						Console.WriteLine($"File share node credential: {fileShare.NodeCredential.Name}");
+
+						var result = JsonConvert.SerializeObject(fileShare);
+					}
 				}
 			}
 			catch (Exception e)
@@ -65,5 +73,62 @@ namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2
 			
 			Assert.IsTrue(tapiUri.IsBaseOf(uri));
 		}
+	}
+
+	internal class SerilogTransferLog : ITransferLog
+	{
+		public void Dispose()
+		{
+		}
+
+		public void LogVerbose(string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogVerbose(Exception exception, string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogDebug(string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogDebug(Exception exception, string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogInformation(string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogInformation(Exception exception, string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogWarning(string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogWarning(Exception exception, string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogError(string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogError(Exception exception, string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogFatal(string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public void LogFatal(Exception exception, string messageTemplate, params object[] propertyValues)
+		{
+		}
+
+		public bool IsEnabled { get; set; }
 	}
 }
