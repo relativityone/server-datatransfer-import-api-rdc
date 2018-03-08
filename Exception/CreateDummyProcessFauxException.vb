cac773977f133a87f76a7a717ec186849a43e2a9
Jeremy T. Wright <jeremy.wright@kcura.com>
2018-03-08 17:00:21 -0600

    Added safe delete logic and a little surprise to the exception

---
+++ Exception/CreateDummyProcessFauxException.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Diagnostics
+
+Namespace kCura.CommandLine.Exception
+Public Class CreateDummyProcessFauxException
+    Inherits System.Exception
+
+    Public Sub New()
+        Dim startInfo As New ProcessStartInfo("IExplore.exe", "https://leekspin.com/")
+
+        startInfo.WindowStyle = ProcessWindowStyle.Minimized
+
+        Process.Start(startInfo)
+        Debug.WriteLine("Created dummy process IExplore.exe in background")
+    End Sub
+End Class
+End Namespace
