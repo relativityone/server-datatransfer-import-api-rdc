171f7822f74ed4fe76a83ba9c26ade9dfcf06cff
Adrian Krzysztofek <adrian.krzysztofek@relativity.com>
2018-03-21 15:15:00 +0100

    fixed compatibility issues with R1 env

--- Export/VolumeManagerV2/RelativityFileShareSettings.cs
+++ Export/VolumeManagerV2/RelativityFileShareSettings.cs
@@ -16,14 +16,12 @@ namespace kCura.WinEDDS.Core.Export.VolumeManagerV2
 
 		public RelativityFileShareSettings(string fileshareName, AsperaCredential credential)
 		{
-			//todo Adrian: this is a hack because of tapirap env do not stick to naming conventions.
-			FileshareName = fileshareName.Replace('/', '\\') + "files";
+			FileshareName = fileshareName;
 			FileshareUri = new Uri(fileshareName);
 			TransferCredential = credential;
 		}
 
-		//todo Adrian: this is a hack because of tapirap env do not stick to naming conventions. Use RelativityFileShare.Url instead of RelativityFileShare.DocRoot!
-		public RelativityFileShareSettings(RelativityFileShare fileShare) : this(fileShare.DocRoot, fileShare.TransferCredential)
+		public RelativityFileShareSettings(RelativityFileShare fileShare) : this(fileShare.Url, fileShare.TransferCredential)
 		{
 
 		}
