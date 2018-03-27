2174d74026e6c1edcf4e92f30a24f7f8942e29bc
Jeremy T. Wright <jeremy.wright@kcura.com>
2018-03-27 11:14:40 -0500

    Converted all properties to auto-properties
    Found all instances of properties that have simple getters/setters and
    replaced them with auto-properties. This is a standalone cleanup task.

--- Command.vb
+++ Command.vb
@@ -1,26 +1,6 @@
 Namespace kCura.CommandLine
 	Public Class Command
-
-		Private _directive As String
-		Private _value As String
-
-		Public Property Directive() As String
-			Get
-				Return _directive
-			End Get
-			Set(ByVal value As String)
-				_directive = value
-			End Set
-		End Property
-
-		Public Property Value() As String
-			Get
-				Return _value
-			End Get
-			Set(ByVal value As String)
-				_value = value
-			End Set
-		End Property
-
+	    Public Property Directive As String
+	    Public Property Value As String
 	End Class
 End Namespace
