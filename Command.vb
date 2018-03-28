5a92584ea366d4ad4feb124e21f74a4f0ed67481
Jeremy T. Wright <jeremy.wright@kcura.com>
2018-03-28 10:08:45 -0500

    Changed whitespaces to tabs

--- Command.vb
+++ Command.vb
@@ -1,6 +1,6 @@
 Namespace kCura.CommandLine
 	Public Class Command
-	    Public Property Directive As String
-	    Public Property Value As String
+		Public Property Directive As String
+		Public Property Value As String
 	End Class
 End Namespace
