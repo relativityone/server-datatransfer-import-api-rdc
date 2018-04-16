aaf6c5b44327f717e7cf0df1b8df30e72261124b
Andrzej Korczyński <andrzej.korczynski@relativity.com>
2018-04-16 14:08:11 +0200

    REL-196317 [refactored performance prefix] Create RDC Failed and total job count SUM metric

--- Monitoring/MessageServiceFactory.vb
+++ Monitoring/MessageServiceFactory.vb
@@ -4,6 +4,9 @@ Imports Relativity.Services.ServiceProxy
 
 Namespace kCura.WinEDDS.Monitoring
 	Public Class MessageServiceFactory
+
+		Private Shared ReadOnly PerformancePrefix As String = "RDC.Performance"
+
 		Public Shared Function SetupMessageService(serviceFactory As IServiceFactory) As IMessageService
 
 			Dim messageService As New MessageService()
@@ -12,19 +15,19 @@ Namespace kCura.WinEDDS.Monitoring
 			messageService.Subscribe(Of TransferJobStartedMessage)(
 				Sub(message)
 					Dim keplerManager As IMetricsManager = messageManagerFactory.CreateSUMKeplerManager(serviceFactory)
-					keplerManager.LogCount($"RDC.Performance.JobStartedCount.{message.JobType}.{message.TransferMode}", 1)
+					keplerManager.LogCount($"{PerformancePrefix}.JobStartedCount.{message.JobType}.{message.TransferMode}", 1)
 				End Sub)
 
 			messageService.Subscribe(Of TransferJobCompletedMessage)(
 				Sub(message)
 					Dim keplerManager As IMetricsManager = messageManagerFactory.CreateSUMKeplerManager(serviceFactory)
-					keplerManager.LogCount($"RDC.Performance.JobCompletedCount.{message.JobType}.{message.TransferMode}", 1)
+					keplerManager.LogCount($"{PerformancePrefix}.JobCompletedCount.{message.JobType}.{message.TransferMode}", 1)
 				End Sub)
 
 			messageService.Subscribe(Of TransferJobFailedMessage)(
 				Sub(message)
 					Dim keplerManager As IMetricsManager = messageManagerFactory.CreateSUMKeplerManager(serviceFactory)
-					keplerManager.LogCount($"RDC.Performance.JobFailedCount.{message.JobType}.{message.TransferMode}", 1)
+					keplerManager.LogCount($"{PerformancePrefix}.JobFailedCount.{message.JobType}.{message.TransferMode}", 1)
 				End Sub)
 			Return messageService
 		End Function
