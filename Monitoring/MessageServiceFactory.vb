f2bc8f1bf54cfeb727983faec453bd6d3da3b1f6
Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
2018-05-15 11:09:11 +0200

    Update

--- Monitoring/MessageServiceFactory.vb
+++ Monitoring/MessageServiceFactory.vb
@@ -6,6 +6,7 @@ Namespace kCura.WinEDDS.Monitoring
 	Public Class MessageServiceFactory
 
 		Private Shared ReadOnly UsagePrefix As String = "RDC.Usage"
+		Private Shared ReadOnly PerformancePrefix As String = "RDC.Performance"
 		Private Shared ReadOnly MessageService As New MessageService()
 		Private Shared ReadOnly MessageManagerFactory As New MetricsManagerFactory()
 
@@ -13,39 +14,44 @@ Namespace kCura.WinEDDS.Monitoring
 
 			MessageService.Subscribe(Of TransferJobStartedMessage)(
 				Sub(message)
-					LogCount(serviceFactory, FormatBucketName("JobStartedCount", message.JobType, message.TransferMode), 1)
+					LogCount(serviceFactory, FormatPerformanceBucketName("JobStartedCount", message.JobType, message.TransferMode), 1)
 				End Sub)
 
 			MessageService.Subscribe(Of TransferJobCompletedMessage)(
 				Sub(message)
-					LogCount(serviceFactory, FormatBucketName("JobCompletedCount", message.JobType, message.TransferMode), 1)
+					LogCount(serviceFactory, FormatPerformanceBucketName("JobCompletedCount", message.JobType, message.TransferMode), 1)
 				End Sub)
 
 			MessageService.Subscribe(Of TransferJobFailedMessage)(
 				Sub(message)
-					LogCount(serviceFactory, FormatBucketName("JobFailedCount", message.JobType, message.TransferMode), 1)
+					LogCount(serviceFactory, FormatPerformanceBucketName("JobFailedCount", message.JobType, message.TransferMode), 1)
 				End Sub)
+
 
 			MessageService.Subscribe(Of TransferJobThroughputMessage)(
 				Sub(message)
-					LogDouble(serviceFactory, FormatBucketName("Throughput", message.JobType, message.TransferMode), message.RecordsPerSecond)
+					LogDouble(serviceFactory, FormatUsageBucketName("Throughput", message.JobType, message.TransferMode), message.RecordsPerSecond)
 				End Sub)
 
 			MessageService.Subscribe(Of TransferJobTotalRecordsCountMessage)(
 				Sub(message)
-					LogDouble(serviceFactory, FormatBucketName("TotalRecordsCount", message.JobType, message.TransferMode), message.TotalRecords)
+					LogDouble(serviceFactory, FormatUsageBucketName("TotalRecordsCount", message.JobType, message.TransferMode), message.TotalRecords)
 				End Sub)
 
 			MessageService.Subscribe(Of TransferJobCompletedRecordsCountMessage)(
 				Sub(message)
-					LogDouble(serviceFactory, FormatBucketName("CompletedRecordsCount", message.JobType, message.TransferMode), message.CompletedRecords)
+					LogDouble(serviceFactory, FormatUsageBucketName("CompletedRecordsCount", message.JobType, message.TransferMode), message.CompletedRecords)
 				End Sub)
 
-			Return messageService
+			Return MessageService
 		End Function
 
-		Private Shared Function FormatBucketName(metricName As String, jobType As String, transferMode As String) As String
+		Private Shared Function FormatUsageBucketName(metricName As String, jobType As String, transferMode As String) As String
 			Return $"{UsagePrefix}.{metricName}.{jobType}.{transferMode}"
+		End Function
+
+		Private Shared Function FormatPerformanceBucketName(metricName As String, jobType As String, transferMode As String) As String
+			Return $"{PerformancePrefix}.{metricName}.{jobType}.{transferMode}"
 		End Function
 
 		Private Shared Sub LogCount(serviceFactory As IServiceFactory, bucketName As String, value As Long)
