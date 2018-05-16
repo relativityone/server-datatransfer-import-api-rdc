84d905dacd7df9da59d17b42b38d9e278f8a9651
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2018-05-16 16:24:52 +0200

    Moved Message box inside Advanced file location control

--- Forms/AdvancedFileLocation.vb
+++ Forms/AdvancedFileLocation.vb
@@ -1,3 +1,5 @@
+Imports Relativity.Transfer
+
 Namespace kCura.EDDS.WinForm
 	Public Class AdvancedFileLocation
 		Inherits System.Windows.Forms.Form
@@ -11,11 +13,10 @@ Namespace kCura.EDDS.WinForm
 			InitializeComponent()
 
 			'Add any initialization after the InitializeComponent() call
-			_application = kCura.EDDS.WinForm.Application.Instance
 			_repositories.Items.Clear()
-			If Not _application.DocumentRepositoryList Is Nothing AndAlso _application.DocumentRepositoryList.Length > 0 Then
-				Dim paths(_application.DocumentRepositoryList.Length - 1) As String
-				System.Array.Copy(_application.DocumentRepositoryList, paths, _application.DocumentRepositoryList.Length)
+			If Not kCura.EDDS.WinForm.Application.Instance.DocumentRepositoryList Is Nothing AndAlso kCura.EDDS.WinForm.Application.Instance.DocumentRepositoryList.Length > 0 Then
+				Dim paths(kCura.EDDS.WinForm.Application.Instance.DocumentRepositoryList.Length - 1) As String
+				System.Array.Copy(kCura.EDDS.WinForm.Application.Instance.DocumentRepositoryList, paths, kCura.EDDS.WinForm.Application.Instance.DocumentRepositoryList.Length)
 				System.Array.Sort(paths)
 				_repositories.Items.AddRange(paths)
 			End If
@@ -44,117 +45,139 @@ Namespace kCura.EDDS.WinForm
 		Friend WithEvents _defaultButton As System.Windows.Forms.Button
 		Friend WithEvents _repositories As System.Windows.Forms.ComboBox
 		Friend WithEvents _copyNativeFiles As System.Windows.Forms.RadioButton
-		Friend WithEvents _keepNativeFiles As System.Windows.Forms.RadioButton
+        Friend WithEvents asperaModeWarningLinkLabel As LinkLabel
+        Friend WithEvents _keepNativeFiles As System.Windows.Forms.RadioButton
 		<System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
-			Dim resources As System.Resources.ResourceManager = New System.Resources.ResourceManager(GetType(AdvancedFileLocation))
-			Me._cancelButton = New System.Windows.Forms.Button
-			Me.Label1 = New System.Windows.Forms.Label
-			Me._repositories = New System.Windows.Forms.ComboBox
-			Me._okButton = New System.Windows.Forms.Button
-			Me._defaultButton = New System.Windows.Forms.Button
-			Me._copyNativeFiles = New System.Windows.Forms.RadioButton
-			Me._keepNativeFiles = New System.Windows.Forms.RadioButton
-			Me.SuspendLayout()
-			'
-			'_cancelButton
-			'
-			Me._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
-			Me._cancelButton.Location = New System.Drawing.Point(548, 104)
-			Me._cancelButton.Name = "_cancelButton"
-			Me._cancelButton.TabIndex = 3
-			Me._cancelButton.Text = "Cancel"
-			'
-			'Label1
-			'
-			Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-			Me.Label1.Location = New System.Drawing.Point(12, 52)
-			Me.Label1.Name = "Label1"
-			Me.Label1.Size = New System.Drawing.Size(132, 16)
-			Me.Label1.TabIndex = 4
-			Me.Label1.Text = "Document Repository"
-			'
-			'_repositories
-			'
-			Me._repositories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-			Me._repositories.Location = New System.Drawing.Point(12, 72)
-			Me._repositories.Name = "_repositories"
-			Me._repositories.Size = New System.Drawing.Size(560, 21)
-			Me._repositories.TabIndex = 9
-			'
-			'_okButton
-			'
-			Me._okButton.Location = New System.Drawing.Point(468, 104)
-			Me._okButton.Name = "_okButton"
-			Me._okButton.TabIndex = 2
-			Me._okButton.Text = "OK"
-			'
-			'_defaultButton
-			'
-			Me._defaultButton.Location = New System.Drawing.Point(572, 72)
-			Me._defaultButton.Name = "_defaultButton"
-			Me._defaultButton.Size = New System.Drawing.Size(50, 21)
-			Me._defaultButton.TabIndex = 10
-			Me._defaultButton.Text = "Default"
-			Me._defaultButton.TextAlign = System.Drawing.ContentAlignment.TopLeft
-			'
-			'_copyNativeFiles
-			'
-			Me._copyNativeFiles.Checked = True
-			Me._copyNativeFiles.Location = New System.Drawing.Point(12, 4)
-			Me._copyNativeFiles.Name = "_copyNativeFiles"
-			Me._copyNativeFiles.Size = New System.Drawing.Size(384, 24)
-			Me._copyNativeFiles.TabIndex = 11
-			Me._copyNativeFiles.TabStop = True
-			Me._copyNativeFiles.Text = "Copy files from current location to selected document repository"
-			'
-			'_keepNativeFiles
-			'
-			Me._keepNativeFiles.CheckAlign = System.Drawing.ContentAlignment.TopLeft
-			Me._keepNativeFiles.Location = New System.Drawing.Point(12, 28)
-			Me._keepNativeFiles.Name = "_keepNativeFiles"
-			Me._keepNativeFiles.Size = New System.Drawing.Size(612, 24)
-			Me._keepNativeFiles.TabIndex = 12
-			Me._keepNativeFiles.Text = "Do not copy files to a Relativity Document Repository. Files already reside in a " & _
-			"valid and Relativity-accessible location"
-			Me._keepNativeFiles.TextAlign = System.Drawing.ContentAlignment.TopLeft
-			'
-			'AdvancedFileLocation
-			'
-			Me.AcceptButton = Me._okButton
-			Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
-			Me.CancelButton = Me._cancelButton
-			Me.ClientSize = New System.Drawing.Size(628, 133)
-			Me.Controls.Add(Me._keepNativeFiles)
-			Me.Controls.Add(Me._copyNativeFiles)
-			Me.Controls.Add(Me._defaultButton)
-			Me.Controls.Add(Me._repositories)
-			Me.Controls.Add(Me.Label1)
-			Me.Controls.Add(Me._cancelButton)
-			Me.Controls.Add(Me._okButton)
-			Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-			Me.MaximizeBox = False
-			Me.MinimizeBox = False
-			Me.Name = "AdvancedFileLocation"
-			Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
-			Me.Text = "File Repository Preferences"
-			Me.ResumeLayout(False)
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AdvancedFileLocation))
+        Me._cancelButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me._repositories = New System.Windows.Forms.ComboBox()
+        Me._okButton = New System.Windows.Forms.Button()
+        Me._defaultButton = New System.Windows.Forms.Button()
+        Me._copyNativeFiles = New System.Windows.Forms.RadioButton()
+        Me._keepNativeFiles = New System.Windows.Forms.RadioButton()
+        Me.asperaModeWarningLinkLabel = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout
+        '
+        '_cancelButton
+        '
+        Me._cancelButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me._cancelButton.Location = New System.Drawing.Point(548, 127)
+        Me._cancelButton.Name = "_cancelButton"
+        Me._cancelButton.Size = New System.Drawing.Size(75, 23)
+        Me._cancelButton.TabIndex = 3
+        Me._cancelButton.Text = "Cancel"
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(132, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Document Repository"
+        '
+        '_repositories
+        '
+        Me._repositories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me._repositories.Location = New System.Drawing.Point(12, 72)
+        Me._repositories.Name = "_repositories"
+        Me._repositories.Size = New System.Drawing.Size(560, 21)
+        Me._repositories.TabIndex = 9
+        '
+        '_okButton
+        '
+        Me._okButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me._okButton.Location = New System.Drawing.Point(468, 127)
+        Me._okButton.Name = "_okButton"
+        Me._okButton.Size = New System.Drawing.Size(75, 23)
+        Me._okButton.TabIndex = 2
+        Me._okButton.Text = "OK"
+        '
+        '_defaultButton
+        '
+        Me._defaultButton.Location = New System.Drawing.Point(572, 72)
+        Me._defaultButton.Name = "_defaultButton"
+        Me._defaultButton.Size = New System.Drawing.Size(50, 21)
+        Me._defaultButton.TabIndex = 10
+        Me._defaultButton.Text = "Default"
+        Me._defaultButton.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        '
+        '_copyNativeFiles
+        '
+        Me._copyNativeFiles.Checked = true
+        Me._copyNativeFiles.Location = New System.Drawing.Point(12, 4)
+        Me._copyNativeFiles.Name = "_copyNativeFiles"
+        Me._copyNativeFiles.Size = New System.Drawing.Size(384, 24)
+        Me._copyNativeFiles.TabIndex = 11
+        Me._copyNativeFiles.TabStop = true
+        Me._copyNativeFiles.Text = "Copy files from current location to selected document repository"
+        '
+        '_keepNativeFiles
+        '
+        Me._keepNativeFiles.CheckAlign = System.Drawing.ContentAlignment.TopLeft
+        Me._keepNativeFiles.Location = New System.Drawing.Point(12, 28)
+        Me._keepNativeFiles.Name = "_keepNativeFiles"
+        Me._keepNativeFiles.Size = New System.Drawing.Size(612, 24)
+        Me._keepNativeFiles.TabIndex = 12
+        Me._keepNativeFiles.Text = "Do not copy files to a Relativity Document Repository. Files already reside in a "& _ 
+    "valid and Relativity-accessible location"
+        Me._keepNativeFiles.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        '
+        'asperaModeWarningLinkLabel
+        '
+        Me.asperaModeWarningLinkLabel.AutoSize = true
+        Me.asperaModeWarningLinkLabel.LinkColor = System.Drawing.Color.Blue
+        Me.asperaModeWarningLinkLabel.Location = New System.Drawing.Point(12, 96)
+        Me.asperaModeWarningLinkLabel.Name = "asperaModeWarningLinkLabel"
+        Me.asperaModeWarningLinkLabel.Size = New System.Drawing.Size(440, 13)
+        Me.asperaModeWarningLinkLabel.TabIndex = 13
+        Me.asperaModeWarningLinkLabel.TabStop = true
+        Me.asperaModeWarningLinkLabel.Text = "Repository settings cannot be changed when RDC runs in Aspera mode. Click to read"& _ 
+    " more."
+        '
+        'AdvancedFileLocation
+        '
+        Me.AcceptButton = Me._okButton
+        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+        Me.CancelButton = Me._cancelButton
+        Me.ClientSize = New System.Drawing.Size(628, 156)
+        Me.Controls.Add(Me.asperaModeWarningLinkLabel)
+        Me.Controls.Add(Me._keepNativeFiles)
+        Me.Controls.Add(Me._copyNativeFiles)
+        Me.Controls.Add(Me._defaultButton)
+        Me.Controls.Add(Me._repositories)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me._cancelButton)
+        Me.Controls.Add(Me._okButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
+        Me.MaximizeBox = false
+        Me.MinimizeBox = false
+        Me.Name = "AdvancedFileLocation"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "File Repository Preferences"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
 
-		End Sub
+End Sub
 
 #End Region
 
-		Private WithEvents _application As kCura.EDDS.WinForm.Application
 		Friend SelectDefaultPath As Boolean = True
-		Private Sub _includeOriginals_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
-			_repositories.Enabled = _copyNativeFiles.Checked
-		End Sub
 
 		Private Sub AdvancedFileLocation_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
-			If Me.SelectDefaultPath Then Me.SelectPath(_application.SelectedCaseInfo.DocumentPath)
+            SetRepositoriesComboBoxAccessibility()
+            _asperaModeWarningLinkLabel.Visible = IsUsingAsperaConnectionMode()
 
-			Config.FlushEddsConfigSettings()
-			_keepNativeFiles.Visible = Not Config.CloudInstance
-		End Sub
+		    If Me.SelectDefaultPath Then
+		        Me.SelectPath(kCura.EDDS.WinForm.Application.Instance.SelectedCaseInfo.DocumentPath)
+		    End If
+
+		    Config.FlushEddsConfigSettings()
+		    _keepNativeFiles.Visible = Not Config.CloudInstance
+        End Sub
 
 		Public Event FileLocationOK(ByVal copyFiles As Boolean, ByVal selectedRepository As String)
 
@@ -181,25 +204,40 @@ Namespace kCura.EDDS.WinForm
 						Exit For
 					End If
 				Next			'
-				If Not foundIt AndAlso path.ToLower <> _application.SelectedCaseInfo.DocumentPath.ToLower Then
-					Me.SelectPath(_application.SelectedCaseInfo.DocumentPath)
+				If Not foundIt AndAlso path.ToLower <> kCura.EDDS.WinForm.Application.Instance.SelectedCaseInfo.DocumentPath.ToLower Then
+					Me.SelectPath(kCura.EDDS.WinForm.Application.Instance.SelectedCaseInfo.DocumentPath)
 				End If
 			End If
 		End Sub
 
+        Public Sub SetRepositoriesComboBoxAccessibility()
+            If IsUsingAsperaConnectionMode() Then
+                _repositories.Enabled = False
+            Else
+                _repositories.Enabled = _copyNativeFiles.Checked
+            End If
+        End Sub
+
+		Private Function IsUsingAsperaConnectionMode() As Boolean
+
+		    Dim clientTransferGuid As Guid = kCura.EDDS.WinForm.Application.Instance.GetConnectionMode().ConfigureAwait(False).GetAwaiter().GetResult()
+		    Return clientTransferGuid = Guid.Parse(TransferClientConstants.AsperaClientId)
+		End Function
+
 		Private Sub _defaultButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _defaultButton.Click
-			Me.SelectPath(_application.SelectedCaseInfo.DocumentPath)
+			Me.SelectPath(kCura.EDDS.WinForm.Application.Instance.SelectedCaseInfo.DocumentPath)
 		End Sub
 
 		Private Sub _keepNativeFiles_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _keepNativeFiles.CheckedChanged, _copyNativeFiles.CheckedChanged
-			_repositories.Enabled = _copyNativeFiles.Checked
-			If _keepNativeFiles.Checked Then
-				_copyNativeFiles.Checked = False
-			End If
-			If _copyNativeFiles.Checked Then
-				_keepNativeFiles.Checked = False
-			End If
+		    SetRepositoriesComboBoxAccessibility()
 		End Sub
-	End Class
+
+        Private Sub AsperaModeWarningLinkLabel_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles asperaModeWarningLinkLabel.LinkClicked
+            MessageBox.Show(Me, "Repository settings cannot be changed when RDC runs in Aspera transfer mode. Aspera Server is " &
+                "configured to upload native files to the default workspace fileshare location. " &
+                "If you want to switch Repository in RDC you need to first change RDC settings in application configuration file to force Web transfer mode. " &
+                "Please note that Web transfer is considerably slower than Aspera", "Relativity Desktop Client", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Sub
+    End Class
 
 End Namespace
