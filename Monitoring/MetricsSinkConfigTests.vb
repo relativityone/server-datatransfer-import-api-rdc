19d06d66324d710531f222da1dd88da97e5664e1
Paweł Grudzień <pawel.grudzien@relativity.com>
2018-06-21 16:31:16 +0200

    fixed wrong config usage

---
+++ Monitoring/MetricsSinkConfigTests.vb
@@ -0,0 +1,34 @@
+﻿
+Imports kCura.WinEDDS.NUnit.Monitoring.Sinks
+Imports NUnit.Framework
+
+Namespace Monitoring
+	<TestFixture>
+	Public Class WhenConfigIsFresh
+
+		'<Test>
+		'Public Sub DefaultValueForSendSumMetricsIsTrue()
+		'	Dim config As MetricsSinkConfig = New MetricsSinkConfig()
+		'	Assert.True(config.SendSumMetrics)
+		'End Sub
+		
+		'<Test>
+		'Public Sub DefaultValueForSendLiveAPMMetricsIsTrue()
+		'	Dim config As MetricsSinkConfig = New MetricsSinkConfig()
+		'	Assert.True(config.SendLiveAPMMetrics)
+		'End Sub
+		
+		'<Test>
+		'Public Sub DefaultValueForSendSummaryApmMetricsIsTrue()
+		'	Dim config As MetricsSinkConfig = New MetricsSinkConfig()
+		'	Assert.True(config.SendSummaryApmMetrics)
+		'End Sub
+		
+		'<Test>
+		'Public Sub DefaultValueForThrottleTimeoutIsZero()
+		'	Dim config As MetricsSinkConfig = New MetricsSinkConfig()
+		'	Assert.AreEqual(TimeSpan.Zero, config.ThrottleTimeout)
+		'End Sub
+
+	End Class
+End Namespace
