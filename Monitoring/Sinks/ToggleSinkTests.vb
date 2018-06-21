838eebc539b481a1e73f0df451731edaef895a5a
Paweł Grudzień <pawel.grudzien@relativity.com>
2018-06-21 16:36:25 +0200

    code refactored to messageObserver, simplified sink classes, implemented throttle and toggle

--- Monitoring/Sinks/ToggleSinkTests.vb
+++ Monitoring/Sinks/ToggleSinkTests.vb
@@ -1,24 +1,24 @@
 ﻿Imports System.Threading.Tasks
+Imports kCura.WinEDDS
 Imports kCura.WinEDDS.Monitoring
+Imports NSubstitute
 Imports NUnit.Framework
 Imports Relativity.DataTransfer.MessageService
-Imports Relativity.Services.ServiceProxy
-Imports NSubstitute
 Imports Relativity.DataTransfer.MessageService.MetricsManager.APM
+Imports Relativity.Services.ServiceProxy
 
-Namespace kCura.WinEDDS.NUnit.Monitoring.Sinks
-
+Namespace Monitoring.Sinks
 	<TestFixture>
-	Public Class ThrottlerTests
+	Public Class ToggleSinkTests
 		<Test>
 		Public Sub WhenToggleIfOff_ThenDontSendMessages
 			Dim aSink As IMetricSink(Of MockMessage) =  Substitute.For(Of IMetricSink(Of MockMessage))
 
-			Dim subject As IMetricSink(Of MockMessage) = New ThrottledMetricSink(Of MockMessage)(aSink, Function() TimeSpan.FromMinutes(1))
+			Dim subject As IMetricSink(Of MockMessage) = New ToggledMetricSink(Of MockMessage)(aSink, Function() False)
 			subject.OnMessage(New MockMessage())
 			subject.OnMessage(New MockMessage())
 
-			aSink.ReceivedWithAnyArgs(1).OnMessage(Arg.Any(Of MockMessage))
+			aSink.DidNotReceiveWithAnyArgs().OnMessage(Arg.Any(Of MockMessage))
 		End Sub
 
 		Public Class MockMessage
