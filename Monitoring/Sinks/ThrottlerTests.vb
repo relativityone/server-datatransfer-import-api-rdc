b4279e7063e85c6b869e158f94c76b0226ae92c7
Paweł Grudzień <pawel.grudzien@relativity.com>
2018-06-21 16:40:17 +0200

    more tests for throttle and a single fix

--- Monitoring/Sinks/ThrottlerTests.vb
+++ Monitoring/Sinks/ThrottlerTests.vb
@@ -1,5 +1,6 @@
 ﻿
 
+Imports System.Threading
 Imports kCura.WinEDDS.Monitoring
 Imports NSubstitute
 Imports NUnit.Framework
@@ -10,14 +11,47 @@ Namespace Monitoring.Sinks
 	<TestFixture>
 	Public Class ThrottlerTests
 		<Test>
-		Public Sub WhenToggleIfOff_ThenDontSendMessages
+		Public Sub WhenThrottlingIsOn_ThenSendOnlyNotThrottledMessage
 			Dim aSink As IMetricSink(Of MockMessage) =  Substitute.For(Of IMetricSink(Of MockMessage))
 
 			Dim subject As IMetricSink(Of MockMessage) = New ThrottledMetricSink(Of MockMessage)(aSink, Function() TimeSpan.FromMinutes(1))
 			subject.OnMessage(New MockMessage())
 			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
 
 			aSink.ReceivedWithAnyArgs(1).OnMessage(Arg.Any(Of MockMessage))
+		End Sub
+
+		<Test>
+		Public Sub WhenThrottlingIsSetToZero_ThenSendOnlyAllMessages
+			Dim aSink As IMetricSink(Of MockMessage) =  Substitute.For(Of IMetricSink(Of MockMessage))
+
+			Dim subject As ThrottledMetricSink(Of MockMessage) = New ThrottledMetricSink(Of MockMessage)(aSink, Function() TimeSpan.Zero)
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+
+			aSink.ReceivedWithAnyArgs(4).OnMessage(Arg.Any(Of MockMessage))
+		End Sub
+
+		<Test>
+		Public Sub WhenThrottlingIsSet_AndTimeouts_ThePassRemainingMessages
+			Dim aSink As IMetricSink(Of MockMessage) =  Substitute.For(Of IMetricSink(Of MockMessage))
+
+			Dim subject As IMetricSink(Of MockMessage) = New ThrottledMetricSink(Of MockMessage)(aSink, Function() TimeSpan.FromMilliseconds(200))
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			Thread.Sleep(300)
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+			subject.OnMessage(New MockMessage())
+
+			aSink.ReceivedWithAnyArgs(2).OnMessage(Arg.Any(Of MockMessage))
 		End Sub
 
 		Public Class MockMessage
