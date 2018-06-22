24c45322a0d5c06921b3b5143e33195d903ca94b
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2018-06-22 17:37:35 +0200

    Fix problems after merge conflicts

--- Monitoring/Sinks/JobApmEndOfLifeSink.vb
+++ Monitoring/Sinks/JobApmEndOfLifeSink.vb
@@ -6,14 +6,14 @@ Imports Relativity.Services.ServiceProxy
 Namespace kCura.WinEDDS.Monitoring
 	Public Class JobApmEndOfLifeSink
 		Inherits MetricSinkBase
-		Implements IMessageSink(Of TransferJobSizeMessage)
+		Implements IMessageSink(Of TransferJobStatisticsMessage)
 
 		Public Sub New(serviceFactory As IServiceFactory, metricsManagerFactory As IMetricsManagerFactory)
 			MyBase.New(serviceFactory, metricsManagerFactory)
 		End Sub
 
-		Public Sub OnMessage(message As TransferJobSizeMessage) Implements IMessageSink(Of TransferJobSizeMessage).OnMessage
-			LogApmDouble(FormatPerformanceBucketName("JobSize", message.JobType, message.TransferMode), message.JobSize, message)
+		Public Sub OnMessage(message As TransferJobStatisticsMessage) Implements IMessageSink(Of TransferJobStatisticsMessage).OnMessage
+			LogApmDouble($"{PerformancePrefix}.JobStatistics", message.JobSizeInBytes, message)
 		End Sub
 	End Class
 End Namespace
--- Monitoring/Sinks/JobSumEndOfLifeSink.vb
+++ Monitoring/Sinks/JobSumEndOfLifeSink.vb
@@ -9,6 +9,7 @@ Namespace kCura.WinEDDS.Monitoring
 		Implements IMessageSink(Of TransferJobThroughputMessage)
 		Implements IMessageSink(Of TransferJobTotalRecordsCountMessage)
 		Implements IMessageSink(Of TransferJobCompletedRecordsCountMessage)
+		Implements IMessageSink(Of TransferJobStatisticsMessage)
 
 		Public Sub New(serviceFactory As IServiceFactory, metricsManagerFactory As IMetricsManagerFactory)
 			MyBase.New(serviceFactory, metricsManagerFactory)
@@ -16,6 +17,7 @@ Namespace kCura.WinEDDS.Monitoring
 
 		Public Sub OnMessage(message As TransferJobThroughputMessage) Implements IMessageSink(Of TransferJobThroughputMessage).OnMessage
 			LogDouble(FormatPerformanceBucketName("Throughput", message.JobType, message.TransferMode), message.RecordsPerSecond)
+			LogDouble(FormatPerformanceBucketName("ThroughputBytes", message.JobType, message.TransferMode), message.BytesPerSecond)
 		End Sub
 
 		Public Sub OnMessage(message As TransferJobTotalRecordsCountMessage) Implements IMessageSink(Of TransferJobTotalRecordsCountMessage).OnMessage
@@ -24,6 +26,10 @@ Namespace kCura.WinEDDS.Monitoring
 
 		Public Sub OnMessage(message As TransferJobCompletedRecordsCountMessage) Implements IMessageSink(Of TransferJobCompletedRecordsCountMessage).OnMessage
 			LogDouble(FormatUsageBucketName("CompletedRecords", message.JobType, message.TransferMode), message.CompletedRecords)
+		End Sub
+
+		Public Sub OnMessage(message As TransferJobStatisticsMessage) Implements IMessageSink(Of TransferJobStatisticsMessage).OnMessage
+			LogDouble(FormatPerformanceBucketName("JobSize", message.JobType, message.TransferMode), message.JobSizeInBytes)
 		End Sub
 	End Class
 End Namespace
