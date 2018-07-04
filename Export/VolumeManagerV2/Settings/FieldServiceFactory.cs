f3127829bde06b3322cdd1953efc21498377daca
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2018-07-04 18:20:37 +0200

    Reverted the old and implemented the new solution to rename fields feature issue

--- Export/VolumeManagerV2/Settings/FieldServiceFactory.cs
+++ Export/VolumeManagerV2/Settings/FieldServiceFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using kCura.WinEDDS.Exporters;
 using Relativity;
 using Relativity.Logging;
 
@@ -7,22 +8,26 @@ namespace kCura.WinEDDS.Core.Export.VolumeManagerV2.Settings
 {
 	public class FieldServiceFactory
 	{
+		private readonly ILoadFileHeaderFormatter _formatter;
 		private readonly IColumnsOrdinalLookupFactory _ordinalLookupFactory;
 		private readonly IColumnsFactory _columnsFactory;
 		private readonly ILog _logger;
 
-		public FieldServiceFactory(IColumnsOrdinalLookupFactory ordinalLookupFactory, IColumnsFactory columnsFactory, ILog logger)
+		public FieldServiceFactory(ILoadFileHeaderFormatter formatter, IColumnsOrdinalLookupFactory ordinalLookupFactory, IColumnsFactory columnsFactory, ILog logger)
 		{
+			_formatter = formatter;
 			_ordinalLookupFactory = ordinalLookupFactory;
 			_columnsFactory = columnsFactory;
 			_logger = logger;
 		}
 
-		public FieldService Create(ExportFile exportSettings, string[] columnNamesInOrder, string columnsHeader)
+		public FieldService Create(ExportFile exportSettings, string[] columnNamesInOrder)
 		{
 			DecideIfExportingFullText(exportSettings);
 
 			ViewFieldInfo[] columns = _columnsFactory.CreateColumns(exportSettings);
+
+			string columnsHeader = _formatter.GetHeader(columns.ToList());
 
 			Dictionary<string, int> ordinalLookup = _ordinalLookupFactory.CreateOrdinalLookup(exportSettings, columnNamesInOrder);
 
