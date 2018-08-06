63f8462de1f736ac0429092740f8ee6caa12711e
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-08-06 12:41:45 +0200

    Added new form to search saved searches and implemented it

---
+++ Specialized/SearchListSelector.Designer.vb
@@ -0,0 +1,95 @@
+﻿Namespace Specialized
+    <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+    Partial Class SearchListSelector
+        Inherits System.Windows.Forms.Form
+
+        'Form overrides dispose to clean up the component list.
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            Try
+                If disposing AndAlso components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            Finally
+                MyBase.Dispose(disposing)
+            End Try
+        End Sub
+
+        'Required by the Windows Form Designer
+        Private components As System.ComponentModel.IContainer
+
+        'NOTE: The following procedure is required by the Windows Form Designer
+        'It can be modified using the Windows Form Designer.  
+        'Do not modify it using the code editor.
+        <System.Diagnostics.DebuggerStepThrough()>
+        Private Sub InitializeComponent()
+            Me.selectionSearchInput = New System.Windows.Forms.TextBox()
+            Me.selectionListBox = New System.Windows.Forms.ListBox()
+            Me._selectButton = New System.Windows.Forms.Button()
+            Me._cancelButton = New System.Windows.Forms.Button()
+            Me.SuspendLayout()
+            '
+            'selectionSearchInput
+            '
+            Me.selectionSearchInput.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me.selectionSearchInput.Location = New System.Drawing.Point(12, 12)
+            Me.selectionSearchInput.Name = "selectionSearchInput"
+            Me.selectionSearchInput.Size = New System.Drawing.Size(247, 20)
+            Me.selectionSearchInput.TabIndex = 0
+            '
+            'selectionListBox
+            '
+            Me.selectionListBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me.selectionListBox.FormattingEnabled = True
+            Me.selectionListBox.Location = New System.Drawing.Point(12, 38)
+            Me.selectionListBox.Name = "selectionListBox"
+            Me.selectionListBox.Size = New System.Drawing.Size(247, 212)
+            Me.selectionListBox.TabIndex = 1
+            '
+            '_selectButton
+            '
+            Me._selectButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me._selectButton.DialogResult = System.Windows.Forms.DialogResult.OK
+            Me._selectButton.Location = New System.Drawing.Point(103, 255)
+            Me._selectButton.Name = "_selectButton"
+            Me._selectButton.Size = New System.Drawing.Size(75, 23)
+            Me._selectButton.TabIndex = 2
+            Me._selectButton.Text = "Select"
+            Me._selectButton.UseVisualStyleBackColor = True
+            '
+            '_cancelButton
+            '
+            Me._cancelButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+            Me._cancelButton.Location = New System.Drawing.Point(184, 256)
+            Me._cancelButton.Name = "_cancelButton"
+            Me._cancelButton.Size = New System.Drawing.Size(75, 23)
+            Me._cancelButton.TabIndex = 3
+            Me._cancelButton.Text = "Cancel"
+            Me._cancelButton.UseVisualStyleBackColor = True
+            '
+            'SearchListSelector
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(271, 290)
+            Me.Controls.Add(Me._cancelButton)
+            Me.Controls.Add(Me._selectButton)
+            Me.Controls.Add(Me.selectionListBox)
+            Me.Controls.Add(Me.selectionSearchInput)
+            Me.Name = "SearchListSelector"
+            Me.Text = "SearchListSelector"
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+
+        End Sub
+
+        Friend WithEvents selectionSearchInput As Windows.Forms.TextBox
+        Friend WithEvents selectionListBox As Windows.Forms.ListBox
+        Friend WithEvents _selectButton As Windows.Forms.Button
+        Friend WithEvents _cancelButton As Windows.Forms.Button
+    End Class
+End Namespace
---
+++ Specialized/SearchListSelector.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Collections.Generic
+Imports System.Data
+Imports System.Globalization
+Imports System.Linq
+Imports System.Windows.Forms
+
+Namespace Specialized
+
+
+    Public Class SearchListSelector
+        Inherits System.Windows.Forms.Form
+        ReadOnly _masterDt As DataTable
+        Private _currentSelection As Object
+        ReadOnly _ci As CultureInfo = New CultureInfo(CultureInfo.CurrentCulture.ToString())
+        Private Sub SetupListBox()
+            selectionListBox.DataSource = _masterDt
+            selectionListBox.DisplayMember = "Name"
+            selectionListBox.ValueMember = "ArtifactID"
+            _currentSelection = selectionListBox.SelectedValue
+        End Sub
+        Public Sub New(dataTable As DataTable, formName As String)
+
+            ' This call is required by the designer.
+            InitializeComponent()
+            ' Add any initialization after the InitializeComponent() call.
+            _masterDt = dataTable
+            Me.Name = formName
+            SetupListBox()
+        End Sub
+
+        Private Function FilterRowsFromDataTable(sourceDt As DataTable, substring As String) As DataTable
+            If Not String.IsNullOrEmpty(substring) Then
+                Static currentDt As New DataTable
+                currentDt.Clear()
+                currentDt = sourceDt.Clone()
+                Dim items As IEnumerable(Of DataRow) = sourceDt.Select.Where(Function(x As DataRow) x.Item("Name").ToString().StartsWith(substring, True, _ci))
+                For Each dataRow As DataRow In items
+                    currentDt.ImportRow(dataRow)
+                Next
+                Return currentDt
+            End If
+            Return sourceDt
+        End Function
+        Private Sub selectionSearchInput_TextChanged(sender As Object, e As EventArgs) Handles selectionSearchInput.TextChanged
+            selectionListBox.DataSource = FilterRowsFromDataTable(_masterDt, selectionSearchInput.Text)
+        End Sub
+        Private Sub selectionListBox_DoubleClick(sender As Object, e As EventArgs) Handles selectionListBox.MouseDoubleClick
+            If selectionListBox.SelectedIndex > -1 Then
+                DialogResult = DialogResult.OK
+                Close()
+            End If
+        End Sub
+        Private Sub selectionListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles selectionListBox.SelectedIndexChanged
+            _currentSelection = selectionListBox.SelectedValue
+        End Sub
+
+        Public Function GetSelectedVal() As Object
+            Return _currentSelection
+        End Function
+    End Class
+End Namespace
