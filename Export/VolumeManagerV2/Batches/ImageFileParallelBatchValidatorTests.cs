e91c8dfd6952cbff7fd9a353bce5a74d5467e0cb
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-08-13 10:57:21 +0200

    REL-246176 Image files validation

---
+++ Export/VolumeManagerV2/Batches/ImageFileParallelBatchValidatorTests.cs
@@ -0,0 +1,15 @@
+﻿using kCura.WinEDDS.Core.Export.VolumeManagerV2.Batches;
+using NUnit.Framework;
+using Relativity.Logging;
+
+namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Batches
+{
+	[TestFixture]
+	public class ImageFileParallelBatchValidatorTests : ImageFileBatchValidatorTests
+	{
+		protected override IBatchValidator CreateSut()
+		{
+			return new ImageFileParallelBatchValidator(_errorFileWriter.Object, _fileHelper.Object, new NullLogger());
+		}
+	}
+}
