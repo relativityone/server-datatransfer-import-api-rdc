5bb689b4404be16787cd57cbda327cb0b7cbcf05
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-08-13 14:00:09 +0200

    Created new control for searchable list

---
+++ Specialized/SearchableList.Designer.vb
@@ -0,0 +1,74 @@
+﻿Namespace Specialized
+	<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+	Partial Class SearchableList
+		Inherits System.Windows.Forms.UserControl
+
+		'UserControl overrides dispose to clean up the component list.
+		<System.Diagnostics.DebuggerNonUserCode()>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			Try
+				If disposing AndAlso components IsNot Nothing Then
+					components.Dispose()
+				End If
+			Finally
+				MyBase.Dispose(disposing)
+			End Try
+		End Sub
+
+		'Required by the Windows Form Designer
+		Private components As System.ComponentModel.IContainer
+
+		'NOTE: The following procedure is required by the Windows Form Designer
+		'It can be modified using the Windows Form Designer.  
+		'Do not modify it using the code editor.
+		<System.Diagnostics.DebuggerStepThrough()>
+		Private Sub InitializeComponent()
+			Me._textBox = New System.Windows.Forms.TextBox()
+			Me._listBox = New kCura.Windows.Forms.ListBox()
+			Me.SuspendLayout()
+			'
+			'_textBox
+			'
+			Me._textBox.Location = New System.Drawing.Point(0, 0)
+			Me._textBox.Name = "_textBox"
+			Me._textBox.Size = New System.Drawing.Size(144, 20)
+			Me._textBox.TabIndex = 0
+			'
+			'_listBox
+			'
+			Me._listBox.AlternateColors = False
+			Me._listBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+			Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+			Me._listBox.ColumnWidth = 200
+			Me._listBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+			Me._listBox.FormattingEnabled = True
+			Me._listBox.HighlightIndex = -1
+			Me._listBox.HorizontalExtent = 700
+			Me._listBox.HorizontalScrollbar = True
+			Me._listBox.HorizontalScrollOffset = 0
+			Me._listBox.IntegralHeight = False
+			Me._listBox.ItemHeight = 16
+			Me._listBox.Location = New System.Drawing.Point(0, 26)
+			Me._listBox.Name = "_listBox"
+			Me._listBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+			Me._listBox.Size = New System.Drawing.Size(144, 251)
+			Me._listBox.TabIndex = 16
+			Me._listBox.VerticalScrollOffset = 0
+			'
+			'SearchableList
+			'
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.Controls.Add(Me._listBox)
+			Me.Controls.Add(Me._textBox)
+			Me.Name = "SearchableList"
+			Me.Size = New System.Drawing.Size(144, 280)
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+		Friend WithEvents _textBox As Windows.Forms.TextBox
+		Friend WithEvents _listBox As kCura.Windows.Forms.ListBox
+	End Class
+End Namespace
---
+++ Specialized/SearchableList.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Collections
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+Imports Relativity
+
+Namespace Specialized
+	Public Class SearchableList
+		Private _timer As Timer
+		Private _dataSource As New List(Of Object)
+		Private Const _DELAYED_TEXT_CHANGED_TIMEOUT_IN_MILLISECONDS As Integer = 600
+
+		Private Function GetMaxTextLength() As Integer
+			Dim maxLength As Integer = 0
+			For Each element As Object In _dataSource
+				Dim currentLength As Integer = TextRenderer.MeasureText(element.ToString(), _listBox.Font).Width
+				If currentLength > maxLength Then
+					maxLength = currentLength
+				End If
+			Next
+			Return maxLength
+		End Function
+
+		Private Sub ResizeScrollBar()
+			_listBox.HorizontalExtent = GetMaxTextLength()
+		End Sub
+
+		Private Sub selectionSearchInput_TextChanged(sender As Object, e As EventArgs) _
+			Handles _textBox.TextChanged
+			If IsNothing(_timer) Then
+				_timer = New Timer()
+				AddHandler _timer.Tick, AddressOf Timer_Tick
+				_timer.Interval = _DELAYED_TEXT_CHANGED_TIMEOUT_IN_MILLISECONDS
+			Else
+				_timer.Stop()
+
+			End If
+
+			_timer.Start()
+		End Sub
+
+		Private Sub Timer_Tick(sender As Object, e As EventArgs)
+			If Not IsNothing(_timer) Then
+				_timer.Stop()
+			End If
+			Cursor = Cursors.WaitCursor
+			_listBox.DataSource = Filter(_dataSource, _textBox.Text)
+			ResizeScrollBar()
+			Cursor = Cursors.Default
+		End Sub
+
+		Private Function Filter(dataSource As List(Of Object), filterText As String) As List(Of Object)
+			If IsNothing(filterText) Then
+				Return dataSource
+			End If
+			Dim filterTextLower As String = filterText.ToLower
+			Dim items As IEnumerable(Of Object) = From c In dataSource Where c.ToString().ToLower().Contains(filterTextLower) Select c
+			Return items.ToList()
+		End Function
+		Public Sub Initialize(list As ArrayList)
+			_dataSource = list.Cast(Of Object)().ToList()
+			_listBox.DataSource = _dataSource
+			ResizeScrollBar()
+		End Sub
+
+		Private Sub RedrawListBox()
+			_listBox.Refresh()
+		End Sub
+		Private Sub _listBox_Scrolled(sender As Object, e As ScrollEventArgs) Handles _listBox.Scrolled
+			RedrawListBox()
+		End Sub
+
+		Private Sub _listBox_KeyDown(sender As Object, e As KeyEventArgs) Handles _listBox.KeyDown
+			If e.KeyCode = Keys.Left OrElse e.KeyCode = Keys.Right Then
+				RedrawListBox()
+			End If
+		End Sub
+	End Class
+End Namespace
