3270e44bec9db6f537aa0e36a6cbae1bae38e0c1
Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
2018-08-20 13:37:04 +0200

    REL-241303 Performance updates for develop

--- Export/VolumeManagerV2/Batches/LongTextBatchValidatorTests.cs
+++ Export/VolumeManagerV2/Batches/LongTextBatchValidatorTests.cs
@@ -15,47 +15,52 @@ namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Batches
 	[TestFixture]
 	public class LongTextBatchValidatorTests
 	{
-		private LongTextBatchValidator _instance;
-		private Mock<ILongTextRepository> _longTextRepository;
-		private Mock<IFileHelper> _fileHelper;
-		private Mock<IStatus> _status;
+		protected IBatchValidator Instance;
+		protected Mock<ILongTextRepository> LongTextRepository;
+		protected Mock<IFileHelper> FileHelper;
+		protected Mock<IStatus> Status;
 
 		[SetUp]
 		public void SetUp()
 		{
-			_longTextRepository = new Mock<ILongTextRepository>();
-			_fileHelper = new Mock<IFileHelper>();
-			_status = new Mock<IStatus>();
+			LongTextRepository = new Mock<ILongTextRepository>();
+			FileHelper = new Mock<IFileHelper>();
+			Status = new Mock<IStatus>();
 
-			_instance = new LongTextBatchValidator(_longTextRepository.Object, _fileHelper.Object, _status.Object, new NullLogger());
+			Instance = CreateValidator();
+		}
+
+		protected virtual IBatchValidator CreateValidator()
+		{
+			return new LongTextBatchValidator(LongTextRepository.Object, FileHelper.Object, Status.Object, new NullLogger());
 		}
 
 		[Test]
 		public void ItShouldPassForFileRequiringDeletion()
 		{
 			LongText longText = Create("location", true, true);
-			_longTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
+			LongTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
 
 			//ACT
-			_instance.ValidateExportedBatch(null, null, CancellationToken.None);
+			Instance.ValidateExportedBatch(null, null, CancellationToken.None);
 
 			//ASSERT
-			_status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Never());
-			_status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Never());
+			Status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Never());
+			Status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Never());
 		}
 
 		[Test]
 		public void ItShouldPassForFileWithoutExportRequest()
 		{
 			LongText longText = Create("location", true, false);
-			_longTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
+			LongTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
 
 			//ACT
-			_instance.ValidateExportedBatch(null, null, CancellationToken.None);
+			Instance.ValidateExportedBatch(null, null, CancellationToken.None);
 
 			//ASSERT
-			_status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Never());
-			_status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Never());
+			Status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Never());
+			Status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Never());
 		}
 
 		[Test]
@@ -63,16 +68,16 @@ namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Batches
 		{
 			string location = "location";
 			LongText longText = Create(location, false, true);
-			_longTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
+			LongTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
 
-			_fileHelper.Setup(x => x.Exists(location)).Returns(false);
+			FileHelper.Setup(x => x.Exists(location)).Returns(false);
 
 			//ACT
-			_instance.ValidateExportedBatch(null, null, CancellationToken.None);
+			Instance.ValidateExportedBatch(null, null, CancellationToken.None);
 
 			//ASSERT
-			_status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Once);
-			_status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Never());
+			Status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Once);
+			Status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Never());
 		}
 
 		[Test]
@@ -80,17 +85,17 @@ namespace kCura.WinEDDS.Core.NUnit.Export.VolumeManagerV2.Batches
 		{
 			string location = "location";
 			LongText longText = Create(location, false, true);
-			_longTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
+			LongTextRepository.Setup(x => x.GetLongTexts()).Returns(new List<LongText> {longText});
 
-			_fileHelper.Setup(x => x.Exists(location)).Returns(true);
-			_fileHelper.Setup(x => x.GetFileSize(location)).Returns(0);
+			FileHelper.Setup(x => x.Exists(location)).Returns(true);
+			FileHelper.Setup(x => x.GetFileSize(location)).Returns(0);
 
 			//ACT
-			_instance.ValidateExportedBatch(null, null, CancellationToken.None);
+			Instance.ValidateExportedBatch(null, null, CancellationToken.None);
 
 			//ASSERT
-			_status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Never());
-			_status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Once);
+			Status.Verify(x => x.WriteError(It.IsAny<string>()), Times.Never());
+			Status.Verify(x => x.WriteWarning(It.IsAny<string>()), Times.Once);
 		}
 
 		private LongText Create(string location, bool requireDeletion, bool toDownload)
