0d6efc8697357c43e8530d950e1fcbac14ab44a8
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2018-08-21 13:39:06 +0200

    Changed a way of building Kepler URLs
    Cherry pick of REL-244966 from develop/bb3bb1dd3ce

--- ServiceFactoryFactory.vb
+++ ServiceFactoryFactory.vb
@@ -6,10 +6,15 @@ Namespace kCura.WinEDDS
 
 	Public Class ServiceFactoryFactory
 		Public Shared Function Create(credentials As NetworkCredential) As Relativity.Services.ServiceProxy.IServiceFactory
-			Dim relativityCredentials As New BearerTokenCredentials(credentials.Password)
-
-			Dim baseUri As New Uri(Config.WebServiceURL)
-			Dim settings As New ServiceFactorySettings(New Uri($"https://{baseUri.Host}/Relativity.Services"), New Uri($"https://{baseUri.Host}/Relativity.Rest/api"), relativityCredentials)
+			Dim relativityCredentials As Relativity.Services.ServiceProxy.Credentials
+			If credentials.UserName = "XxX_BearerTokenCredentials_XxX" Then
+				relativityCredentials = New BearerTokenCredentials(credentials.Password)
+			Else
+				relativityCredentials = New UsernamePasswordCredentials(credentials.UserName, credentials.Password)
+			End If
+			Dim webServiceUri As New Uri(Config.WebServiceURL)
+			Dim baseUri As New Uri(webServiceUri.GetLeftPart(UriPartial.Authority))
+			Dim settings As New ServiceFactorySettings(New Uri(baseUri, "/Relativity.Services"), New Uri(baseUri, "/Relativity.Rest/api"), relativityCredentials)
 			Dim factory As Relativity.Services.ServiceProxy.IServiceFactory = New ServiceFactory(settings)
 
 			Return factory
