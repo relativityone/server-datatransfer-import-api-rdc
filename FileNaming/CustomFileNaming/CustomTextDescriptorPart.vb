3a23aa27fe67ab5c9ff9f6aecffc88f4902a11fa
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-08-23 14:56:33 +0200

    REL-246787 add custom file naming model

---
+++ FileNaming/CustomFileNaming/CustomTextDescriptorPart.vb
@@ -0,0 +1,12 @@
+﻿Namespace FileNaming.CustomFileNaming
+	<Serializable>
+	Public Class CustomTextDescriptorPart
+		Inherits ValueDescriptorPart
+
+		Public Sub New(ByVal text As String)
+			Me.Value = text
+		End Sub
+
+		Public ReadOnly Property Value As String
+	End Class
+End Namespace
