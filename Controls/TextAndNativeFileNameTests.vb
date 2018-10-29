4c7beb1471a22b7c5aea7e9f0017ecb9c6326593
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-10-29 12:50:05 +0100

    Added test class

---
+++ Controls/TextAndNativeFileNameTests.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Windows.Forms
+Imports kCura.EDDS.WinForm.Forms
+Imports NUnit.Framework
+Namespace kCura.EDDS.WinForm.Tests
+	<TestFixture()> Public Class TextAndNativeFileNameTests
+		Inherits TextAndNativeFileNameForm
+
+		<Test()> Public Sub ShouldReturnFirstItemIfNoMatch()
+
+
+		End Sub
+	End Class
+End Namespace
