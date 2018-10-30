7a11345b0da561d25b59d58c485fc75bc1109458
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-10-30 08:54:15 +0100

    Added more tests and refactored existing ones

--- Controls/TextAndNativeFileNameTests.vb
+++ Controls/TextAndNativeFileNameTests.vb
@@ -5,26 +5,32 @@ Namespace kCura.EDDS.WinForm.Tests
 	<TestFixture()> Public Class TextAndNativeFileNameTests
 		Inherits TextAndNativeFileNameForm
 
-		Dim _items As ComboBox.ObjectCollection
+		Dim _items As ComboBox.ObjectCollection = New ComboBox.ObjectCollection(New ComboBox)
 		<OneTimeSetUp()> Public Sub SetUp()
-			_items = New ComboBox.ObjectCollection(New ComboBox)
 			_items.Add(New FieldSelection("Name1", 10))
 			_items.Add(New FieldSelection("Something", 20))
 			_items.Add(New FieldSelection("Long Name", 30))
 		End Sub
-		<Test()> Public Sub ShouldReturnFirstItemIfNoMatch()
-			Dim result As Integer = GetIndexOfBestMatchFromList(_items, "anything")
-			Assert.AreEqual(0, result)
+
+		<TestCase("anything", 0)>
+		<TestCase("Something", 1)>
+		<TestCase("lon", 2)>
+		Public Sub ShouldReturnProperMatchIndex(str As String, index As Integer)
+			Dim result As Integer = GetIndexOfBestMatchFromList(_items, str)
+			Assert.AreEqual(index, result)
 		End Sub
 
-		<Test()> Public Sub ShouldReturnFirstItemThatMatchesExactly()
-			Dim result As Integer = GetIndexOfBestMatchFromList(_items, "Something")
-			Assert.AreEqual(1, result)
+		<Test()>
+		Public Sub ShouldThrowExceptionIfItemListIstEmpty()
+			Dim itemsList As ComboBox.ObjectCollection = New ComboBox.ObjectCollection(New ComboBox)
+			Dim ex As ArgumentException = Assert.Throws(Of ArgumentException)(Sub() GetIndexOfBestMatchFromList(itemsList, ""))
+			Assert.AreEqual(ex.Message, "The list cannot be empty!")
 		End Sub
 
-		<Test()> Public Sub ShouldReturnFirstItemThatMatchesPartialy()
-			Dim result As Integer = GetIndexOfBestMatchFromList(_items, "lon")
-			Assert.AreEqual(2, result)
+		<Test()>
+		Public Sub ShouldThrowExceptionIfListIsNull()
+			Dim ex As ArgumentException = Assert.Throws(Of ArgumentException)(Sub() GetIndexOfBestMatchFromList(Nothing, ""))
+			Assert.AreEqual(ex.Message, "The list is not initialized!")
 		End Sub
 	End Class
 End Namespace
