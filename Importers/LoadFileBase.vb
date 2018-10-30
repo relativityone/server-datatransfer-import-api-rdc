931a5da8dd1cb2f732827d88f7d7a70c6e23abd3
Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
2018-10-30 14:10:42 +0100

    REL-264967 Extend import process to allow direct usage of SupportedByViewer field

--- Importers/LoadFileBase.vb
+++ Importers/LoadFileBase.vb
@@ -63,7 +63,6 @@ Namespace kCura.WinEDDS
 		Public Property FileSizeMapped() As Boolean
 		Public Property FileSizeColumn() As String
 		Public Property FileNameColumn As String
-		Public Property SupportedByViewerMapped As Boolean
 		Public Property SupportedByViewerColumn As String
 #End Region
 
@@ -145,7 +144,6 @@ Namespace kCura.WinEDDS
 			FileSizeMapped = args.FileSizeMapped
 			FileSizeColumn = args.FileSizeColumn
 			FileNameColumn = args.FileNameColumn
-			SupportedByViewerMapped = args.SupportedByViewerMapped
 			SupportedByViewerColumn = args.SupportedByViewerColumn
 			_timeZoneOffset = timezoneoffset
 			_autoDetect = autoDetect
