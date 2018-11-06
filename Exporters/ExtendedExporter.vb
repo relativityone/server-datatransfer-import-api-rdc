0acf78e2fe908c604bcfeda8b91e1feb62883026
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-11-06 12:56:24 +0100

    Populate native file name view fields for custom file naming

--- Exporters/ExtendedExporter.vb
+++ Exporters/ExtendedExporter.vb
@@ -1,6 +1,6 @@
 ﻿Imports System.Collections.Generic
+Imports FileNaming.CustomFileNaming
 Imports kCura.Windows.Process
-Imports kCura.WinEDDS.Core.Model.Export
 Imports kCura.WinEDDS.Exporters
 Imports kCura.WinEDDS.FileNaming.CustomFileNaming
 Imports kCura.WinEDDS.Service.Export
@@ -26,7 +26,27 @@ Namespace kCura.WinEDDS
 				{.SelectedNativeFileNameViewFields = ExportSettings.SelectedNativesNameViewFields.ToList()}
 		End Function
 
+		Private Shared Function FilterFields(exportFile As ExportFile, fieldsIds As List(Of Integer)) As ViewFieldInfo()
+			Return _
+				exportFile.AllExportableFields.Where(Function(x) fieldsIds.Any(Function(fieldId) fieldId = x.FieldArtifactId)).ToArray().OrderBy(
+					Function(x)
+						Dim index As Integer = fieldsIds.IndexOf(x.FieldArtifactId)
+						Return If(index < 0, Integer.MaxValue, index)
+					End Function).ToArray()
+		End Function
+
+
+		Private Sub PopulateNativeFileNameViewFields()
+			Dim fieldDescriptorParts As IEnumerable(Of FieldDescriptorPart) = If(ExportSettings.CustomFileNaming.DescriptorParts IsNot Nothing, ExportSettings.CustomFileNaming.DescriptorParts.OfType(Of FieldDescriptorPart)(), Enumerable.Empty(Of FieldDescriptorPart)())
+
+			If fieldDescriptorParts.Any() Then
+				ExportSettings.SelectedNativesNameViewFields = FilterFields(ExportSettings, fieldDescriptorParts.[Select](Function(item) item.Value).ToList()).ToList()
+			End If
+		End Sub
+
+
 		Protected Overrides Function GetAvfIds() As List(Of Integer)
+			PopulateNativeFileNameViewFields()
 			Dim selectedAvfIds As HashSet(Of Integer) = New HashSet(Of Integer)(MyBase.GetAvfIds())
 			selectedAvfIds.UnionWith(ExportSettings.SelectedNativesNameViewFields.[Select](Function(item) item.AvfId))
 			Return selectedAvfIds.ToList()
