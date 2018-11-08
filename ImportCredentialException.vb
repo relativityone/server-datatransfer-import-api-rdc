6bed8cfe53b3dca3765d468b3df29056c086301d
Scott Parillo <scott.parillo@relativity.com>
2018-11-08 20:49:52 +0100

    REL-266229 - Reviewed all Exception derived class types in WinEDDS and addressed all improper serialization misses. - CreateRelativityVersionMismatchException method added to provide a more sensible error message instead of assuming that all usage is w/ the RDC.

--- ImportCredentialException.vb
+++ ImportCredentialException.vb
@@ -1,8 +1,29 @@
-﻿Public Class ImportCredentialException
+﻿''' <summary>
+''' The exception thrown when invalid credentials are found within the credential cache or a failure occurs attempting to login.
+''' </summary>
+<Serializable>
+Public Class ImportCredentialException
 	Inherits System.Exception
 
+	''' <summary>
+	''' Initializes a new instance of the <see cref="ImportCredentialException"/> class.
+	''' </summary>
+	''' <param name="message">
+	''' The error message that explains the reason for the exception.
+	''' </param>
+	''' <param name="userName">
+	''' The user name used that failed.
+	''' </param>
+	''' <param name="URL">
+	''' The web service URL used that failed.
+	''' </param>
 	Public Sub New(ByVal message As String, ByVal userName As String, ByVal URL As String)
 		MyBase.new(String.Format("{0}" + vbCrLf + "Username: {1}" + vbCrLf + "URL: {2}", message, userName, URL))
+	End Sub
 
+	''' <inheritdoc />
+	<System.Security.Permissions.SecurityPermissionAttribute(System.Security.Permissions.SecurityAction.Demand, SerializationFormatter:=True)>
+	Protected Sub New(ByVal info As System.Runtime.Serialization.SerializationInfo, ByVal context As System.Runtime.Serialization.StreamingContext)
+		MyBase.New(info, context)
 	End Sub
 End Class
--- ImportSettingsException.vb
+++ ImportSettingsException.vb
@@ -1,59 +1,124 @@
-﻿
-
+﻿''' <summary>
+''' The exception thrown when attempting to set a configurable setting with an invalid or out of range value.
+''' </summary>
 Public Class ImportSettingsException
 	Inherits Exception
 
-	Private _setting As String
-	Private _additionalInfo As String
-
-	Public Sub New(ByVal SettingName As String)
-		Me.New(SettingName, String.Empty)
+	''' <summary>
+	''' Initializes a new instance of the <see cref="ImportSettingsException"/> class.
+	''' </summary>
+	''' <param name="setting">
+	''' The invalid configurable setting name.
+	''' </param>
+	Public Sub New(ByVal setting As String)
+		Me.New(setting, String.Empty)
 	End Sub
 
-	Public Sub New(ByVal SettingName As String, ByVal AdditionalInfo As String)
-		MyBase.new(GenerateMessage(SettingName, AdditionalInfo))
-		_setting = Setting
-		_additionalInfo = AdditionalInfo
+	''' <summary>
+	''' Initializes a new instance of the <see cref="ImportSettingsException"/> class.
+	''' </summary>
+	''' <param name="setting">
+	''' The invalid configurable setting name.
+	''' </param>
+	''' <param name="additionalInfo">
+	''' Additional information about the invalid configuration setting value.
+	''' </param>
+	Public Sub New(ByVal setting As String, ByVal additionalInfo As String)
+		MyBase.new(GenerateMessage(setting, additionalInfo))
+		Me.Setting = setting
+		Me.AdditionalInfo = additionalInfo
 	End Sub
 
+	''' <inheritdoc />
+	<System.Security.Permissions.SecurityPermissionAttribute(System.Security.Permissions.SecurityAction.Demand, SerializationFormatter:=True)>
+	Protected Sub New(ByVal info As System.Runtime.Serialization.SerializationInfo, ByVal context As System.Runtime.Serialization.StreamingContext)
+		MyBase.New(info, context)
+		Me.Setting = info.GetString("Setting")
+		Me.AdditionalInfo = info.GetString("AdditionalInfo")
+	End Sub
+
+	''' <summary>
+	''' Gets the invalid configurable setting name.
+	''' </summary>
+	''' <value>
+	''' The setting name.
+	''' </value>
 	Public ReadOnly Property Setting As String
-		Get
-			Return _setting
-		End Get
-	End Property
 
+	''' <summary>
+	''' Gets additional information about the invalid configuration setting value.
+	''' </summary>
+	''' <value>
+	''' Additional information.
+	''' </value>
 	Public ReadOnly Property AdditionalInfo As String
-		Get
-			Return _additionalInfo
-		End Get
-	End Property
 
-	Private Shared Function GenerateMessage(ByVal settingName As String, ByVal additionalInfoString As String) As String
-		Return String.Format("{0} must be set.  {1}", settingName, additionalInfoString)
+	''' <inheritdoc />
+	<System.Security.Permissions.SecurityPermissionAttribute(System.Security.Permissions.SecurityAction.Demand, SerializationFormatter:=True)>
+	Public Overrides Sub GetObjectData(info As System.Runtime.Serialization.SerializationInfo, context As System.Runtime.Serialization.StreamingContext)
+		info.AddValue("Setting", Me.Setting)
+		info.AddValue("AdditionalInfo", Me.AdditionalInfo)
+		MyBase.GetObjectData(info, context)
+	End Sub
+
+	Private Shared Function GenerateMessage(ByVal setting As String, ByVal additionalInfo As String) As String
+		Return String.Format("{0} must be set.  {1}", setting, additionalInfo)
 	End Function
 End Class
 
+''' <summary>
+''' The exception thrown when attempting to set a configurable setting with an invalid or out of range value due to a conflict with another configurable setting.
+''' </summary>
 Public Class ImportSettingsConflictException
 	Inherits Exception
 
-	Private _setting As String
-	Private _conflictSetting As String
-
+	''' <summary>
+	''' Initializes a new instance of the <see cref="ImportSettingsConflictException"/> class.
+	''' </summary>
+	''' <param name="setting">
+	''' The invalid configurable setting name.
+	''' </param>
+	''' <param name="conflictingSetting">
+	''' The conflicting configurable setting name.
+	''' </param>
+	''' <param name="message">
+	''' The error message that explains the reason for the exception.
+	''' </param>
 	Public Sub New(ByVal setting As String, ByVal conflictingSetting As String, ByVal message As String)
 		MyBase.New(message)
-		_setting = setting
-		_conflictSetting = conflictingSetting
+		Me.Setting = setting
+		Me.ConflictSetting = conflictingSetting
 	End Sub
 
-	Public ReadOnly Property Setting As String
-		Get
-			Return _setting
-		End Get
-	End Property
+	''' <inheritdoc />
+	<System.Security.Permissions.SecurityPermissionAttribute(System.Security.Permissions.SecurityAction.Demand, SerializationFormatter:=True)>
+	Protected Sub New(ByVal info As System.Runtime.Serialization.SerializationInfo, ByVal context As System.Runtime.Serialization.StreamingContext)
+		MyBase.New(info, context)
+		Me.Setting = info.GetString("Setting")
+		Me.ConflictSetting = info.GetString("ConflictSetting")
+	End Sub
 
+	''' <summary>
+	''' Gets the invalid configurable setting name.
+	''' </summary>
+	''' <value>
+	''' The setting name.
+	''' </value>
+	Public ReadOnly Property Setting As String
+
+	''' <summary>
+	''' Gets the conflicting configurable setting name.
+	''' </summary>
+	''' <value>
+	''' The setting name.
+	''' </value>
 	Public ReadOnly Property ConflictSetting As String
-		Get
-			Return _conflictSetting
-		End Get
-	End Property
+
+	''' <inheritdoc />
+	<System.Security.Permissions.SecurityPermissionAttribute(System.Security.Permissions.SecurityAction.Demand, SerializationFormatter:=True)>
+	Public Overrides Sub GetObjectData(info As System.Runtime.Serialization.SerializationInfo, context As System.Runtime.Serialization.StreamingContext)
+		info.AddValue("Setting", Me.Setting)
+		info.AddValue("ConflictSetting", Me.ConflictSetting)
+		MyBase.GetObjectData(info, context)
+	End Sub
 End Class
