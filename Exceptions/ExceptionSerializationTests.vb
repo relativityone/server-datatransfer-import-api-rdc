9382efa1f9b3946701f9945740786326028754d2
Scott Parillo <scott.parillo@relativity.com>
2018-11-09 18:47:15 +0100

    REL-266229 - Unit tests added to validate all exceptions modified to support serialization. - Resolved missing GetObjectData methods.

---
+++ Exceptions/ExceptionSerializationTests.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+Imports System.Runtime.Serialization
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Imports NUnit.Framework
+
+Namespace kCura.Relativity.DataReaderClient.NUnit.Exceptions
+
+	<TestFixture()>
+	Public Class ExceptionSerializationTests
+
+		<Test>
+		Public Sub ItShouldSerializeAndDeserializeTheDataReaderClientException()
+	        SerializeAndDeserialize(new ImportCredentialException("message", "username", "url"))
+			SerializeAndDeserialize(new ImportSettingsException("setting", "additionalInfo"))
+			SerializeAndDeserialize(new ImportSettingsConflictException("setting", "conflictingSettings", "message"))
+		End Sub	
+
+		Private Sub SerializeAndDeserialize(ByVal exception As System.Exception)
+			Dim formatter As IFormatter = New BinaryFormatter()
+			Using stream As MemoryStream = New MemoryStream()
+				formatter.Serialize(stream, exception)
+				stream.Seek(0, SeekOrigin.Begin)
+				Dim deserializedException As Exception = CType(formatter.Deserialize(stream), Exception)
+				Assert.IsNotNull(deserializedException)
+			End Using
+		End Sub
+	End Class
+End Namespace
