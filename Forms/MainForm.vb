754c45bf55fc62c7fd759b2bafee492df40b87da
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-11-14 15:33:14 +0100

    Centered rdc on screen on launch

--- Forms/MainForm.vb
+++ Forms/MainForm.vb
@@ -444,6 +444,7 @@ End Sub
 
             ServicePointManager.DefaultConnectionLimit = Environment.ProcessorCount * 12
             LoadWindowSize()
+            Me.CenterToScreen()
             Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
             _application.TemporaryForceFolderPreview = kCura.WinEDDS.Config.ForceFolderPreview
             If kCura.WinEDDS.Config.WebServiceURL = String.Empty Then
