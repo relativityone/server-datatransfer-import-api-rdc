f2e4d570d80e60cf1dd0fe46d0e2201ef7bb1d9e
Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
2018-11-19 14:59:41 +0100

    REL-272642 RDC throwing errors when native file path is empty

--- Helpers/CaseInsensitiveFilePathHelperTests.vb
+++ Helpers/CaseInsensitiveFilePathHelperTests.vb
@@ -1,11 +1,12 @@
 ﻿Imports kCura.WinEDDS.Helpers
 Imports NSubstitute
+Imports NSubstitute.ExceptionExtensions
 Imports NUnit.Framework
 
 Namespace kCura.WinEDDS.NUnit.Helpers
 	<TestFixture>
 	Public Class CaseInsensitiveFilePathHelperTests
-		Private _filePathHelper As CaseSensitiveFilePathHelper
+		Private _filePathHelper As CaseInsensitiveFilePathHelper
 		Private _systemIoMock As ISystemIoFileWrapper
 
 		Private Const _SAMPLE_PATH As String = "\\dir\somePath.ext"
@@ -15,17 +16,30 @@ Namespace kCura.WinEDDS.NUnit.Helpers
 			_systemIoMock.GetExtension(Arg.Any(Of String)).ReturnsForAnyArgs(function(info) System.IO.Path.GetExtension(info.Arg(Of String)()))
 			_systemIoMock.ChangeExtension(Arg.Any(Of String), Arg.Any(Of String)).ReturnsForAnyArgs(function(info) System.IO.Path.ChangeExtension(info.ArgAt(Of String)(0), info.ArgAt(Of String)(1)))
 
-			_filePathHelper = New CaseSensitiveFilePathHelper(_systemIoMock)
+			_filePathHelper = New CaseInsensitiveFilePathHelper(_systemIoMock)
 		End Sub
 
 		<Test>
-		Public Sub ShouldThrowWhenArgumentIsNothing()
-			Assert.Throws(Of ArgumentException)(sub() _filePathHelper.GetExistingFilePath(Nothing))
+		Public Sub ShouldReturnNothingWhenArgumentIsNothing()
+			Dim actual As String = _filePathHelper.GetExistingFilePath(Nothing)
+
+			Assert.IsNull(actual)
 		End Sub
 
 		<Test>
-		Public Sub ShouldThrowWhenArgumentIsEmpty()
-			Assert.Throws(Of ArgumentException)(sub() _filePathHelper.GetExistingFilePath(String.Empty))
+		Public Sub ShouldReturnNothingWhenArgumentIsEmpty()
+			Dim actual As String = _filePathHelper.GetExistingFilePath(String.Empty)
+
+			Assert.IsNull(actual)
+		End Sub
+
+		<Test>
+		Public Sub ShouldReturnNothingWhenWrapperThrows()
+			_systemIoMock.Exists(Arg.Any(Of String)()).Throws(New Exception())
+
+			Dim actual As String = _filePathHelper.GetExistingFilePath(Nothing)
+
+			Assert.IsNull(actual)
 		End Sub
 
 		<Test>
