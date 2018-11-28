515bf76e88d94fea08793bd4ad29d2d63c0dc707
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-11-28 14:24:46 +0100

    Append original file name to text files

---
+++ Exporters/ObjectExportInfoTests.vb
@@ -0,0 +1,162 @@
+﻿Imports kCura.WinEDDS.Exporters
+Imports NUnit.Framework
+
+<TestFixture> Public Class ObjectExportInfoTests
+	Private Const _IDENTIFIER As String = "AZIPPER003232"
+	Private Const _ORIGINAL_FILE_NAME_XSL As String= "originalFile.xsl"
+	Private Const _ORIGINAL_FILE_NAME_TXT As String= "originalFile.tXT"
+	Private Const _PRODUCTION_BEGIN_BATES As String = "PROD001"
+	Private Const _TEXT_EXTENSION As String = ".txt"
+
+	<Test> Public Sub ItShouldReturnIdentifierValueWhenNameAfterIdentifierAndNotTryProduction()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, False)
+
+		'Assert
+		Dim expectedFileName As String = _IDENTIFIER & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnIdentifierValueWhenNameAfterIdentifierAndTryProductionAndProductionBeginBatesIsEmpty()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = ""
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, True)
+
+		'Assert
+		Dim expectedFileName As String = _IDENTIFIER & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnProductionBeginBatesValueWhenNameAfterIdentifierAndTryProductionAndProductionBeginBatesNotEmpty()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, True)
+
+		'Assert
+		Dim expectedFileName As String = _PRODUCTION_BEGIN_BATES & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnIdentifierValueWhenNotNameAfterIdentifierAndTryProductionAndProductionBeginBatesIsEmpty()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = ""
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(False, True)
+
+		'Assert
+		Dim expectedFileName As String = _IDENTIFIER & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<TestCase(_PRODUCTION_BEGIN_BATES)> <TestCase("")> <TestCase(Nothing)> Public Sub ItShouldReturnProductionBeginBatesValueWhenNotNameAfterIdentifierAndNotTryProduction(productionBeginBates As String)
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = productionBeginBates
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(False, False)
+
+		'Assert
+		Dim expectedFileName As String =If(productionBeginBates, "") &_TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnIdentifierValueWhenNameAfterIdentifierAndNoTryProductionAndIdentifierIsEmpty()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = ""
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, False)
+
+		'Assert
+		Dim expectedFileName As String = _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldAppendOriginalFileNameToIdentifierValue()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, False, True)
+
+		'Assert
+		Dim expectedFileName As String = _IDENTIFIER & "_" & _ORIGINAL_FILE_NAME_XSL & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldReplaceInvalidFieldNameCharacters()
+		'Arrange
+		Const invalidFileName As String = "invali/d"
+		Const expectedOriginalFileName As String = "invali_d"
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = invalidFileName
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, False, True)
+
+		'Assert
+		Dim expectedFileName As String = _IDENTIFIER & "_" & expectedOriginalFileName & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldAppendOriginalFileNameToProductionBeginBatesValue()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_XSL
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(False, True, True)
+
+		'Assert
+		Dim expectedFileName As String = _PRODUCTION_BEGIN_BATES & "_" & _ORIGINAL_FILE_NAME_XSL & _TEXT_EXTENSION
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+
+	<Test> Public Sub ItShouldNotAppendDoubleTextExtension()
+		'Arrange
+		Dim sut As ObjectExportInfo = New ObjectExportInfo()
+		sut.IdentifierValue = _IDENTIFIER
+		sut.OriginalFileName = _ORIGINAL_FILE_NAME_TXT
+		sut.ProductionBeginBates = _PRODUCTION_BEGIN_BATES
+
+		'Act
+		Dim textFileName As String = sut.FullTextFileName(True, False, True)
+
+		'Assert
+		Dim expectedFileName As String = _IDENTIFIER & "_" & _ORIGINAL_FILE_NAME_TXT
+		Assert.AreEqual(expectedFileName, textFileName)
+	End Sub
+End Class
---
+++ Helpers/FileNameHelperTests.vb
@@ -0,0 +1,107 @@
+﻿Imports NUnit.Framework
+
+<TestFixture> Public Class FileNameHelperTests
+	<Test> Public Sub ItShouldAddExtensionWhenIsMissing()
+		'Arrage
+		Dim fileName As String = "file"
+		Dim extension As String = ".html" 
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Dim expectedFileName As String = fileName & extension
+		Assert.AreEqual(expectedFileName, result)
+	End Sub
+
+	<Test> Public Sub ItShouldAddExtensionWhenIsOtherExtensionIsPresent()
+		'Arrage
+		Dim fileName As String = "file.txt"
+		Dim extension As String = ".html" 
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Dim expectedFileName As String = fileName & extension
+		Assert.AreEqual(expectedFileName, result)
+	End Sub
+
+	<Test> Public Sub ItShouldAddExtensionToEmptyFileName()
+		'Arrage
+		Dim fileName As String = ""
+		Dim extension As String = ".html" 
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Dim expectedFileName As String = fileName & extension
+		Assert.AreEqual(expectedFileName, result)
+	End Sub
+
+	<TestCase("index.html")>
+	<TestCase("index.HTML")>
+	<TestCase("index.hTMl")>
+	Public Sub ItShouldNotAddExtensionWhenIsAlreadyPresent(fileName As String)
+		'Arrage
+		Dim extension As String = ".html" 
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Assert.AreEqual(fileName, result)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnNullWhenFileNameIsNull()
+		'Arrage
+		Dim fileName As String = Nothing
+		Dim extension As String = ".html" 
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Assert.IsNull(result)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnFileNameWhenExtensionIsNull()
+		'Arrage
+		Dim fileName As String = "filename"
+		Dim extension As String = Nothing
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Dim expectedFileName As String = fileName
+		Assert.AreEqual(expectedFileName, result)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnFileNameWhenExtensionIsEmpty()
+		'Arrage
+		Dim fileName As String = "filename"
+		Dim extension As String = ""
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Dim expectedFileName As String = fileName
+		Assert.AreEqual(expectedFileName, result)
+	End Sub
+
+	<Test> Public Sub ItShouldReturnFileNameWhenExtensionIsWhitespace()
+		'Arrage
+		Dim fileName As String = "filename"
+		Dim extension As String = "   "
+
+		'Act
+		Dim result As String = FileNameHelper.AppendExtensionToFileWhenMissing(fileName, extension)
+
+		'Assert
+		Dim expectedFileName As String = fileName
+		Assert.AreEqual(expectedFileName, result)
+	End Sub
+End Class
