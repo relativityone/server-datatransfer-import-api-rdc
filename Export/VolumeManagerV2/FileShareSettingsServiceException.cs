4aa8f3442c3e008cd880c86c000a0b52660fe7e5
Adrian Krzysztofek <adrian.krzysztofek@relativity.com>
2018-12-05 15:25:14 +0100

    Improve error message when there are 0 available fileshares.

---
+++ Export/VolumeManagerV2/FileShareSettingsServiceException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace kCura.WinEDDS.Core.Export.VolumeManagerV2
+{
+	public partial class FileShareSettingsService
+	{
+		[Serializable]
+        public class FileShareSettingsServiceException : Exception
+        {
+            public FileShareSettingsServiceException(string message): base(message)
+            {
+            }
+
+            public FileShareSettingsServiceException(): base()
+            {
+            }
+
+	        public FileShareSettingsServiceException(string message, System.Exception innerException) : base(message,
+		        innerException)
+	        {
+	        }
+
+	        protected FileShareSettingsServiceException(System.Runtime.Serialization.SerializationInfo info,
+		        System.Runtime.Serialization.StreamingContext context) : base(info, context)
+	        {
+	        }
+        }
+	
+	}
+}
