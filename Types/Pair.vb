3ca05960a951bdaa5189a26888a742f348ae8bbc
Michal Grygierzec <michal.grygierzec@relativity.com>
2018-12-10 17:25:23 +0100

    Fix exception when Pairs were sorted in List collection; add IComparable
    (cherry picked from commit 7ac4f00912963c8da6fca26e4dd56d6ece07891c)
    Signed-off-by: Michal Grygierzec <michal.grygierzec@relativity.com>

--- Types/Pair.vb
+++ Types/Pair.vb
@@ -1,5 +1,7 @@
 Namespace kCura.WinEDDS
 	<Serializable()> Public Class Pair
+		Implements IComparable
+
 		Public Value As String
 		Public Display As String
 		Public Overrides Function ToString() As String
@@ -12,5 +14,11 @@ Namespace kCura.WinEDDS
 		Public Shadows Function equals(ByVal other As kCura.WinEDDS.Pair) As Boolean
 			Return (Me.Display = other.Display And Me.Value = other.Value)
 		End Function
+
+		Public Function CompareTo(ByVal other As object) As Integer Implements IComparable.CompareTo
+			If other Is Nothing Then Return 1
+			Return String.Compare(Display, other.ToString(), StringComparison.InvariantCulture)
+		End Function
+
 	End Class
 End Namespace
