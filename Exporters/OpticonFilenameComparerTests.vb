abb102eb60017b0a9cddc365db0f5a8ced1ab8ae
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2018-12-12 19:21:36 +0100

    Created a constant to hold a common sub-system name for WinEDDS logging; added logging to OpticonFilenameComparer

--- Exporters/OpticonFilenameComparerTests.vb
+++ Exporters/OpticonFilenameComparerTests.vb
@@ -1,5 +1,7 @@
 ﻿Imports kCura.WinEDDS.Exporters
+Imports NSubstitute
 Imports NUnit.Framework
+Imports Relativity.Logging
 
 Namespace kCura.WinEDDS.NUnit.Exporters
     <TestFixture> Public Class OpticonFilenameComparerTests
@@ -20,10 +22,19 @@ Namespace kCura.WinEDDS.NUnit.Exporters
             "TEST0000004,VOL01,.\VOL01\IMAGES\IMG001\TEST0000004_110.tif,,,,"
         }
 
+		Private ReadOnly _malformedPageLines As List(Of String) = New List(Of String) From {
+			"TEST0000005,VOL01,.\VOL01\IMAGES\IMG001\TEST0000004_1.tif,Y,,",
+			"TEST0000005,VOL01,.\VOL01\IMAGES\IMG001\TEST0000004_2.tif,,",
+			"TEST0000005,VOL01,.\VOL01\IMAGES\IMG001\TEST0000004_3.tif,",
+			"TEST0000005,VOL01,.\VOL01\IMAGES\IMG001\TEST0000004_4.tif"
+		}
+
+        Private _logger As ILog
         Private _comparer As OpticonFilenameComparer
 
         <SetUp> Public Sub SetUp()
-            _comparer = New OpticonFilenameComparer()
+			_logger = Substitute.For(Of ILog)
+            _comparer = New OpticonFilenameComparer(_logger)
         End Sub
 
         <Test> Public Sub OneDocumentPagesAreSortedCorrectly()
@@ -40,6 +51,7 @@ Namespace kCura.WinEDDS.NUnit.Exporters
                     End If
                 Next
             Next
+			_logger.DidNotReceive().LogError(Arg.Any(Of String), Arg.Any(Of Object()))
         End Sub
 
         <Test> Public Sub DifferentDocumentPagesAreSortedCorrectly()
@@ -50,7 +62,25 @@ Namespace kCura.WinEDDS.NUnit.Exporters
 					Assert.Less(comparisonResult, 0, message)
                 Next
             Next
+	        _logger.DidNotReceive().LogError(Arg.Any(Of String), Arg.Any(Of Object()))
         End Sub
+
+		<Test> Public Sub MalformedDocumentPagesAreSortedCorrectlyAndErrorIsLogged()
+			For i As Integer = 0 To _malformedPageLines.Count - 1
+				For j As Integer = 0 To _malformedPageLines.Count - 1
+					Dim comparisonResult As Integer = _comparer.Compare(_malformedPageLines(i), _malformedPageLines(j))
+					Dim message As String = $"{_malformedPageLines(i)} <-> {_malformedPageLines(j)}, i: {i}, j: {j}, result: {comparisonResult}"
+					If i = j Then
+						Assert.AreEqual(0, comparisonResult, message)
+					ElseIf i < j Then
+						Assert.Less(comparisonResult, 0, message)
+					Else
+						Assert.Greater(comparisonResult, 0, message)
+					End If
+				Next
+			Next
+			_logger.Received(2 * _malformedPageLines.Count * _malformedPageLines.Count).LogError(OpticonFilenameComparer.InvalidFileColumnCount, Arg.Any(Of Object()))
+		End Sub
 
     End Class
 End Namespace
