f9f0bd420bc4054a781e83d9161d31612c331cdc
Michal Grygierzec <michal.grygierzec@relativity.com>
2018-12-14 10:07:35 +0100

    Fix GetTextName method and add unit tests

--- Export/Natives/Name/IdentifierExportFileNameProviderTests.cs
+++ Export/Natives/Name/IdentifierExportFileNameProviderTests.cs
@@ -22,16 +22,31 @@ namespace kCura.WinEDDS.Core.NUnit.Export.Natives.Name
 		[TestCase(false)]
 		[TestCase(true)]
 		[Test]
-		public void GetNameShouldCallNativeFileNameMethodWithAppendOriginalFileNameProperty(bool input)
+		public void GetNameShouldCallNativeFileNameMethodWithAppendOriginalFileNameProperty(bool appendOriginalFileName)
 		{
-			string expected = input.ToString();
-			_exportFileMock.AppendOriginalFileName = input;
+			string expected = appendOriginalFileName.ToString();
+			_exportFileMock.AppendOriginalFileName = appendOriginalFileName;
 			_objectExportInfoMock.Setup(x => x.NativeFileName(It.IsAny<bool>())).Returns((bool b) => b.ToString());
 
 			string actual = _sut.GetName(_objectExportInfoMock.Object);
 
 			Assert.AreEqual(expected, actual, "GetName should call NativeFileName method with AppendOriginalFileName value and return proper result.");
 		}
+
+		[TestCase(false)]
+		[TestCase(true)]
+		[Test]
+		public void GetTextNameShouldCallFullTextFileNameMethodWithAppendOriginalFileNameProperty(bool appendOriginalFileName)
+		{
+			string expected = $"{true}{false}{appendOriginalFileName}";
+			_exportFileMock.AppendOriginalFileName = appendOriginalFileName;
+			_objectExportInfoMock.Setup(x => x.FullTextFileName(It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns((bool b1, bool b2, bool b3) => $"{b1}{b2}{b3}");
+
+			string actual = _sut.GetTextName(_objectExportInfoMock.Object);
+
+			Assert.AreEqual(expected, actual, "GetTextName should call FullTextFileName method with AppendOriginalFileName value and return proper result.");
+		}
+
 
 	}
 
