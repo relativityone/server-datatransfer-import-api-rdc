6a11c64ea70c1d2cc172dedb05ffb470a13226bc
Jay Korhonen <jay.korhonen@relativity.com>
2019-01-15 10:36:19 -0600

    [REL-277150] Int->Integer

--- LoadFileBaseTests.vb
+++ LoadFileBaseTests.vb
@@ -7,8 +7,8 @@ Namespace kCura.WinEDDS.NUnit
 		<Test()>
 		Public Sub GetFieldStringValue_Throw_Error_Correct_Test()
 			Dim value As String = "More Than 5 Chars"
-			Dim len As Int = value.length
-			Dim maxLen As Int = 5
+			Dim len As Integer = value.length
+			Dim maxLen As Integer = 5
 			Dim thrownException As kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException = Assert.Throws(Of kCura.Utility.DelimitedFileImporter.InputStringExceedsFixedLengthException)(Sub()
 																																																				 kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar("Morethat5CharsForsureeee", 2, maxLen, 10, "Test")
 																																																			 End Sub)
@@ -17,7 +17,7 @@ Namespace kCura.WinEDDS.NUnit
 
 		<Test()>
 		Public Sub GetFieldStringValue_Return_Correct_Value_Not_Empty()
-			Dim value As String = "Morethat5CharsForsureeee"
+			Dim value As String = "More Than 5 Chars"
 			Assert.AreEqual(value, kCura.Utility.ImprovedDelimitedFileImporter.ValidateStringForVarChar(value, 2, 100, 10, "Test"))
 		End Sub
 
