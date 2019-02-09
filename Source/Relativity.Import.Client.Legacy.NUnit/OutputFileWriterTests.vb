4635f1d4c5be18f5c25296a3abde9bae1d8a0748
Scott Parillo <scott.parillo@relativity.com>
2019-02-09 09:35:47 -0800

    REL-277572 - OutputFileWriter uses new abstracted IStreamWriter, implements IDisposable, and has responsibility for all 4 load files. Dispose and base stream null checks added. - BulkLoadFileImporter now relies on OutputFileWriter to manage the load files. The object is constructed on demand and is disposed via finally block to ensure that all files get deleted. - Note: this was manually migrated from relativity->import-api-rdc and commit 3d90f6a6.

--- Source/Relativity.Import.Client.Legacy.NUnit/OutputFileWriterTests.vb
+++ Source/Relativity.Import.Client.Legacy.NUnit/OutputFileWriterTests.vb
@@ -13,44 +13,113 @@ Namespace Relativity.Import.Client.NUnit
 	<TestFixture>
 	Public Class OutputFileWriterTests
 
-		<Test>
-		Public Sub OutputFileWriter_Open_OpensFile()
-			Dim writer As kCura.WinEDDS.OutputFileWriter = New OutputFileWriter()
-			writer.Open()
-			Assert.IsNotNull(writer.OutputNativeFileWriter.BaseStream)
-			Assert.IsNotNull(writer.OutputDataGridFileWriter.BaseStream)
-			writer.Close()
-			System.IO.File.Delete(writer.OutputNativeFilePath)
-			System.IO.File.Delete(writer.OutputDataGridFilePath)
+		Private _writer As OutputFileWriter
+
+		<SetUp()>
+		Public Sub Setup()
+			_writer = New OutputFileWriter()
 		End Sub
 
-		<Test>
-		Public Sub OutputFileWriter_Close_ClosesFile()
-			Dim writer As kCura.WinEDDS.OutputFileWriter = New OutputFileWriter()
-			writer.Open()
-			writer.Close()
-			Assert.IsNull(writer.OutputNativeFileWriter.BaseStream)
-			Assert.IsNull(writer.OutputDataGridFileWriter.BaseStream)
-			System.IO.File.Delete(writer.OutputNativeFilePath)
-			System.IO.File.Delete(writer.OutputDataGridFilePath)
+		<TearDown()>
+		Public Sub TearDown()
+			If (System.IO.File.Exists(_writer.OutputNativeFilePath)) Then
+				System.IO.File.Delete(_writer.OutputNativeFilePath)
+			End If
+
+			If (System.IO.File.Exists(_writer.OutputDataGridFilePath)) Then
+				System.IO.File.Delete(_writer.OutputDataGridFilePath)
+			End If
+
+			If (System.IO.File.Exists(_writer.OutputCodeFilePath)) Then
+				System.IO.File.Delete(_writer.OutputCodeFilePath)
+			End If
+
+			If (System.IO.File.Exists(_writer.OutputObjectFilePath)) Then
+				System.IO.File.Delete(_writer.OutputObjectFilePath)
+			End If
 		End Sub
 
-		<Test>
-		Public Sub OutputFileWriter_RollbackDocumentLinWrites_RollsbackToCorrectPosition()
-			Dim writer As kCura.WinEDDS.OutputFileWriter = New OutputFileWriter()
-			writer.Open()
-			writer.OutputNativeFileWriter.Write("Test 1")
-			writer.OutputDataGridFileWriter.Write("Test 1")
-			writer.MarkRollbackPosition()
-			writer.OutputNativeFileWriter.Write("Test 2")
-			writer.OutputDataGridFileWriter.Write("Test 2")
-			writer.RollbackDocumentLineWrites()
-			Assert.AreEqual(14, writer.OutputNativeFileWriter.BaseStream.Length)
-			Assert.AreEqual(14, writer.OutputDataGridFileWriter.BaseStream.Length)
-			writer.Close()
-			System.IO.File.Delete(writer.OutputNativeFilePath)
-			System.IO.File.Delete(writer.OutputDataGridFilePath)
+		<Test()>
+		Public Sub ItShouldThrowWhenTheFileSystemIsNull()
+			Assert.Throws(Of System.ArgumentNullException)(
+				Sub()
+					Dim writer As OutputFileWriter = New OutputFileWriter(Nothing)
+				End Sub)
 		End Sub
 
+		<Test()>
+		Public Sub ItShouldThrowWhenDisposed()
+			_writer.Dispose()
+			Dim exception As System.ObjectDisposedException
+			exception = Assert.Throws(Of System.ObjectDisposedException)(Sub() _writer.Close())
+			Console.WriteLine(exception.Message)
+			exception = Assert.Throws(Of System.ObjectDisposedException)(Sub() _writer.DeleteFiles())
+			Console.WriteLine(exception.Message)
+			exception = Assert.Throws(Of System.ObjectDisposedException)(Sub() _writer.MarkRollbackPosition())
+			Console.WriteLine(exception.Message)
+			exception = Assert.Throws(Of System.ObjectDisposedException)(Sub() _writer.Open())
+			Console.WriteLine(exception.Message)
+			exception = Assert.Throws(Of System.ObjectDisposedException)(Sub() _writer.RollbackDocumentLineWrites())
+			Console.WriteLine(exception.Message)
+		End Sub
+
+		<Test()>
+		Public Sub ItShouldDeleteTheFilesWhenDisposed()
+			_writer.Dispose()
+			Assert.That(_writer.OutputDataGridFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputDataGridFilePath, Does.Not.Exist)
+			Assert.That(_writer.OutputNativeFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputNativeFilePath, Does.Not.Exist)
+			Assert.That(_writer.OutputCodeFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputCodeFilePath, Does.Not.Exist)
+			Assert.That(_writer.OutputObjectFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputObjectFilePath, Does.Not.Exist)
+		End Sub
+
+		<Test()>
+		Public Sub ItShouldCreateTheTempFilesWhenConstructed()
+			Assert.That(_writer.OutputDataGridFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputDataGridFilePath, Does.Exist)
+			Assert.That(_writer.OutputNativeFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputNativeFilePath, Does.Exist)
+			Assert.That(_writer.OutputCodeFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputCodeFilePath, Does.Exist)
+			Assert.That(_writer.OutputObjectFilePath, [Is].Not.Null.Or.Empty)
+			Assert.That(_writer.OutputObjectFilePath, Does.Exist)
+		End Sub
+
+		<Test()>
+		Public Sub ItShouldOpenAllFiles()
+			_writer.Open()
+			Assert.IsNotNull(_writer.OutputNativeFileWriter.BaseStream)
+			Assert.IsNotNull(_writer.OutputDataGridFileWriter.BaseStream)
+			Assert.IsNotNull(_writer.OutputObjectFileWriter.BaseStream)
+			Assert.IsNotNull(_writer.OutputCodeFileWriter.BaseStream)
+			_writer.Close()
+		End Sub
+
+		<Test()>
+		Public Sub ItShouldCloseAllFiles()
+			_writer.Open()
+			_writer.Close()
+			Assert.IsNull(_writer.OutputNativeFileWriter.BaseStream)
+			Assert.IsNull(_writer.OutputDataGridFileWriter.BaseStream)
+			Assert.IsNull(_writer.OutputObjectFileWriter.BaseStream)
+			Assert.IsNull(_writer.OutputCodeFileWriter.BaseStream)
+		End Sub
+
+		<Test()>
+		Public Sub ItShouldRollbackToTheCorrectPosition()
+			_writer.Open()
+			_writer.OutputNativeFileWriter.Write("Test 1")
+			_writer.OutputDataGridFileWriter.Write("Test 1")
+			_writer.MarkRollbackPosition()
+			_writer.OutputNativeFileWriter.Write("Test 2")
+			_writer.OutputDataGridFileWriter.Write("Test 2")
+			_writer.RollbackDocumentLineWrites()
+			Assert.AreEqual(14, _writer.OutputNativeFileWriter.BaseStream.Length)
+			Assert.AreEqual(14, _writer.OutputDataGridFileWriter.BaseStream.Length)
+			_writer.Close()
+		End Sub
 	End Class
 End Namespace
