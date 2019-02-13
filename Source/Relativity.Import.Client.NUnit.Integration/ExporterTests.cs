1f28b2fd2ad812bb8a55b69c7fc8227210c456f8
Scott Parillo <scott.parillo@relativity.com>
2019-02-12 23:49:43 -0800

    REL-147994 - Added export integration test support. - The test passes but isn't quite functional when docs are imported. Will address in upcoming story. - Test docs and images copied from sample.

---
+++ Source/Relativity.Import.Client.NUnit.Integration/ExporterTests.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// <copyright file="ExporterTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// <summary>
+//   Represents <see cref="Exporter"/> tests.
+// </summary>
+// -----------------------------------------------------------------------------------------------------
+
+namespace Relativity.Import.Client.NUnit.Integration
+{
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using kCura.WinEDDS;
+
+	using global::NUnit.Framework;
+
+	using Relativity.ImportExport.UnitTestFramework;
+
+	/// <summary>
+	/// Represents <see cref="Exporter"/> tests.
+	/// </summary>
+	[TestFixture]
+	public class ExporterTests : ExporterTestBase
+	{
+		[Test]
+		public async Task ShouldExportAsync()
+		{
+			this.GivenTheExportType(ExportFile.ExportType.ParentSearch);
+			Relativity.CaseInfo caseInfo = await this.WhenGettingTheWorkspaceInfoAsync();
+			this.GivenTheSelectedFolderId(caseInfo.RootFolderID);
+			this.GivenTheIdentifierColumnName(WellKnownFields.ControlNumber);
+			this.GivenTheEncoding(Encoding.Unicode);
+			await this.WhenCreatingTheExportFileAsync(caseInfo);
+			this.WhenExecutingTheExportSearch();
+			this.ThenTheSearchResultShouldEqual(true);
+			this.ThenTheAlertCriticalErrorsCountShouldEqual(0);
+		}
+	}
+}
---
+++ Source/Relativity.ImportExport.UnitTestFramework/WellKnownFields.cs
@@ -0,0 +1,59 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="WellKnownFields.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.ImportExport.UnitTestFramework
+{
+	/// <summary>
+	/// Defines well-known field constants.
+	/// </summary>
+	public static class WellKnownFields
+	{
+		/// <summary>
+		/// The bates number field name.
+		/// </summary>
+		public const string BatesNumber = "bates number";
+
+		/// <summary>
+		/// The control number field name.
+		/// </summary>
+		public const string ControlNumber = "control number";
+
+		/// <summary>
+		/// The control number field identifier.
+		/// </summary>
+		public const int ControlNumberId = 1003667;
+
+		/// <summary>
+		/// The file location field name.
+		/// </summary>
+		public const string FileLocation = "file location";
+
+		/// <summary>
+		/// The file path field.
+		/// </summary>
+		public const string FilePath = "file path";
+
+		/// <summary>
+		/// The native file size field.
+		/// </summary>
+		public const string NativeFileSize = "NativeFileSize";
+
+		/// <summary>
+		/// The folder name field.
+		/// </summary>
+		public const string FolderName = "folder name";
+
+		/// <summary>
+		/// The Outside In file identifier field.
+		/// </summary>
+		public const string OutsideInFileId = "OutsideInFileId";
+
+		/// <summary>
+		/// The Outside In file type name field.
+		/// </summary>
+		public const string OutsideInFileType = "OutsideInFileType";
+	}
+}
