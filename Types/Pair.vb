8ee39021676956b5bd481a48cc666c2a73de92f0
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-02-15 14:59:52 +0100

    Refactored Pair class

--- Types/Pair.vb
+++ Types/Pair.vb
@@ -2,22 +2,31 @@ Namespace kCura.WinEDDS
 	<Serializable()> Public Class Pair
 		Implements IComparable
 
+		Public Sub New(value As String, display As String)
+			Me.Value = value
+			Me.Display = display
+		End Sub
+
 		Public Value As String
 		Public Display As String
+
 		Public Overrides Function ToString() As String
-			Return Me.Display
-		End Function
-		Public Sub New(ByVal v As String, ByVal d As String)
-			Me.Value = v
-			Me.Display = d
-		End Sub
-		Public Shadows Function equals(ByVal other As kCura.WinEDDS.Pair) As Boolean
-			Return (Me.Display = other.Display And Me.Value = other.Value)
+			Return Display
 		End Function
 
-		Public Function CompareTo(ByVal other As object) As Integer Implements IComparable.CompareTo
-			If other Is Nothing Then Return 1
-			Return String.Compare(Display, other.ToString(), StringComparison.InvariantCulture)
+		Public Shadows Function Equals(other As Pair) As Boolean
+			Return Display = other.Display And Value = other.Value
+		End Function
+
+		Public Function CompareTo(other As object) As Integer Implements IComparable.CompareTo
+			If other Is Nothing Then
+				Return 1
+			End If
+			Dim otherPair As Pair = TryCast(other, Pair)
+			If otherPair Is Nothing Then
+				Throw New ArgumentException("Object is not a Pair")
+			End If
+			Return String.Compare(Display, otherPair.Display, StringComparison.InvariantCulture)
 		End Function
 
 	End Class
