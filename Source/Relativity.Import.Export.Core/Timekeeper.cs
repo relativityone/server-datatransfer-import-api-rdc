e585ba94b58f3d38874533b08572f1e903a7a2e2
Scott Parillo <scott.parillo@relativity.com>
2019-03-02 09:34:03 -0800

    REL-273624 - Ported the Timekeeper class from the kCura assembly to C#. Made a few small changes to improve testability. - Added test coverage. Using the CsvHelper library to independently verify CSV functionality and expect to use for other test scenarios going forward.

---
+++ Source/Relativity.Import.Export.Core/Timekeeper.cs
@@ -0,0 +1,330 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Timekeeper.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.Import.Export
+{
+	using System;
+	using System.Collections.Concurrent;
+	using System.IO;
+	using System.Security;
+	using System.Text;
+
+	using Relativity.Import.Export.Io;
+
+	/// <summary>
+	/// Represents a class object that tracks operational metrics.
+	/// </summary>
+	public class Timekeeper
+	{
+		/// <summary>
+		/// The default thread when one isn't specified.
+		/// </summary>
+		private const int DefaultThread = 0;
+		private readonly ConcurrentDictionary<string, ConcurrentDictionary<int, TimekeeperEntry>> dictionary;
+		private readonly IFileSystem fileSystem;
+		private readonly IAppSettings settings;
+		private bool? logAllEvents;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Timekeeper"/> class.
+		/// </summary>
+		public Timekeeper()
+			: this(FileSystem.Instance.DeepCopy(), AppSettings.Instance)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Timekeeper"/> class.
+		/// </summary>
+		/// <param name="fileSystem">
+		/// The file system wrapper.
+		/// </param>
+		/// <param name="settings">
+		/// The application settings.
+		/// </param>
+		public Timekeeper(IFileSystem fileSystem, IAppSettings settings)
+		{
+			if (fileSystem == null)
+			{
+				throw new ArgumentNullException(nameof(fileSystem));
+			}
+
+			if (settings == null)
+			{
+				throw new ArgumentNullException(nameof(settings));
+			}
+
+			this.fileSystem = fileSystem;
+			this.settings = settings;
+			this.dictionary = new ConcurrentDictionary<string, ConcurrentDictionary<int, TimekeeperEntry>>();
+		}
+
+		/// <summary>
+		/// Gets the time keeper entry count.
+		/// </summary>
+		/// <value>
+		/// The total number of entries.
+		/// </value>
+		public int Count => this.dictionary.Count;
+
+		/// <summary>
+		/// Captures the ending time of an operation.
+		/// </summary>
+		/// <param name="key">
+		/// The operation key.
+		/// </param>
+		public virtual void MarkEnd(string key)
+		{
+			this.MarkEnd(key, DefaultThread);
+		}
+
+		/// <summary>
+		/// Captures the ending time of an operation.
+		/// </summary>
+		/// <param name="key">
+		/// The operation key.
+		/// </param>
+		/// <param name="thread">
+		/// The thread key associated with the import operation.
+		/// </param>
+		public virtual void MarkEnd(string key, int thread)
+		{
+			if (!this.LogAllEvents())
+			{
+				return;
+			}
+
+			ConcurrentDictionary<int, TimekeeperEntry> entries = this.dictionary[key];
+			TimekeeperEntry entry = entries?[thread];
+			if (entry == null)
+			{
+				return;
+			}
+
+			entry.Count += 1;
+			entry.Length += (System.DateTime.Now.Ticks - entry.StartTime) / 10000;
+			entry.StartTime = System.DateTime.Now.Ticks;
+		}
+
+		/// <summary>
+		/// Retrieves the entry for the specified operation key.
+		/// </summary>
+		/// <param name="key">
+		/// The operation key.
+		/// </param>
+		/// <returns>
+		/// The <see cref="TimekeeperEntry"/> instance.
+		/// </returns>
+		public TimekeeperEntry GetEntry(string key)
+		{
+			return this.GetEntry(key, DefaultThread);
+		}
+
+		/// <summary>
+		/// Retrieves the entry for the specified operation key and thread.
+		/// </summary>
+		/// <param name="key">
+		/// The operation key.
+		/// </param>
+		/// <param name="thread">
+		/// The thread key associated with the operation.
+		/// </param>
+		/// <returns>
+		/// The <see cref="TimekeeperEntry"/> instance.
+		/// </returns>
+		public TimekeeperEntry GetEntry(string key, int thread)
+		{
+			if (!this.dictionary.ContainsKey(key))
+			{
+				return null;
+			}
+
+			ConcurrentDictionary<int, TimekeeperEntry> entryDictionary = this.dictionary[key];
+			return entryDictionary.ContainsKey(thread) ? entryDictionary[thread] : null;
+		}
+
+		/// <summary>
+		/// Captures the starting time of an operation.
+		/// </summary>
+		/// <param name="key">
+		/// The operation key.
+		/// </param>
+		public virtual void MarkStart(string key)
+		{
+			this.MarkStart(key, 0);
+		}
+
+		/// <summary>
+		/// Captures the starting time of an operation.
+		/// </summary>
+		/// <param name="key">
+		/// The operation key.
+		/// </param>
+		/// <param name="thread">
+		/// The thread key associated with the operation.
+		/// </param>
+		public virtual void MarkStart(string key, int thread)
+		{
+			if (!this.LogAllEvents())
+			{
+				return;
+			}
+
+			if (this.dictionary.ContainsKey(key))
+			{
+				ConcurrentDictionary<int, TimekeeperEntry> entryDictionary = this.dictionary[key];
+				if (entryDictionary.ContainsKey(thread))
+				{
+					entryDictionary[thread].StartTime = System.DateTime.Now.Ticks;
+				}
+				else
+				{
+					entryDictionary[thread] = new TimekeeperEntry();
+				}
+			}
+			else
+			{
+				this.dictionary[key] = new ConcurrentDictionary<int, TimekeeperEntry>()
+					                       {
+						                       [thread] = new TimekeeperEntry()
+					                       };
+			}
+		}
+
+		/// <summary>
+		/// Generates a CSV report of all entries to the current working directory, with individual items represented as columns.
+		/// </summary>
+		/// <exception cref="ArgumentException">
+		/// The file name is empty, contains only white spaces, or contains invalid characters.
+		/// </exception>
+		/// <exception cref="ArgumentNullException">
+		/// The file path is null.
+		/// </exception>
+		/// <exception cref="FileNotFoundException">
+		/// The file does not exist.
+		/// </exception>
+		/// <exception cref="NotSupportedException">
+		/// The file path is in an invalid format.
+		/// </exception>
+		/// <exception cref="SecurityException">
+		/// The caller does not have the required permission.
+		/// </exception>
+		/// <exception cref="UnauthorizedAccessException">
+		/// Access to file path is denied.
+		/// </exception>
+		/// <exception cref="PathTooLongException">
+		/// The file path is too long (greater than 260 characters).
+		/// </exception>
+		public void GenerateCsvReportItemsAsRows()
+		{
+			this.GenerateCsvReportItemsAsRows(string.Empty, string.Empty);
+		}
+
+		/// <summary>
+		/// Generates a CSV report of all entries to the specified directory, with individual items represented as columns.
+		/// </summary>
+		/// <param name="filenameSuffix">
+		/// The suffix with which to look up files.
+		/// </param>
+		/// <param name="directory">
+		/// The directory in which to store the result.
+		/// </param>
+		/// <exception cref="ArgumentException">
+		/// The file name is empty, contains only white spaces, or contains invalid characters.
+		/// </exception>
+		/// <exception cref="ArgumentNullException">
+		/// The file path is null.
+		/// </exception>
+		/// <exception cref="FileNotFoundException">
+		/// The file does not exist.
+		/// </exception>
+		/// <exception cref="NotSupportedException">
+		/// The file path is in an invalid format.
+		/// </exception>
+		/// <exception cref="SecurityException">
+		/// The caller does not have the required permission.
+		/// </exception>
+		/// <exception cref="UnauthorizedAccessException">
+		/// Access to file path is denied.
+		/// </exception>
+		/// <exception cref="PathTooLongException">
+		/// The file path is too long (greater than 260 characters).
+		/// </exception>
+		public void GenerateCsvReportItemsAsRows(string filenameSuffix, string directory)
+		{
+			if (!this.LogAllEvents())
+			{
+				return;
+			}
+
+			string fileName = this.GetFileName(filenameSuffix);
+			string file = directory + fileName;
+			if (!string.IsNullOrEmpty(directory))
+			{
+				file = this.fileSystem.Path.Combine(directory, fileName);
+			}
+
+			using (IStreamWriter writer = this.fileSystem.CreateStreamWriter(file, false, Encoding.Default))
+			{
+				writer.WriteLine("\"Function Name\",\"Number of calls\",\"Total Length (ms)\"");
+				string[] keys = new string[checked(this.dictionary.Keys.Count - 1 + 1)];
+				this.dictionary.Keys.CopyTo(keys, 0);
+				System.Array.Sort<string>(keys);
+				foreach (string key in keys)
+				{
+					ConcurrentDictionary<int, TimekeeperEntry> dict = this.dictionary[key];
+					int count = 0;
+					long length = 0;
+					foreach (TimekeeperEntry entry in dict.Values)
+					{
+						count += entry.Count;
+						length += entry.Length;
+					}
+
+					writer.WriteLine($"\"{key}\",\"{count}\",\"{length}\"");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the name of the file.
+		/// </summary>
+		/// <param name="filenameSuffix">
+		/// The filename suffix.
+		/// </param>
+		/// <returns>
+		/// The filename.
+		/// </returns>
+		private string GetFileName(string filenameSuffix)
+		{
+			if (!this.LogAllEvents())
+			{
+				return null;
+			}
+
+			System.Text.StringBuilder filename = new System.Text.StringBuilder();
+			filename.Append(DateTime.Now.Year);
+			filename.Append(DateTime.Now.Month.ToString().PadLeft(2, '0'));
+			filename.Append(DateTime.Now.Day.ToString().PadLeft(2, '0'));
+			filename.Append(DateTime.Now.Hour.ToString().PadLeft(2, '0'));
+			filename.Append(DateTime.Now.Minute.ToString().PadLeft(2, '0'));
+			filename.Append(DateTime.Now.Second.ToString().PadLeft(2, '0'));
+			filename.Append(filenameSuffix);
+			filename.Append(".csv");
+			return filename.ToString();
+		}
+
+		private bool LogAllEvents()
+		{
+			if (this.logAllEvents == null)
+			{
+				this.logAllEvents = this.settings.LogAllEvents;
+			}
+
+			return this.logAllEvents == true;
+		}
+	}
+}
---
+++ Source/Relativity.Import.Export.NUnit/TimekeeperTests.cs
@@ -0,0 +1,120 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// <copyright file="TimekeeperTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// <summary>
+//   Represents <see cref="Timekeeper"/> tests.
+// </summary>
+// -----------------------------------------------------------------------------------------------------
+
+namespace Relativity.Import.Export.NUnit
+{
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+
+	using CsvHelper;
+
+	using global::NUnit.Framework;
+
+	using Moq;
+
+	using Relativity.Import.Export;
+	using Relativity.Import.Export.Io;
+	using Relativity.Import.Export.TestFramework;
+
+	/// <summary>
+	/// Represents <see cref="Timekeeper"/> tests.
+	/// </summary>
+	[TestFixture]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage(
+		"Microsoft.Design",
+		"CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable",
+		Justification = "The test class handles the disposal.")]
+	public class TimekeeperTests
+	{
+		private TempDirectory tempDirectory;
+		private IFileSystem fileSystem;
+		private Mock<IAppSettings> mockAppSettings;
+		private Timekeeper timekeeper;
+
+		[SetUp]
+		public void Setup()
+		{
+			this.fileSystem = new FileSystemWrap();
+			this.tempDirectory = new TempDirectory();
+			this.tempDirectory.Create();
+			this.mockAppSettings = new Mock<IAppSettings>();
+			this.timekeeper = new Timekeeper(this.fileSystem, this.mockAppSettings.Object);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			this.tempDirectory.Dispose();
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		[Category(TestCategories.Framework)]
+		public void ShouldOnlyTrackTimeWhenTheLogAllEventsSettingIsTrue(bool logAllEvents)
+        {
+	        this.mockAppSettings.SetupGet(x => x.LogAllEvents).Returns(logAllEvents);
+			this.timekeeper.MarkStart("a");
+			this.timekeeper.MarkEnd("a");
+			Assert.That(this.timekeeper.Count, !logAllEvents ? Is.Zero : Is.EqualTo(1));
+			Assert.That(this.timekeeper.GetEntry("a"), !logAllEvents ? Is.Null : Is.Not.Null);
+			this.timekeeper.MarkStart("a");
+			this.timekeeper.MarkEnd("a");
+			Assert.That(this.timekeeper.Count, !logAllEvents ? Is.Zero : Is.EqualTo(1));
+			Assert.That(this.timekeeper.GetEntry("a"), !logAllEvents ? Is.Null : Is.Not.Null);
+			this.timekeeper.MarkStart("b", 1);
+			this.timekeeper.MarkEnd("b", 1);
+			Assert.That(this.timekeeper.Count, !logAllEvents ? Is.Zero : Is.EqualTo(2));
+			Assert.That(this.timekeeper.GetEntry("b", 1), !logAllEvents ? Is.Null : Is.Not.Null);
+			this.timekeeper.MarkStart("b", 1);
+			this.timekeeper.MarkEnd("b", 1);
+			Assert.That(this.timekeeper.Count, !logAllEvents ? Is.Zero : Is.EqualTo(2));
+			Assert.That(this.timekeeper.GetEntry("b", 1), !logAllEvents ? Is.Null : Is.Not.Null);
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		[Category(TestCategories.Framework)]
+		public void ShouldOnlyGenerateTheCsvReportWhenTheLogAllEventsSettingIsTrue(bool logAllEvents)
+		{
+			this.mockAppSettings.SetupGet(x => x.LogAllEvents).Returns(logAllEvents);
+			this.timekeeper.MarkStart("a");
+			this.timekeeper.MarkEnd("a");
+			this.timekeeper.MarkStart("a", 1);
+			this.timekeeper.MarkEnd("a", 1);
+			this.timekeeper.GenerateCsvReportItemsAsRows("import-api", this.tempDirectory.Directory);
+			List<string> files = System.IO.Directory.GetFiles(this.tempDirectory.Directory).ToList();
+			if (logAllEvents)
+			{
+				Assert.That(files.Count, Is.EqualTo(1));
+				string file = files[0];
+				using (var reader = new StreamReader(file))
+				{
+					var csv = new CsvReader(reader);
+					csv.Read();
+					bool header = csv.ReadHeader();
+					Assert.That(header, Is.True);
+					csv.Read();
+					string functionName = csv.GetField<string>(0);
+					int numberOfCalls = csv.GetField<int>(1);
+					long totalLength = csv.GetField<long>(2);
+					Assert.That(functionName, Is.Not.Null.Or.Empty);
+					Assert.That(numberOfCalls, Is.EqualTo(2));
+					Assert.That(totalLength, Is.GreaterThan(0));
+				}
+			}
+			else
+			{
+				Assert.That(files.Count, Is.Zero);
+			}
+		}
+	}
+}
