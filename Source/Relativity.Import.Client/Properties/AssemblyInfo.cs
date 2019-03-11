785abd2a907aa70777c4bc8608ce9b1d96c5704a
Scott Parillo <scott.parillo@relativity.com>
2019-03-10 18:59:15 -0700

    REL-300446 - Added Relativity.Import.Client C# project. - This represents the project that will contain the IAPI source.

---
+++ Source/Relativity.Import.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Relativity.Import.Client")]
+[assembly: AssemblyDescription("A Relativity import client class library.")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
