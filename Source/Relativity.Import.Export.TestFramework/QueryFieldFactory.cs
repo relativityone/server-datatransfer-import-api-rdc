af3c97a3859f4048945af196c62190d3a931d433
Scott Parillo <scott.parillo@relativity.com>
2019-05-04 06:58:12 -0700

    RDC Unit Tests - Included all migrated RDC unit test source in test project. - Updated project settings and namespaces build all migrated tests. - It should be noted that all tests passed without any code changes.

--- Source/Relativity.Import.Export.TestFramework/QueryFieldFactory.cs
+++ Source/Relativity.Import.Export.TestFramework/QueryFieldFactory.cs
@@ -264,6 +264,48 @@ namespace Relativity.Import.Export.TestFramework
 				false);
 		}
 
+		public kCura.WinEDDS.ViewFieldInfo GetSameNameDifferentIdGenericLongTextField()
+		{
+			return this.GenerateQueryField(
+				6,
+				1441000,
+				FieldCategory.FullText,
+				FieldType.Text,
+				10,
+				"Document",
+				-1,
+				false,
+				"Long Text",
+				"LongText",
+				"Long Text",
+				string.Empty,
+				-1,
+				true,
+				false,
+				string.Empty,
+				-1,
+				string.Empty,
+				"Long Text",
+				-1,
+				-1,
+				string.Empty,
+				-1,
+				-1,
+				string.Empty,
+				false,
+				"MainTable",
+				"Document",
+				string.Empty,
+				string.Empty,
+				string.Empty,
+				ParentReflectionType.Empty,
+				string.Empty,
+				string.Empty,
+				string.Empty,
+				string.Empty,
+				false);
+		}
+
 		public kCura.WinEDDS.ViewFieldInfo GetGenericBooleanField()
 		{
 			return this.GenerateQueryField(
