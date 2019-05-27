43f0998569b8d3c143a92e0ff44807d58bad3832
Szymon Choinski <szymon.choinski@relativity.com>
2019-05-27 16:28:30 +0200

    REL-312317 Changing to Lazy

--- Source/Relativity.DataExchange.Core/Io/FileTypeIdentifierService.cs
+++ Source/Relativity.DataExchange.Core/Io/FileTypeIdentifierService.cs
@@ -6,30 +6,21 @@
 
 namespace Relativity.DataExchange.Io
 {
+	using System;
+
 	/// <summary>
 	/// Provides a way to access the FileTypeIdentifier.
 	/// </summary>
 	public static class FileTypeIdentifierService
 	{
 		/// <summary>
-		/// An instance of the OutsideIn file identifier service.
+		/// A singleton instance of the file identifier service.
 		/// </summary>
-		private static OutsideInFileTypeIdentifierService instance;
+		private static readonly Lazy<OutsideInFileTypeIdentifierService> LazyInstance = new Lazy<OutsideInFileTypeIdentifierService>();
 
 		/// <summary>
-		/// Gets an instance of the OutsideIn file identifier service.
+		/// Gets the file identification singleton instance.
 		/// </summary>
-		public static IFileTypeIdentifier Instance
-		{
-			get
-			{
-				if (instance == null)
-				{
-					instance = new OutsideInFileTypeIdentifierService();
-				}
-
-				return instance;
-			}
-		}
+		public static IFileTypeIdentifier Instance => LazyInstance.Value;
 	}
 }
