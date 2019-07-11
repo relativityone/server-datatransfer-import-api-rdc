7e1719d740fa6b1336b04b1f40f90d1da1c23898
Scott Parillo <scott.parillo@relativity.com>
2019-07-10 22:51:53 -0700

    REL-296742 - Added unit test coverage.

--- Source/Relativity.DataExchange.NUnit/TapiTotalsTests.cs
+++ Source/Relativity.DataExchange.NUnit/TapiTotalsTests.cs
@@ -20,25 +20,61 @@ namespace Relativity.DataExchange.NUnit
 	[TestFixture]
 	public class TapiTotalsTests
 	{
+		private long testTotalCompletedFileTransfers;
+		private long testTotalFileTransferRequests;
+		private long testTotalSuccessfulFileTransfers;
+		private TapiTotals totals;
+
+		[SetUp]
+		public void Setup()
+		{
+			this.testTotalCompletedFileTransfers = RandomHelper.NextInt64(1, int.MaxValue);
+			this.testTotalFileTransferRequests = RandomHelper.NextInt64(1, int.MaxValue);
+			this.testTotalSuccessfulFileTransfers = RandomHelper.NextInt64(1, int.MaxValue);
+			this.totals = new TapiTotals(
+				this.testTotalCompletedFileTransfers,
+				this.testTotalFileTransferRequests,
+				this.testTotalSuccessfulFileTransfers);
+			Assert.That(this.totals.TotalCompletedFileTransfers, Is.EqualTo(this.testTotalCompletedFileTransfers));
+			Assert.That(this.totals.TotalFileTransferRequests, Is.EqualTo(this.testTotalFileTransferRequests));
+			Assert.That(this.totals.TotalSuccessfulFileTransfers, Is.EqualTo(this.testTotalSuccessfulFileTransfers));
+		}
+
 		[Test]
 		public void ShouldDeepCopyTheTotals()
 		{
-			long testTotalCompletedFileTransfers = RandomHelper.NextInt64(1, int.MaxValue);
-			long testTotalFileTransferRequests = RandomHelper.NextInt64(1, int.MaxValue);
-			long testTotalSuccessfulFileTransfers = RandomHelper.NextInt64(1, int.MaxValue);
-			TapiTotals totals = new TapiTotals(
-				testTotalCompletedFileTransfers,
-				testTotalFileTransferRequests,
-				testTotalSuccessfulFileTransfers);
+			TapiTotals copy = this.totals.DeepCopy();
+			Assert.That(copy, Is.Not.SameAs(this.totals));
+			Assert.That(copy.TotalCompletedFileTransfers, Is.EqualTo(this.testTotalCompletedFileTransfers));
+			Assert.That(copy.TotalFileTransferRequests, Is.EqualTo(this.testTotalFileTransferRequests));
+			Assert.That(copy.TotalSuccessfulFileTransfers, Is.EqualTo(this.testTotalSuccessfulFileTransfers));
+		}
 
-			Assert.That(totals.TotalCompletedFileTransfers, Is.EqualTo(testTotalCompletedFileTransfers));
-			Assert.That(totals.TotalFileTransferRequests, Is.EqualTo(testTotalFileTransferRequests));
-			Assert.That(totals.TotalSuccessfulFileTransfers, Is.EqualTo(testTotalSuccessfulFileTransfers));
-			TapiTotals copy = totals.DeepCopy();
-			Assert.That(copy, Is.Not.SameAs(totals));
-			Assert.That(copy.TotalCompletedFileTransfers, Is.EqualTo(testTotalCompletedFileTransfers));
-			Assert.That(copy.TotalFileTransferRequests, Is.EqualTo(testTotalFileTransferRequests));
-			Assert.That(copy.TotalSuccessfulFileTransfers, Is.EqualTo(testTotalSuccessfulFileTransfers));
+		[Test]
+		public void ShouldClearTheTotals()
+		{
+			Assert.That(this.totals.TotalCompletedFileTransfers, Is.Not.Zero);
+			Assert.That(this.totals.TotalFileTransferRequests, Is.Not.Zero);
+			Assert.That(this.totals.TotalSuccessfulFileTransfers, Is.Not.Zero);
+			this.totals.Clear();
+			Assert.That(this.totals.TotalCompletedFileTransfers, Is.Zero);
+			Assert.That(this.totals.TotalFileTransferRequests, Is.Zero);
+			Assert.That(this.totals.TotalSuccessfulFileTransfers, Is.Zero);
+		}
+
+		[Test]
+		public void ShouldIncrementTheTotals()
+		{
+			const int Iterations = 10;
+			for (int i = 0; i < Iterations; i++)
+			{
+				this.totals.IncrementTotalFileTransferRequests();
+				this.totals.IncrementTotalCompletedFileTransfers();
+				this.totals.IncrementTotalSuccessfulFileTransfers();
+				Assert.That(this.totals.TotalCompletedFileTransfers, Is.EqualTo(this.testTotalCompletedFileTransfers + i + 1));
+				Assert.That(this.totals.TotalFileTransferRequests, Is.EqualTo(this.testTotalFileTransferRequests + i + 1));
+				Assert.That(this.totals.TotalSuccessfulFileTransfers, Is.EqualTo(this.testTotalSuccessfulFileTransfers + i + 1));
+			}
 		}
 	}
 }
