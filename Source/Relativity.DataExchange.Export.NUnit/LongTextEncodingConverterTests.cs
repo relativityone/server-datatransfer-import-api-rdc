0dc827605d695f0bcf40db86d6a4213eb33dce5b
Scott Parillo <scott.parillo@relativity.com>
2019-07-24 14:07:08 -0700

    REL-342821 - Fixed an issue where extracted text encoding conversion could fail if the source/target encodings are different. OnTapiProgress now checks the Successful property. - This issue was introduced when the progress event was modified to fire when the transfer starts and ends. - Checked all other usages of the event and verified this was the only event handler that didn't check the args for transfer success. - GetLongTextForFile now uses the Path class to fetch the filename instead of expensive FileInfo. - Added more verbose logging and unit test coverage.

--- Source/Relativity.DataExchange.Export.NUnit/LongTextEncodingConverterTests.cs
+++ Source/Relativity.DataExchange.Export.NUnit/LongTextEncodingConverterTests.cs
@@ -15,12 +15,10 @@ namespace Relativity.DataExchange.Export.NUnit
 	using Moq;
 
 	using Relativity.DataExchange.Export.VolumeManagerV2.Download.EncodingHelpers;
-	using Relativity.DataExchange.Export.VolumeManagerV2.Download.TapiHelpers;
 	using Relativity.DataExchange.Export.VolumeManagerV2.Metadata.Text;
 	using Relativity.DataExchange.Export.VolumeManagerV2.Repository;
 	using Relativity.DataExchange.Transfer;
 	using Relativity.Logging;
-	using Relativity.Transfer;
 
 	[TestFixture]
 	public class LongTextEncodingConverterTests
@@ -31,15 +29,20 @@ namespace Relativity.DataExchange.Export.NUnit
 
 		private Mock<IFileEncodingConverter> _fileEncodingConverter;
 		private Mock<ITapiBridge> _tapiBridge;
+		private Mock<ILog> _logger;
 
 		[SetUp]
 		public void SetUp()
 		{
-			this._longTextRepository = new LongTextRepository(null, new NullLogger());
+			this._logger = new Mock<ILog>();
+			this._longTextRepository = new LongTextRepository(null, this._logger.Object);
 			this._fileEncodingConverter = new Mock<IFileEncodingConverter>();
 			this._tapiBridge = new Mock<ITapiBridge>();
-
-			this._instance = new LongTextEncodingConverter(this._longTextRepository, this._fileEncodingConverter.Object, new NullLogger(), CancellationToken.None);
+			this._instance = new LongTextEncodingConverter(
+				this._longTextRepository,
+				this._fileEncodingConverter.Object,
+				this._logger.Object,
+				CancellationToken.None);
 		}
 
 		[Test]
@@ -60,6 +63,32 @@ namespace Relativity.DataExchange.Export.NUnit
 
 			// ASSERT
 			this._fileEncodingConverter.Verify(x => x.Convert(It.IsAny<string>(), It.IsAny<Encoding>(), It.IsAny<Encoding>(), CancellationToken.None), Times.Never);
+		}
+
+		[Test]
+		[TestCase(false)]
+		[TestCase(true)]
+		public void ItShouldNotConvertFileWhenTheFileIsNotSuccessfullyTransferred(bool completed)
+		{
+			const string fileName = "fileName";
+
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
+			longText.SourceEncoding = Encoding.ASCII;
+
+			// ACT
+			this._instance.StartListening(this._tapiBridge.Object);
+			this._instance.StopListening(this._tapiBridge.Object);
+			const bool Successful = false;
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs(fileName, completed, Successful, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+
+			this._instance.WaitForConversionCompletion();
+
+			// ASSERT
+			this._fileEncodingConverter.Verify(
+				x => x.Convert(It.IsAny<string>(), It.IsAny<Encoding>(), It.IsAny<Encoding>(), CancellationToken.None),
+				Times.Never);
 		}
 
 		[Test]
@@ -112,14 +141,8 @@ namespace Relativity.DataExchange.Export.NUnit
 
 			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
 			longText.SourceEncoding = Encoding.UTF8;
-
-			bool waited = false;
-
-			this._fileEncodingConverter.Setup(x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None)).Callback(() =>
-			{
-				Thread.Sleep(2000);
-				waited = true;
-			});
+			this._fileEncodingConverter.Setup(
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None));
 
 			// ACT
 			this._instance.StartListening(this._tapiBridge.Object);
@@ -131,7 +154,34 @@ namespace Relativity.DataExchange.Export.NUnit
 			this._instance.WaitForConversionCompletion();
 
 			// ASSERT
-			Assert.That(waited, Is.True);
+			this._fileEncodingConverter.Verify(
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None),
+				Times.Once);
+		}
+
+		[Test]
+		public void ItShouldThrowWhenTheFileDoesNotExistInTheRepository()
+		{
+			// Simulate an inability to find the LongText object within the repository.
+			const string fileName = "fileName";
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				fileName + "-file-not-found",
+				Encoding.Unicode);
+			longText.SourceEncoding = Encoding.UTF8;
+
+			// ACT
+			this._instance.StartListening(this._tapiBridge.Object);
+			this._tapiBridge.Raise(x => x.TapiProgress += null, new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+			this._instance.StopListening(this._tapiBridge.Object);
+
+			// ASSERT
+			Assert.Throws<ArgumentException>(() => this._instance.WaitForConversionCompletion());
+			this._fileEncodingConverter.Verify(
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None),
+				Times.Never);
+			this._logger.Verify(x => x.LogError(It.IsAny<string>(), It.IsAny<object[]>()), Times.Once);
 		}
 	}
 }
--- Source/Relativity.DataExchange.Export/Export/VolumeManagerV2/Download/EncodingHelpers/LongTextEncodingConverter.cs
+++ Source/Relativity.DataExchange.Export/Export/VolumeManagerV2/Download/EncodingHelpers/LongTextEncodingConverter.cs
@@ -13,47 +13,55 @@
 
 	public class LongTextEncodingConverter : IDisposable, ILongTextEncodingConverter
 	{
-		private Task _conversionTask;
-
 		private readonly BlockingCollection<string> _longTextFilesToConvert;
 		private readonly LongTextRepository _longTextRepository;
 		private readonly IFileEncodingConverter _fileEncodingConverter;
 		private readonly ILog _logger;
 		private readonly CancellationToken _cancellationToken;
+		private Task _conversionTask;
 
-		public LongTextEncodingConverter(LongTextRepository longTextRepository, IFileEncodingConverter fileEncodingConverter, ILog logger,
+		public LongTextEncodingConverter(
+			LongTextRepository longTextRepository,
+			IFileEncodingConverter fileEncodingConverter,
+			ILog logger,
 			CancellationToken cancellationToken)
 		{
-			_longTextRepository = longTextRepository;
-			_fileEncodingConverter = fileEncodingConverter;
-			_logger = logger;
+			_longTextRepository = longTextRepository.ThrowIfNull(nameof(longTextRepository));
+			_fileEncodingConverter = fileEncodingConverter.ThrowIfNull(nameof(fileEncodingConverter));
+			_logger = logger.ThrowIfNull(nameof(logger));
 			_cancellationToken = cancellationToken;
-
 			_longTextFilesToConvert = new BlockingCollection<string>();
 		}
 
 		public void StartListening(ITapiBridge tapiBridge)
 		{
-			_logger.LogVerbose("Start conversion task.");
-			_conversionTask = Task.Run(() => ConvertLongTextFiles(), _cancellationToken);
-			tapiBridge.TapiProgress += OnTapiProgress;
+			_logger.LogVerbose("Start long text encoding conversion task.");
+			_conversionTask = Task.Run(this.ConvertLongTextFiles, _cancellationToken);
+			tapiBridge.TapiProgress += this.OnTapiProgress;
 		}
 
 		public void StopListening(ITapiBridge tapiBridge)
 		{
-			_logger.LogVerbose("Stop listening for new files to convert.");
-			tapiBridge.TapiProgress -= OnTapiProgress;
+			_logger.LogVerbose("Stop listening for new long text encoding files to convert.");
+			tapiBridge.TapiProgress -= this.OnTapiProgress;
 			_longTextFilesToConvert.CompleteAdding();
 		}
 
 		private void OnTapiProgress(object sender, TapiProgressEventArgs e)
 		{
-			_longTextFilesToConvert.Add(e.FileName);
+			_logger.LogVerbose(
+				"Long text encoding conversion progress event for file {fileName} with status {Successful}.",
+				e.FileName,
+				e.Successful);
+			if (e.Successful)
+			{
+				_longTextFilesToConvert.Add(e.FileName, _cancellationToken);
+			}
 		}
 
 		public void WaitForConversionCompletion()
 		{
-			_logger.LogVerbose("Waiting for conversion to complete.");
+			_logger.LogVerbose("Waiting for long text encoding conversion to complete.");
 			_conversionTask.ConfigureAwait(false).GetAwaiter().GetResult();
 		}
 
@@ -64,12 +72,16 @@
 				string longTextFile;
 				while (_longTextFilesToConvert.TryTake(out longTextFile, Timeout.Infinite, _cancellationToken))
 				{
-					_logger.LogVerbose("New item in conversion queue {file}. Proceeding.", longTextFile);
-					LongText longText = GetLongTextForFile(longTextFile);
-					if (ConversionRequired(longText))
+					_logger.LogVerbose("New item in long text encoding conversion queue {file}. Proceeding.", longTextFile);
+					LongText longText = this.GetLongTextForFile(longTextFile);
+					if (this.ConversionRequired(longText))
 					{
-						_logger.LogVerbose("Encoding conversion required for file {file}.", longTextFile);
-						ConvertLongTextFile(longText);
+						_logger.LogVerbose("Long text encoding conversion required for file {file}.", longTextFile);
+						this.ConvertLongTextFile(longText);
+					}
+					else
+					{
+						_logger.LogVerbose("Long text encoding conversion NOT required for file {file}.", longTextFile);
 					}
 				}
 			}
@@ -92,15 +104,15 @@
 		{
 			foreach (LongText longText in _longTextRepository.GetLongTexts().Where(x => !string.IsNullOrEmpty(x.Location)))
 			{
-				string fileName = new System.IO.FileInfo(longText.Location).Name;
-				if (fileName == longTextFile)
+				string fileName = System.IO.Path.GetFileName(longText.Location);
+				if (string.Compare(fileName, longTextFile, StringComparison.OrdinalIgnoreCase) == 0)
 				{
 					return longText;
 				}
 			}
 
-			_logger.LogError("Could not found LongText for file {file}.", longTextFile);
-			throw new ArgumentException($"Could not found LongText for file {longTextFile}.");
+			_logger.LogError("Failed to find the LongText file {file} in the repository.", longTextFile);
+			throw new ArgumentException($"The long text file {longTextFile} cannot be converted because it doesn't exist within the export request.");
 		}
 
 		private bool ConversionRequired(LongText longText)
