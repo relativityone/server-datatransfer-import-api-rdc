1694a63ad5b2b5300516fb3b7288cf795aaf0ec9
Scott Parillo <scott.parillo@relativity.com>
2019-08-04 06:47:06 -0700

    REL-345204 - Added more verbose logging. - Added a Count property for improved diagnostics and test asserts. - Increased test coverage.

--- Source/Relativity.DataExchange.Export.NUnit/LongTextEncodingConverterTests.cs
+++ Source/Relativity.DataExchange.Export.NUnit/LongTextEncodingConverterTests.cs
@@ -23,17 +23,22 @@ namespace Relativity.DataExchange.Export.NUnit
 	[TestFixture]
 	public class LongTextEncodingConverterTests
 	{
+		private CancellationTokenSource _cancellationTokenSource;
+
 		private LongTextEncodingConverter _instance;
 
 		private LongTextRepository _longTextRepository;
 
 		private Mock<IFileEncodingConverter> _fileEncodingConverter;
+
 		private Mock<ITapiBridge> _tapiBridge;
+
 		private Mock<ILog> _logger;
 
 		[SetUp]
 		public void SetUp()
 		{
+			this._cancellationTokenSource = new CancellationTokenSource();
 			this._logger = new Mock<ILog>();
 			this._longTextRepository = new LongTextRepository(null, this._logger.Object);
 			this._fileEncodingConverter = new Mock<IFileEncodingConverter>();
@@ -42,7 +47,7 @@ namespace Relativity.DataExchange.Export.NUnit
 				this._longTextRepository,
 				this._fileEncodingConverter.Object,
 				this._logger.Object,
-				CancellationToken.None);
+				this._cancellationTokenSource.Token);
 		}
 
 		[Test]
@@ -50,19 +55,32 @@ namespace Relativity.DataExchange.Export.NUnit
 		{
 			const string fileName = "fileName";
 
-			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				fileName,
+				Encoding.Unicode);
 			longText.SourceEncoding = Encoding.ASCII;
 
 			// ACT
 			this._instance.StartListening(this._tapiBridge.Object);
 			this._instance.StopListening(this._tapiBridge.Object);
 
-			this._tapiBridge.Raise(x => x.TapiProgress += null, new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
 
 			this._instance.WaitForConversionCompletion();
 
 			// ASSERT
-			this._fileEncodingConverter.Verify(x => x.Convert(It.IsAny<string>(), It.IsAny<Encoding>(), It.IsAny<Encoding>(), CancellationToken.None), Times.Never);
+			this._fileEncodingConverter.Verify(
+				x => x.Convert(
+					It.IsAny<string>(),
+					It.IsAny<Encoding>(),
+					It.IsAny<Encoding>(),
+					this._cancellationTokenSource.Token),
+				Times.Never);
+			Assert.That(this._instance.Count, Is.Zero);
 		}
 
 		[Test]
@@ -73,7 +91,11 @@ namespace Relativity.DataExchange.Export.NUnit
 			const bool Successful = false;
 			const string fileName = "fileName";
 
-			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				fileName,
+				Encoding.Unicode);
 			longText.SourceEncoding = Encoding.ASCII;
 
 			// ACT
@@ -87,8 +109,13 @@ namespace Relativity.DataExchange.Export.NUnit
 
 			// ASSERT
 			this._fileEncodingConverter.Verify(
-				x => x.Convert(It.IsAny<string>(), It.IsAny<Encoding>(), It.IsAny<Encoding>(), CancellationToken.None),
+				x => x.Convert(
+					It.IsAny<string>(),
+					It.IsAny<Encoding>(),
+					It.IsAny<Encoding>(),
+					this._cancellationTokenSource.Token),
 				Times.Never);
+			Assert.That(this._instance.Count, Is.Zero);
 		}
 
 		[Test]
@@ -96,58 +123,19 @@ namespace Relativity.DataExchange.Export.NUnit
 		{
 			const string fileName = "fileName";
 
-			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				fileName,
+				Encoding.Unicode);
 			longText.SourceEncoding = Encoding.Unicode;
 
 			// ACT
 			this._instance.StartListening(this._tapiBridge.Object);
 
-			this._tapiBridge.Raise(x => x.TapiProgress += null, new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
-
-			this._instance.StopListening(this._tapiBridge.Object);
-
-			this._instance.WaitForConversionCompletion();
-
-			// ASSERT
-			this._fileEncodingConverter.Verify(x => x.Convert(It.IsAny<string>(), It.IsAny<Encoding>(), It.IsAny<Encoding>(), CancellationToken.None), Times.Never);
-		}
-
-		[Test]
-		public void ItShouldConvertFile()
-		{
-			const string fileName = "fileName";
-
-			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
-			longText.SourceEncoding = Encoding.UTF8;
-
-			// ACT
-			this._instance.StartListening(this._tapiBridge.Object);
-
-			this._tapiBridge.Raise(x => x.TapiProgress += null, new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
-
-			this._instance.StopListening(this._tapiBridge.Object);
-
-			this._instance.WaitForConversionCompletion();
-
-			// ASSERT
-			this._fileEncodingConverter.Verify(x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None), Times.Once);
-			Assert.That(longText.SourceEncoding, Is.EqualTo(longText.DestinationEncoding));
-		}
-
-		[Test]
-		public void ItShouldWaitForConversionToComplete()
-		{
-			const string fileName = "fileName";
-
-			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(1, this._longTextRepository, fileName, Encoding.Unicode);
-			longText.SourceEncoding = Encoding.UTF8;
-			this._fileEncodingConverter.Setup(
-				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None));
-
-			// ACT
-			this._instance.StartListening(this._tapiBridge.Object);
-
-			this._tapiBridge.Raise(x => x.TapiProgress += null, new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
 
 			this._instance.StopListening(this._tapiBridge.Object);
 
@@ -155,8 +143,109 @@ namespace Relativity.DataExchange.Export.NUnit
 
 			// ASSERT
 			this._fileEncodingConverter.Verify(
-				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None),
+				x => x.Convert(
+					It.IsAny<string>(),
+					It.IsAny<Encoding>(),
+					It.IsAny<Encoding>(),
+					this._cancellationTokenSource.Token),
+				Times.Never);
+			Assert.That(this._instance.Count, Is.Zero);
+		}
+
+		[Test]
+		public void ItShouldConvertFile()
+		{
+			const string fileName = "fileName";
+
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				fileName,
+				Encoding.Unicode);
+			longText.SourceEncoding = Encoding.UTF8;
+
+			// ACT
+			this._instance.StartListening(this._tapiBridge.Object);
+
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+
+			this._instance.StopListening(this._tapiBridge.Object);
+
+			this._instance.WaitForConversionCompletion();
+
+			// ASSERT
+			this._fileEncodingConverter.Verify(
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, this._cancellationTokenSource.Token),
 				Times.Once);
+			Assert.That(longText.SourceEncoding, Is.EqualTo(longText.DestinationEncoding));
+			Assert.That(this._instance.Count, Is.Zero);
+		}
+
+		[Test]
+		public void ItShouldThrowWhenProgressIsRaisedAfterTheQueueIsMarkedComplete()
+		{
+			// ARRANGE
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				"fileName",
+				Encoding.Unicode);
+			longText.SourceEncoding = Encoding.UTF8;
+
+			// ACT - This simulates the scenario where events are raised despite having been unsubscribed.
+			this._instance.StartListening(this._tapiBridge.Object);
+			this._instance.MarkQueueComplete();
+
+			// ASSERT
+			InvalidOperationException exception = Assert.Throws<InvalidOperationException>(
+				() =>
+					{
+						this._tapiBridge.Raise(
+							x => x.TapiProgress += null,
+							new TapiProgressEventArgs(
+								longText.Location,
+								true,
+								true,
+								1,
+								1,
+								DateTime.MinValue,
+								DateTime.MaxValue));
+					});
+			this._logger.Verify(x => x.LogError(exception, It.IsAny<string>(), It.IsAny<object[]>()), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldWaitForConversionToComplete()
+		{
+			const string fileName = "fileName";
+
+			LongText longText = ModelFactory.GetLongTextWithLocationAndEncoding(
+				1,
+				this._longTextRepository,
+				fileName,
+				Encoding.Unicode);
+			longText.SourceEncoding = Encoding.UTF8;
+			this._fileEncodingConverter.Setup(
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, this._cancellationTokenSource.Token));
+
+			// ACT
+			this._instance.StartListening(this._tapiBridge.Object);
+
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+
+			this._instance.StopListening(this._tapiBridge.Object);
+
+			this._instance.WaitForConversionCompletion();
+
+			// ASSERT
+			this._fileEncodingConverter.Verify(
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, this._cancellationTokenSource.Token),
+				Times.Once);
+			Assert.That(this._instance.Count, Is.Zero);
 		}
 
 		[Test]
@@ -173,15 +262,32 @@ namespace Relativity.DataExchange.Export.NUnit
 
 			// ACT
 			this._instance.StartListening(this._tapiBridge.Object);
-			this._tapiBridge.Raise(x => x.TapiProgress += null, new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs(fileName, true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
 			this._instance.StopListening(this._tapiBridge.Object);
 
 			// ASSERT
 			Assert.Throws<ArgumentException>(() => this._instance.WaitForConversionCompletion());
 			this._fileEncodingConverter.Verify(
-				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, CancellationToken.None),
+				x => x.Convert(fileName, Encoding.UTF8, Encoding.Unicode, this._cancellationTokenSource.Token),
 				Times.Never);
 			this._logger.Verify(x => x.LogError(It.IsAny<string>(), It.IsAny<object[]>()), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldDisposeTheBlockingCollection()
+		{
+			// ARRANGE
+			this._tapiBridge.Raise(
+				x => x.TapiProgress += null,
+				new TapiProgressEventArgs("filename1", true, true, 1, 1, DateTime.MinValue, DateTime.MaxValue));
+
+			// ACT
+			this._instance.Dispose();
+
+			// ASSERT
+			Assert.Throws<ObjectDisposedException>(() => this._instance.MarkQueueComplete());
 		}
 	}
 }
