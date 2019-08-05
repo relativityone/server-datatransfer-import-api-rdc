5866e3bf0cb8ceae84bd8eba50dcdbf53dd55243
Krzysztof Geleta <krzysztof.geleta@relativity.com>
2019-08-05 17:05:27 +0200

    Changed MetricSinkConfigAPI to use AppSettings

--- Source/Relativity.DataExchange.Legacy/Monitoring/Sinks/MetricSinkConfigIAPI.vb
+++ Source/Relativity.DataExchange.Legacy/Monitoring/Sinks/MetricSinkConfigIAPI.vb
@@ -1,26 +1,28 @@
-﻿Imports kCura.WinEDDS.Monitoring
+﻿Imports System.Diagnostics
+Imports kCura.WinEDDS.Monitoring
+Imports Relativity.DataExchange
 
 Public Class MetricSinkConfigIAPI
     Implements IMetricsSinkConfig
 
     Public ReadOnly Property ThrottleTimeout As TimeSpan Implements IMetricsSinkConfig.ThrottleTimeout
     Get
-        Return TimeSpan.FromSeconds(30)
+        Return TimeSpan.FromSeconds(AppSettings.Instance.IapiMetricsThrottlingSeconds)
     End Get
     End Property
     Public ReadOnly Property SendLiveApmMetrics As Boolean Implements IMetricsSinkConfig.SendLiveApmMetrics
     Get
-        Return True
+        Return  AppSettings.Instance.IapiSubmitApmMetrics
     End Get
     End Property
     Public ReadOnly Property SendSumMetrics As Boolean Implements IMetricsSinkConfig.SendSumMetrics
     Get
-        Return True
+        Return AppSettings.Instance.IapiSubmitSumMetrics
     End Get
     End Property
     Public ReadOnly Property SendSummaryApmMetrics As Boolean Implements IMetricsSinkConfig.SendSummaryApmMetrics
     Get
-        Return True
+        Return AppSettings.Instance.IapiSubmitApmMetrics
     End Get
     End Property
 End Class
