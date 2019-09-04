2e5de27d05e57770e6024c5277104ee060d0430f
Krzysztof Geleta <krzysztof.geleta@relativity.com>
2019-09-04 10:30:01 +0200

    Changed unit tests and added new ones

---
+++ Source/Relativity.DataExchange.NUnit/Monitoring/Sinks/MetricServiceTest.cs
@@ -0,0 +1,96 @@
+﻿// // ----------------------------------------------------------------------------
+// <copyright file="MetricServiceTest.cs" company="Relativity ODA LLC">
+// © Relativity All Rights Reserved.
+// </copyright>
+// // ----------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit
+{
+	using System;
+	using System.Threading.Tasks;
+
+	using global::NUnit.Framework;
+
+	using Monitoring;
+	using Monitoring.Sinks;
+
+	using Moq;
+
+	using Relativity.Services.ServiceProxy;
+	using Relativity.Telemetry.Services.Interface;
+	using Relativity.Telemetry.Services.Metrics;
+
+	[TestFixture]
+	public class MetricServiceTest
+	{
+		private IMetricService metricService;
+		private System.Collections.Generic.List<APMMetric> loggedApmMetrics;
+		private Mock<IServiceFactory> mockServiceFactory;
+		private Mock<IMetricSinkConfig> mockMetricSinkConfig;
+
+		[SetUp]
+		public void Setup()
+		{
+			var mockApmManager = new Mock<IAPMManager>();
+			mockApmManager.Setup(foo => foo.LogCountAsync(It.IsAny<APMMetric>(), It.IsAny<long>())).Callback(
+				(APMMetric metric, long count) => this.loggedApmMetrics.Add(metric)).Returns(Task.CompletedTask);
+			this.mockServiceFactory = new Mock<IServiceFactory>();
+			this.mockServiceFactory.Setup(foo => foo.CreateProxy<IAPMManager>()).Returns(mockApmManager.Object);
+
+			this.loggedApmMetrics = new System.Collections.Generic.List<APMMetric>();
+			this.mockMetricSinkConfig = new Mock<IMetricSinkConfig>();
+			this.mockMetricSinkConfig.Setup(foo => foo.ThrottleTimeout).Returns(TimeSpan.FromSeconds(30));
+			this.mockMetricSinkConfig.Setup(foo => foo.SendSumMetrics).Returns(true);
+			this.mockMetricSinkConfig.Setup(foo => foo.SendApmMetrics).Returns(true);
+		}
+
+		[Test]
+		public void ShouldLogApmMetricsWhenEnabled()
+		{
+			// Arrange
+			this.metricService = new MetricService(this.mockMetricSinkConfig.Object, this.mockServiceFactory.Object);
+
+			// Act
+			this.metricService.Log(new MetricJobEndReport());
+			this.metricService.Log(new MetricJobStarted());
+			this.metricService.Log(new MetricJobProgress());
+
+			// Assert
+			Assert.AreEqual(3, this.loggedApmMetrics.Count);
+		}
+
+		[Test]
+		public void ShouldNotLogApmMetricsWhenDisabled()
+		{
+			// Arrange
+			this.mockMetricSinkConfig.Setup(foo => foo.SendApmMetrics).Returns(false);
+			this.metricService = new MetricService(this.mockMetricSinkConfig.Object, this.mockServiceFactory.Object);
+
+			// Act
+			this.metricService.Log(new MetricJobEndReport());
+			this.metricService.Log(new MetricJobStarted());
+			this.metricService.Log(new MetricJobProgress());
+
+			// Assert
+			Assert.AreEqual(0, this.loggedApmMetrics.Count);
+		}
+
+		[Test]
+		public void ShouldUpdateSinksConfiguration()
+		{
+			// Arrange
+			this.metricService = new MetricService(this.mockMetricSinkConfig.Object, this.mockServiceFactory.Object);
+			this.mockMetricSinkConfig.Setup(foo => foo.SendApmMetrics).Returns(false);
+			this.mockMetricSinkConfig.Setup(foo => foo.SendSumMetrics).Returns(false);
+			this.metricService.MetricSinkConfig = this.mockMetricSinkConfig.Object;
+
+			// Act
+			this.metricService.Log(new MetricJobEndReport());
+			this.metricService.Log(new MetricJobStarted());
+			this.metricService.Log(new MetricJobProgress());
+
+			// Assert
+			Assert.AreEqual(0, this.loggedApmMetrics.Count);
+		}
+	}
+}
---
+++ Source/Relativity.DataExchange.NUnit/Monitoring/Sinks/MetricSinkApmTest.cs
@@ -0,0 +1,65 @@
+﻿// // ----------------------------------------------------------------------------
+// <copyright file="MetricSinkApmTest.cs" company="Relativity ODA LLC">
+// © Relativity All Rights Reserved.
+// </copyright>
+// // ----------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit
+{
+	using System.Linq;
+	using System.Threading.Tasks;
+	using global::Monitoring.Sinks;
+	using global::NUnit.Framework;
+	using Monitoring;
+	using Moq;
+	using Relativity.Services.ServiceProxy;
+	using Relativity.Telemetry.Services.Interface;
+	using Relativity.Telemetry.Services.Metrics;
+
+	[TestFixture]
+	public class MetricSinkApmTest
+	{
+		private MetricSinkApm apmSink;
+		private System.Collections.Generic.List<APMMetric> loggedMetrics;
+
+		[SetUp]
+		public void Setup()
+		{
+			var mockApmManager = new Mock<IAPMManager>();
+			mockApmManager.Setup(foo => foo.LogCountAsync(It.IsAny<APMMetric>(), It.IsAny<long>())).Callback(
+				(APMMetric metric, long count) => this.loggedMetrics.Add(metric)).Returns(Task.CompletedTask);
+			var mockServiceFactory = new Mock<IServiceFactory>();
+			mockServiceFactory.Setup(foo => foo.CreateProxy<IAPMManager>()).Returns(mockApmManager.Object);
+
+			this.loggedMetrics = new System.Collections.Generic.List<APMMetric>();
+			this.apmSink = new MetricSinkApm(mockServiceFactory.Object, true);
+		}
+
+		[Test]
+		public void ShouldLogMetricJobStarted()
+		{
+			var metric = new MetricJobStarted();
+			this.apmSink.Log(metric);
+			Assert.AreEqual(1, this.loggedMetrics.Count);
+			Assert.AreEqual(TelemetryConstants.BucketName.METRIC_JOB_STARTED, this.loggedMetrics.Last().Name);
+		}
+
+		[Test]
+		public void ShouldLogMetricJobProgress()
+		{
+			var metric = new MetricJobProgress();
+			this.apmSink.Log(metric);
+			Assert.AreEqual(1, this.loggedMetrics.Count);
+			Assert.AreEqual(TelemetryConstants.BucketName.METRIC_JOB_PROGRESS, this.loggedMetrics.Last().Name);
+		}
+
+		[Test]
+		public void ShouldLogMetricJobEndReport()
+		{
+			var metric = new MetricJobEndReport();
+			this.apmSink.Log(metric);
+			Assert.AreEqual(1, this.loggedMetrics.Count);
+			Assert.AreEqual(TelemetryConstants.BucketName.METRIC_JOB_END_REPORT, this.loggedMetrics.Last().Name);
+		}
+	}
+}
