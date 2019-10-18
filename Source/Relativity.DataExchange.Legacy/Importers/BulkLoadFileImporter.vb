099f43023e642306d5bf6b6a10184c8696794caf
Roland ten Have <roland.tenHave@relativity.com>
2019-10-18 10:25:59 +0200

    Revert "reset 1 public property"
    This reverts commit 84b6a4e98b25aa11635116b9f2c1857596f077ea.

--- Source/Relativity.DataExchange.Legacy/Importers/BulkLoadFileImporter.vb
+++ Source/Relativity.DataExchange.Legacy/Importers/BulkLoadFileImporter.vb
@@ -215,8 +215,8 @@ Namespace kCura.WinEDDS
 				Return retval
 			End Get
 		End Property
-		
-		Public ReadOnly Property UnmappedRelationalFields() As System.Collections.ArrayList
+
+		Public ReadOnly Property UnmappedRelationalFields() As List(Of EDDS.WebAPI.DocumentManagerBase.Field)
 			Get
 				If _unmappedRelationalFields Is Nothing Then
 					Dim mappedRelationalFieldIds As New List(Of Int32)
@@ -232,7 +232,7 @@ Namespace kCura.WinEDDS
 						End If
 					Next
 				End If
-				Return new System.Collections.ArrayList(_unmappedRelationalFields)
+				Return _unmappedRelationalFields
 			End Get
 		End Property
 
