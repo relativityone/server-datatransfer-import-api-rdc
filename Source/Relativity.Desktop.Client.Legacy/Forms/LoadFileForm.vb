b3f2de7a56ae7eb60133cf58fa01a0c4ff0817b6
Roland ten Have <Roland.tenHave@relativity.com>
2019-10-25 06:09:14 -0500

    removed dead code

--- Source/Relativity.Desktop.Client.Legacy/Forms/LoadFileForm.vb
+++ Source/Relativity.Desktop.Client.Legacy/Forms/LoadFileForm.vb
@@ -1050,16 +1050,7 @@ Namespace Relativity.Desktop.Client
 				Return False
 			End If
 			Me.LoadFile.FieldMap = Utility.ExtractFieldMap(_fieldMap.FieldColumns, _fieldMap.LoadFileColumns, currentFields, Me.LoadFile.ArtifactTypeID, Me.LoadFile.ObjectFieldIdListContainsArtifactId)
-			'Dim groupIdentifier As DocumentField = _application.CurrentGroupIdentifierField
-			'If _identifiersDropDown.SelectedIndex > 0 Then
-			'	Dim columnname As String = CType(_identifiersDropDown.SelectedItem, String)
-			'	Dim openParenIndex As Int32 = columnname.LastIndexOf("("c) + 1
-			'	Dim closeParenIndex As Int32 = columnname.LastIndexOf(")"c)
-			'	Dim fieldColumnIndex As Int32 = Int32.Parse(columnname.Substring(openParenIndex, closeParenIndex - openParenIndex)) - 1
-			'	If Not groupIdentifier Is Nothing Then
-			'		Me.LoadFile.FieldMap.Add(New kCura.WinEDDS.LoadFileFieldMap.LoadFileFieldMapItem(groupIdentifier, fieldColumnIndex))
-			'	End If
-			'End If
+
 			If LoadFile.ArtifactTypeID = 0 Then
 				LoadFile.ArtifactTypeID = _application.ArtifactTypeID
 			End If
@@ -1222,7 +1213,7 @@ Namespace Relativity.Desktop.Client
 				Await Me.MarkIdentifierField(caseFields)
 				_fieldMap.FieldColumns.LeftSearchableList.AddFields(caseFields)
 			End If
-			'_identifiersDropDown.Items.AddRange(_application.IdentiferFieldDropdownPopulator)
+			'_identifiersDropDown.Items.AddRange(_application.IdentifierFieldDropdownPopulator)
 			_overwriteDropdown.SelectedItem = Me.GetOverwriteDropdownItem(LoadFile.OverwriteDestination)
 			_overlayBehavior.SelectedItem = Me.GetOverlayBehaviorDropdownItem(LoadFile.OverlayBehavior)
 			_overlayBehavior.Enabled = Await IsOverlayBehaviorEnabled()
@@ -1285,10 +1276,6 @@ Namespace Relativity.Desktop.Client
 					Next
 				End If
 			End If
-			'If Not Me.LoadFile.GroupIdentifierColumn Is Nothing AndAlso Me.LoadFile.GroupIdentifierColumn <> "" AndAlso _
-			''_identifiersDropDown.Items.Contains(LoadFile.GroupIdentifierColumn) Then
-			'	'_identifiersDropDown.SelectedItem = LoadFile.GroupIdentifierColumn
-			'End If
 
 			If Me.LoadFile.ArtifactTypeID = ArtifactType.Document Then
 				_extractedTextValueContainsFileLocation.Enabled = Await Me.AnyLongTextIsMapped
@@ -1303,13 +1290,6 @@ Namespace Relativity.Desktop.Client
 
 			_fullTextFileEncodingPicker.Enabled = _extractedTextValueContainsFileLocation.Enabled And _extractedTextValueContainsFileLocation.Checked
 
-			'If LoadFile.OverwriteDestination AndAlso Not LoadFile.SelectedIdentifierField Is Nothing Then
-			'	_overWrite.Checked = True
-			'	caseFieldName = _application.GetSelectedIdentifier(LoadFile.SelectedIdentifierField)
-			'	If caseFieldName <> String.Empty Then
-			'		_identifiersDropDown.SelectedItem = caseFieldName
-			'	End If
-			'End If
 			_fieldMap.FieldColumns.EnsureHorizontalScrollbars()
 			_fieldMap.LoadFileColumns.EnsureHorizontalScrollbars()
 			_startLineNumber.Value = CType(LoadFile.StartLineNumber, Decimal)
@@ -1325,9 +1305,6 @@ Namespace Relativity.Desktop.Client
 				If _loadFile.CookieContainer Is Nothing Then
 					_loadFile.CookieContainer = Global.Relativity.Desktop.Client.Application.Instance.CookieContainer
 				End If
-				'If _loadFile.Identity Is Nothing Then
-				'	_loadFile.Identity = Global.Relativity.Desktop.Client.Application.Instance.Identity
-				'End If
 				Return _loadFile
 			End Get
 			Set(ByVal value As kCura.WinEDDS.LoadFile)
@@ -1601,7 +1578,6 @@ Namespace Relativity.Desktop.Client
 		Private Sub _filePath_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _filePath.TextChanged
 			ActionMenuEnabled = ReadyToRun
 			LoadFile.FilePath = _filePath.Text
-			'RefreshNativeFilePathFieldAndFileColumnHeaders()
 		End Sub
 
 		Private Async Sub ImportFileMenu_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ImportFileMenu.Click
@@ -1615,7 +1591,6 @@ Namespace Relativity.Desktop.Client
 		End Sub
 
 		Private Sub LoadFileForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-			'Relativity.Desktop.Client.EnhancedMenuProvider.Hook(Me)
 			_loadFileEncodingPicker.InitializeDropdown()
 			_fullTextFileEncodingPicker.InitializeDropdown()
 			_importMenuForceFolderPreviewItem.Checked = _application.TemporaryForceFolderPreview
@@ -1632,9 +1607,6 @@ Namespace Relativity.Desktop.Client
 		Private Sub _loadNativeFiles_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _loadNativeFiles.CheckedChanged
 			_nativeFilePathField.Enabled = _loadNativeFiles.Checked
 			_advancedButton.Enabled = _loadNativeFiles.Checked
-			'If Not _nativeFilePathField.Items.Count = 0 Then
-			'	_nativeFilePathField.SelectedItem = _nativeFilePathField.Items(0)
-			'End If
 			_nativeFilePathField.SelectedItem = Nothing
 			_nativeFilePathField.Text = "Select ..."
 			ActionMenuEnabled = ReadyToRun
@@ -1727,7 +1699,6 @@ Namespace Relativity.Desktop.Client
 			If Not System.IO.File.Exists(_saveFieldMapDialog.FileName) Then
 				System.IO.File.Create(_saveFieldMapDialog.FileName).Close()
 			End If
-			'PopulateLoadFileObject()
 			_application.SaveLoadFile(Me.LoadFile, _saveFieldMapDialog.FileName)
 			Me.Cursor = System.Windows.Forms.Cursors.Default
 		End Sub
