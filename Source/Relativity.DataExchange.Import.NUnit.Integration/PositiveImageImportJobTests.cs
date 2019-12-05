92e836e44fada0a3e2ac54ca8c30a1334ea1dd48
Michał Wojarski <michal.wojarski@relativity.com>
2019-12-05 15:27:02 +0100

    REL-383738 Added image import positive integration test for JPEG and TIFF

---
+++ Source/Relativity.DataExchange.Import.NUnit.Integration/PositiveImageImportJobTests.cs
@@ -0,0 +1,89 @@
+// -----------------------------------------------------------------------------------------------------
+// <copyright file="PositiveImageImportJobTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// <summary>
+//   Represents positive import job tests.
+// </summary>
+// -----------------------------------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.Import.NUnit.Integration
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Data;
+	using System.Globalization;
+	using System.Linq;
+
+	using global::NUnit.Framework;
+
+	using Relativity.DataExchange.Import.NUnit.Integration.Dto;
+	using Relativity.DataExchange.Media;
+	using Relativity.DataExchange.TestFramework;
+	using Relativity.DataExchange.Transfer;
+	using Relativity.Testing.Identification;
+
+	[TestFixture]
+	[Feature.DataTransfer.ImportApi.Operations.ImportDocuments]
+	public class PositiveImageImportJobTests : ImageImportJobTestBase
+	{
+		[Category(TestCategories.ImportDoc)]
+		[Category(TestCategories.Integration)]
+		[Category(TestCategories.TransferApi)]
+		[IdentifiedTest("9db2e7f4-0bc8-46a8-9e95-621ca9bcc5c1")]
+		[Test]
+		[Pairwise]
+		public void ShouldImportTheFiles(
+			[Values(TapiClient.Aspera, TapiClient.Direct, TapiClient.Web)] TapiClient client,
+			[Values(true, false)] bool disableImageLocationValidation,
+			[Values(true, false)] bool disableImageTypeValidation,
+			[Values(ImageFormat.Jpeg, ImageFormat.Tiff)] ImageFormat imageFormat)
+		{
+			// ARRANGE
+			TapiClientModeAvailabilityChecker.SkipTestIfModeNotAvailable(AssemblySetup.TestParameters, client);
+
+			ForceClient(client);
+			AppSettings.Instance.DisableImageLocationValidation = disableImageLocationValidation;
+			AppSettings.Instance.DisableImageTypeValidation = disableImageTypeValidation;
+
+			this.GivenTheImportJob();
+			this.GiveImageFilePathSourceDocumentImportJob();
+
+			const int NumberOfDocumentsToImport = 5;
+			const int NumberOfImagesPerDocument = 3;
+
+			using (var dataTable = new DataTable())
+			{
+				dataTable.Locale = CultureInfo.InvariantCulture;
+				GenerateRandomDataTable(dataTable, this.TempDirectory.Directory, NumberOfDocumentsToImport, NumberOfImagesPerDocument, imageFormat);
+
+				// ACT
+				this.WhenExecutingTheJob(dataTable);
+			}
+
+			// ASSERT
+			const int TotalNumberOfImagesToImport = NumberOfDocumentsToImport * NumberOfImagesPerDocument;
+			this.ThenTheImportJobIsSuccessful(TotalNumberOfImagesToImport);
+			Assert.That(this.TestJobResult.JobMessages, Has.Count.Positive);
+
+			// test result job returns ProgressCompletedRows larger by 1 than real files count
+			Assert.That(this.TestJobResult.ProgressCompletedRows, Has.Count.EqualTo(TotalNumberOfImagesToImport + 1));
+		}
+
+		private static void GenerateRandomDataTable(DataTable dataTable, string directory, int numberOfDocumentsToImport, int numberOfImagesPerDocument, ImageFormat imageFormat)
+		{
+			dataTable.Columns.Add("BatesNumber", typeof(string));
+			dataTable.Columns.Add("DocumentIdentifier", typeof(string));
+			dataTable.Columns.Add("FileLocation", typeof(string));
+
+			for (int i = 1; i <= numberOfDocumentsToImport; i++)
+			{
+				string documentIdentifier = $"{Guid.NewGuid():D}";
+				for (int j = 1; j <= numberOfImagesPerDocument; j++)
+				{
+					dataTable.Rows.Add($"{documentIdentifier}_{j}", $"{documentIdentifier}", RandomHelper.NextImageFile(imageFormat, directory));
+				}
+			}
+		}
+	}
+}
--- Source/Relativity.DataExchange.TestFramework/RandomHelper.cs
+++ Source/Relativity.DataExchange.TestFramework/RandomHelper.cs
@@ -9,6 +9,8 @@ namespace Relativity.DataExchange.TestFramework
 	using System;
 	using System.Collections;
 	using System.Collections.Generic;
+	using System.Drawing;
+	using System.Drawing.Imaging;
 	using System.IO;
 	using System.Linq;
 	using System.Security.Cryptography;
@@ -29,6 +31,18 @@ namespace Relativity.DataExchange.TestFramework
 		/// The random generator instance.
 		/// </summary>
 		private static readonly RandomGenerator RandomGeneratorInstance = new RandomGenerator();
+
+		/// <summary>
+		/// Gets random file name.
+		/// </summary>
+		/// <returns>File name.</returns>
+		public static string GetRandomFileName
+		{
+			get
+			{
+				return $"Iapi_TestFile_{DateTime.Now.Ticks}_{Guid.NewGuid():D}";
+			}
+		}
 
 		/// <summary>
 		/// Creates a new binary file whose file size is between <paramref name="minLength"/> and <paramref name="maxLength"/>.
@@ -179,7 +193,7 @@ namespace Relativity.DataExchange.TestFramework
 		/// </returns>
 		public static string NextTextFile(int minLength, int maxLength, string directory, bool readOnly)
 		{
-			var fileName = "Iapi_TestFile_" + DateTime.Now.Ticks + "_" + Guid.NewGuid().ToString("D");
+			var fileName = GetRandomFileName;
 			var file = NextTextFile(minLength, maxLength, directory, fileName);
 			if (!readOnly)
 			{
@@ -225,6 +239,188 @@ namespace Relativity.DataExchange.TestFramework
 				File.WriteAllText(file, text);
 				return file;
 			}
+		}
+
+		/// <summary>
+		/// Creates a new random pdf file with text content whose text size is between <paramref name="minLength"/> and <paramref name="maxLength"/>.
+		/// </summary>
+		/// <param name="minLength">
+		/// The minimum file length.
+		/// </param>
+		/// <param name="maxLength">
+		/// The maximum file length.
+		/// </param>
+		/// <param name="directory">
+		/// The directory to create the file.
+		/// </param>
+		/// <returns>
+		/// The file.
+		/// </returns>
+		/// This method should be used later in production import integration tests
+		public static string NextPdfFile(int minLength, int maxLength, string directory)
+		{
+			var fileName = GetRandomFileName;
+			var file = NextPdfFile(minLength, maxLength, directory, fileName);
+
+			return file;
+		}
+
+		/// <summary>
+		///  Creates a new random pdf file with text content whose text size is between <paramref name="minTextLength"/> and <paramref name="maxTextLength"/>.
+		/// </summary>
+		/// <param name="minTextLength">
+		/// The minimum value.
+		/// </param>
+		/// <param name="maxTextLength">
+		/// The maximum value.
+		/// </param>
+		/// <param name="directory">
+		/// The directory to create the file.
+		/// </param>
+		/// <param name="fileName">
+		/// The file name.
+		/// </param>
+		/// <returns>
+		/// The file.
+		/// </returns>
+		public static string NextPdfFile(int minTextLength, int maxTextLength, string directory, string fileName)
+		{
+			Directory.CreateDirectory(directory);
+
+			if (string.IsNullOrEmpty(Path.GetExtension(fileName)))
+			{
+				fileName = Path.ChangeExtension(fileName, ".pdf");
+			}
+
+			var text = NextString(minTextLength, maxTextLength);
+			var file = Path.Combine(directory, fileName);
+
+			using (FileStream fs = new FileStream(file, FileMode.Create))
+			using (var document = new iTextSharp.text.Document())
+			{
+				iTextSharp.text.pdf.PdfWriter.GetInstance(document, fs);
+				document.Open();
+				document.Add(new iTextSharp.text.Paragraph(text));
+			}
+
+			return file;
+		}
+
+		/// <summary>
+		/// Creates a new random image file.
+		/// </summary>
+		/// <param name="imageFormat">
+		/// The image format. Jpeg and Tiff are supported.
+		/// </param>
+		/// <param name="directory">
+		/// The directory to create the file.
+		/// </param>
+		/// <returns>
+		/// The file.
+		/// </returns>
+		public static string NextImageFile(Relativity.DataExchange.Media.ImageFormat imageFormat, string directory)
+		{
+			var fileName = GetRandomFileName;
+			var imageWidth = 200;
+			var imageHeight = 200;
+
+			switch (imageFormat)
+			{
+				case Relativity.DataExchange.Media.ImageFormat.Jpeg:
+					return NextJpegFile(directory, fileName, imageWidth, imageHeight);
+				case Relativity.DataExchange.Media.ImageFormat.Tiff:
+					return NextTiffFile(directory, fileName, imageWidth, imageHeight);
+			}
+
+			throw new InvalidOperationException("Not supported image format");
+		}
+
+		/// <summary>
+		///  Creates a new random jpg file whose resolution is defined by <paramref name="width"/> and <paramref name="height"/>.
+		/// </summary>
+		/// <param name="directory">
+		/// The directory to create the file.
+		/// </param>
+		/// <param name="fileName">
+		/// The file name.
+		/// </param>
+		/// <param name="width">
+		/// The image width.
+		/// </param>
+		/// <param name="height">
+		/// The image height.
+		/// </param>
+		/// <returns>
+		/// The file.
+		/// </returns>
+		public static string NextJpegFile(string directory, string fileName, int width, int height)
+		{
+			Directory.CreateDirectory(directory);
+
+			if (string.IsNullOrEmpty(Path.GetExtension(fileName)))
+			{
+				fileName = Path.ChangeExtension(fileName, ".jpeg");
+			}
+
+			var file = Path.Combine(directory, fileName);
+			using (var bitmap = new Bitmap(width, height))
+			{
+				// adding anything to bitmap
+				var graphics = Graphics.FromImage(bitmap);
+				graphics.Clear(System.Drawing.Color.Orange);
+				bitmap.Save(file, System.Drawing.Imaging.ImageFormat.Jpeg);
+			}
+
+			return file;
+		}
+
+		/// <summary>
+		///  Creates a new random tiff file whose resolution is defined by <paramref name="width"/> and <paramref name="height"/>.
+		/// </summary>
+		/// <param name="directory">
+		/// The directory to create the file.
+		/// </param>
+		/// <param name="fileName">
+		/// The file name.
+		/// </param>
+		/// <param name="width">
+		/// The image width.
+		/// </param>
+		/// <param name="height">
+		/// The image height.
+		/// </param>
+		/// <returns>
+		/// The file.
+		/// </returns>
+		public static string NextTiffFile(string directory, string fileName, int width, int height)
+		{
+			Directory.CreateDirectory(directory);
+
+			if (string.IsNullOrEmpty(Path.GetExtension(fileName)))
+			{
+				fileName = Path.ChangeExtension(fileName, ".tiff");
+			}
+
+			var file = Path.Combine(directory, fileName);
+			using (var bitmap = new Bitmap(width, height))
+			{
+				// adding anything to bitmap
+				var graphics = Graphics.FromImage(bitmap);
+				graphics.Clear(System.Drawing.Color.Orange);
+
+				// there is some special logic as Relativity requires Tiff Group4 compression
+				ImageCodecInfo encoder = GetEncoder("image/tiff");
+				var myEncoder = Encoder.Compression;
+				using (var encoderParameters = new EncoderParameters(1))
+				{
+					var encoderParameter = new EncoderParameter(myEncoder, (long)EncoderValue.CompressionCCITT4);
+					encoderParameters.Param[0] = encoderParameter;
+
+					bitmap.Save(file, encoder, encoderParameters);
+				}
+			}
+
+			return file;
 		}
 
 		/// <summary>
@@ -441,6 +637,21 @@ namespace Relativity.DataExchange.TestFramework
 			}
 
 			return list[random.NextIndex(list)];
+		}
+
+		private static ImageCodecInfo GetEncoder(string mimeType)
+		{
+			ImageCodecInfo[] encoders;
+			encoders = ImageCodecInfo.GetImageEncoders();
+			for (int i = 0; i < encoders.Length; ++i)
+			{
+				if (encoders[i].MimeType == mimeType)
+				{
+					return encoders[i];
+				}
+			}
+
+			return null;
 		}
 	}
 }
