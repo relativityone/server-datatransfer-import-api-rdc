a59452c5260f5c66c401a8491d9d5926814875f7
Michał Wojarski <michal.wojarski@relativity.com>
2020-01-16 12:22:53 +0000

    Merge pull request #304 in DTX/import-api-rdc from REL-389150-improve-om-long-text-retry-ux to relativity-indigo
    Squashed commit of the following:
    
    commit d943eb6f431a43c9e966067a5c7b5090b6a0c99c
    Author: Scott Parillo <scott.parillo@relativity.com>
    Date:   Tue Dec 24 16:31:01 2019 -0800
    
        REL-389150
        - Added an interface to support notification messages during retry attempts.
        - Modified LongTextStreamService to supply notification messages during OnRetry methods.
        - Refactored existing retry messages into ErrorMessageFormatter and added a new method to format server related retry messages.

---
+++ Source/Relativity.DataExchange.NUnit/ErrorMessageFormatterTests.cs
@@ -0,0 +1,86 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// <copyright file="ErrorMessageFormatterTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// <summary>
+//   Represents <see cref="ErrorMessageFormatter"/> tests.
+// </summary>
+// -----------------------------------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit
+{
+	using System;
+	using System.Collections;
+	using System.Text;
+
+	using global::NUnit.Framework;
+
+	/// <summary>
+	/// Represents <see cref="ErrorMessageFormatter"/> tests.
+	/// </summary>
+	[TestFixture]
+	public class ErrorMessageFormatterTests
+	{
+		private static IEnumerable WebServiceRetryTestCases
+		{
+			get
+			{
+				string expected1 =
+					"A non-fatal issue occurred attempting to call the 'Ping' web service: Endpoint does not exist. Retrying in 30 second(s) - 9 retry(s) left.";
+				yield return new TestCaseData("Ping", "Endpoint does not exist", 30, 1, 10, expected1);
+				yield return new TestCaseData("Ping", "Endpoint does not exist.", 30, 1, 10, expected1);
+				string expected2 =
+					"A non-fatal issue occurred attempting to call the 'Ping' web service: Endpoint does not exist. Retrying in 10 second(s) - 1 retry(s) left.";
+				yield return new TestCaseData("Ping", "Endpoint does not exist", 10, 9, 10, expected2);
+				yield return new TestCaseData("Ping", "Endpoint does not exist.", 10, 9, 10, expected2);
+				string expected3 =
+					"A non-fatal issue occurred attempting to call the 'Ping' web service: Endpoint does not exist. No more retry attempts left.";
+				yield return new TestCaseData("Ping", "Endpoint does not exist", 10, 10, 10, expected3);
+				yield return new TestCaseData("Ping", "Endpoint does not exist.", 10, 10, 10, expected3);
+			}
+		}
+
+		private static IEnumerable AppendRetryDetailsTestCases
+		{
+			get
+			{
+				string expected1 = "The file does not exist. Retrying in 30 second(s) - 5 retry(s) left.";
+				yield return new TestCaseData("The file does not exist.", 30, 5, expected1);
+				string expected2 = "The file does not exist. No more retry attempts left.";
+				yield return new TestCaseData("The file does not exist.", 10, 0, expected2);
+			}
+		}
+
+		[TestCaseSource(nameof(WebServiceRetryTestCases))]
+		public void ShouldFormatTheWebServiceRetryMessage(
+			string serviceOperation,
+			string errorMessage,
+			int durationSeconds,
+			int retryAttempt,
+			int maxRetries,
+			string expectedMessage)
+		{
+			string formattedMessage = ErrorMessageFormatter.FormatWebServiceRetryMessage(
+				serviceOperation,
+				errorMessage,
+				TimeSpan.FromSeconds(durationSeconds),
+				retryAttempt,
+				maxRetries);
+			Assert.That(formattedMessage, Is.EqualTo(expectedMessage));
+		}
+
+		[TestCaseSource(nameof(AppendRetryDetailsTestCases))]
+		public void ShouldAppendTheRetryDetailsToTheMessage(
+			string message,
+			int durationSeconds,
+			int retriesLeft,
+			string expectedMessage)
+		{
+			string appendedMessage = ErrorMessageFormatter.AppendRetryDetails(
+				message,
+				TimeSpan.FromSeconds(durationSeconds),
+				retriesLeft);
+			Assert.That(appendedMessage, Is.EqualTo(expectedMessage));
+		}
+	}
+}
