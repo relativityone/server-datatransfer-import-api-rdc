fcf186fa03d5f522366c2697dce5eee654c2d46d
Szymon Choinski <szymon.choinski@relativity.com>
2020-03-25 12:54:17 +0000

    Merge pull request #411 in DTX/import-api-rdc from REL-408216-DisableAnalyzers to develop
    Squashed commit of the following:
    
    commit 44f72e8cc097325fc6e0fb0ce71562c791284c2c
    Author: Szymon Choinski <szymon.choinski@relativity.com>
    Date:   Thu Mar 19 11:47:43 2020 +0100
    
        REL-413410 Fixing tabs.
    
    commit cffabf6073c05ba9ddd63ac142e1ecec2187295f
    Author: Szymon Choinski <szymon.choinski@relativity.com>
    Date:   Thu Mar 19 11:44:36 2020 +0100
    
        REL-413410 Fixing an oversight.
    
    commit efc55db37b92131741556f210ad602ae840fe525
    Author: Szymon Choinski <szymon.choinski@relativity.com>
    Date:   Thu Mar 19 11:40:01 2020 +0100
    
        REL-413423 Disable FxCopCmd for Debug
    
    commit 35aec7d29ffa2dd933fae84adfe1b3a4506c081c
    Author: Szymon Choinski <szymon.choinski@relativity.com>
    Date:   Thu Mar 19 11:17:41 2020 +0100
    
        REL-413410 Adding script for disabling the analyzers for Debug
    
    commit 6f824230afb6dc899871d540c677ee17dad8639e
    Author: Szymon Choinski <szymon.choinski@relativity.com>
    Date:   Thu Mar 19 11:14:39 2020 +0100
    
        REL-413417 Upgrade analyzers

---
+++ buildtools/DisableAnalyzersForDebug.ps1
@@ -0,0 +1,22 @@
+﻿Add-Type -AssemblyName 'System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+Add-Type -AssemblyName 'System.Xml.Linq, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+
+[string]$vs = "http://schemas.microsoft.com/developer/msbuild/2003"
+$xmlNamespaceManager = New-Object -TypeName System.Xml.XmlNamespaceManager -ArgumentList (New-Object -TypeName System.Xml.NameTable)
+$xmlNamespaceManager.AddNamespace("vs",$vs)
+
+foreach ($filename in Get-ChildItem -Path ..\ -Recurse -Filter *.??proj -File)
+{
+    [System.Xml.Linq.XDocument]$document = [System.Xml.Linq.XDocument]::Load($filename.FullName, [System.Xml.Linq.LoadOptions]::PreserveWhitespace)
+    foreach ($itemGroup in [System.Xml.XPath.Extensions]::XPathSelectElements($document, "/vs:Project/vs:ItemGroup[vs:Analyzer]", $xmlNamespaceManager))
+    {
+        [System.Xml.Linq.XAttribute]$condition = New-Object -TypeName System.Xml.Linq.XAttribute -ArgumentList "Condition", "'`$(Configuration)'!='Debug'"
+
+        [System.Xml.Linq.XElement]$when = New-Object -TypeName System.Xml.Linq.XElement -ArgumentList ([System.Xml.Linq.XName]::Get("When",$vs), $condition, $itemGroup)
+
+        [System.Xml.Linq.XElement]$choose = New-Object -TypeName System.Xml.Linq.XElement -ArgumentList ([System.Xml.Linq.XName]::Get("Choose",$vs), $when)
+
+        $itemGroup.ReplaceWith($choose)
+    }
+    $document.Save($filename.FullName)
+}
