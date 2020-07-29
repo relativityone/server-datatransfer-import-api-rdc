ab16b00134e4070966a4ca6e3d7fea806749c59b
Roland ten Have <Roland.tenHave@relativity.com>
2020-07-29 10:45:57 +0200

    changed to local git call

--- Trident/Tools/Tools.groovy
+++ Trident/Tools/Tools.groovy
@@ -64,7 +64,7 @@ def runCommandWithOutput(String command)
 def tagGitCommit(String commitHash, String tag, String username, String password) {
     try {
         powershell """
-				\$serverHasTag = git ls-remote origin refs/tags/$tag
+				\$serverHasTag = $(git tag -l "$tag")
 				if(\$serverHasTag)
 				{
 					\$variableAsJson = ConvertTo-Json -\$serverHasTag
