9a1b841a1be3241b5d30de0a95636c9a1e3c1f2d
Małgorzata Kwiatkowska <malgorzata.kwiatkowska@relativity.com>
2020-09-22 09:13:36 +0000

    Merge pull request #750 in DTX/import-api-rdc from REL-476356-update-hopper-images-used-in-tests-ninebark to release-1.12-ninebark
    Squashed commit of the following:
    
    commit 3966e36dc4dd05cdc5a006dc02e3e32e82fefde6
    Author: Małgorzata Kwiatkowska <malgorzata.kwiatkowska@relativity.com>
    Date:   Tue Sep 22 09:18:08 2020 +0200
    
        REL-476356-update-hopper-images-used-in-tests
    
        Use hopper image to aio-lanceleaf-0 for tests on lanceleaf version

--- Trident/Jobs/loadtest.groovy
+++ Trident/Jobs/loadtest.groovy
@@ -10,7 +10,7 @@ properties([
 	parameters([
 		choice(defaultValue: 'Release', choices: ["Release","Debug"], description: 'Build config', name: 'buildConfig'),
 		choice(defaultValue: 'normal', choices: ["quiet", "minimal", "normal", "detailed", "diagnostic"], description: 'Build verbosity', name: 'buildVerbosity'),
-		choice(defaultValue: 'aio-mayapple-eau', choices: ["lanceleafAA1","aio-mayapple-eau"], description: 'The template used to prepare hopper instance', name: 'hopperTemplate'),
+		string(defaultValue: 'aio-mayapple-eau', description: 'The template used to prepare hopper instance', name: 'hopperTemplate'),
 		string(defaultValue: 'develop', description: 'Name of folder in bld-pkgs Packages Relativity', name: 'relativityInstallerSource'),
 	]),
 	pipelineTriggers([cron(cronString)])
