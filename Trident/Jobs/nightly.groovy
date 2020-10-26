3a18dd1f73e63d8bc95fb7a05c6c0d823d9daff4
Krzysztof Geleta <krzysztof.geleta@relativity.com>
2020-10-26 09:35:21 +0000

    Merge pull request #807 in DTX/import-api-rdc from REL-489013-importapi-update-hopper-templates-mayapple to release-1.11-mayapple
    Squashed commit of the following:
    
    commit c7ed56b5e0fa415f2fb3d06ce61237ec06e6f631
    Author: Krzysztof Geleta <krzysztof.geleta@relativity.com>
    Date:   Mon Oct 26 07:12:41 2020 +0100
    
        updated hopper templates

--- Trident/Jobs/nightly.groovy
+++ Trident/Jobs/nightly.groovy
@@ -9,7 +9,7 @@ properties([
 	parameters([
 		choice(defaultValue: 'Release', choices: ["Release","Debug"], description: 'Build config', name: 'buildConfig'),
 		choice(defaultValue: 'normal', choices: ["quiet", "minimal", "normal", "detailed", "diagnostic"], description: 'Build verbosity', name: 'buildVerbosity'),
-		string(defaultValue: 'aio-larkspur-3,aio-blazingstar-3,aio-foxglove-3,aio-goatsbeard-3,aio-indigo-2,aio-juniper-2,aio-lanceleaf-0,aio-mayapple-eau', description: 'Comma separated list of SUT templates', name: 'temlatesStr')
+		string(defaultValue: 'aio-larkspur-3,aio-blazingstar-3,aio-foxglove-3,aio-goatsbeard-3,aio-indigo-2,aio-juniper-2,aio-lanceleaf-0,aio-mayapple-0', description: 'Comma separated list of SUT templates', name: 'temlatesStr')
 	]),
 	pipelineTriggers([cron(cronString)])
 ])
