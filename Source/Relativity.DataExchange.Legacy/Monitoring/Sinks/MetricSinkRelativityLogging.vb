906c16cb7f7de516827093989e0de420bc5fb321
Wojciech Niedźwiedź <wojciech.niedzwiedz@relativity.com>
2020-12-16 16:30:15 +0000

    Merge pull request #862 in DTX/import-api-rdc from REL-500844-iapi-metrics-ninebark to release-1.12-ninebark
    Squashed commit of the following:
    
    commit 2ed66489fcfeda6b12bdeb869fdd4b4885e3b50f
    Merge: 034b33500 3ece3a490
    Author: Wojciech Niedźwiedź <wojciech.niedzwiedz@relativity.com>
    Date:   Wed Dec 16 15:42:02 2020 +0100
    
        Merge branch 'release-1.12-ninebark' into REL-500844-iapi-metrics-ninebark
    
    commit 034b33500184659557224c5e0fedff313442b68e
    Author: Wojciech Niedźwiedź <wojciech.niedzwiedz@relativity.com>
    Date:   Thu Dec 10 14:53:42 2020 +0100
    
        Add relativity sink.
        Modify SendMetricJobStarted to make overriding easier.
        Log metrics from LoadFile when Job is started.

---
+++ Source/Relativity.DataExchange.Legacy/Monitoring/Sinks/MetricSinkRelativityLogging.vb
@@ -0,0 +1,25 @@
+﻿Imports Relativity.Services.ServiceProxy
+Imports Relativity.Telemetry.Services.Interface
+Imports Relativity.Telemetry.Services.Metrics
+
+Namespace Monitoring.Sinks
+	Public Class MetricSinkRelativityLogging
+		Implements IMetricSink
+
+		Public Sub New (isEnabled As Boolean)
+			Me.IsEnabled = isEnabled
+		End Sub
+
+		''' <inheritdoc/>
+		Sub Log(metric As MetricBase) Implements IMetricSink.Log
+			' For now log as warning because information is not logged from RDC to Splunk
+			' For now only log MetricJobStarted
+			Dim metricJobStarted As MetricJobStarted = TryCast(metric, MetricJobStarted)
+			If Not metricJobStarted Is Nothing Then
+				Relativity.Logging.Log.Logger.LogWarning("Relativity.DataExchange metric. Bucket: {bucketName}, value: {@Metrics}", metricJobStarted.BucketName, metricJobStarted.CustomData)
+			End If
+		End Sub
+
+		Public Property IsEnabled As Boolean Implements IMetricSink.IsEnabled
+	End Class
+End NameSpace
