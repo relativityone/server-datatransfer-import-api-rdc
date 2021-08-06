359641bc1006bde90ab32b20c591a5a12ae89a42
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2021-08-06 08:41:16 +0000

    Merge pull request #1084 in DTX/import-api-rdc from REL-581803-prairiesmoke1-release-with-osier-0-template to release-1.14-prairiesmoke
    Squashed commit of the following:
    
    commit 00bf9bceb14e28570e3477efc1c28a54e0909f3d
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Aug 5 15:55:37 2021 +0200
    
        REL-580761 fixed permissions tests
    
    commit dbbe5131a9e5079a787ab5f0875d430dcb80de5f
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Aug 5 13:24:54 2021 +0200
    
        Revert "REL-581803 update hopper templates to aio-prairiesmoke-0"
    
        This reverts commit 89b578061517df53f694860d43c3ca3948cb2f26.
    
    commit 89b578061517df53f694860d43c3ca3948cb2f26
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Aug 5 11:37:33 2021 +0200
    
        REL-581803 update hopper templates to aio-prairiesmoke-0
    
    commit 366b1736416bea6d8a99209ccc0c8710be02ff6a
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Aug 5 11:34:34 2021 +0200
    
        REL-581803 updated dependencies

--- Source/Relativity.DataExchange.Import.NUnit.Integration/PermissionTests.cs
+++ Source/Relativity.DataExchange.Import.NUnit.Integration/PermissionTests.cs
@@ -79,6 +79,14 @@ namespace Relativity.DataExchange.Import.NUnit.Integration
 				this.groupId = await GroupHelper.CreateNewGroupAsync(this.TestParameters, Guid.NewGuid().ToString()).ConfigureAwait(false);
 				await GroupHelper.AddMemberAsync(this.TestParameters, this.groupId, this.userId).ConfigureAwait(false);
 				await PermissionsHelper.AddGroupToWorkspaceAsync(this.TestParameters, this.groupId).ConfigureAwait(false);
+				await PermissionsHelper.AddGroupToAdminAsync(this.TestParameters, this.groupId).ConfigureAwait(false);
+				await PermissionsHelper
+					.SetAdminObjectSecurityAsync(
+						this.TestParameters,
+						this.groupId,
+						new List<string> { "InstanceSetting" },
+						true)
+					.ConfigureAwait(false);
 			}
 		}
 
