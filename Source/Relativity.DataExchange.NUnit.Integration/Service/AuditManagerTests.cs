17249fd00e4cdaf9d4c610cbe9351820c2f6a4c1
Krzysztof Geleta <krzysztof.geleta@relativity.com>
2021-08-16 12:58:01 +0000

    Merge pull request #1107 in DTX/import-api-rdc from REL-578835-kepler-audit-manager to develop
    Squashed commit of the following:
    
    commit de7edfc01028c0be6abb73fe0b37104174f5a456
    Author: Krzysztof Geleta <krzysztof.geleta@relativity.com>
    Date:   Mon Aug 16 13:14:07 2021 +0200
    
        Changing expected message based on useKepler parameter
    
    commit d8230629a06c0b40a9aff283e187f403260e4426
    Author: Krzysztof Geleta <krzysztof.geleta@relativity.com>
    Date:   Mon Aug 16 11:24:00 2021 +0200
    
        Updated assert statements
    
    commit 808ab39db9446b3220cad9905964a4dc42738251
    Merge: 00ac918a9 273f644a0
    Author: Krzysztof Geleta <krzysztof.geleta@relativity.com>
    Date:   Mon Aug 16 10:31:14 2021 +0200
    
        merged develop
    
    commit 00ac918a9d5eacd7d45f2b77d212bc474e2e2b00
    Author: Krzysztof Geleta <krzysztof.geleta@relativity.com>
    Date:   Mon Aug 16 09:27:36 2021 +0200
    
        Added positive tests
    
    commit fd26ebf8eb4a1a6c94ffebbc1af0e332086127e2
    Author: Krzysztof Geleta <krzysztof.geleta@relativity.com>
    Date:   Fri Aug 13 09:22:47 2021 +0200
    
        Added audit manager negative test cases

---
+++ Source/Relativity.DataExchange.NUnit.Integration/Service/AuditManagerTests.cs
@@ -0,0 +1,327 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="AuditManagerTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit.Integration.Service
+{
+	using System;
+	using System.Web.Services.Protocols;
+
+	using global::NUnit.Framework;
+	using global::NUnit.Framework.Constraints;
+
+	using kCura.EDDS.WebAPI.AuditManagerBase;
+	using kCura.WinEDDS.Service;
+	using kCura.WinEDDS.Service.Replacement;
+
+	using Relativity.DataExchange.TestFramework;
+
+	[TestFixture(true)]
+	[TestFixture(false)]
+	public class AuditManagerTests : KeplerServiceTestBase
+	{
+		private const int WorkspaceRootFolderId = 1003697;
+
+		public AuditManagerTests(bool useKepler)
+			: base(useKepler)
+		{
+		}
+
+		[Test]
+		public void ShouldAuditImageImport()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			string runId = Guid.NewGuid().ToString();
+
+			ImageImportStatistics statistics = new ImageImportStatistics
+			{
+				BatchSizes = new int[0],
+				DestinationFolderArtifactID = WorkspaceRootFolderId,
+				DestinationProductionArtifactID = 0,
+				ExtractedTextDefaultEncodingCodePageID = 0,
+				ExtractedTextReplaced = false,
+				FilesCopiedToRepository = @"\emttest\DefaultFileRepository",
+				LoadFileName = "loadFile.opt",
+				NumberOfDocumentsCreated = 10,
+				NumberOfDocumentsUpdated = 0,
+				NumberOfErrors = 0,
+				NumberOfFilesLoaded = 20,
+				NumberOfWarnings = 0,
+				OverlayBehavior = null,
+				OverlayIdentifierFieldArtifactID = WellKnownFields.ControlNumberId,
+				Overwrite = OverwriteType.Append,
+				RepositoryConnection = RepositoryConnectionType.Web,
+				RunTimeInMilliseconds = 1_000,
+				SendNotification = false,
+				TotalFileSize = 1_000,
+				TotalMetadataBytes = 1_000,
+			};
+
+			using (IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc))
+			{
+				// Act
+				bool notificationSent = sut.AuditImageImport(this.TestParameters.WorkspaceId, runId, IsFatalError, statistics);
+
+				// Assert
+				Assert.False(notificationSent, "Should not send the notification.");
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditImageImportWithInvalidWorkspaceId()
+		{
+			// Arrange
+			const int InvalidWorkspaceId = 42;
+			const bool IsFatalError = false;
+			string runId = Guid.NewGuid().ToString();
+
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditImageImport(InvalidWorkspaceId, runId, IsFatalError, new ImageImportStatistics()), this.GetExpectedExceptionConstraintForNonExistingWorkspace(InvalidWorkspaceId));
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditImageImportWithNullImportStats()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			string runId = Guid.NewGuid().ToString();
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditImageImport(this.TestParameters.WorkspaceId, runId, IsFatalError, null), this.GetExceptionConstraintForNullReferenceException("AuditImageImportAsync"));
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditImageImportWithNullRunId()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditImageImport(this.TestParameters.WorkspaceId, null, IsFatalError, new ImageImportStatistics()), this.GetExceptionConstraintForNullReferenceException("AuditImageImportAsync"));
+		}
+
+		[Test]
+		public void ShouldAuditObjectImport()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			string runId = Guid.NewGuid().ToString();
+
+			ObjectImportStatistics statistics = new ObjectImportStatistics()
+			{
+				ArtifactTypeID = 10,
+				BatchSizes = new[] { 1_000 },
+				Bound = '^',
+				Delimiter = '|',
+				DestinationFolderArtifactID = WorkspaceRootFolderId,
+				ExtractedTextFileEncodingCodePageID = 0,
+				ExtractedTextPointsToFile = false,
+
+				// FieldsMapped =
+				FileFieldColumnName = string.Empty,
+				FilesCopiedToRepository = @"\emttest\DefaultFileRepository",
+				FolderColumnName = string.Empty,
+				LoadFileEncodingCodePageID = 65001,
+				LoadFileName = "loadFile.dat",
+				MultiValueDelimiter = ';',
+				NestedValueDelimiter = '\\',
+				NewlineProxy = '®',
+				NumberOfChoicesCreated = 0,
+				NumberOfDocumentsCreated = 10,
+				NumberOfDocumentsUpdated = 0,
+				NumberOfErrors = 0,
+				NumberOfFilesLoaded = 0,
+				NumberOfWarnings = 0,
+				OverlayBehavior = null,
+				OverlayIdentifierFieldArtifactID = WellKnownFields.ControlNumberId,
+				Overwrite = OverwriteType.Append,
+				RepositoryConnection = RepositoryConnectionType.Web,
+				RunTimeInMilliseconds = 1_000,
+				SendNotification = false,
+				StartLine = 0,
+				TotalFileSize = 0,
+				TotalMetadataBytes = 1_000,
+			};
+
+			using (IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc))
+			{
+				// Act
+				bool notificationSent = sut.AuditObjectImport(this.TestParameters.WorkspaceId, runId, IsFatalError, statistics);
+
+				// Assert
+				Assert.False(notificationSent, "Should not send the notification.");
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditObjectImportWithInvalidWorkspaceId()
+		{
+			// Arrange
+			const int InvalidWorkspaceId = 42;
+			const bool IsFatalError = false;
+			string runId = Guid.NewGuid().ToString();
+
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditObjectImport(InvalidWorkspaceId, runId, IsFatalError, new ObjectImportStatistics()), this.GetExpectedExceptionConstraintForNonExistingWorkspace(InvalidWorkspaceId));
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditObjectImportWithNullImportStats()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			string runId = Guid.NewGuid().ToString();
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditObjectImport(this.TestParameters.WorkspaceId, runId, IsFatalError, null), this.GetExceptionConstraintForNullReferenceException("AuditObjectImportAsync"));
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditObjectImportWithNullRunId()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditObjectImport(this.TestParameters.WorkspaceId, null, IsFatalError, new ObjectImportStatistics()), this.GetExceptionConstraintForNullReferenceException("AuditObjectImportAsync"));
+		}
+
+		[Test]
+		public void ShouldAuditExport()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			ExportStatistics exportStatistics = new ExportStatistics
+			{
+				AppendOriginalFilenames = false,
+				ArtifactTypeID = 10,
+				Bound = '^',
+				CopyFilesFromRepository = false,
+				DataSourceArtifactID = 1038123,
+				Delimiter = '|',
+				DestinationFilesystemFolder = @"path\to\export\destination\",
+				DocumentExportCount = 2,
+				ErrorCount = 0,
+				ExportImages = false,
+				ExportMultipleChoiceFieldsAsNested = true,
+				ExportNativeFiles = false,
+				ExportSearchablePDFs = false,
+				ExportTextFieldAsFiles = false,
+				ExportedTextFieldID = 0,
+				ExportedTextFileEncodingCodePage = 0,
+				Fields = new[] { WellKnownFields.ControlNumberId },
+				FileExportCount = 0,
+				FilePathSettings = "Use Relative Paths",
+				ImageFileType = ImageFileExportType.SinglePage,
+				ImageLoadFileFormat = ImageLoadFileFormatType.Opticon,
+				ImagesToExport = ImagesToExportType.Original,
+				MetadataLoadFileEncodingCodePage = 65001,
+				MetadataLoadFileFormat = LoadFileFormat.Dat,
+				MultiValueDelimiter = ';',
+				NestedValueDelimiter = '\\',
+				NewlineProxy = '®',
+				OverwriteFiles = false,
+				ProductionPrecedence = new[] { -1 },
+				RunTimeInMilliseconds = 1_000,
+				SourceRootFolderID = WorkspaceRootFolderId,
+				StartExportAtDocumentNumber = 1,
+				SubdirectoryImagePrefix = "IMG",
+				SubdirectoryMaxFileCount = 500,
+				SubdirectoryNativePrefix = "NATIVE",
+				SubdirectoryPDFPrefix = "PDF",
+				SubdirectoryStartNumber = 1,
+				SubdirectoryTextPrefix = "TEXT",
+				TextAndNativeFilesNamedAfterFieldID = WellKnownFields.ControlNumberId,
+				TotalFileBytesExported = 0,
+				TotalMetadataBytesExported = 1_000,
+				Type = "Folder",
+				VolumeMaxSize = 650,
+				VolumePrefix = "VOL",
+				VolumeStartNumber = 1,
+				WarningCount = 0,
+			};
+
+			using (IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc))
+			{
+				// Act
+				bool result = sut.AuditExport(this.TestParameters.WorkspaceId, IsFatalError, exportStatistics);
+
+				// Assert
+				Assert.False(result, "Audit Export always returns false.");
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditExportWithInvalidWorkspaceId()
+		{
+			// Arrange
+			const int InvalidWorkspaceId = 42;
+			const bool IsFatalError = false;
+
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditExport(InvalidWorkspaceId, IsFatalError, new ExportStatistics()), this.GetExpectedExceptionConstraintForNonExistingWorkspace(InvalidWorkspaceId));
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenCallingAuditExportWithNullExportStats()
+		{
+			// Arrange
+			const bool IsFatalError = false;
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.That(() => sut.AuditExport(this.TestParameters.WorkspaceId, IsFatalError, null), this.GetExceptionConstraintForNullReferenceException("AuditExportAsync"));
+		}
+
+		[Test]
+		public void ShouldNotThrowExceptionWhenCallingDeleteAuditTokenWithNull()
+		{
+			// Arrange
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.DoesNotThrow(() => sut.DeleteAuditToken(null));
+		}
+
+		[Test]
+		public void ShouldNotThrowExceptionWhenCallingDeleteAuditTokenWithInvalidGuid()
+		{
+			// Arrange
+			IAuditManager sut = ManagerFactory.CreateAuditManager(this.Credential, this.CookieContainer, this.CorrelationIdFunc);
+
+			// Act & Assert
+			Assert.DoesNotThrow(() => sut.DeleteAuditToken("invalid GUID"));
+		}
+
+		private IResolveConstraint GetExceptionConstraintForNullReferenceException(string methodName)
+		{
+			string expectedErrorMessage;
+			if (this.UseKepler)
+			{
+				expectedErrorMessage =
+					$"Error during call {methodName}." +
+					" InnerExceptionType: System.NullReferenceException," +
+					" InnerExceptionMessage: Object reference not set to an instance of an object.";
+			}
+			else
+			{
+				expectedErrorMessage = "Object reference not set to an instance of an object.";
+			}
+
+			return Throws.Exception.InstanceOf<SoapException>().With.Message.EqualTo(expectedErrorMessage);
+		}
+	}
+}
