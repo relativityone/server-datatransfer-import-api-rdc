c25f45d33000a86c9736104ee61fd9d6a638e522
Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
2021-08-16 13:07:14 +0000

    Merge pull request #1109 in DTX/import-api-rdc from REL-578849 to develop
    Squashed commit of the following:
    
    commit e626863cebe313e19d984a2b5e6b8682c811d933
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 12:58:15 2021 +0200
    
        change function names - remove login word
    
    commit 2e52fbba2d0e509a6a65daccc797868d687c9c8d
    Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
    Date:   Mon Aug 16 12:10:25 2021 +0200
    
        REL-578849 - Added test from RelativityManager

---
+++ Source/Relativity.DataExchange.NUnit.Integration/Service/KeplerRelativityManagerTests.cs
@@ -0,0 +1,103 @@
+﻿// <copyright file="KeplerRelativityManagerTests.cs" company="Relativity ODA LLC">
+// © Relativity All Rights Reserved.
+// </copyright>
+
+namespace Relativity.DataExchange.NUnit.Integration.Service
+{
+	using global::NUnit.Framework;
+
+	using kCura.WinEDDS.Service;
+	using kCura.WinEDDS.Service.Replacement;
+
+	[TestFixture(true)]
+	[TestFixture(false)]
+	public class KeplerRelativityManagerTests : KeplerServiceTestBase
+	{
+		public KeplerRelativityManagerTests(bool useKepler)
+			: base(useKepler)
+		{
+		}
+
+		[Test]
+		public void ShouldReturnFalseForCheckIfEmailNotificationEnabled()
+		{
+			// arrange
+			using (IRelativityManager sut = ManagerFactory.CreateRelativityManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act & assert
+				Assert.That(sut.IsImportEmailNotificationEnabled(), Is.False);
+			}
+		}
+
+		[Test]
+		public void ShouldReturnCurrencySymbol()
+		{
+			// arrange
+			using (IRelativityManager sut = ManagerFactory.CreateRelativityManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act & assert
+				Assert.That(sut.RetrieveCurrencySymbol(), Is.EqualTo("$"));
+			}
+		}
+
+		[Test]
+		public void ShouldReturnFalseForValidateSuccessfulLogForValidCredentials()
+		{
+			// arrange
+			using (IRelativityManager sut = ManagerFactory.CreateRelativityManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act & assert
+				Assert.That(sut.ValidateSuccessfulLogin(), Is.EqualTo(false));
+			}
+		}
+
+		[Test]
+		public void ShouldReturnFalseForValidateSuccessfulLogForInvalidCredentials()
+		{
+			// Arrange
+			this.Credential.UserName = "INVALID USERNAME";
+			this.Credential.Password = "INVALID PASSWORD";
+
+			using (IRelativityManager sut = ManagerFactory.CreateRelativityManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act & assert
+				Assert.That(sut.ValidateSuccessfulLogin(), Is.EqualTo(false));
+			}
+		}
+
+		[Test]
+		public void ShouldReturnCorrectRdcConfiguration()
+		{
+			// arrange
+			using (IRelativityManager sut = ManagerFactory.CreateRelativityManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				var rdcConfiguration = sut.RetrieveRdcConfiguration();
+
+				// assert
+				Assert.That(rdcConfiguration, Is.Not.Null);
+				var table = rdcConfiguration.Tables[0];
+				Assert.That(table.Columns.Count, Is.EqualTo(3));
+				Assert.That(table.Columns.IndexOf("Section"), Is.EqualTo(0));
+				Assert.That(table.Columns.IndexOf("Name"), Is.EqualTo(1));
+				Assert.That(table.Columns.IndexOf("Value"), Is.EqualTo(2));
+				Assert.That(table.Rows.Count, Is.GreaterThan(0));
+			}
+		}
+	}
+}
