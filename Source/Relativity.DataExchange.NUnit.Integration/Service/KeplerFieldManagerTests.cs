ac34e79ed169cee86ded31a30da79cb54cccfe28
Mateusz Kulik <mateusz.kulik@relativity.com>
2021-08-18 06:43:13 +0000

    REL-578841 kepler/webapi ifieldmanager tests
    Squashed commit of the following:
    
    commit 936a97ad926aa61b8eff9d0955e23494e6430f53
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Tue Aug 17 08:19:54 2021 +0200
    
        remove fn
    
    commit f774b9b37588694e8ba1e5f5261d93187b5d2c1d
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Tue Aug 17 07:49:22 2021 +0200
    
        change to inline invocation
    
    commit e4a3429b92d9914163d2d3e9f2055f44f983c67f
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 13:56:42 2021 +0200
    
        configutre await
    
    commit 0a989ff3f9b5304435a4d7f409ba95dedd94a152
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 13:33:30 2021 +0200
    
        OneTimeTearDown
    
    commit dbea3a706e2554fb302ad1e87cd39770c9e78ead
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 12:37:03 2021 +0200
    
        remove redundant code
    
    commit a22a84346214e478f0e3b871da8f8c6e26250dac
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 12:12:45 2021 +0200
    
        added [OneTimeTearDown]
    
    commit ea4d00459c6d41414041a3f3c436ddcdf1f02690
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 11:21:58 2021 +0200
    
        added configureawait
    
    commit 05c072fc859bcd91005db7688412077d61381a85
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 11:09:23 2021 +0200
    
        REL-578841 tests for IFieldManager

---
+++ Source/Relativity.DataExchange.NUnit.Integration/Service/KeplerFieldManagerTests.cs
@@ -0,0 +1,86 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="KeplerFieldManagerTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit.Integration.Service
+{
+	using System;
+	using System.Threading.Tasks;
+	using System.Web.Services.Protocols;
+	using global::NUnit.Framework;
+	using kCura.WinEDDS.Service;
+	using kCura.WinEDDS.Service.Replacement;
+	using Relativity.DataExchange.TestFramework.RelativityHelpers;
+
+	[TestFixture(true)]
+	[TestFixture(false)]
+	public class KeplerFieldManagerTests : KeplerServiceTestBase
+	{
+		private const int TestFieldObjectArtifactTypeID = 10;
+		private string _testFieldName = Guid.NewGuid().ToString();
+		private int _testFieldArtifactID;
+
+		public KeplerFieldManagerTests(bool useKepler)
+			: base(useKepler)
+		{
+		}
+
+		[OneTimeSetUp]
+		public async Task KeplerFieldManagerTestsOneTimeSetup() =>
+			this._testFieldArtifactID = await FieldHelper.CreateDecimalFieldAsync(this.TestParameters, TestFieldObjectArtifactTypeID, this._testFieldName).ConfigureAwait(false);
+
+		[OneTimeTearDown]
+		public async Task KeplerFieldManagerTestsOneTimeTearDownAsync() =>
+			await FieldHelper.DeleteFieldAsync(this.TestParameters, this._testFieldArtifactID).ConfigureAwait(false);
+
+		[Test]
+		public void ShouldRetrieveControlNumberField()
+		{
+			// arrange
+			using (IFieldManager sut = ManagerFactory.CreateFieldManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				kCura.EDDS.WebAPI.FieldManagerBase.Field retreivedField = sut.Read(this.TestParameters.WorkspaceId, this._testFieldArtifactID);
+
+				// assert
+				Assert.That(retreivedField, Is.Not.Null);
+				Assert.That(retreivedField.DisplayName, Is.EqualTo(this._testFieldName));
+				Assert.That(retreivedField.FieldArtifactTypeID, Is.EqualTo(TestFieldObjectArtifactTypeID));
+				Assert.That(retreivedField.IsRequired, Is.False);
+			}
+		}
+
+		[Test]
+		public void ShouldCatchExceptionWhenFieldDoesNotExist()
+		{
+			// arrange
+			using (IFieldManager sut = ManagerFactory.CreateFieldManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act & assert
+				Assert.Catch<SoapException>(() => sut.Read(this.TestParameters.WorkspaceId, -1));
+			}
+		}
+
+		[Test]
+		public void ShouldCatchExceptionWhenWorkspaceDoesNotExist()
+		{
+			// arrange
+			using (IFieldManager sut = ManagerFactory.CreateFieldManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act & assert
+				Assert.Catch<SoapException>(() => sut.Read(0, this._testFieldArtifactID));
+			}
+		}
+	}
+}
