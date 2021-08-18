108594a39d604cc0278667c98f6d618aad966871
Mateusz Kulik <mateusz.kulik@relativity.com>
2021-08-18 08:00:21 +0000

    REL-578839- kepler/webapi idocumentmanager tests
    Squashed commit of the following:
    
    commit bc79f162698e635c27cbd2b325e5bcf0c7f48076
    Merge: 819a5dbb1 ec3adc044
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 08:48:06 2021 +0200
    
        Merge branch 'develop' into REL-578839-idocumentmanager-tests
    
    commit 819a5dbb1d03124052f108dadb7427b35330775d
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Tue Aug 17 08:21:40 2021 +0200
    
        assert for not empty
    
    commit cc1f2de43023be8da3fe04105470f0ed69cfa0b6
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Mon Aug 16 12:30:13 2021 +0200
    
        IDocumentManager test

---
+++ Source/Relativity.DataExchange.NUnit.Integration/Service/KeplerDocumentManagerTests.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="KeplerDocumentManagerTests.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit.Integration.Service
+{
+	using global::NUnit.Framework;
+	using kCura.WinEDDS.Service;
+
+	/// <summary>
+	/// Additional tests for IDocumentManger in DataTransfer.Legacy
+	/// <see href="https://git.kcura.com/projects/DTX/repos/relativity-data-transfer-legacy-sdk/browse/Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility"/>.
+	/// </summary>
+	[TestFixture(true)]
+	[TestFixture(false)]
+	public class KeplerDocumentManagerTests : KeplerServiceTestBase
+	{
+		public KeplerDocumentManagerTests(bool useKepler)
+			: base(useKepler)
+		{
+		}
+
+		[Test]
+		public void ShouldRetrieveAllUnsupportedOiFileIds()
+		{
+			// arrange
+			using (kCura.WinEDDS.Service.Replacement.IDocumentManager sut = ManagerFactory.CreateDocumentManager(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				int[] actualResult = sut.RetrieveAllUnsupportedOiFileIds();
+
+				// assert
+				Assert.That(actualResult, Is.Not.Null);
+				Assert.That(actualResult, Is.Not.Empty);
+			}
+		}
+	}
+}
