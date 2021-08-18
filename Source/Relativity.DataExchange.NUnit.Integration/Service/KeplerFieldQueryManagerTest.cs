8ef24d3fcf7102b7b7d0f6238b5597c6747be071
Mateusz Kulik <mateusz.kulik@relativity.com>
2021-08-18 10:28:41 +0000

    REL-578842 kepler/webapi ifieldquerymanagertest
    Squashed commit of the following:
    
    commit 6b0a926b6d40688cadc254692c001ed007c4d017
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 11:39:01 2021 +0200
    
        changes after pr v2
    
    commit bed060d7507e1bf45b27319fcebdaab7b162ccb0
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 10:06:10 2021 +0200
    
        changes after merge
    
    commit bc4a18567ee906245604bffd201dc0e988af8352
    Merge: ef09a0198 9d54661bb
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 10:04:47 2021 +0200
    
        Merge branch 'develop' into REL-578842-ifieldquerymanager-test
    
    commit ef09a019895d2919a1d7f7822f9522b98acfe0f1
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 09:45:30 2021 +0200
    
        changes after pr
    
    commit 83e0e1f0ff4d3d2bcc4be54f505771d3654353b0
    Merge: 96196944a ec3adc044
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 08:51:42 2021 +0200
    
        Merge branch 'develop' into REL-578842-ifieldquerymanager-test
    
    commit 96196944a1e391ebc34658d8064a6980e01aa4f2
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 08:42:18 2021 +0200
    
        name changes
    
    commit ef6dad746a721fb433ebf8568de4c3db2be17b7c
    Author: Mateusz Kulik <mateusz.kulik@relativity.com>
    Date:   Wed Aug 18 08:04:34 2021 +0200
    
        IFileQuery tests

---
+++ Source/Relativity.DataExchange.NUnit.Integration/Service/KeplerFieldQueryManagerTest.cs
@@ -0,0 +1,330 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="KeplerFieldQueryManagerTest.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.DataExchange.NUnit.Integration.Service
+{
+	using System;
+	using System.Data;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using global::NUnit.Framework;
+
+	using kCura.WinEDDS;
+	using kCura.WinEDDS.Service;
+	using kCura.WinEDDS.Service.Replacement;
+
+	using Relativity.DataExchange.TestFramework;
+	using Relativity.DataExchange.TestFramework.RelativityHelpers;
+
+	[TestFixture(true)]
+	[TestFixture(false)]
+	public class KeplerFieldQueryManagerTest : KeplerServiceTestBase
+	{
+		private const int TestFieldObjectArtifactTypeID = (int)ArtifactType.Document;
+		private const int NonExistingWorkspaceID = 0;
+		private const int NonExistingArtifactTypeID = -15;
+		private string _testDecimalFieldName = Guid.NewGuid().ToString();
+		private string _testFixLenTextFieldName = Guid.NewGuid().ToString();
+		private int _testDecimalFieldArtifactID;
+		private int _testFixLenTextFieldArtifactID;
+
+		public KeplerFieldQueryManagerTest(bool useKepler)
+			: base(useKepler)
+		{
+		}
+
+		[OneTimeSetUp]
+		public async Task KeplerFieldManagerTestsOneTimeSetup()
+		{
+			this._testDecimalFieldArtifactID = await FieldHelper.CreateDecimalFieldAsync(
+				                                   this.TestParameters,
+				                                   TestFieldObjectArtifactTypeID,
+				                                   this._testDecimalFieldName).ConfigureAwait(false);
+
+			this._testFixLenTextFieldArtifactID = await FieldHelper.CreateFixedLengthTextFieldAsync(
+				                                      this.TestParameters,
+				                                      TestFieldObjectArtifactTypeID,
+				                                      this._testFixLenTextFieldName,
+				                                      false,
+				                                      255).ConfigureAwait(false);
+		}
+
+		[OneTimeTearDown]
+		public async Task KeplerFieldManagerTestsOneTimeTearDownAsync()
+		{
+			await FieldHelper.DeleteFieldAsync(this.TestParameters, this._testDecimalFieldArtifactID).ConfigureAwait(false);
+			await FieldHelper.DeleteFieldAsync(this.TestParameters, this._testFixLenTextFieldArtifactID).ConfigureAwait(false);
+		}
+
+		[Test]
+		public void ShouldRetrieveAllMappable()
+		{
+			// arrange
+			string filterString = $"ArtifactID = {this._testDecimalFieldArtifactID}";
+
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				DataSet actualResult = sut.RetrieveAllMappable(this.TestParameters.WorkspaceId, (int)ArtifactType.Document);
+
+				// assert
+				Assert.That(actualResult, Is.Not.Null);
+				Assert.That(actualResult.Tables[0].Rows.Count, Is.Not.Negative);
+				Assert.That(actualResult.Tables[0].Select(filterString).Length, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenWorkspaceDoesNotExist()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act && assert
+				Assert.That(
+					() => sut.RetrieveAllMappable(NonExistingWorkspaceID, (int)ArtifactType.Document),
+					this.GetExpectedExceptionConstraintForNonExistingWorkspace(NonExistingWorkspaceID));
+			}
+		}
+
+		[Test]
+		public void ShouldReturnEmptyTableWhenArtifactTypeIdDoesNotExist()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				DataSet actualReposnse = sut.RetrieveAllMappable(this.TestParameters.WorkspaceId, NonExistingArtifactTypeID);
+
+				// assert
+				Assert.That(actualReposnse, Is.Not.Null);
+				Assert.That(actualReposnse.Tables[0].Rows.Count, Is.EqualTo(0));
+			}
+		}
+
+		[Test]
+		public void ShouldRetreiveDataSetFromRetrievePotentialBeginBatesFields()
+		{
+			// arrange
+			string filterString = $"ArtifactID = {this._testFixLenTextFieldArtifactID}";
+
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				DataSet actualResponse = sut.RetrievePotentialBeginBatesFields(this.TestParameters.WorkspaceId);
+
+				// assert
+				Assert.That(actualResponse, Is.Not.Null);
+				Assert.That(actualResponse.Tables[0].Rows.Count, Is.Not.Negative);
+				Assert.That(actualResponse.Tables[0].Select(filterString).Length, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenWorkspaceDoesNotExistFromRetrievePotentialBeginBatesFields()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act && assert
+				Assert.That(
+					() => sut.RetrievePotentialBeginBatesFields(NonExistingWorkspaceID),
+					this.GetExpectedExceptionConstraintForNonExistingWorkspace(NonExistingWorkspaceID));
+			}
+		}
+
+		[Test]
+		public void ShouldTReturnFalseFromIsFieldIndexed()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				bool actualResult = sut.IsFieldIndexed(this.TestParameters.WorkspaceId, this._testDecimalFieldArtifactID);
+
+				// assert
+				Assert.That(actualResult, Is.False);
+			}
+		}
+
+		[Test]
+		public void ShouldReturnTrueForControlNumberFromIsFieldIndexed()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				bool actualResult = sut.IsFieldIndexed(this.TestParameters.WorkspaceId, WellKnownFields.ControlNumberId);
+
+				// assert
+				Assert.That(actualResult, Is.True);
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionWhenWorkspaceDoesNotExistFromIsFieldIndexed()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act && assert
+				Assert.That(
+					() => sut.IsFieldIndexed(NonExistingWorkspaceID, this._testDecimalFieldArtifactID),
+					this.GetExpectedExceptionConstraintForNonExistingWorkspace(NonExistingWorkspaceID));
+			}
+		}
+
+		[Test]
+		public void ShouldReturnFalseWhenFieldDoesNotExistFromIsFieldIndexed()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				bool actualResult = sut.IsFieldIndexed(this.TestParameters.WorkspaceId, NonExistingArtifactTypeID);
+
+				// assert
+				Assert.That(actualResult, Is.False);
+			}
+		}
+
+		[Test]
+		public void ShouldReturnCollectionFromRetrieveAllAsDocumentFieldCollection()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				DocumentFieldCollection actualResult = sut.RetrieveAllAsDocumentFieldCollection(this.TestParameters.WorkspaceId, TestFieldObjectArtifactTypeID);
+
+				// assert
+				Assert.That(actualResult, Is.Not.Null);
+				Assert.That(actualResult.ToList().Where(x => x.FieldName == this._testDecimalFieldName || x.FieldName == this._testFixLenTextFieldName).Count(), Is.EqualTo(2));
+			}
+		}
+
+		[Test]
+		public void ShouldReturnEmptyWithNonExistingArtifactTypeIdFromRetrieveAllAsDocumentFieldCollection()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				DocumentFieldCollection actualResult = sut.RetrieveAllAsDocumentFieldCollection(this.TestParameters.WorkspaceId, NonExistingArtifactTypeID);
+
+				// assert
+				Assert.That(actualResult, Is.Empty);
+			}
+		}
+
+		[Test]
+		public void ShouldThrowExceptionFromRetrieveAllAsDocumentFieldCollection()
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act && assert
+				Assert.That(
+					() => sut.RetrieveAllAsDocumentFieldCollection(NonExistingWorkspaceID, TestFieldObjectArtifactTypeID),
+					this.GetExpectedExceptionConstraintForNonExistingWorkspace(NonExistingWorkspaceID));
+			}
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void ShouldReturnCollectionFromRetrieveAllAsDocumentFieldCollection(bool includeUnmappable)
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				kCura.EDDS.WebAPI.DocumentManagerBase.Field[] actualResult = sut.RetrieveAllAsArray(this.TestParameters.WorkspaceId, TestFieldObjectArtifactTypeID, includeUnmappable);
+
+				// assert
+				Assert.That(actualResult, Is.Not.Null);
+				Assert.That(actualResult.ToList().Where(x => x.DisplayName == this._testDecimalFieldName || x.DisplayName == this._testFixLenTextFieldName).Count(), Is.EqualTo(2));
+			}
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void ShouldReturnEmptyWithNonExistingArtifactTypeIdForRetrieveAllAsArray(bool includeUnmappable)
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act
+				kCura.EDDS.WebAPI.DocumentManagerBase.Field[] actualResult = sut.RetrieveAllAsArray(this.TestParameters.WorkspaceId, NonExistingArtifactTypeID, includeUnmappable);
+
+				// assert
+				Assert.That(actualResult, Is.Empty);
+			}
+		}
+
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void ShouldThrowExceptionForRetrieveAllAsArray(bool includeUnmappable)
+		{
+			// arrange
+			using (IFieldQuery sut = ManagerFactory.CreateFieldQuery(
+				this.Credential,
+				this.CookieContainer,
+				this.CorrelationIdFunc))
+			{
+				// act && assert
+				Assert.That(
+					() => sut.RetrieveAllAsArray(NonExistingWorkspaceID, TestFieldObjectArtifactTypeID, includeUnmappable),
+					this.GetExpectedExceptionConstraintForNonExistingWorkspace(NonExistingWorkspaceID));
+			}
+		}
+	}
+}
