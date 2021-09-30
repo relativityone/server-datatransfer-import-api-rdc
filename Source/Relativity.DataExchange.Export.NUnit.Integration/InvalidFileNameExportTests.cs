6790d0f93af61eb2ef17e22ec798f4028772cee2
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2021-09-30 11:25:00 +0000

    Merge pull request #1138 in DTX/import-api-rdc from REL-597178-sundrop to release-1.15-sundrop
    Squashed commit of the following:
    
    commit 3789158bad00976005e4388cad2ab9d9789f0729
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Sep 24 16:52:45 2021 +0200
    
        REL-597178 fixed issue with exporting files with trailing whitespaces
    
        that defect caused REL-596212 incident

---
+++ Source/Relativity.DataExchange.Export.NUnit.Integration/InvalidFileNameExportTests.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="InvalidFileNameExportTests.cs" company="Relativity ODA LLC">
+// © Relativity All Rights Reserved.
+// </copyright>
+
+namespace Relativity.DataExchange.Export.NUnit.Integration
+{
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using global::NUnit.Framework;
+
+	using Relativity.DataExchange.TestFramework;
+	using Relativity.DataExchange.TestFramework.RelativityHelpers;
+	using Relativity.Testing.Identification;
+
+	[TestFixture(true)]
+	[TestFixture(false)]
+	[TestType.Error]
+	[Feature.DataTransfer.DocumentExportApi.Operations.ExportFolderAndSubfolders]
+	public class InvalidFileNameExportTests : ExportTestBase
+	{
+		private SqlQueryHelper queryHelper;
+
+		public InvalidFileNameExportTests(bool useKepler)
+			: base(useKepler)
+		{
+		}
+
+		protected override IntegrationTestParameters TestParameters => AssemblySetup.TestParameters;
+
+		[OneTimeSetUp]
+		public Task OneTimeSetUpAsync()
+		{
+			if (IntegrationTestHelper.IsRegressionEnvironment())
+			{
+				Assert.Ignore("This fixture requires access to SQL.");
+			}
+
+			this.queryHelper = new SqlQueryHelper(TestParameters);
+			return this.AddTrailingWhitespacesToFileNames();
+		}
+
+		[OneTimeTearDown]
+		public Task OneTimeTearDownAsync()
+		{
+			return this.RemoveTrailingWhitespacesFromFileNames();
+		}
+
+		[IdentifiedTest("fa7a5189-4eae-46fb-b4b8-473ba136badf")]
+		public async Task ShouldExportFilesWithTrailingWhitespacesInFileNameColumn()
+		{
+			// ARRANGE
+			ExtendedExportFileSetup.SetupDocumentExport(ExtendedExportFile);
+			ExtendedExportFileSetup.SetupImageExport(ExtendedExportFile);
+			ExtendedExportFileSetup.SetupPaddings(ExtendedExportFile);
+			ExtendedExportFileSetup.SetupDelimiters(ExtendedExportFile);
+
+			// ACT
+			this.ExecuteFolderAndSubfoldersAndVerify();
+
+			// ASSERT
+			this.ThenTheExportJobIsSuccessful(TestData.SampleDocFiles.Count());
+
+			ExportedFilesValidator.ValidateNativesCount(this.ExtendedExportFile, TestData.SampleDocFiles.Count());
+			await ExportedFilesValidator.ValidateNativeFilesAsync(this.ExtendedExportFile).ConfigureAwait(false);
+
+			ExportedFilesValidator.ValidateImagesCount(this.ExtendedExportFile, TestData.SampleImageFiles.Count());
+			await ExportedFilesValidator.ValidateImageFilesAsync(this.ExtendedExportFile).ConfigureAwait(false);
+
+			await this.ThenTheExportedDocumentLoadFileIsAsExpectedAsync().ConfigureAwait(false);
+			await this.ThenTheExportedImageLoadFileIsAsExpectedAsync().ConfigureAwait(false);
+		}
+
+		private Task AddTrailingWhitespacesToFileNames()
+		{
+			string query = $@"UPDATE [EDDS{TestParameters.WorkspaceId}].[EDDSDBO].[File] SET Filename = Filename + '		  '";
+			return this.queryHelper.ExecuteQueryAsync(query);
+		}
+
+		private Task RemoveTrailingWhitespacesFromFileNames()
+		{
+			string query = $@"UPDATE [EDDS{TestParameters.WorkspaceId}].[EDDSDBO].[File] SET Filename = TRIM('	 ' FROM Filename)";
+			return this.queryHelper.ExecuteQueryAsync(query);
+		}
+	}
+}
