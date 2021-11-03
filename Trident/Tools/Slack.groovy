1a4e0e91bdd68b1e67729d0c85123bb6cf192d4b
Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
2021-11-03 15:58:11 +0000

    Merge pull request #1170 in DTX/import-api-rdc from REL-581791-slack-tigerlily to release-1.16-tigerlily
    Squashed commit of the following:
    
    commit 3e6784beb277be04a903841aa17e35d19fc32082
    Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
    Date:   Wed Nov 3 11:10:41 2021 +0000
    
        Merge pull request #1166 in DTX/import-api-rdc from REL-581791-slack to develop
    
        Squashed commit of the following:
    
        commit 6a0379563687881e22c1b0aa256114a51845cd6a
        Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
        Date:   Thu Oct 28 15:54:24 2021 +0200
    
            REL-581791 Send Post request from groovy to slack #6
    
        commit 91f015d28a4cbd0be896b3a7eb46c5aeae3ad0a4
        Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
        Date:   Thu Oct 28 15:17:18 2021 +0200
    
            REL-581791 Send Post request from groovy to slack #5
    
        commit 1bb13d59ca3c3f5993756ad31e11a708345ebc94
        Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
        Date:   Thu Oct 28 14:53:59 2021 +0200
    
            REL-581791 Send Post request from groovy to slack #4
    
        commit 8ec2ef6a39be3b9c896983af864ef1debec3d2eb
        Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
        Date:   Thu Oct 28 14:38:43 2021 +0200
    
            REL-581791 Send Post request from groovy to slack #3
    
        commit b5a87fd7773bc3628c79316bbd16b6a445e19f78
        Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
        Date:   Thu Oct 28 14:09:43 2021 +0200
    
            REL-581791 Send Post request from groovy to slack #2
    
        commit f715ca8cc33071418e5cef3bacda05e09302e697
        Author: Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
        Date:   Thu Oct 28 14:01:10 2021 +0200
    
            REL-581791 Send Post request from groovy to slack #1
    
        (cherry picked from commit 1e9b3f9668b93f4c12c791189adeb2d0bf352dc5)

--- Trident/Tools/Slack.groovy
+++ Trident/Tools/Slack.groovy
@@ -5,6 +5,29 @@ def SendSlackNotification(String serverUnderTestName, String version, String bra
     def script = this
 	def email = powershell(returnStdout: true, script: "git --no-pager show -s --format='%ae'").trim()
 	def channel = "notify-iapi-pipeline-" + "${slackChannel}"
+    // Url are defined in the Import API slack application https://api.slack.com/apps/A028WEQG63F
+    def url = "https://hooks.slack.com/services/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"
+    switch(slackChannel) {
+        case "notify-iapi-pipeline-build":
+            url = "https://hooks.slack.com/services/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"
+        break
+        case "notify-iapi-pipeline-compatibility":
+            url = "https://hooks.slack.com/services/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"
+        break
+        case "notify-iapi-pipeline-load-tests":
+            url = "https://hooks.slack.com/services/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"
+        break
+        case "notify-iapi-pipeline-mass-import-verification":
+            url = "https://hooks.slack.com/services/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"
+        break
+        case "notify-iapi-pipeline-regression":
+            url = "https://hooks.slack.com/services/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"
+        break
+        case "notify-iapi-pipeline-release-branches":
+            url = "https://hooks.slack.com/services/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"
+        break
+    }
+
     echo "*************************************************" +
         "\n" +
         "\n" + "sendCDSlackNotification Parameters: " +
@@ -16,6 +39,7 @@ def SendSlackNotification(String serverUnderTestName, String version, String bra
         "\n" + "buildType: " + buildType +
         "\n" + "slackChannel: " + channel +
         "\n" + "email: " + email +
+        "\n" + "buildUrl: " + env.BUILD_URL +
         "\n" + "numberOfFailedTests: " + numberOfFailedTests +
         "\n" + "numberOfPassedTests: " + numberOfPassedTests +
         "\n" + "numberOfSkippedTests: " + numberOfSkippedTests +
@@ -24,7 +48,100 @@ def SendSlackNotification(String serverUnderTestName, String version, String bra
         "\n*************************************************"
     try
     {
-        sendCDSlackNotification(script, serverUnderTestName, version, branch, buildType, channel, email, ['tests': ['passed': numberOfPassedTests, 'failed': numberOfFailedTests, 'skipped': numberOfSkippedTests]], message, "CD" )
+        echo 'Sending POST request to ' + url
+        def post = new URL(url).openConnection();
+        def body = """
+                    {
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Build result*\nImage: ${serverUnderTestName}\nBranch: ${branch}\nVersion: ${version}\nBuildType: ${buildType}\n Email: ${email}\nMessage: ${message}"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+                    "url": "${env.BUILD_URL}",
+					"value": "job",
+					"action_id": "actionId-0",
+					"text": {
+						"type": "plain_text",
+						"text": "Jenkins Job",
+						"emoji": true
+					},
+					"style": "primary"
+				}
+			]
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Tests:"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+                    "url": "${env.BUILD_URL}",
+					"value": "passed",
+					"action_id": "actionId-1",
+					"text": {
+						"type": "plain_text",
+						"text": "Passed: ${numberOfPassedTests}",
+						"emoji": true
+					},
+					"style": "primary"
+				},
+				{
+					"type": "button",
+                    "url": "${env.BUILD_URL}",
+					"value": "failed",
+					"action_id": "actionId-2",
+					"text": {
+						"type": "plain_text",
+						"text": "Failed: ${numberOfFailedTests}",
+						"emoji": true
+					},
+					"style": "danger"
+				},
+				{
+					"type": "button",
+                    "url": "${env.BUILD_URL}",
+					"value": "skipped",
+					"action_id": "actionId-3",
+					"text": {
+						"type": "plain_text",
+						"text": "Skipped: ${numberOfSkippedTests}",
+						"emoji": true
+					}
+				}
+			]
+		}
+	]
+}
+                    """
+        post.setRequestMethod("POST")
+        post.setDoOutput(true)
+        post.setRequestProperty("Content-Type", "application/json")
+        post.getOutputStream().write(body.getBytes("UTF-8"));
+        def postRC = post.getResponseCode();
+        echo 'Response Code: ' + postRC
+        
+        if (postRC.equals(200)) {
+            def response = post.getInputStream().getText();
+            echo 'Response: ' + response
+        }
+
     }
     catch(err)
     {
