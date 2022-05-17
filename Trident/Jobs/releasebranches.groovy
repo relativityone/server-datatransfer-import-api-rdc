f799a963e54e9358ea50bf746c11ac34d8a93841
Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
2022-05-17 11:31:09 +0200

    REL-680918 Create Yarrow branch and update jenkins values

--- Trident/Jobs/releasebranches.groovy
+++ Trident/Jobs/releasebranches.groovy
@@ -267,8 +267,8 @@ def GetHopperTemplate(String branchName)
 	// This section should be updated when new release appears
 	// Hopper images should be reviewed and updated to latest available
 	switch(versionName) {
-		case "prairiesmoke":
-			hopperImage = "aio-osier-0"
+		case "yarrow":
+			hopperImage = "aio-whitesedge-eau"
 			break
 		default:
 			echo "Define the hopper image which should be used in tests on branch '${branchName}'"
